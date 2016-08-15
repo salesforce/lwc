4406f1c610e6c7b491fcb6da2b851f39eff0d260
Caridy <cpatino@salesforce.com>
2016-08-15 11:13:21 -0400

    initial brainstorm around ads

--- src/namespaces/abc/components/adsBaz/adsBaz.js
+++ src/namespaces/abc/components/adsBaz/adsBaz.js
@@ -1,10 +1,39 @@
 import {
-    AuraComponent,
-} from "aura";
+    ADS,
+} from "aura:service";
 
-export default class extends AuraComponent {
-    constructor() {
-        super();
-    }
-    render() {}
+class adsBaz {
+    foo = [];
+    bar = {};
+    @attribute()
+    baz = "3453";
 }
+
+const adsBazProxyHandler = {
+    get(name) {
+        if (name === 'baz') {
+            return target[name] || "3453"; // or default value
+        }
+    }
+    set(name, value) {
+        if (name === 'baz') {
+            assert('value for baz attribute to validate during dev')
+            target[name] = value;
+        }
+    }
+}
+
+export default ADS(adsBaz, {
+  fragments: {
+    user: () => ADS.QL`
+      fragment on User {
+        profilePhoto(size: $baz) {
+          uri,
+        },
+      }
+    `,
+  },
+});
+
+
+<adsBaz foo="something" />
