4150f5a4ff8e2083d593ae4c53b2d006aa5a5817
Caridy <cpatino@salesforce.com>
2016-08-16 13:57:24 -0400

    filling the blanks for rendering

--- src/framework/rendering.js
+++ src/framework/rendering.js
@@ -1,10 +1,14 @@
 // this file contains some rendering ideas...
 
 import {
-    getAttr,
+    updateAttr,
 } from "dom";
 
-// this map hold the weak references between a component and its root DOM element
+import {
+    ComponentStateMap,
+} from "./createElement.js";
+
+// this map holds the weak references between a component and its root DOM element
 // whenever a component is rendered, its domNode should be stored here
 const virtualDOMWeakMap = new WeakMap();
 
@@ -16,14 +20,51 @@ export function updateAttributeInMarkup(componentInstance, refId, attrName, attr
     let divComponentInstance = componentInstance.getRef(refId);
     if (divComponentInstance) {
         let divDomNode = getDOMNode(divComponentInstance);
-        const attrConfig = getAttr(attrName); // we might want to memoize this
-        attrConfig.set(divDomNode, attrName, attrValue);
+        updateAttr(divDomNode, attrName, attrValue);
     }
 }
 
-export function isDirty() {}
+function isComputedPropertyDirty(deps, dirtyPropNames) {
+    // the propName is a computed property, and should be computed accordingly
+    for (let depPropName in deps) {
+        if (dirtyPropNames[depPropName]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+export function isDirty(component, propName) {
+    const {dirty, dirtyPropNames} = ComponentStateMap.get(component);
+    if (!propName) {
+        return dirty;
+    }
+    if (!dirtyPropNames.hasOwnProperty(propName)) {
+        let descriptor = Object.getOwnPropertyDescriptor(component, propName);
+        if (descriptor && descriptor.get) {
+            let deps = descriptor.get.dependencies;
+            if (deps) {
+                if (isComputedPropertyDirty(deps, dirtyPropNames)) {
+                    return true;
+                }
+             } else {
+                dirtyPropNames[propName] = false;
+            }
+        }
+    }
+    return dirtyPropNames[propName] || false;
+}
+
 export function updateRefComponetAttribute() {}
 export function updateContentInMarkup() {}
 export function unmountRefComponent() {}
 export function mountComponentAfterMarker() {}
-export function componentWasRehydrated() {}
+
+export function componentWasRehydrated(component) {
+    const state = ComponentStateMap.get(component);
+    const {dirtyPropNames} = state;
+    state.dirty = false;
+    for (let propName of dirtyPropNames) {
+        dirtyPropNames[propName] = false;
+    }
+}
