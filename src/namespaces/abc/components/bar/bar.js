f9b09576182af0ff81b1eef19af134895b2281c8
Caridy <cpatino@salesforce.com>
2016-10-02 15:59:16 -0700

    updating bar after adding reactivity

--- src/namespaces/abc/components/bar/bar.js
+++ src/namespaces/abc/components/bar/bar.js
@@ -3,6 +3,21 @@ import { attribute } from "aura";
 const DefaultMinValue = 5;
 const DefaultMaxValue = 50;
 
+function produceNewData(oldData, min, max) {
+    const len = Math.floor(Math.random() * (max - min)) + min;
+    const data = [];
+    for (let i = 0; i < len; i += 1) {
+        if (Math.round(Math.random()) === 1 && oldData[i]) {
+            data.push(oldData[i]);
+        } else {
+            data.push({
+                x: Math.floor(Math.random() * 100)
+            });
+        }
+    }
+    return data;
+}
+
 export default class Bar {
     @attribute min = DefaultMinValue;
     @attribute max = DefaultMaxValue;
@@ -16,27 +31,14 @@ export default class Bar {
     }
 
     updated() {
-        this.data = this.produceNewData(this.data);
-    }
-
-    produceNewData(oldData = []) {
-        const len = Math.floor(Math.random() * (this.max - this.min)) + this.min;
-        const data = [];
-        for (let i = 0; i < len; i += 1) {
-            if (Math.round(Math.random()) === 1 && oldData[i]) {
-                data.push(oldData[i]);
-            } else {
-                data.push({
-                    x: Math.floor(Math.random() * 100)
-                });
-            }
-        }
-        return data;
+        this.data = produceNewData(this.data, this.min, this.max);
     }
 
     handleClick() {
-        console.log('clicked');
         this.counter += 1;
+        const newData = produceNewData(this.data, this.min, this.max);
+        this.data = newData;
+        console.log('clicked');
     }
 
 
