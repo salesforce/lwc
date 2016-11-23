7f00f20c217c0a4c59ae26a76b6c6eca8be4c629
Diego <dval@salesforce.com>
2016-11-22 19:20:35 -0800

    Changing example

--- packages/babel-plugin-transform-html-template/scripts/test-cli.js
+++ packages/babel-plugin-transform-html-template/scripts/test-cli.js
@@ -8,15 +8,34 @@ import * as babel from 'babel-core';
 import plugin from '../src/index';
 
 const testSrc = `
-    <template>
-        <section data-foo="bar">
-            <p class="foo" style="color:red">Test</p>
-            <lightning:button label="foo" />
-        </section>
-    </template>
-`; 
+   <template>
+    <section class:bind="computeMyClass">
 
-const result = babel.transform(testSrc, {
+         <!-- conditional (binded by default) -->
+        <p if:true="test">True</p>
+
+         <!-- Negation condition the idea is compile both in a ternary operator -->
+        <p if:false="test">False</p> 
+
+        <!-- For loop -->
+        <ul repeat:for="(index, item) of items">
+             <!-- Bind to a function (should be memoizable) -->
+            <p label:bind="publicComputeLabel(index)"></p>
+        </ul>
+
+          <!-- Binding callback -->
+        <lightning:button label="btn1" press:bind="pressCallback" />
+
+        <!-- We want to fail here?, we shouldn't allow empty functions in favor of getters -->
+        <lightning:button label:bind="publicComputeLabel()" press:bind="pressCallback" />
+    </section>
+</template>
+`;
+
+const src = testSrc.replace(/<!--([\s\S]*?)-->/g);
+
+
+const result = babel.transform(src, {
     plugins: [ plugin ]
 });
 
