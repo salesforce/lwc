84c00331e803d257ff35a084c1cd385e3fbbd0dc
Diego <dval@salesforce.com>
2016-11-28 20:02:05 -0800

    Small function refactor

--- packages/raptor-compiler-core/src/lib/babel-plugin-optimize-class.js
+++ packages/raptor-compiler-core/src/lib/babel-plugin-optimize-class.js
@@ -8,22 +8,25 @@ export default function ({ types: t }) {
             )
         );
     }
+    
+    function addUsedTemplateProps (path, templateProps) {
+        const body = path.node.body;
+        const exportDeclaration = body.pop();
+
+        if (!t.isExportDeclaration(exportDeclaration)) {
+            throw new Error();
+        }
+
+        body.push(addClassStaticMember(exportDeclaration.declaration.name, '$t$', t.valueToNode(templateProps)));
+        body.push(exportDeclaration);
+    }
 
     return {
         name: "optimize-transform", // not required
         visitor: {
             Program(path, state) {
-                const body = path.node.body;
-                const exportDeclaration = body.pop();
-
-                if (!t.isExportDeclaration(exportDeclaration)) {
-                    throw new Error();
-                }
-
                 const templateProps = state.opts.templateProps || {};
-                const className = exportDeclaration.declaration.name;
-                body.push(addClassStaticMember(className, '$t$', t.valueToNode(templateProps)));
-                body.push(exportDeclaration);
+                addUsedTemplateProps(path, templateProps);
             }
         }
     };
