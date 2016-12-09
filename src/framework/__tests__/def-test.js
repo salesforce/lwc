5cdb4e031142102b70dd5fe4337f85bb8be7a0fe
Caridy <cpatino@salesforce.com>
2016-12-09 11:24:56 -0500

    introducing mocha, power-asssert and sinon. pulling tests from #16

--- src/framework/__tests__/def-test.js
+++ src/framework/__tests__/def-test.js
@@ -1,12 +1,20 @@
-import expect from 'expect';
-import { getComponentDef } from "../def.js";
+import * as target from "../def.js";
+import assert from 'power-assert';
 
-describe('getComponentDef', () => {
+describe('def.js', () => {
+    describe('#getComponentDef()', () => {
 
-    it('should understand empty constructors', () => {
-        class Ctor {} 
-        var def = getComponentDef({ Ctor });
-        expect(def).toExist();
+        it('should understand empty constructors', () => {
+            const def = class MyComponent {}
+            assert.deepEqual(target.getComponentDef({ Ctor: def }), {
+                name: 'MyComponent',
+                props: {},
+                attrs: {},
+                methods: {},
+                observedAttrs: {},
+                observedProps: {},
+            });
+        });
+
     });
-
 });
