f47ff3ae664f8b86365daab58a33aa6c77f74820
Diego <dval@salesforce.com>
2016-12-14 00:54:28 -0800

    Fixing forLoop corner case

--- packages/babel-plugin-transform-html-template/src/for-scope.js
+++ packages/babel-plugin-transform-html-template/src/for-scope.js
@@ -1,30 +1,3 @@
-// -- For loop utils
-export function addScopeForLoop(path, forExpression) {
-    forExpression.node = path.node;
-    path.scope.data.for = path.scope.data.for || [];
-    path.scope.data.for.push(forExpression);
-}
-
-export function getVarsScopeForLoop (path) {
-    const list = [];
-    const scoped = path.scope.data.for;
-
-    if (scoped) {
-        scoped.reduce((l, f) => { l.push(...f.args); return l; }, list);
-    }
-
-    return list;
-}
-
-export function removeScopeForLoop (path) {
-    path.scope.data.for.pop();
-}
-
-export function hasScopeForLoop (path) {
-    const forScope = path.scope.data.for;
-    return forScope && forScope.length && forScope[forScope.length - 1].node === path.node;
-}
-
 export const customScope = {
     scoped: [],
     hasBinding(bindingName) {
@@ -45,3 +18,34 @@ export const customScope = {
         return this.scoped.reduce((l, f) => { l.push(...f.bindings); return l; }, []);
     }
 };
+
+
+// -- REFACTOR this
+// -- For loop utils
+export function addScopeForLoop(path, forExpression) {
+    forExpression.node = path.node;
+    path.scope.data.for = path.scope.data.for || [];
+    path.scope.data.for.push(forExpression);
+    customScope.registerBindings(path, forExpression.args);
+}
+
+export function getVarsScopeForLoop (path) {
+    const list = [];
+    const scoped = path.scope.data.for;
+
+    if (scoped) {
+        scoped.reduce((l, f) => { l.push(...f.args); return l; }, list);
+    }
+
+    return list;
+}
+
+export function removeScopeForLoop (path) {
+    path.scope.data.for.pop();
+    customScope.removeBindings(path);
+}
+
+export function hasScopeForLoop (path) {
+    const forScope = path.scope.data.for;
+    return forScope && forScope.length && forScope[forScope.length - 1].node === path.node;
+}
