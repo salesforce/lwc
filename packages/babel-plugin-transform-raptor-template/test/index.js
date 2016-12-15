a5c9859a36b42c7508b86e8404d501e2c324f7e6
p-dartus <p.dartus@salesforce.com>
2016-12-15 18:49:26 +0100

    Fixture tests handeling errors

--- packages/babel-plugin-transform-raptor-template/test/index.js
+++ packages/babel-plugin-transform-raptor-template/test/index.js
@@ -1,3 +1,5 @@
+/* eslint-env mocha, node */
+
 import * as babel from 'babel-core';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -5,46 +7,15 @@ import * as path from 'path';
 import assert from 'power-assert';
 import plugin from '../src/index';
 
+const FIXTURE_DIR = path.join(__dirname, 'fixtures');
+
 function trim(str) {
     return str.toString().replace(/^\s+|\s+$/, '');
 }
 
-const skipTests = [
-    'all',
-    'multiRootError',
-    '.babelrc',
-    '.DS_Store',
-];
-
-const fixturesDir = path.join(__dirname, 'fixtures');
-
-describe('emit asserts for: ', () => {
-    fs.readdirSync(fixturesDir).map((caseName) => {
-        if (skipTests.indexOf(caseName) >= 0) return;
-
-        it(`output match: ${caseName}`, () => {
-            const fixtureDir = path.join(fixturesDir, caseName);
-
-            const actual = transform(path.join(fixtureDir, 'actual.html'));
-
-            // Check code output
-            const expected = fs.readFileSync(path.join(fixtureDir, 'expected.js'));
-            assert.equal(trim(actual), trim(expected));
-        });
-    });
-});
-
-describe('emit errors', () => {
-    it ('throw on multiple roots', () => {
-        const fixturePath = path.join(fixturesDir, 'multiRootError');
-        try {
-            transform(fixturePath, 'actual.html');
-        } catch (e) {
-            assert(e);
-        }
-    });
-
-});
+function isOnly(dir) {
+    return fs.existsSync(path.join(dir, 'only'));
+}
 
 function transform(filePath, options = {}) {
     function getPluginConfig() {
@@ -57,3 +28,41 @@ function transform(filePath, options = {}) {
     }).code;
 }
 
+
+function generateTestCase(dir, name) {
+    (isOnly(dir) ? it.only : it)(name, () => {
+        try {
+            const actual = transform(path.join(dir, 'actual.html'));
+
+            // Check code output
+            const expected = fs.readFileSync(path.join(dir, 'expected.js'));
+            assert.equal(trim(actual), trim(expected));   
+        } catch (error) {
+            try {
+                const expected = JSON.parse(fs.readFileSync(path.join(dir, 'error.json')));
+
+                const messageContent = error.message.slice(error.message.indexOf(':') + 2); 
+                assert.equal(messageContent, expected.message, 'Messages are not matching');
+                assert(error.loc.line == expected.line, `Error lines are not matching`);
+                assert(error.loc.column == expected.column, `Error columns are not matching`);
+            } catch (missingError) {
+                if (missingError.message.includes('ENOENT')) {
+                    throw error;
+                }
+
+                throw missingError;
+            }
+        }
+    });
+}
+
+describe('fixtures', () => {
+    fs.readdirSync(FIXTURE_DIR).map((caseName) => {
+        const fixtureDir = path.join(FIXTURE_DIR, caseName);
+        if (!fs.statSync(fixtureDir).isDirectory()) {
+            return;
+        }
+
+        generateTestCase(fixtureDir, caseName);
+    });
+});
