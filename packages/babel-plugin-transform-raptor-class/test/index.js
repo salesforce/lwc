418da16755519847bc4759d9b4ef1727b6b4b69e
p-dartus <p.dartus@salesforce.com>
2016-12-21 16:26:21 +0100

    Incorporate in class plugin

--- packages/babel-plugin-transform-raptor-class/test/index.js
+++ packages/babel-plugin-transform-raptor-class/test/index.js
@@ -1,35 +1,15 @@
-/* eslint-env node, mocha */
+/* eslint-env node */
 
-const babel = require('babel-core');
-const fs = require('fs');
 const path = require('path');
-const assert = require('power-assert');
-const plugin = require('../src/index');
+const { babelFixtureTransform } = require('raptor-helper-fixture');
 
-const fixturesDir = path.join(__dirname, 'fixtures');
+const transfromRaptorClass = require('../src/index');
 
-const BABEL_CONFIG = {
-    babelrc: false,
-    plugins: [plugin],
-    filename: './test.js',
+babelFixtureTransform(path.join(__dirname, 'fixtures'), {
+    plugins: [
+        transfromRaptorClass
+    ],
     parserOpts: {
         plugins: ['*']
     }
-};
-
-function trim(str) {
-    return str.toString().replace(/^\s+|\s+$/, '');
-}
-
-describe('assert', () => {
-    it ('import and render added', () => {
-        const fixtureClass = path.join(fixturesDir, 'simpleProp');
-        const config = Object.assign(BABEL_CONFIG, { filename: './simpleProp.js' });
-        const src = fs.readFileSync(path.join(fixtureClass, 'actual.js')).toString();
-
-        const actual = babel.transform(src, config).code;
-        const expected = fs.readFileSync(path.join(fixtureClass, 'expected.js')).toString();
- 
-        assert.equal(trim(actual), trim(expected));
-    });
 });
