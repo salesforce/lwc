b66652426fe0cb936060d3709485a6ef97480ecc
p-dartus <p.dartus@salesforce.com>
2016-12-21 16:25:19 +0100

    Fixture test to standalone package

--- packages/babel-plugin-transform-raptor-template/test/index.js
+++ packages/babel-plugin-transform-raptor-template/test/index.js
@@ -1,68 +1,13 @@
-/* eslint-env mocha, node */
+/* eslint-env node */
 
-import * as babel from 'babel-core';
-import * as fs from 'fs';
 import * as path from 'path';
+import { babelFixtureTransform } from 'raptor-helper-fixture';
 
-import assert from 'power-assert';
-import plugin from '../src/index';
+import transfromRaptorTemplate from '../src/index';
 
-const FIXTURE_DIR = path.join(__dirname, 'fixtures');
-
-function trim(str) {
-    return str.toString().replace(/^\s+|\s+$/, '');
-}
-
-function isOnly(dir) {
-    return fs.existsSync(path.join(dir, 'only'));
-}
-
-function transform(filePath, options = {}) {
-    function getPluginConfig() {
-        return [plugin, options];
-    }
-
-    return babel.transformFileSync(filePath, {
-        babelrc: false,
-        plugins: [getPluginConfig()],
-    }).code;
-}
-
-
-function generateTestCase(dir, name) {
-    (isOnly(dir) ? it.only : it)(name, () => {
-        try {
-            const actual = transform(path.join(dir, 'actual.html'));
-
-            // Check code output
-            const expected = fs.readFileSync(path.join(dir, 'expected.js'));
-            assert.equal(trim(actual), trim(expected));   
-        } catch (error) {
-            try {
-                const expected = JSON.parse(fs.readFileSync(path.join(dir, 'error.json')));
-
-                const messageContent = error.message.slice(error.message.indexOf(':') + 2); 
-                assert.equal(messageContent, expected.message, 'Messages are not matching');
-                assert(error.loc.line == expected.line, `Error lines are not matching`);
-                assert(error.loc.column == expected.column, `Error columns are not matching`);
-            } catch (missingError) {
-                if (missingError.message.includes('ENOENT')) {
-                    throw error;
-                }
-
-                throw missingError;
-            }
-        }
-    });
-}
-
-describe('fixtures', () => {
-    fs.readdirSync(FIXTURE_DIR).map((caseName) => {
-        const fixtureDir = path.join(FIXTURE_DIR, caseName);
-        if (!fs.statSync(fixtureDir).isDirectory()) {
-            return;
-        }
-
-        generateTestCase(fixtureDir, caseName);
-    });
+babelFixtureTransform(path.join(__dirname, 'fixtures'), {
+    actualFileExtension: 'html',
+    plugins: [
+        transfromRaptorTemplate
+    ]
 });
