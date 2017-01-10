61bf8810d47e592d99476732ab49f9f1de2406b1
Diego <dval@salesforce.com>
2017-01-09 18:09:14 -0800

    Standalone cli

--- packages/raptor-compiler-core/bin/cli.js
+++ packages/raptor-compiler-core/bin/cli.js
@@ -4,51 +4,32 @@ import * as fs from 'fs';
 import {compile} from '../src/index';
 import path from 'path';
 
-const skipTests = [
-    '.babelrc',
-    '.DS_Store',
-    'empty'
-];
-
-const fixturesDir = path.join(__dirname + '/../test/', 'fixtures');
 
 const TEST_SRCS = {
-    empty: {
-        sourceTemplate : `
-            <template>
-                <section>
-                    <p>{test}</p>
-                </section>
-            </template>
-        `, 
-        sourceClass: `
-            export default class Bar {
-                test = 'foo';
-                constructor() {}
-            }
-        `,
-        sourceCSS: ''
-    }
+    sourceTemplate : `
+        <template>
+            <section>
+                <p>{test}</p>
+            </section>
+        </template>
+    `, 
+    sourceClass: `
+        export default class Foo {
+            test = 'foo';
+            constructor() {}
+        }
+    `,
+    sourceCSS: ''
 };
 
-fs.readdirSync(fixturesDir).map((caseName) => {
-    if (skipTests.indexOf(caseName) >= 0) return;
-    const fixtureCaseDir = path.join(fixturesDir, caseName);
-
-    const initialConfig = { componentPath: path.join(fixtureCaseDir, caseName + '.js') };
-    const config = Object.assign({}, TEST_SRCS[caseName] || {}, initialConfig);
-
-    return runCompile(config, {})
-    .then((actual) => {
-        console.log(actual.code);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+const config = Object.assign(TEST_SRCS, {
+    componentPath: 'ns/foo/foo.js',
 });
 
-
-
-function runCompile(config, options = {}) {
-    return compile(config, options);
-}
+compile(config)
+.then((res) => {
+    console.log(res.code);
+})
+.catch((err) => {
+    console.log(err);
+});
