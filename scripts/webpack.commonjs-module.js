9156b16cb57b03ed860ced7b6bc1e6ad06cd1766
Diego <dval@salesforce.com>
2017-01-30 22:08:05 -0800

    Added dist for prod builds

--- scripts/webpack.commonjs-module.js
+++ scripts/webpack.commonjs-module.js
@@ -1,10 +1,49 @@
 /* eslint-env node */
-module.exports = {
-  target: 'node',
-  entry: './packages/raptor-compiler-core/dist/index.js',
-  output: {
-    path: './dist/node',
-    filename: 'compiler.js',
-    libraryTarget: "commonjs-module"
-  }
+
+const webpack = require('webpack');
+
+module.exports = function (env) {
+    const nodeEnv = env && env.prod ? 'production' : 'development';
+    const isProd = nodeEnv === 'production';
+    const loaders = [];
+    const plugins = [
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        }),
+    ];
+
+    let filename = 'compiler.js';
+
+    if (isProd) {
+        plugins.push(new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            compress: {
+            warnings: true,
+            }
+        }));
+
+        loaders.push({
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+            presets: ['es2015']
+        }
+        })
+        filename = 'compiler.min.js';
+    }
+
+    return {
+        target: 'node',
+        entry: './packages/raptor-compiler-core/dist/index.js',
+        output: {
+            path: './dist/node',
+            filename: filename,
+            libraryTarget: "commonjs-module"
+        },
+        plugins: plugins,
+        module: {
+            loaders: loaders
+        }
+    };
 }
