8e2b2f753d0d669ef5a8298cf544b87fc68fded7
Diego <dval@salesforce.com>
2017-02-10 19:14:31 -0800

    Add test for event

---
+++ packages/babel-plugin-transform-raptor-template/test/fixtures/onEvent/expected.js
@@ -0,0 +1,34 @@
+const _m2 = function ($api, $cmp) {
+    return $cmp.handlePress.bind($cmp);
+};
+
+const _m = function ($api, $cmp) {
+    return $cmp.handleClick.bind($cmp);
+};
+
+const memoized = Symbol();
+export default function ($api, $cmp) {
+    const m = $cmp[memoized] || ($cmp[memoized] = {});
+    return [$api.h(
+        "section",
+        {},
+        [$api.h(
+            "div",
+            {
+                on: {
+                    click: m._m || (m._m = _m($api, $cmp))
+                }
+            },
+            ["x"]
+        ), $api.h(
+            "div",
+            {
+                on: {
+                    press: m._m2 || (m._m2 = _m2($api, $cmp))
+                }
+            },
+            ["x"]
+        )]
+    )];
+}
+export const templateUsedIds = [];
