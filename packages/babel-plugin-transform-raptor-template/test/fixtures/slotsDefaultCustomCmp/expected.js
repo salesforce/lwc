515f63c0efb5c278c43f80901f4efe92d0f52492
Diego <dval@salesforce.com>
2017-02-14 17:11:41 -0800

    Adding slot tests

---
+++ packages/babel-plugin-transform-raptor-template/test/fixtures/slotsDefaultCustomCmp/expected.js
@@ -0,0 +1,14 @@
+const memoized = Symbol();
+export default function ($api, $cmp, $slotset) {
+    const m = $cmp[memoized] || ($cmp[memoized] = {});
+    return [$api.h(
+        "section",
+        {},
+        [$slotset.$default$ || [$api.h(
+            "p",
+            {},
+            ["Default slot content"]
+        )]]
+    )];
+}
+export const templateUsedIds = [];
---
+++ packages/babel-plugin-transform-raptor-template/test/fixtures/slotsNamedCustomCmp/expected.js
@@ -0,0 +1,14 @@
+const memoized = Symbol();
+export default function ($api, $cmp, $slotset) {
+    const m = $cmp[memoized] || ($cmp[memoized] = {});
+    return [$api.h(
+        "section",
+        {},
+        [$slotset.test || [$api.h(
+            "p",
+            {},
+            ["Test slot content"]
+        )]]
+    )];
+}
+export const templateUsedIds = [];
