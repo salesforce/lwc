378e89140c2411d963687147a059ca8cf56ea5d1
Diego <dval@salesforce.com>
2017-02-27 15:38:21 -0800

    Removed index.js

--- src/index.js
+++
@@ -1,6 +0,0 @@
-console.log(`
-*********************************************
-*       This is a multi-package repo.       *
-*  All modules are located under /packages  *
-*********************************************
-`);
