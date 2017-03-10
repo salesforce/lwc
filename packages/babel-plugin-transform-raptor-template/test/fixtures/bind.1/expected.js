119bc9ea92628a27d5636d13ae568035e4465663
Diego Ferreiro Val <elfogris@gmail.com>
2017-03-10 10:57:47 -0800

    Add bind test

---
+++ packages/babel-plugin-transform-raptor-template/test/fixtures/bind.1/expected.js
@@ -0,0 +1,18 @@
+const memoized = Symbol();
+export default function ($api, $cmp, $slotset) {
+    const m = $cmp[memoized] || ($cmp[memoized] = {});
+    return [$api.h(
+        "section",
+        {},
+        [$api.c(
+            "ns-foo",
+            _nsFoo,
+            {
+                props: {
+                    foo: m._m || (m._m = _m($api, $cmp))
+                }
+            }
+        )]
+    )];
+}
+export const templateUsedIds = ["myFooCallback"];
