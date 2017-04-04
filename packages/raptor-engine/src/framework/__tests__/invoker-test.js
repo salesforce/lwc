1d207b4644e89e4c3811b22bb6d781208b49f4bd
Caridy <cpatino@salesforce.com>
2017-04-04 15:03:34 -0400

    issue #76: add tests for connectedCallback and disconnectedCallback

---
+++ packages/raptor-engine/src/framework/__tests__/invoker-test.js
@@ -0,0 +1,58 @@
+// import * as target from '../invoker.js';
+import * as api from "../api.js";
+import { patch } from '../patch.js';
+import { Element } from "../html-element.js";
+import assert from 'power-assert';
+
+describe('invoker.js', () => {
+
+    describe('integration', () => {
+
+        it('should invoke connectedCallback() after all child are inserted into the dom', () => {
+            let counter = 0;
+            const elm = document.createElement('x-foo');
+            const child = document.createElement('p');
+            const def = class MyComponent2 extends Element {
+                connectedCallback() {
+                    counter++;
+                    assert.strictEqual(elm.childNodes[0], child, 'the child element is not in the dom yet');
+                }
+                render() {
+                    return child;
+                }
+            }
+            const vnode = api.c('x-foo', def, {});
+            patch(elm, vnode); // insert `x-foo`
+            return Promise.resolve().then(() => {
+                assert.strictEqual(counter, 1);
+            });
+        });
+
+        it('should invoke disconnectedCallback() after all child are removed from the dom', () => {
+            let counter = 0;
+            const elm = document.createElement('x-foo');
+            const child = document.createElement('p');
+            document.body.appendChild(elm);
+            const def = class MyComponent2 extends Element {
+                disconnectedCallback() {
+                    counter++;
+                    assert.strictEqual(elm.childNodes.length, 0, 'the child element is not removed from the dom yet');
+                }
+                render() {
+                    return child;
+                }
+            }
+            const vnode1 = api.c('x-foo', def, {});
+            const vnode2 = api.h('div', {}, []);
+            patch(elm, vnode1); // insert `x-foo`
+            patch(vnode1, vnode2); // replace it with a `div`
+            return Promise.resolve().then(() => {
+                assert.strictEqual(document.body.childNodes.length, 1);
+                assert.strictEqual(document.body.childNodes[0].tagName, 'DIV');
+                assert.strictEqual(counter, 1);
+            });
+        });
+
+    });
+
+});
