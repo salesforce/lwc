c5521a6986a28e34567c2d7a9a353d712e14a00f
Caridy <cpatino@salesforce.com>
2017-04-04 15:47:03 -0400

    closes #91: all hooks are async, and after children are inserted in the dom

--- packages/raptor-engine/src/framework/__tests__/invoker-test.js
+++ packages/raptor-engine/src/framework/__tests__/invoker-test.js
@@ -12,7 +12,7 @@ describe('invoker.js', () => {
             let counter = 0;
             const elm = document.createElement('x-foo');
             const child = document.createElement('p');
-            const def = class MyComponent2 extends Element {
+            const def = class MyComponent1 extends Element {
                 connectedCallback() {
                     counter++;
                     assert.strictEqual(elm.childNodes[0], child, 'the child element is not in the dom yet');
@@ -23,6 +23,7 @@ describe('invoker.js', () => {
             }
             const vnode = api.c('x-foo', def, {});
             patch(elm, vnode); // insert `x-foo`
+            assert.strictEqual(counter, 0, 'connectedCallback should be invoked async');
             return Promise.resolve().then(() => {
                 assert.strictEqual(counter, 1);
             });
@@ -46,9 +47,31 @@ describe('invoker.js', () => {
             const vnode2 = api.h('div', {}, []);
             patch(elm, vnode1); // insert `x-foo`
             patch(vnode1, vnode2); // replace it with a `div`
+            assert.strictEqual(counter, 0, 'disconnectedCallback should be invoked async');
             return Promise.resolve().then(() => {
                 assert.strictEqual(document.body.childNodes.length, 1);
                 assert.strictEqual(document.body.childNodes[0].tagName, 'DIV');
+                assert.strictEqual(counter, 1);
+            });
+        });
+
+        it('should invoke renderedCallback() async after every change after all child are inserted', () => {
+            let counter = 0;
+            const elm = document.createElement('x-foo');
+            const child = document.createElement('p');
+            const def = class MyComponent3 extends Element {
+                renderedCallback() {
+                    counter++;
+                    assert.strictEqual(elm.childNodes[0], child, 'the child element is not in the dom yet');
+                }
+                render() {
+                    return child;
+                }
+            }
+            const vnode = api.c('x-foo', def, {});
+            patch(elm, vnode); // insert `x-foo`
+            assert.strictEqual(counter, 0, 'renderedCallback should be invoked async');
+            return Promise.resolve().then(() => {
                 assert.strictEqual(counter, 1);
             });
         });
