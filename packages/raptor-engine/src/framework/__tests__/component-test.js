7d619802b4795d9f0251e3e4d7a92d800ec5fb14
Caridy <cpatino@salesforce.com>
2017-05-19 15:36:06 -0400

    adding more tests attributeChangedCallback() to please @ekashida

--- packages/raptor-engine/src/framework/__tests__/component-test.js
+++ packages/raptor-engine/src/framework/__tests__/component-test.js
@@ -1,5 +1,8 @@
 import * as target from '../component';
 import assert from 'power-assert';
+import { Element } from "../html-element";
+import * as api from "../api";
+import { patch } from '../patch';
 
 describe('component', function () {
 
@@ -12,4 +15,55 @@ describe('component', function () {
             assert.throws(() => target.createComponent(1), "Number value");
         });
     });
+
+
+    describe('attribute-change-life-cycle', () => {
+        it('should not invoke attributeChangeCallback() when initializing props', () => {
+            let counter = 0;
+            class MyComponent extends Element {
+                constructor() {
+                    super();
+                    this.x = 1;
+                }
+                attributeChangedCallback() {
+                    counter++;
+                }
+            }
+            MyComponent.publicProps = {x: true}
+            MyComponent.observedAttributes = ['x'];
+            const elm = document.createElement('x-foo');
+            const vnode = api.c('x-foo', MyComponent, {});
+            patch(elm, vnode);
+            return Promise.resolve().then(() => {
+                assert.strictEqual(0, counter);
+            });
+        });
+        it('should invoke attributeChangeCallback() with previous value from constructor', () => {
+            let keyValue, oldValue, newValue, counter = 0;
+            class MyComponent extends Element {
+                constructor() {
+                    super();
+                    this.x = 1;
+                }
+                attributeChangedCallback(k, o, n) {
+                    oldValue = o;
+                    newValue = n;
+                    keyValue = k;
+                    counter++;
+                }
+            }
+            MyComponent.publicProps = {x: true}
+            MyComponent.observedAttributes = ['x'];
+            const elm = document.createElement('x-foo');
+            const vnode = api.c('x-foo', MyComponent, { props: { x: 2 } });
+            patch(elm, vnode);
+            return Promise.resolve().then(() => {
+                assert.strictEqual(1, counter);
+                assert.strictEqual('x', keyValue);
+                assert.strictEqual(1, oldValue);
+                assert.strictEqual(2, newValue);
+            });
+        });
+    });
+
 });
