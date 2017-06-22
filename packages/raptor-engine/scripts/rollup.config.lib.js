f3e6d85b08baed80263f259fdec031d49575363f
Trevor <tbliss@salesforce.com>
2017-06-22 10:57:23 -0700

    Generate raptor.test.js for tests to use (#425)

--- packages/raptor-engine/scripts/rollup.config.lib.js
+++ packages/raptor-engine/scripts/rollup.config.lib.js
@@ -7,10 +7,20 @@
 const p = require('path');
 const typescript = require('rollup-plugin-typescript');
 const { copyright } = require('./utils.js');
+const strip = require('rollup-plugin-strip-caridy-patched');
+const isTest = process.env.NODE_ENV === 'test';
+
+const stripConfig = {
+    debugger  : false,
+    functions : ['console.*'],
+    include   : '**/*.ts',
+};
 
 module.exports = {
     entry: p.resolve('src/framework/main.ts'),
-    targets: [
+    targets: isTest ? [
+        { dest: 'lib/raptor.test.js', format: 'cjs' }
+        ] : [
         { dest: 'lib/raptor.js', format: 'cjs' },
         { dest: 'lib/raptor.es.js', format: 'es' },
     ],
@@ -20,5 +30,6 @@ module.exports = {
         typescript({
             typescript: require('typescript'),
         }),
+        isTest && strip(stripConfig),
     ],
 };
