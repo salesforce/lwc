14223d330967aaf494eaef582de04f0cf5f45fd5
Diego Ferreiro Val <dval@salesforce.com>
2017-07-07 14:25:32 -0700

    Fixing engine package

--- packages/raptor-engine/scripts/rollup.config.lib.js
+++ packages/raptor-engine/scripts/rollup.config.lib.js
@@ -8,28 +8,27 @@ const p = require('path');
 const typescript = require('rollup-plugin-typescript');
 const { copyright } = require('./utils.js');
 const strip = require('rollup-plugin-strip-caridy-patched');
+const isCompat = process.env.MODE === 'compat';
 const isTest = process.env.NODE_ENV === 'test';
 
+const stripList = ['alert'].concat(isCompat ? [] : ['compat']).concat( isTest ? ['console.*'] : []);
 const stripConfig = {
-    debugger  : false,
-    functions : ['console.*'],
+    debugger  : true,
+    functions : Array.from(new Set(stripList)),
     include   : '**/*.ts',
 };
 
 module.exports = {
     entry: p.resolve('src/framework/main.ts'),
-    targets: isTest ? [
-        { dest: 'lib/raptor.test.js', format: 'cjs' }
-        ] : [
+    targets: isTest ? [ { dest: 'lib/raptor.test.js', format: 'cjs' } ] :
+    [
         { dest: 'lib/raptor.js', format: 'cjs' },
         { dest: 'lib/raptor.es.js', format: 'es' },
     ],
     banner: copyright,
     external: [],
     plugins: [
-        typescript({
-            typescript: require('typescript'),
-        }),
-        isTest && strip(stripConfig),
+        typescript({ typescript: require('typescript') }),
+        strip(stripConfig)
     ],
 };
