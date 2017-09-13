0e0bb62a5cf6dcaedad51a8153eb17848a39de39
Trevor Bliss <tbliss@salesforce.com>
2017-09-13 09:16:02 -0700

    fixes #618: compat mode regenerator-runtime binding (#621)

--- packages/babel-helpers-raptor/src/index.js
+++ packages/babel-helpers-raptor/src/index.js
@@ -54,22 +54,19 @@ function build(namespace, globalVar = "global", builder) {
         ]))
     ]);
 
-    // AST for:  var global = globalMember = Global.globalMember = {};
-    // Note: See buildRegenerator for the need of the `global` scoped var
+    // AST for:  var globalMember = Global.globalMember = {};
     body.push(t.variableDeclaration("var", [
         t.variableDeclarator(
-            t.identifier("global"),
-            t.assignmentExpression('=', namespace,
-                t.assignmentExpression("=", t.memberExpression(
+            t.identifier(namespace.name),
+            t.assignmentExpression("=", t.memberExpression(
                     t.identifier(globalVar), namespace),
                     t.objectExpression([t.objectProperty(t.identifier('helpers'), t.objectExpression([]))])
-                )
             )
         ),
         t.variableDeclarator(t.identifier('helpers'), t.memberExpression(namespace, t.identifier('helpers')))
     ]));
 
-    builder(body);
+    builder(body, namespace, globalVar);
     return tree;
 }
 
@@ -84,23 +81,31 @@ function buildHelpers(body, whitelist) {
     });
 }
 // Regenerator is a special dependency that we want to include
-// It stores itself in global.regeneratorRuntime (if it exists)
-function buildRegenerator(body) {
+// It stores itself in this.regeneratorRuntime
+function buildRegenerator(body, namespace, globalVar) {
     const regenerateRuntime = fs.readFileSync(runtimePath).toString();
     const test = template(regenerateRuntime);
     body.push(test());
+
+    // Regenerator must be attached to `this` so copy over to our exposed global object
+    body.push(t.assignmentExpression("=", t.memberExpression(
+        t.memberExpression(t.identifier(globalVar), namespace), t.identifier('regeneratorRuntime')),
+        t.memberExpression(t.identifier('this'), t.identifier('regeneratorRuntime'))));
+
+    // Clean up original global assignment
+    body.push(t.unaryExpression('delete', t.memberExpression(t.identifier('this'), t.identifier('regeneratorRuntime'))));
 }
 
-function buildAll(body, whitelist) {
+function buildAll(body, whitelist, namespace, globalVar) {
     buildHelpers(body, whitelist);
-    buildRegenerator(body);
+    buildRegenerator(body, namespace, globalVar);
 }
 
 // -- Public API ----
 
 exports.build = function(ns, globalVar) {
     const namespace = t.identifier(ns);
-    const builderFactory = (body) => buildAll(body, whitelist);
+    const builderFactory = (body, namespace, globalVar) => buildAll(body, whitelist, namespace, globalVar);
 
     const tree = build(namespace, globalVar, builderFactory);
     const result = generate(tree).code;
