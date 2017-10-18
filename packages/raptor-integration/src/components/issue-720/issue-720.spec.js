e8041e4a00da1f57a97671a580ae005baec181c1
David Turissini <dturissini@salesforce.com>
2017-10-18 15:36:56 -0700

    REmoving log test (#726)

--- packages/raptor-integration/src/components/issue-720/issue-720.spec.js
+++ packages/raptor-integration/src/components/issue-720/issue-720.spec.js
@@ -13,12 +13,4 @@ describe('Issue 720: Wrap all string literal variables with toString method', ()
         assert.deepEqual(hasError.value.length, 0);
         assert.deepEqual(noError.value.length, 1);
     });
-
-    it('should not have logged a warning about reactive properties', function () {
-        const logs = browser.log('browser');
-        const warnings = logs.value.filter((log) => {
-            return log.level === 'WARNING';
-        });
-        assert.deepEqual(warnings.length, 0);
-    });
 });
