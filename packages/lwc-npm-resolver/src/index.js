d2b960bdd1764f3a8cf72b35a7a1b17971d350dd
Diego Ferreiro Val <dval@salesforce.com>
2017-12-13 23:37:37 -0800

    Fix resolver logic (#914)
    - Differenciate between relative entries and root entries
    - Fixing option passing in rollup

--- packages/lwc-npm-resolver/src/index.js
+++ packages/lwc-npm-resolver/src/index.js
@@ -1,24 +1,45 @@
 const glob = require('glob');
 const path = require('path');
 const fs = require('fs');
-const RAPTOR_MODULES_PREFIX = 'lwc-';
-const PATTERN_PATH = `node_modules/${RAPTOR_MODULES_PREFIX}*/package.json`;
+const PREFIX = 'lwc';
+const RAPTOR_MODULES_PREFIX = `${PREFIX}-`;
+const PATTERN_PATH = `*/package.json`;
 const BLACKLIST = new Set(['lwc-npm-resolver']);
+const VISITED = new Set();
+
+function isLWCPackage(name) {
+    return name.startsWith(RAPTOR_MODULES_PREFIX);
+}
 
 module.exports = function resolve(options = {}) {
-    return glob.sync(PATTERN_PATH).reduce((mapping, file) => {
-        const pkg = JSON.parse(fs.readFileSync(file, 'utf8'));
-        const es6ModuleEntry = pkg.module || pkg['jsnext:main'];
+    return module.paths.reduce((m, nodeModulesDir) => {
+        return glob.sync(PATTERN_PATH, { cwd: nodeModulesDir }).reduce((mappings, file) => {
+            const fullPath = path.join(nodeModulesDir, file);
+            const pkg = require(fullPath);
 
-        if (es6ModuleEntry) {
-            const modulePath = path.join(path.dirname(file), es6ModuleEntry);
-            const moduleName = pkg.name.replace(RAPTOR_MODULES_PREFIX, '');
+            const pkgName = pkg.name;
+            const es6ModuleEntry = pkg.module || pkg['jsnext:main'];
 
-            if (!BLACKLIST.has(moduleName)) {
-                mapping[moduleName] = modulePath;
+            if (es6ModuleEntry && !BLACKLIST.has(pkgName)) {
+                if (VISITED.has(pkgName)) {
+                    console.log(`Package ${pkgName} is already resolved.`);
+                } else {
+                    VISITED.add(pkgName)
+                    if (isLWCPackage(pkgName)) {
+                        const normalizedName = pkgName.replace(RAPTOR_MODULES_PREFIX, '');
+                        const modulePath = path.join(path.dirname(fullPath), es6ModuleEntry);
+
+                        mappings[normalizedName] = {
+                            name: normalizedName,
+                            namespace: PREFIX,
+                            fullName: pkgName,
+                            entry: modulePath,
+                        };
+                    }
+                }
             }
-        }
 
-        return mapping;
+            return mappings;
+        }, m);
     }, {});
-}
+};
--- packages/rollup-plugin-lwc-compiler/src/index.js
+++ packages/rollup-plugin-lwc-compiler/src/index.js
@@ -17,17 +17,24 @@ const { DEFAULT_NS, DEFAULT_OPTIONS } = require('./constants');
 function getModuleQualifiedName(file, opts) {
     const parts = file.split('/');
     const ext = path.extname(file);
-    let name = path.basename(parts.pop(), ext);
+    const fileName = parts.pop();
+    let name = path.basename(fileName, ext);
     const nameParts = name.split('-');
     const noNamespace = nameParts.length === 1;
+    let isRootEntry;
     let ns;
 
     // If mapping folder structure override namespace
     if (opts.mapNamespaceFromPath) {
+        isRootEntry = name === parts.pop();
         ns = parts.pop();
     } else if (noNamespace) {
+        isRootEntry = true;
         ns = DEFAULT_NS;
     } else {
+        // This is a hack for integration test package allows cases like test-{name} prefix
+        isRootEntry = parts.pop().endsWith(name);
+
         ns = nameParts.shift();
         name = nameParts.join('-');
     }
@@ -39,11 +46,9 @@ function getModuleQualifiedName(file, opts) {
     return {
         name,
         namespace: ns,
-        fullName: mappingEntry,
+        moduleName: isRootEntry ? mappingEntry : `relative::${mappingEntry}`,
         entry: absoluteFile,
-        root
     };
-
 }
 
 function getSourceModulePaths(opts, entry) {
@@ -53,7 +58,7 @@ function getSourceModulePaths(opts, entry) {
 
     glob.sync(pattern).forEach(file => {
         const moduleRegistry = getModuleQualifiedName(file, opts);
-        mapping[moduleRegistry.fullName] = moduleRegistry;
+        mapping[moduleRegistry.moduleName] = moduleRegistry;
     });
 
     return mapping;
@@ -79,7 +84,7 @@ module.exports = function rollupRaptorCompiler(opts = {}) {
                 ? lwcNpmResolver()
                 : {};
             const sourcePaths = options.resolveFromSource
-                ? getSourceModulePaths(opts, entry)
+                ? getSourceModulePaths(options, entry)
                 : {};
 
             modulePaths = Object.assign({}, externalPaths, sourcePaths);
@@ -112,9 +117,6 @@ module.exports = function rollupRaptorCompiler(opts = {}) {
 
             // Resolve module definition
             const moduleDefinition = Object.values(modulePaths).find(moduleDef => {
-                if (!moduleDef.root) {
-                    return;
-                }
                 return id.startsWith(moduleDef.entry);
             }) || getModuleQualifiedName(id, opts);
 
