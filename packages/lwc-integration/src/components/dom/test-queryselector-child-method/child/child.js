a5384494fc35a826635ea25cc7f51e3f724c2548
Pierre-Marie <p.dartus@salesforce.com>
2018-01-05 13:12:13 +0100

    Fix all the integration tests

--- packages/lwc-integration/src/components/dom/test-queryselector-child-method/child/child.js
+++ packages/lwc-integration/src/components/dom/test-queryselector-child-method/child/child.js
@@ -2,8 +2,8 @@ import { Element, api, track } from 'engine';
 
 export default class Child extends Element {
     @track text;
-    @api
-    foo() {
+
+    @api foo() {
         this.text = 'method executed successfully';
     }
 }
--- packages/lwc-integration/src/components/lifecycle/error-boundary-suite/boundary-child-rendered-throw/boundary-child-rendered-throw.js
+++ packages/lwc-integration/src/components/lifecycle/error-boundary-suite/boundary-child-rendered-throw/boundary-child-rendered-throw.js
@@ -1,4 +1,4 @@
-import { Element } from 'engine'
+import { Element, track } from 'engine'
 
 export default class BoundaryChildRenderedThrow extends Element {
     @track state = {};
