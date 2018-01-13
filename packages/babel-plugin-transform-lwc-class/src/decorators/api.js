4ee890fe08484c5cbb7a9132a61e8871b89f2632
David Turissini <dturissini@dturissini-ltm.internal.salesforce.com>
2018-01-12 16:20:02 -0800

    Allowing data to be a valid propname

--- packages/babel-plugin-transform-lwc-class/src/decorators/api.js
+++ packages/babel-plugin-transform-lwc-class/src/decorators/api.js
@@ -87,7 +87,7 @@ function validatePropertyName(property) {
         throw property.buildCodeFrameError(
             `Invalid property name ${propertyName}. Properties starting with "on" are reserved for event handlers.`
         );
-    } else if (propertyName.startsWith('data')) {
+    } else if (propertyName.startsWith('data') && propertyName !== 'data') {
         throw property.buildCodeFrameError(
             `Invalid property name ${propertyName}. Properties starting with "data" are reserved attributes.`
         );
