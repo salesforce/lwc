3af3f13e30107fd1b14abcdc94b91d32a444f96c
David Turissini <davidairborne@gmail.com>
2018-01-25 10:30:02 -0800

    fix(engine): Wrapping iframe content window in facade (#13) (#36)
    * contentWindow wrapper
    * unwrapping iframe contentWindow
    * tests

---
+++ packages/lwc-integration/src/components/dom/test-unwrap-crossorigin-iframe/unwrap-crossorigin-iframe.js
@@ -0,0 +1,16 @@
+import { Element, api, track, unwrap } from 'engine';
+
+export default class UnwrapCrossOriginIframe extends Element {
+    @track
+    errorMessage = 'no error';
+
+    @api
+    unwrapContentWindow() {
+        const contentWindow = this.root.querySelector('iframe').contentWindow;
+        try {
+            unwrap(contentWindow);
+        } catch (e) {
+            this.errorMessage = e.message;
+        }
+    }
+}
