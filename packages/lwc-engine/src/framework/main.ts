1884a7a3fa90b590866492baf38198afa6b8ee16
Ravi Jayaramappa <ravi.jayaramappa@salesforce.com>
2018-02-05 15:46:29 -0800

    [POC] Allow locker piercing hook to handle Element.prototype.querySelector and Element.prototype.querySelectorAll @W-4240480@

--- packages/lwc-engine/src/framework/main.ts
+++ packages/lwc-engine/src/framework/main.ts
@@ -3,6 +3,8 @@ export { getComponentDef } from "./def";
 export { Element } from "./html-element";
 export { register } from "./services";
 export { unwrap } from "./membrane";
+// Temporary export for locker, will be removed once the shadow-dom polyfil is available
+export { isNodeOwnedByComponent } from "./root";
 export { default as api } from "./decorators/api";
 export { default as track } from "./decorators/track";
 export { default as wire } from "./decorators/wire";
