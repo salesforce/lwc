de195ce0c9202747ea4a54113ef3d5d43d276ca0
Pierre-Marie <p.dartus@salesforce.com>
2018-02-05 17:40:45 -0800

    Migrate remaining benchmark to best

---
+++ packages/benchmark/src/__benchmarks__/create-10k.benchmark.js
@@ -0,0 +1,31 @@
+import { createElement } from 'engine';
+import Table from 'benchmark-table';
+import TableComponent from 'benchmark-table-component';
+
+import { Store } from '../table-store';
+import { insertTableComponent, destroyTableComponent } from '../utils';
+
+const tagNameMapping = {
+    'benchmark-table': Table,
+    'benchmark-table-component': TableComponent
+};
+
+Object.keys(tagNameMapping).forEach(tagName => {
+    benchmark(`${tagName}/create/10k`, () => {
+        let tableElement;
+        before(() => {
+            tableElement = createElement(tagName, { is: tagNameMapping[tagName] });
+            return insertTableComponent(tableElement);
+        });
+
+        run(() => {
+            const store = new Store();
+            store.runLots();
+            tableElement.rows = store.data;
+        });
+
+        after(() => {
+            destroyTableComponent(tableElement);
+        });
+    });
+});
