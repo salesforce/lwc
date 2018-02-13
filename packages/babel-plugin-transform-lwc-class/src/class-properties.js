5cbe9f28acd301b860ca90def252885f659d39b8
Pierre-Marie <p.dartus@salesforce.com>
2018-02-13 15:15:23 -0800

    fix(compiler): Validate decorated properties

--- packages/babel-plugin-transform-lwc-class/src/class-properties.js
+++
@@ -1,5 +0,0 @@
-const classProperty = require('babel-plugin-transform-class-properties');
-
-module.exports = function({ types }) {
-    return classProperty({ types }).visitor;
-}
--- packages/babel-plugin-transform-lwc-class/src/index.js
+++ packages/babel-plugin-transform-lwc-class/src/index.js
@@ -1,15 +1,36 @@
+const classProperty = require('babel-plugin-transform-class-properties');
+
 const component = require('./component');
 const decorators = require('./decorators');
-const classProperties = require('./class-properties');
 
+/**
+ * The transform is done in 2 passes:
+ *    - First, apply in a single AST traversal the decorators and the component transformation.
+ *    - Then, in a second path transform class properties using the official babel plugin "babel-plugin-transform-class-properties".
+ */
 module.exports = function ({ types, traverse }) {
     const { merge: mergeVisitors } = traverse.visitors;
 
+    const { visitor: classPropertyVisitor } = classProperty({ types });
+
     return {
+        manipulateOptions(opts, parserOpts) {
+            parserOpts.plugins.push('decorators');
+            parserOpts.plugins.push('classProperties');
+        },
         visitor: mergeVisitors([
             decorators({ types }),
             component({ types }),
-            classProperties({ types }),
+            {
+                Program: {
+                    exit(path, state) {
+                        path.traverse(
+                            classPropertyVisitor,
+                            state
+                        );
+                    }
+                }
+            },
         ])
     }
 }
