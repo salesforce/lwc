84648ef1c9b292ab224c8840b12d8bb43f1957dc
David Turissini <dturissini@dturissini-ltm.internal.salesforce.com>
2018-02-13 15:36:49 -0800

    Cleanup in class transform

--- packages/babel-plugin-transform-lwc-class/src/constants.js
+++ packages/babel-plugin-transform-lwc-class/src/constants.js
@@ -3,20 +3,6 @@ const GLOBAL_ATTRIBUTE_SET = new Set([
     'id', 'itemprop', 'lang', 'slot', 'spellcheck', 'style', 'tabindex', 'title',
 ]);
 
-const GLOBAL_ATTRIBUTE_PROP_SET = new Set([
-     'accessKey', 'contentEditable', 'contextmenu', 'dir', 'draggable', 'hidden',
-    'id',  'lang', 'spellcheck', 'tabIndex', 'title',
-])
-
-const HTML_ATTRIBUTE_SET = new Set([
-    'role', 'class', 'itemprop', 'slot', 'spellcheck', 'style'
-]);
-
-const INVALID_LOWERCASE_ATTRIBUTE_PROP_SET = new Set([
-    'accesskey', 'contenteditable', 'tabindex'
-]);
-
-module.exports = { GLOBAL_ATTRIBUTE_PROP_SET, HTML_ATTRIBUTE_SET, GLOBAL_ATTRIBUTE_SET, INVALID_LOWERCASE_ATTRIBUTE_PROP_SET };
 const LWC_PACKAGE_ALIAS = 'engine';
 
 const LWC_PACKAGE_EXPORTS = {
