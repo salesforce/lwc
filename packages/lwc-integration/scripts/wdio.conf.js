938b8a6a5786ddb60d3a4f856956a790f29be2e9
Pierre-Marie <p.dartus@salesforce.com>
2018-02-13 16:27:23 -0800

    Decrease the concurrency

--- packages/lwc-integration/scripts/wdio.conf.js
+++ packages/lwc-integration/scripts/wdio.conf.js
@@ -77,7 +77,7 @@ const config = {
     // from the same test should run tests.
     //
     // TODO: figure out why tests fail when maxInstances set to anything higher than 1
-    maxInstances: 15,
+    maxInstances: 3,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
