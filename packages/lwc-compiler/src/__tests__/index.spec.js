2060f63d48e63c495a1c9a5ba50d453eae168ad2
Alex <apapko@salesforce.com>
2018-02-18 16:53:35 -0800

    re-adding transform functions into index file to prevent test failures

--- packages/lwc-compiler/src/__tests__/index.spec.js
+++ packages/lwc-compiler/src/__tests__/index.spec.js
@@ -1,6 +1,6 @@
 /* eslint-env node, jest */
 
-const { compile } = require('../index');
+const { compile, transform } = require('../index');
 const { pretify } = require('./utils');
 
 const VALID_TEST_JS = `
@@ -67,139 +67,139 @@ describe('compile', () => {
     // });
 });
 
-// describe('transform', () => {
-//     it('should validate presence of src', () => {
-//         expect(() => transform()).toThrow(
-//             /Expect a string for source. Received undefined/,
-//         );
-//     });
+describe('transform', () => {
+    it('should validate presence of src', () => {
+        expect(() => transform()).toThrow(
+            /Expect a string for source. Received undefined/,
+        );
+    });
 
-//     it('should validate presence of id', () => {
-//         expect(() => transform(`console.log('Hello')`)).toThrow(
-//             /Expect a string for id. Received undefined/,
-//         );
-//     });
+    it('should validate presence of id', () => {
+        expect(() => transform(`console.log('Hello')`)).toThrow(
+            /Expect a string for id. Received undefined/,
+        );
+    });
 
-//     it('should apply transformation for javascript file', async () => {
-//         const actual = `
-//             import { Element } from 'engine';
-//             export default class Foo extends Element {}
-//         `;
+    it('should apply transformation for javascript file', async () => {
+        const actual = `
+            import { Element } from 'engine';
+            export default class Foo extends Element {}
+        `;
 
-//         const expected = `
-//             import _tmpl from './foo.html';
-//             import { Element } from 'engine';
-//             export default class Foo extends Element {
-//                 render() {
-//                     return _tmpl;
-//                 }
-//             }
-//             Foo.style = _tmpl.style;
-//         `;
+        const expected = `
+            import _tmpl from './foo.html';
+            import { Element } from 'engine';
+            export default class Foo extends Element {
+                render() {
+                    return _tmpl;
+                }
+            }
+            Foo.style = _tmpl.style;
+        `;
 
-//         const { code } = await transform(actual, 'foo.js', {
-//             moduleNamespace: 'x',
-//             moduleName: 'foo',
-//         });
+        const { code } = await transform(actual, 'foo.js', {
+            moduleNamespace: 'x',
+            moduleName: 'foo',
+        });
 
-//         expect(pretify(code)).toBe(pretify(expected));
-//     });
+        expect(pretify(code)).toBe(pretify(expected));
+    });
 
-//     it('should apply transformation for template file', async () => {
-//         const actual = `
-//             <template>
-//                 <div>Hello</div>
-//             </template>
-//         `;
+    it('should apply transformation for template file', async () => {
+        const actual = `
+            <template>
+                <div>Hello</div>
+            </template>
+        `;
 
-//         const expected = `
-//             import style from './foo.css'
+        const expected = `
+            import style from './foo.css'
 
-//             export default function tmpl($api, $cmp, $slotset, $ctx) {
-//                 const {
-//                     t: api_text,
-//                     h: api_element
-//                 } = $api;
+            export default function tmpl($api, $cmp, $slotset, $ctx) {
+                const {
+                    t: api_text,
+                    h: api_element
+                } = $api;
 
-//                 return [api_element("div", {
-//                     key: 1
-//                 }, [api_text("Hello")])];
-//             }
+                return [api_element("div", {
+                    key: 1
+                }, [api_text("Hello")])];
+            }
 
-//             if (style) {
-//                 const tagName = 'x-foo';
-//                 const token = 'x-foo_foo';
-//                 tmpl.token = token;
-//                 tmpl.style = style(tagName, token);
-//             }
-//         `;
+            if (style) {
+                const tagName = 'x-foo';
+                const token = 'x-foo_foo';
+                tmpl.token = token;
+                tmpl.style = style(tagName, token);
+            }
+        `;
 
-//         const { code } = await transform(actual, 'foo.html', {
-//             moduleNamespace: 'x',
-//             moduleName: 'foo',
-//         });
+        const { code } = await transform(actual, 'foo.html', {
+            moduleNamespace: 'x',
+            moduleName: 'foo',
+        });
 
-//         expect(pretify(code)).toBe(pretify(expected));
-//     });
+        expect(pretify(code)).toBe(pretify(expected));
+    });
 
-//     it('should apply transformation for stylesheet file', async () => {
-//         const actual = `
-//             div {
-//                 background-color: red;
-//             }
-//         `;
+    it('should apply transformation for stylesheet file', async () => {
+        const actual = `
+            div {
+                background-color: red;
+            }
+        `;
 
-//         const expected = `
-//             function style(tagName, token) {
-//                 return \`
-//             div[\${token}] {
-//                 background-color: red;
-//             }
-//                 \`;
-//             }
-//             export default style;
-//         `;
+        const expected = `
+            function style(tagName, token) {
+                return \`
+            div[\${token}] {
+                background-color: red;
+            }
+                \`;
+            }
+            export default style;
+        `;
 
-//         const { code } = await transform(actual, 'foo.css', {
-//             moduleNamespace: 'x',
-//             moduleName: 'foo',
-//         });
+        const { code } = await transform(actual, 'foo.css', {
+            moduleNamespace: 'x',
+            moduleName: 'foo',
+        });
 
-//         expect(pretify(code)).toBe(pretify(expected));
-//     });
+        expect(pretify(code)).toBe(pretify(expected));
+    });
 
-//     it('javascript metadata', async () => {
-//         const content = `
-//             import { Element, api } from 'engine';
-//             /** Foo doc */
-//             export default class Foo extends Element {
-//                 _privateTodo;
-//                 @api get todo () {
-//                     return this._privateTodo;
-//                 }
-//                 @api set todo (val) {
-//                     return this._privateTodo = val;
-//                 }
-//                 @api
-//                 index;
-//             }
-//         `;
+    it('javascript metadata', async () => {
+        const content = `
+            import { Element, api } from 'engine';
+            /** Foo doc */
+            export default class Foo extends Element {
+                _privateTodo;
+                @api get todo () {
+                    return this._privateTodo;
+                }
+                @api set todo (val) {
+                    return this._privateTodo = val;
+                }
+                @api
+                index;
+            }
+        `;
 
-//         const result = await transform(content, 'foo.js', {
-//             moduleNamespace: 'x',
-//             moduleName: 'foo',
-//         });
-//         const metadata = result.metadata;
-//         expect(metadata.decorators).toEqual([
-//             {
-//                 type: 'api',
-//                 targets: [
-//                     { type: 'property', name: 'todo' },
-//                     { type: 'property', name: 'index' }
-//                 ]
-//             }
-//         ]);
-//         expect(metadata.doc).toBe('Foo doc');
-//         expect(metadata.declarationLoc).toEqual({ start: { line: 4, column: 12 }, end: { line: 14, column: 13 } });
-//     });
-// });
+        const result = await transform(content, 'foo.js', {
+            moduleNamespace: 'x',
+            moduleName: 'foo',
+        });
+        const metadata = result.metadata;
+        expect(metadata.decorators).toEqual([
+            {
+                type: 'api',
+                targets: [
+                    { type: 'property', name: 'todo' },
+                    { type: 'property', name: 'index' }
+                ]
+            }
+        ]);
+        expect(metadata.doc).toBe('Foo doc');
+        expect(metadata.declarationLoc).toEqual({ start: { line: 4, column: 12 }, end: { line: 14, column: 13 } });
+    });
+});
--- packages/lwc-compiler/src/index.js
+++ packages/lwc-compiler/src/index.js
@@ -1,5 +1,41 @@
 import { compile as compileBundle} from './compiler';
+import { MODES, isProd } from './modes';
+import transformFile from './transform';
+import * as replacePlugin from "rollup-plugin-replace";
+import minifyPlugin from "./rollup-plugins/minify";
+import { isString } from './utils';
+
+const DEFAULT_TRANSFORM_OPTIONS = { mode: MODES.DEV };
 
 export function compile(entry, options = {}) {
     return compileBundle(entry, options);
+}
+
+export function transform(src, id, options) {
+    if (!isString(src)) {
+        throw new Error(`Expect a string for source. Received ${src}`);
+    }
+
+    if (!isString(id)) {
+        throw new Error(`Expect a string for id. Received ${id}`);
+    }
+
+    options = Object.assign({}, DEFAULT_TRANSFORM_OPTIONS, options);
+
+    return transformFile(src, id, options);
+}
+
+export function transformBundle(src, options) {
+    const mode = options.mode;
+
+    if (isProd(mode)) {
+        const rollupReplace = replacePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') });
+        const rollupMinify = minifyPlugin(options);
+        const resultReplace = rollupReplace.transform(src, '$__tmpBundleSrc');
+        const result = rollupMinify.transformBundle(resultReplace ? resultReplace.code : src);
+
+        src = result.code;
+    }
+
+    return src;
 }
