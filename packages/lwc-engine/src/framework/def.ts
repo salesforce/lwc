e6b48fa2a7e76df856ef872598d0ea9238f2bf13
David Turissini <dturissini@dturissini-ltm.internal.salesforce.com>
2018-02-27 15:58:31 -0800

    Remove public prop check

--- packages/lwc-engine/src/framework/def.ts
+++ packages/lwc-engine/src/framework/def.ts
@@ -273,11 +273,6 @@ function createMethodCaller(key: string) {
 }
 
 function getAttributePatched(this: VMElement, attrName: string): string | null {
-    if (process.env.NODE_ENV !== 'production') {
-        const vm = getCustomElementVM(this);
-        assertPublicAttributeColission(vm, attrName);
-    }
-
     return getAttribute.apply(this, ArraySlice.call(arguments));
 }
 
@@ -287,7 +282,6 @@ function setAttributePatched(this: VMElement, attrName: string, newValue: any) {
     vm.hostAttrs[attrName] = 1; // marking the set is needed for the AOM polyfill
     if (process.env.NODE_ENV !== 'production') {
         assertTemplateMutationViolation(vm, attrName);
-        assertPublicAttributeColission(vm, attrName);
     }
     setAttribute.apply(this, ArraySlice.call(arguments));
 }
@@ -297,7 +291,6 @@ function setAttributeNSPatched(this: VMElement, attrNameSpace: string, attrName:
 
     if (process.env.NODE_ENV !== 'production') {
         assertTemplateMutationViolation(vm, attrName);
-        assertPublicAttributeColission(vm, attrName);
     }
     setAttributeNS.apply(this, ArraySlice.call(arguments));
 }
@@ -308,7 +301,6 @@ function removeAttributePatched(this: VMElement, attrName: string) {
     vm.hostAttrs[attrName] = 1; // marking the set is needed for the AOM polyfill
     if (process.env.NODE_ENV !== 'production') {
         assertTemplateMutationViolation(vm, attrName);
-        assertPublicAttributeColission(vm, attrName);
     }
     removeAttribute.apply(this, ArraySlice.call(arguments));
 }
@@ -318,22 +310,8 @@ function removeAttributeNSPatched(this: VMElement, attrNameSpace: string, attrNa
 
     if (process.env.NODE_ENV !== 'production') {
         assertTemplateMutationViolation(vm, attrName);
-        assertPublicAttributeColission(vm, attrName);
     }
     removeAttributeNS.apply(this, ArraySlice.call(arguments));
-}
-
-function assertPublicAttributeColission(vm: VM, attrName: string) {
-    if (process.env.NODE_ENV === 'production') {
-        // this method should never leak to prod
-        throw new ReferenceError();
-    }
-    const propName = isString(attrName) ? getPropNameFromAttrName(attrName.toLocaleLowerCase()) : null;
-    const { def: { props: propsConfig } } = vm;
-
-    if (propsConfig && propName && propsConfig[propName]) {
-        assert.logError(`Invalid attribute "${attrName.toLocaleLowerCase()}" for ${vm}. Instead access the public property with \`element.${propName};\`.`);
-    }
 }
 
 function assertTemplateMutationViolation(vm: VM, attrName: string) {
