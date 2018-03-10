72559e4ed4f1467bcf06eff593240018a9947a4f
Trevor <tbliss@salesforce.com>
2018-03-09 17:23:59 -0800

    fix(script): Fix aura bin script

--- bin/generate-aura-dist.sh
+++ bin/generate-aura-dist.sh
@@ -5,15 +5,19 @@ if [ -z "$1" ]
     exit 1
 fi
 
-FILEPATH=$1
+AURA_FILEPATH=$1
+COMPAT_FILEPATH=$2
 npm run build:artifacts
 # engine versions
-cp  ./packages/lwc-engine/dist/umd/es2017/engine* $FILEPATH/aura-resources/src/main/resources/aura/resources/engine/
-cp  ./packages/lwc-engine/dist/umd/es5/engine.js $FILEPATH/aura-resources/src/main/resources/aura/resources/engine/engine_compat.js
-cp  ./packages/lwc-engine/dist/umd/es5/engine_debug.js $FILEPATH/aura-resources/src/main/resources/aura/resources/engine/engine_compat_debug.js
-cp  ./packages/lwc-engine/dist/umd/es5/engine.min.js $FILEPATH/aura-resources/src/main/resources/aura/resources/engine/engine_compat.min.js
-# compat-helpers
-cp  ./packages/proxy-compat-build/dist/umd/compat.* $FILEPATH/aura-resources/src/main/resources/aura/resources/compat-helpers/
-cp  ./packages/proxy-compat-build/dist/umd/downgrade.js $FILEPATH/aura-resources/src/main/resources/aura/resources/compat-helpers/downgrade.js
+cp  ./packages/lwc-engine/dist/umd/es2017/engine* $AURA_FILEPATH/aura-resources/src/main/resources/aura/resources/engine/
+cp  ./packages/lwc-engine/dist/umd/es5/engine.js $AURA_FILEPATH/aura-resources/src/main/resources/aura/resources/engine/engine_compat.js
+cp  ./packages/lwc-engine/dist/umd/es5/engine_debug.js $AURA_FILEPATH/aura-resources/src/main/resources/aura/resources/engine/engine_compat_debug.js
+cp  ./packages/lwc-engine/dist/umd/es5/engine.min.js $AURA_FILEPATH/aura-resources/src/main/resources/aura/resources/engine/engine_compat.min.js
+
 # compiler
-cp  ./packages/lwc-compiler/dist/umd/compiler.js $FILEPATH/aura-modules/src/main/resources/modules/compiler.js
+cp  ./packages/lwc-compiler/dist/umd/compiler.js $AURA_FILEPATH/aura-modules/src/main/resources/modules/compiler.js
+
+# compat-helpers
+cd $COMPAT_FILEPATH
+npm run build:bundle
+cp  ./dist/* $AURA_FILEPATH/aura-resources/src/main/resources/aura/resources/compat-helpers/
