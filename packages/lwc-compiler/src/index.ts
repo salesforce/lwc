d48c935c24acb57a44ec18227e72941db96094de
Alex <apapko@salesforce.com>
2018-03-19 09:22:18 -0700

    fix: retrieve actual version from pkg json

--- packages/lwc-compiler/src/index.ts
+++ packages/lwc-compiler/src/index.ts
@@ -1,5 +1,5 @@
 import * as moduleFinder from 'root-require';
 
-export const version = moduleFinder('package.json');
+export const version = moduleFinder('package.json').version;
 export { compile } from './compiler';
 export { transform } from './transformers/transformer';
