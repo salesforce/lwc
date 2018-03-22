3d0e4d453edd09d0405083c24849a1b9d1feec6c
David Turissini <dturissini@dturissini-ltm.internal.salesforce.com>
2018-03-22 13:50:14 -0700

    WIP: Linting

--- packages/lwc-engine/src/framework/wc.ts
+++ packages/lwc-engine/src/framework/wc.ts
@@ -4,21 +4,13 @@ import { keys, isUndefined, ArrayPush } from "./language";
 import { createVM, appendVM, renderVM, removeVM } from "./vm";
 import { getCustomElementVM } from "./html-element";
 import assert from "./assert";
-import { getPropNameFromAttrName, getAttrNameFromPropName } from "./utils";
+import { getAttrNameFromPropName } from "./utils";
 
 export function customElement(Ctor: ComponentConstructor): Function {
     const def = getComponentDef(Ctor);
-    const attrs = keys(def.observedAttrs);
     let mustBeRegistered = true;
     if (process.env.NODE_ENV !== 'production') {
         assert.isTrue(isUndefined(Ctor.forceTagName), `The experimental support for web components does not include the support for \`static forceTagName\` to "${Ctor.forceTagName}" declaration in the class definition for ${Ctor}.`);
-        // adding attributes that cannot be reflected into attributes
-        // into the attrs collection so we can provide the error message
-        // the they are used.
-        keys(def.props).forEach((propName) => {
-            const attrName = getAttrNameFromPropName(propName);
-            ArrayPush.call(attrs, attrName);
-        });
     }
     return class extends HTMLElement {
         constructor() {
@@ -39,6 +31,5 @@ export function customElement(Ctor: ComponentConstructor): Function {
             const vm = getCustomElementVM(this);
             removeVM(vm);
         }
-        static observedAttributes = attrs;
     };
 }
