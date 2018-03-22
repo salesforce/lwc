cc4bd5581f945c44ef8e2a431918bedb5c23354e
David Turissini <dturissini@dturissini-ltm.internal.salesforce.com>
2018-03-22 13:59:16 -0700

    WIP: Types

--- packages/lwc-engine/src/framework/wc.ts
+++ packages/lwc-engine/src/framework/wc.ts
@@ -6,7 +6,6 @@ import { getCustomElementVM } from "./html-element";
 import assert from "./assert";
 
 export function customElement(Ctor: ComponentConstructor): Function {
-    const def = getComponentDef(Ctor);
     let mustBeRegistered = true;
     if (process.env.NODE_ENV !== 'production') {
         assert.isTrue(isUndefined(Ctor.forceTagName), `The experimental support for web components does not include the support for \`static forceTagName\` to "${Ctor.forceTagName}" declaration in the class definition for ${Ctor}.`);
