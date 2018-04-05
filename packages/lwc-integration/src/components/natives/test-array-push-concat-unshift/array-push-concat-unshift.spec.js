480e99a4a9e5541b0025038a85768e074bc80c96
David Turissini <davidairborne@gmail.com>
2018-04-05 14:20:08 -0700

    fix: Misc. compat fixes (#198)
    * chore: Integration test for default prevented
    * chore(integration): Asserting
    * chore: Bump proxy-compat packages to version 0.17.36
    * test(integration): Add JSON.stringify integration tests
    * test(integration): Fix wrong usages of assert
    * test(integration): Remove date from integration test to avoid running in timezone issues
    * test(integration): Disable piercing hook integration test
    * test(integration): Add Object.entries integration tests
    * test(integration): Add integration tests for Object.values
    * chore: Bump proxy-compat packages to version 0.17.38
    * chore: Update yarnlock after install

--- packages/lwc-integration/src/components/natives/test-array-push-concat-unshift/array-push-concat-unshift.spec.js
+++ packages/lwc-integration/src/components/natives/test-array-push-concat-unshift/array-push-concat-unshift.spec.js
@@ -1,7 +1,6 @@
 const assert = require('assert');
 describe('Array prototype methods', () => {
     const URL = 'http://localhost:4567/array-push-concat-unshift';
-    let element;
 
     before(() => {
         browser.url(URL);
@@ -11,37 +10,37 @@ describe('Array prototype methods', () => {
         const el = browser.element('#unshift-list');
         el.click();
         const elements = browser.elements('#unshift-list li');
-        assert(elements.value[0].getText(), 'unshifted');
-        assert(elements.value[1].getText(), 'first');
-        assert(elements.value[2].getText(), 'second');
+        assert.strictEqual(elements.value[0].getText(), 'unshifted');
+        assert.strictEqual(elements.value[1].getText(), 'first');
+        assert.strictEqual(elements.value[2].getText(), 'second');
     });
 
     it('should display pushed items correctly', function () {
         const el = browser.element('#push-list');
         el.click();
         const elements = browser.elements('#push-list li');
-        assert(elements.value[0].getText(), 'first');
-        assert(elements.value[1].getText(), 'second');
-        assert(elements.value[2].getText(), 'pushed');
+        assert.strictEqual(elements.value[0].getText(), 'first');
+        assert.strictEqual(elements.value[1].getText(), 'second');
+        assert.strictEqual(elements.value[2].getText(), 'pushed');
     });
 
     it('should display concat items correctly', function () {
         const el = browser.element('#concat-list');
         el.click();
         const elements = browser.elements('#concat-list li');
-        assert(elements.value[0].getText(), 'first');
-        assert(elements.value[1].getText(), 'second');
-        assert(elements.value[2].getText(), 'concat 1');
-        assert(elements.value[3].getText(), 'concat 2');
+        assert.strictEqual(elements.value[0].getText(), 'first');
+        assert.strictEqual(elements.value[1].getText(), 'second');
+        assert.strictEqual(elements.value[2].getText(), 'concat 1');
+        assert.strictEqual(elements.value[3].getText(), 'concat 2');
     });
 
     it('should display concat items correctly', function () {
         const el = browser.element('#prop-concat-list');
         el.click();
         const elements = browser.elements('#prop-concat-list li');
-        assert(elements.value[0].getText(), 'concat 1');
-        assert(elements.value[1].getText(), 'concat 2');
-        assert(elements.value[2].getText(), 'first');
-        assert(elements.value[3].getText(), 'second');
+        assert.strictEqual(elements.value[0].getText(), 'concat 1');
+        assert.strictEqual(elements.value[1].getText(), 'concat 2');
+        assert.strictEqual(elements.value[2].getText(), 'first');
+        assert.strictEqual(elements.value[3].getText(), 'second');
     });
 });
--- packages/lwc-integration/src/components/natives/test-plain-array/plain-array.spec.js
+++ packages/lwc-integration/src/components/natives/test-plain-array/plain-array.spec.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
+
 describe('Plain array methods', () => {
     const URL = 'http://localhost:4567/plain-array';
-    let element;
 
     before(() => {
         browser.url(URL);
@@ -9,32 +9,32 @@ describe('Plain array methods', () => {
 
     it('should display unshifted items correctly', function () {
         const elements = browser.elements('#push-list-plain li');
-        assert(elements.value[0].getText(), '1');
-        assert(elements.value[1].getText(), '2');
-        assert(elements.value[2].getText(), '3');
-        assert(elements.value[3].getText(), '4');
+        assert.strictEqual(elements.value[0].getText(), '1');
+        assert.strictEqual(elements.value[1].getText(), '2');
+        assert.strictEqual(elements.value[2].getText(), '3');
+        assert.strictEqual(elements.value[3].getText(), '4');
     });
 
     it('should display pushed items correctly', function () {
         const elements = browser.elements('#push-list li');
-        assert(elements.value[0].getText(), 'first');
-        assert(elements.value[1].getText(), 'second');
-        assert(elements.value[2].getText(), 'proxy');
-        assert(elements.value[3].getText(), 'fourth');
+        assert.strictEqual(elements.value[0].getText(), 'first');
+        assert.strictEqual(elements.value[1].getText(), 'second');
+        assert.strictEqual(elements.value[2].getText(), 'proxy');
+        assert.strictEqual(elements.value[3].getText(), 'fourth');
     });
 
     it('should display concat items correctly', function () {
         const elements = browser.elements('#concat-list-plain li');
-        assert(elements.value[0].getText(), '1');
-        assert(elements.value[1].getText(), '2');
-        assert(elements.value[2].getText(), '3');
-        assert(elements.value[3].getText(), '4');
+        assert.strictEqual(elements.value[0].getText(), '1');
+        assert.strictEqual(elements.value[1].getText(), '2');
+        assert.strictEqual(elements.value[2].getText(), '3');
+        assert.strictEqual(elements.value[3].getText(), '4');
     });
 
     it('should display concat items correctly', function () {
         const elements = browser.elements('#concat-list-proxy li');
-        assert(elements.value[0].getText(), 'first');
-        assert(elements.value[1].getText(), 'second');
-        assert(elements.value[2].getText(), 'proxy');
+        assert.strictEqual(elements.value[0].getText(), 'first');
+        assert.strictEqual(elements.value[1].getText(), 'second');
+        assert.strictEqual(elements.value[2].getText(), 'proxy');
     });
 });
