bbfeef104eb536bb65d0a6aa958297b954b78fb8
Vince Chen <yungcheng.chen@salesforce.com>
2018-04-05 15:54:45 -0700

    refactor(wire): delete wire-service package

--- packages/lwc-wire-service/playground/app.js
+++
@@ -1,8 +0,0 @@
-// bootstrapping process for App
-import { createElement, register } from 'engine';
-import App from 'x-demo';
-
-const container = document.getElementById('main');
-const element = createElement('x-demo', { is: App });
-
-container.appendChild(element);
