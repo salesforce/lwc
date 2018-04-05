274064659fb74808dab7622878c7c86786e0a651
Pierre-Marie <p.dartus@salesforce.com>
2018-04-04 17:31:02 -0700

    test(integration): Fix wrong usages of assert

--- packages/lwc-integration/src/components/natives/test-function-arguments/function-arguments.spec.js
+++ packages/lwc-integration/src/components/natives/test-function-arguments/function-arguments.spec.js
@@ -1,21 +1,20 @@
 const assert = require('assert');
 describe('Function arguments', () => {
     const URL = 'http://localhost:4567/function-arguments';
-    let element;
 
     before(() => {
         browser.url(URL);
     });
 
     it('should have correct message', () => {
-        const el = browser.element('.message-assert');
-        assert(el.getText(), 'bar');
+        const element = browser.element('.message-assert');
+        assert.strictEqual(element.getText(), 'bar');
     });
 
     it('should iterate correctly', () => {
         const elements = browser.elements('.iterate-list li');
-        assert(elements.value[0].getText(), '2');
-        assert(elements.value[1].getText(), '3');
-        assert(elements.value[2].getText(), '4');
+        assert.strictEqual(elements.value[0].getText(), '2');
+        assert.strictEqual(elements.value[1].getText(), '3');
+        assert.strictEqual(elements.value[2].getText(), '4');
     });
 });
