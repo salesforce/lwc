f367e1bab8752231c58f5f8d12c2eb27d90f8b4e
Pierre-Marie <p.dartus@salesforce.com>
2018-05-16 07:42:28 -0700

    feat: add style injection

--- packages/lwc-compiler/src/transformers/template.js
+++ packages/lwc-compiler/src/transformers/template.js
@@ -1,42 +1,51 @@
 import * as path from 'path';
 import compile from 'lwc-template-compiler';
 
-export function getTemplateToken(filename, options) {
-    const templateId = path.basename(filename, path.extname(filename));
-    return `${options.moduleNamespace}-${options.moduleName}_${templateId}`;
-}
+function attachStyleToTemplate(src, { moduleName, moduleNamespace, filename }) {
+    const templateFilename = path.basename(filename, path.extname(filename));
 
-/**
- * Transforms a HTML template into module exporting a template function.
- * The transform also add a style import for the default stylesheet associated with
- * the template regardless if there is an actual style or not.
- */
-export default function(src, options) {
-    const { filename, moduleName, moduleNamespace } = options;
-    const { code: template, metadata, warnings } = compile(src, {});
+    // Use the component tagname and a unique style token to scope the compiled
+    // styles to the component.
+    const tagName = `${moduleNamespace}-${moduleName}`;
+    const scopingToken = `${tagName}_${templateFilename}`;
 
-    const fatalError = warnings.find(warning => warning.level === 'error');
-    if (fatalError) {
-        throw new Error(fatalError.message);
-    }
-
-
-    const token = getTemplateToken(filename, options);
-    const cssName = path.basename(filename, path.extname(filename)) + '.css';
-
-    const code = [
-        `import style from './${cssName}'`,
+    return [
+        `import stylesheet from './${templateFilename}.css'`,
         '',
-        template,
+        src,
         '',
-        `if (style) {`,
-        `   const tagName = '${moduleNamespace}-${moduleName}';`,
-        `   const token = '${token}';`,
+
+        // The compiler resolves the style import to undefined if the stylesheet
+        // doesn't exists.
+        `if (stylesheet) {`,
+
+        // The engine picks the style token from the template during rendering to
+        // add the token to all generated elements.
+        `    tmpl.token = '${scopingToken}';`,
         ``,
-        `   tmpl.token = token;`,
-        `   tmpl.style = style(tagName, token);`,
+
+        // Inject the component style in a new style tag the document head.
+        `    const style = document.createElement('style');`,
+        `    style.type = 'text/css';`,
+        `    style.dataset.token = '${scopingToken}'`,
+        `    style.textContent = stylesheet('${tagName}', '${scopingToken}');`,
+        `    document.head.appendChild(style)`,
         `}`,
     ].join('\n');
+}
 
-    return { code, metadata };
+export default function(src, options) {
+    // Transform HTML template to javascript
+    const { code, metadata, warnings } = compile(src, {});
+
+    // Throw if a fatal error ocurred during the compilation
+    const error = warnings.find(warning => warning.level === 'error');
+    if (error) {
+        throw new Error(error.message);
+    }
+
+    return {
+        code: attachStyleToTemplate(code, options),
+        metadata,
+    };
 }
