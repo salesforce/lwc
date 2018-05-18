3ed92c198043d9b2630ae7bea36ad5d7fd4f891c
Caridy <cpatino@salesforce.com>
2018-05-18 14:47:12 -0400

    refactor(engine): optimizing the condition for props update

--- packages/lwc-engine/src/framework/modules/props.ts
+++ packages/lwc-engine/src/framework/modules/props.ts
@@ -1,6 +1,6 @@
 import assert from "../assert";
 import { isUndefined, keys, StringToLowerCase } from "../language";
-import { EmptyObject, getAttrNameFromPropName, makeReadOnlyRecord, hasPropertyGetter } from "../utils";
+import { getAttrNameFromPropName, makeReadOnlyRecord, hasPropertyGetter } from "../utils";
 import { prepareForPropUpdate } from "../decorators/api";
 import { VNode, Module } from "../../3rdparty/snabbdom/types";
 import { ViewModelReflection } from "../utils";
@@ -17,7 +17,7 @@ function update(oldVnode: VNode, vnode: VNode) {
     if (isUndefined(props)) {
         return;
     }
-    let oldProps = oldVnode.data.props;
+    const oldProps = oldVnode.data.props;
     if (oldProps === props) {
         return;
     }
@@ -31,35 +31,37 @@ function update(oldVnode: VNode, vnode: VNode) {
     let old: any;
     const elm = vnode.elm as Element;
     const vm: VM = elm[ViewModelReflection];
-    oldProps = isUndefined(oldProps) ? EmptyObject : oldProps;
+    const isFirstPatch = isUndefined(oldProps);
 
     for (key in props) {
         cur = props[key];
-        old = (oldProps as any)[key];
+        let shouldUpdate = isFirstPatch;
 
-        if (process.env.NODE_ENV !== 'production') {
-            if (old !== cur && !(key in elm)) {
-                // TODO: this should never really happen because the compiler should always validate
-                assert.fail(`Unknown public property "${key}" of element <${StringToLowerCase.call(elm.tagName)}>. This is likely a typo on the corresponding attribute "${getAttrNameFromPropName(key)}".`);
+        if (!isFirstPatch) {
+            old = (oldProps as any)[key];
+
+            if (process.env.NODE_ENV !== 'production') {
+                if (old !== cur && !(key in elm)) {
+                    // TODO: this should never really happen because the compiler should always validate
+                    assert.fail(`Unknown public property "${key}" of element <${StringToLowerCase.call(elm.tagName)}>. This is likely a typo on the corresponding attribute "${getAttrNameFromPropName(key)}".`);
+                }
             }
-        }
-
-        let shouldUpdate;
-        if (isUndefined(vm)) {
-            // regular element
-            const { sel } = vnode as any;
-            shouldUpdate = (
-                // condition: is an especial element out of sync prop
-                (sel in EspecialTagAndPropMap && key in EspecialTagAndPropMap[sel]) ?
-                    elm[key] !== cur
-                    // condition: diff is out of sync for valid prop
-                    : (old !== cur && key in elm)
-            );
-        } else {
-            // custom element
-            const propDef = vm.def.props[key];
-            // condition: is a custom element out of sync prop
-            shouldUpdate = (!isUndefined(propDef) && hasPropertyGetter(propDef.config)) ? elm[key] !== cur : old !== cur;
+            if (isUndefined(vm)) {
+                // regular element
+                const { sel } = vnode as any;
+                shouldUpdate = (
+                    // condition: is an especial element out of sync prop
+                    (sel in EspecialTagAndPropMap && key in EspecialTagAndPropMap[sel]) ?
+                        elm[key] !== cur
+                        // condition: diff is out of sync for valid prop
+                        : (old !== cur && key in elm)
+                );
+            } else {
+                // custom element
+                const propDef = vm.def.props[key];
+                // condition: is a custom element out of sync prop
+                shouldUpdate = (!isUndefined(propDef) && hasPropertyGetter(propDef.config)) ? elm[key] !== cur : old !== cur;
+            }
         }
 
         if (shouldUpdate) {
