803109b6796680d434c82e5accea85ec6f98cd90
Pierre-Marie <p.dartus@salesforce.com>
2018-05-21 08:38:59 -0700

    style: make jest.config.consistent

--- packages/lwc-engine/jest.config.js
+++ packages/lwc-engine/jest.config.js
@@ -2,7 +2,6 @@ const BASE_CONFIG = require('../../scripts/jest/base.config');
 
 module.exports = {
     ...BASE_CONFIG,
-
     displayName: 'lwc-engine',
     setupTestFrameworkScriptFile: '<rootDir>/scripts/jest/setup.ts',
 };
