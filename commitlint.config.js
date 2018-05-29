bbe337282d48eb1b1e9fe9051b56483ce88e320a
Alex <apapko@salesforce.com>
2018-05-29 11:51:12 -0700

    wip(git): explicit rules list

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,26 @@
 module.exports = {
     extends: [
-        './commitlint.types.js',
         '@commitlint/config-conventional', // scoped packages are not prefixed
     ],
+    rules: {
+        'type-enum': [
+			2,
+			'always',
+			[
+				'build',
+				'chore',
+				'ci',
+				'docs',
+				'feat',
+				'fix',
+                'perf',
+                'proposal',
+				'refactor',
+				'revert',
+				'style',
+                'test',
+                'wip'
+			]
+		]
+    },
 }
