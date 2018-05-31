b0187fbffb7e3cb6b926cc6d768317bf906f90b5
Caridy Patiño <caridy@gmail.com>
2018-05-31 19:05:54 -0400

    refactor(engine): updating customElement for 0.22.x

--- packages/lwc-engine/src/framework/wc.ts
+++ packages/lwc-engine/src/framework/wc.ts
@@ -1,24 +1,27 @@
-import { getComponentDef, registerComponent } from "./def";
 import { ComponentConstructor } from "./component";
-import { isUndefined } from "./language";
-import { createVM, appendVM, renderVM, removeVM } from "./vm";
-import { getCustomElementVM } from "./html-element";
+import { isUndefined, isObject, isNull } from "./language";
+import { createVM, appendVM, renderVM, removeVM, getCustomElementVM } from "./vm";
 import assert from "./assert";
+import { resolveCircularModuleDependency } from "./utils";
 
-export function customElement(Ctor: ComponentConstructor): Function {
-    let mustBeRegistered = true;
+export function customElement(Ctor: ComponentConstructor, options?: Record<string, boolean>): Function {
+    Ctor = resolveCircularModuleDependency(Ctor);
     if (process.env.NODE_ENV !== 'production') {
         assert.isTrue(isUndefined(Ctor.forceTagName), `The experimental support for web components does not include the support for \`static forceTagName\` to "${Ctor.forceTagName}" declaration in the class definition for ${Ctor}.`);
+    }
+    const normalizedOptions = { fallback: false, mode: 'open', isRoot: true };
+    if (isObject(options) && !isNull(options)) {
+        const { mode, fallback } = (options as any);
+        // TODO: for now, we default to open, but eventually it should default to 'closed'
+        if (mode === 'closed') { normalizedOptions.mode = mode; }
+        // fallback defaults to false to favor shadowRoot
+        if (fallback === true) { normalizedOptions.fallback = true; }
     }
     return class extends HTMLElement {
         constructor() {
             super();
             const tagName = this.tagName.toLocaleLowerCase();
-            if (mustBeRegistered) {
-                mustBeRegistered = false;
-                registerComponent(tagName, Ctor);
-            }
-            createVM(tagName, this);
+            createVM(tagName, this, Ctor, normalizedOptions);
         }
         connectedCallback() {
             const vm = getCustomElementVM(this);
