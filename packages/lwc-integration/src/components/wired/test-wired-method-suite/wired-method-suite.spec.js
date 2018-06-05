8d9dd0a7c2fc69920fcae0b6248576a5b5604ce8
David Turissini <dturissini@dturissini-ltm.internal.salesforce.com>
2018-06-05 10:30:25 -0700

    fix(engine): integration tests

--- packages/lwc-integration/src/components/wired/test-wired-method-suite/wired-method-suite.spec.js
+++ packages/lwc-integration/src/components/wired/test-wired-method-suite/wired-method-suite.spec.js
@@ -8,16 +8,21 @@ describe('Component with a wired method', () => {
     });
 
     it('should display data correctly', () => {
-        const element = browser.element('wired-method');
-        assert.equal(element.getText(), 'Title:task 0 Completed:true');
+        const text = browser.execute(function () {
+            return document.querySelector('wired-method').textContent;
+        });
+        assert.equal(text.value, 'Title:task 0 Completed:true');
     });
 
     it('should update data correctly', () => {
         const element = browser.element('wired-method');
         const button = browser.element('button');
         button.click();
-        browser.waitUntil(() => {
-            return element.getText() === 'Title:task 1 Completed:false';
-        }, 500, 'expect text to be different after 0.5s');
+        browser.pause(50);
+        const text = browser.execute(function () {
+            return document.querySelector('wired-method').textContent;
+        });
+
+        assert.equal(text.value, 'Title:task 1 Completed:false');
     });
 });
--- packages/lwc-integration/src/components/wired/test-wired-prop-suite/wired-prop-suite.spec.js
+++ packages/lwc-integration/src/components/wired/test-wired-prop-suite/wired-prop-suite.spec.js
@@ -8,16 +8,21 @@ describe('Component with a wired property', () => {
     });
 
     it('should display data correctly', () => {
-        const element = browser.element('wired-prop');
-        assert.equal(element.getText(), 'Title:task 0 Completed:true');
+        const text = browser.execute(function () {
+            return document.querySelector('wired-prop').textContent;
+        });
+        assert.equal(text.value, 'Title:task 0 Completed:true');
     });
 
     it('should update data correctly', () => {
         const element = browser.element('wired-prop');
         const button = browser.element('button');
         button.click();
-        browser.waitUntil(() => {
-            return element.getText() === 'Title:task 1 Completed:false';
-        }, 500, 'expect text to be different after 0.5s');
+        browser.pause(50);
+        const text = browser.execute(function () {
+            return document.querySelector('wired-prop').textContent;
+        });
+
+        assert.equal(text.value, 'Title:task 1 Completed:false');
     });
 });
