4d63c1c66aaefdc7b5829a4b2530646e5a279336
Trevor <tbliss@salesforce.com>
2018-06-08 16:39:20 -0700

    wip: add lwc-jest-serializer

---
+++ packages/lwc-jest-serializer/index.js
@@ -0,0 +1,45 @@
+export function test(value) {
+    if (!value) {
+        return false;
+    }
+
+    const { nodeType } = value;
+    if (!nodeType) {
+        return false;
+    }
+    return (
+        nodeType === 1 || // element
+        nodeType === 3 || // text
+        nodeType === 6    // comment
+    )
+}
+
+const {
+    getOwnPropertyDescriptor,
+    defineProperty,
+} = Object;
+
+const childNodesGetter = getOwnPropertyDescriptor(Node.prototype, 'childNodes').get;
+
+export function serialize(node, config, ...everythingElse) {
+    const htmlSerializer = config.plugins.filter((p) => {
+        return p.test(node) && p.test !== test;
+    })[0];
+
+    const oldDescriptor = getOwnPropertyDescriptor(node, 'childNodes');
+    if (oldDescriptor) {
+        defineProperty(node, 'childNodes', {
+            get() {
+                return childNodesGetter.call(this);
+            },
+            configurable: true,
+        });
+    }
+    const result = htmlSerializer.serialize(node, config, ...everythingElse);
+    if (oldDescriptor) {
+        defineProperty(node, 'childNodes', oldDescriptor);
+    }
+
+    return result;
+}
+
