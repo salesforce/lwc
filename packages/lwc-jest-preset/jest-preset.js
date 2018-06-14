853c30f2873cea17ddcb4d79659c32055ce9d023
Trevor <tbliss@salesforce.com>
2018-06-14 00:12:32 -0700

    Replace json preset with js

---
+++ packages/lwc-jest-preset/jest-preset.js
@@ -0,0 +1,20 @@
+/* eslint-env node */
+
+module.exports = {
+    moduleFileExtensions: ['js', 'html'],
+    moduleNameMapper: {
+        '^assert$': require.resolve('./dist/assertStub.js'),
+        '^aura$': require.resolve('./dist/auraStub.js'),
+        '^aura-instrumentation$': require.resolve('./dist/auraInstrumentationStub.js'),
+        '^instrumentation-service$': require.resolve('./dist/auraInstrumentationStub.js'),
+        '^aura-storage$': require.resolve('./dist/auraStorageStub.js'),
+        '^logger$': require.resolve('./dist/loggerStub.js'),
+        '^lwc-test-utils$': require.resolve('lwc-test-utils'),
+    },
+    resolver: require.resolve('lwc-jest-resolver'),
+    transform: {
+        '^.+\\.(js|html|css)$': require.resolve('lwc-jest-transformer'),
+    },
+    snapshotSerializers: [require.resolve('lwc-jest-serializer')],
+    testMatch: [ '**/__tests__/**/?(*.)(spec|test).js' ],
+};
