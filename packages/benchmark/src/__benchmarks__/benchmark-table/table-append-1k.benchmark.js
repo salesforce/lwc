b54d09b9d2b75e54dc0310e18add1a2a5884290f
David Turissini <dturissini@dturissini-ltm.internal.salesforce.com>
2018-06-14 17:04:17 -0700

    fix(benchmark): native shadow dom

--- packages/benchmark/src/__benchmarks__/benchmark-table/table-append-1k.benchmark.js
+++ packages/benchmark/src/__benchmarks__/benchmark-table/table-append-1k.benchmark.js
@@ -9,7 +9,7 @@ benchmark(`benchmark-table/append/1k`, () => {
     let store;
 
     before(async () => {
-        tableElement = createElement('benchmark-table', { is: Table });
+        tableElement = createElement('benchmark-table', { is: Table, fallback: false });
         await insertTableComponent(tableElement);
 
         store = new Store();
