c95dde790dd4146a6b520c92674e9a49ad729799
David Turissini <dturissini@dturissini-ltm.internal.salesforce.com>
2018-07-03 16:44:41 -0700

    fix(engine): readme

--- packages/lwc-engine/src/framework/def.ts
+++ packages/lwc-engine/src/framework/def.ts
@@ -361,7 +361,7 @@ export function getComponentDef(Ctor: ComponentConstructor): ComponentDef {
 // Initialization Routines
 import "../polyfills/proxy-concat/main";
 import "../polyfills/event-composed/main";
-import "../polyfills/focuse-event-composed/main";
+import "../polyfills/focus-event-composed/main";
 import "../polyfills/aria-properties/main";
 
 const HTML_PROPS: PropsDef = create(null);
--- packages/lwc-engine/src/polyfills/focus-event-composed/polyfill.ts
+++ packages/lwc-engine/src/polyfills/focus-event-composed/polyfill.ts
@@ -1,10 +1,4 @@
-// This must be run AFTER our event-composed polyfill
 export default function () {
-     // Fix for FF not respecting spec'd composed flat
-    // for `focusout` events
-    // This is defined here because we are guaranteed
-    // to have the composed polyfill applied to Event.
-    // https://bugzilla.mozilla.org/show_bug.cgi?id=1472887
     const originalComposedGetter = Object.getOwnPropertyDescriptor(Event.prototype, 'composed')!.get!;
     Object.defineProperties(FocusEvent.prototype, {
         composed: {
