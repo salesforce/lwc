3b57eb7ef0dcdb0f8f4317c17cf0d9cdcda646bc
David Turissini <davidairborne@gmail.com>
2018-07-03 16:33:45 -0700

    fix(engine): composed event whitelist (#462)
    * fix(engine): change event is no longer composed: true
    * fix(engine): fixing event setter
    * fix(engine): jest
    * fix(engine): linting
    * fix(engine): using getter for composed on custom event because ie11

---
+++ packages/lwc-integration/src/components/events/test-change-event-composed/change-event-composed.spec.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+
+describe('Composed change event', () => {
+    const URL = 'http://localhost:4567/change-event-composed/';
+
+    before(() => {
+        browser.url(URL);
+    });
+
+    it('should be composed: false', function () {
+        // Force native "change" event to fire
+        browser.click('input');
+        browser.keys('foo');
+        browser.click('body');
+        assert.deepEqual(browser.getText('.verify-not-composed'), 'Not Composed');
+    });
+});
