473a1b38d82a6a4ba12efa0122694c57b542135a
Caridy Patiño <caridy@gmail.com>
2018-07-06 14:06:48 -0400

    fix(engine): closes issue #225 - value and checked props are live (#471)

--- packages/lwc-engine/src/framework/modules/props.ts
+++ packages/lwc-engine/src/framework/modules/props.ts
@@ -1,16 +1,28 @@
 import assert from "../assert";
-import { isUndefined, keys, StringToLowerCase } from "../language";
-import { ViewModelReflection, EmptyObject, getInternalField } from "../utils";
+import { isUndefined, keys, StringToLowerCase, create } from "../language";
+import { ViewModelReflection, getInternalField } from "../utils";
 import { prepareForPropUpdate } from "../decorators/api";
 import { VNode, Module } from "../../3rdparty/snabbdom/types";
 import { getAttrNameFromPropName } from "../attributes";
+
+const EspecialTagAndPropMap = create(null, {
+    input: { value: create(null, { value: { value: 1 }, checked: { value: 1 } }) },
+    select: { value: create(null, { value: { value: 1 } }) },
+    textarea: { value: create(null, { value: { value: 1 } }) },
+});
+
+function isLiveBindingProp(sel: string, key: string): boolean {
+    // checked and value properties are considered especial, and even if the old tracked value is equal to the new tracked value
+    // we still check against the element's corresponding value to be sure.
+    return sel in EspecialTagAndPropMap && key in EspecialTagAndPropMap[sel];
+}
 
 function update(oldVnode: VNode, vnode: VNode) {
     const props = vnode.data.props;
     if (isUndefined(props)) {
         return;
     }
-    let oldProps = oldVnode.data.props;
+    const oldProps = oldVnode.data.props;
     if (oldProps === props) {
         return;
     }
@@ -21,29 +33,25 @@ function update(oldVnode: VNode, vnode: VNode) {
 
     let key: string;
     let cur: any;
-    let old: any;
     const elm = vnode.elm as Element;
     const vm = getInternalField(elm, ViewModelReflection);
-    oldProps = isUndefined(oldProps) ? EmptyObject : oldProps;
+    const isFirstPatch = isUndefined(oldProps);
+    const isCustomElement = !isUndefined(vm);
+    const { sel } = vnode;
 
     for (key in props) {
         cur = props[key];
-        old = (oldProps as any)[key];
 
         if (process.env.NODE_ENV !== 'production') {
-            if (old !== cur && !(key in elm)) {
+            if (!(key in elm)) {
                 // TODO: this should never really happen because the compiler should always validate
                 assert.fail(`Unknown public property "${key}" of element <${StringToLowerCase.call(elm.tagName)}>. This is likely a typo on the corresponding attribute "${getAttrNameFromPropName(key)}".`);
             }
         }
 
-        if (old !== cur && (key in elm) && (key !== 'value' || elm[key] !== cur)) {
-            if (process.env.NODE_ENV !== 'production') {
-                if (elm[key] === cur && old !== undefined) {
-                    console.warn(`Unnecessary update of property "${key}" in element <${StringToLowerCase.call(elm.tagName)}>.`); // tslint:disable-line
-                }
-            }
-            if (!isUndefined(vm)) {
+        // if it is the first time this element is patched, or the current value is different to the previous value...
+        if (isFirstPatch || cur !== (isLiveBindingProp(sel as string, key) ? elm[key] : (oldProps as any)[key])) {
+            if (isCustomElement) {
                 prepareForPropUpdate(vm); // this is just in case the vnode is actually a custom element
             }
             // touching the dom if the prop really changes.
