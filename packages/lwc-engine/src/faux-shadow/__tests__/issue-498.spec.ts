5b063658127a56777268a1b863b57f204310523e
David Turissini <dturissini@dturissini-ltm.internal.salesforce.com>
2018-07-12 12:10:20 -0700

    fix(engine): tests

--- packages/lwc-engine/src/faux-shadow/__tests__/issue-498.spec.ts
+++ packages/lwc-engine/src/faux-shadow/__tests__/issue-498.spec.ts
@@ -65,4 +65,41 @@ describe('issue #498', () => {
         }).not.toThrow();
     });
 
+    it('should not throw when property is not defined on object', () => {
+        class Custom extends LightningElement {
+            render() {
+                return function ($api) {
+                    return [
+                        $api.h('div', { key: 0 }, []),
+                    ];
+                }
+            }
+        }
+        const elm = createElement('x-foo', { is: Custom });
+        document.body.appendChild(elm);
+        const div = getHostShadowRoot(elm).querySelector('div');
+        expect(() => {
+            div.foo;
+        }).not.toThrow();
+    });
+
+    it('should not throw when property is defined on prototype', () => {
+        class Custom extends LightningElement {
+            render() {
+                return function ($api) {
+                    return [
+                        $api.h('div', { key: 0 }, []),
+                    ];
+                }
+            }
+        }
+        const elm = createElement('x-foo', { is: Custom });
+        document.body.appendChild(elm);
+        const div = getHostShadowRoot(elm).querySelector('div');
+        Object.setPrototypeOf(div, { foo: {} })
+        expect(() => {
+            div.foo;
+        }).not.toThrow();
+    });
+
 });
