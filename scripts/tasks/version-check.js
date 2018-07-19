2c8c58ff3d1c9a333f6ef49d5bf6cde39294fbf5
Pierre-Marie <p.dartus@salesforce.com>
2018-07-19 06:51:22 -0700

    build: add peer dependency validation script

---
+++ scripts/tasks/version-check.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const path = require('path');
+const glob = require('glob');
+const semver = require('semver');
+
+const PACKAGES_DIR = path.resolve(__dirname, '../../packages');
+const PACKAGES = glob.sync('*/package.json', {
+    absolute: true,
+    cwd: PACKAGES_DIR,
+});
+
+let isVersionInSync = true;
+for (const location of PACKAGES) {
+    const {
+        name,
+        peerDependencies = {},
+        devDependencies = {},
+    } = require(location);
+
+    for (const dep of Object.keys(peerDependencies)) {
+        if (
+            devDependencies.hasOwnProperty(dep) &&
+            !semver.satisfies(devDependencies[dep], peerDependencies[dep])
+        ) {
+            const error = [
+                `Peer dependency version mismatch of ${dep} in ${name}.`,
+                `(Expected: ${peerDependencies[dep]}, Actual: ${devDependencies[dep]})`,
+            ].join(' ');
+
+            console.error(error);
+            isVersionInSync = false;
+        }
+    }
+}
+
+if (!isVersionInSync) {
+    process.exit(1);
+}
