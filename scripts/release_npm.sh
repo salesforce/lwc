42b70bd647009389e376a13ec111d0407849182e
Diego <dval@salesforce.com>
2018-07-22 13:02:38 -0700

    chore: adding prerelease option

--- scripts/release_npm.sh
+++ scripts/release_npm.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Get the package version
+# Normalize params
 [ ! -z "$1" ] && PACKAGE_VERSION="$1" || PACKAGE_VERSION=$NPM_PACKAGE_VERSION;
+[ ! -z "$2" ] && CANARY="--canary=beta" || CANARY="";
 
 if [ -f $PACKAGE_VERSION ]; then
         echo "You must specify a version to release"
+        echo "Comand options: semver [--prerelease] (ex: 0.12.1 --prelease)"
         exit 1;
 fi
 
@@ -18,7 +20,7 @@ fi
 
 # Command to push the packages
 CMD_PREPARE="yarn prepare"
-CMD_PUBLISH_PACKAGES="lerna publish --exact --force-publish=* --registry='https://npm.lwcjs.org' --skip-git --yes --repo-version ${PACKAGE_VERSION}"
+CMD_PUBLISH_PACKAGES="lerna publish --exact --force-publish=* --registry='https://npm.lwcjs.org' --skip-git --yes ${CANARY} --repo-version ${PACKAGE_VERSION}"
 
 # Run
 $CMD_PREPARE && $CMD_PUBLISH_PACKAGES;
