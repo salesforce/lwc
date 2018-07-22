e24aab0b2523f4d3195d3206d1001ddb0fb02e51
Diego <dval@salesforce.com>
2018-07-22 13:22:15 -0700

    chore: add changelog script

---
+++ scripts/release_changelog.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Normalize params
+[ ! -z "$1" ] && PACKAGE_VERSION="$1" || PACKAGE_VERSION=$NPM_PACKAGE_VERSION;
+[ ! -z "$2" ] && CANARY="--canary=beta";
+
+if [ -f $PACKAGE_VERSION ]; then
+        echo "You must specify a version to create the changelog"
+        exit 1;
+fi
+
+if [ ! -z $CANARY ]; then
+    echo "Prerelease version, no changelog needed"
+    exit 0;
+fi
+
+if git rev-parse $PACKAGE_VERSION >/dev/null 2>&1
+then
+    echo "Release already exists, skipping changelog"
+else
+    yarn changelog:generate
+fi
