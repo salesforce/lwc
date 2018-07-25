88ef2b0ab7f35f8f628aa1e47e3c57e5d7eef9fc
Eugene Kashida <ekashida@gmail.com>
2018-07-24 20:51:04 -0700

    fix(engine): refactor condition to apply event-composed polyfill

--- packages/lwc-engine/src/polyfills/event-composed/detect.ts
+++ packages/lwc-engine/src/polyfills/event-composed/detect.ts
@@ -2,11 +2,11 @@
 import { PatchedCustomEvent } from './polyfill';
 
 export default function detect(): boolean {
-    const isComposedPresent = Object.getOwnPropertyDescriptor(Event.prototype, 'composed') === undefined;
+    const isComposedPresent = Object.getOwnPropertyDescriptor(Event.prototype, 'composed') !== undefined;
 
     // We need to check if CustomEvent is out PatchedCustomEvent
     // because jest will recycle the window object, but not refresh
     // Event.prototype.
-    const isCustomEventPatched = (window as any).CustomEvent !== PatchedCustomEvent;
-    return isComposedPresent || isCustomEventPatched;
+    const isCustomEventPatched = (window as any).CustomEvent === PatchedCustomEvent;
+    return !isComposedPresent && !isCustomEventPatched;
 }
