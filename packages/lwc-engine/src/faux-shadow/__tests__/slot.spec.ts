3d369fc18c62d0445784679522f61c8106903bf9
Eugene Kashida <ekashida@gmail.com>
2018-08-01 18:22:13 -0700

    test: pr feedback (declarative binding tests)

--- packages/lwc-engine/src/faux-shadow/__tests__/slot.spec.ts
+++ packages/lwc-engine/src/faux-shadow/__tests__/slot.spec.ts
@@ -1,99 +1,183 @@
 import { LightningElement, getHostShadowRoot } from "../../framework/html-element";
 import { createElement } from "../../framework/upgrade";
 import { compileTemplate } from 'test-utils';
+import { getHost } from "../../../../../node_modules/lwc-engine/src/faux-shadow/shadow-root";
 
 interface LightningSlotElement extends HTMLSlotElement {
     assignedElements(): Element[];
 }
 
 describe.skip('slotchange event', () => {
-    // Initialized before each test
-    let element;
+    describe('declarative binding', () {
+        // Initialized before each test
+        let element;
 
-    beforeEach(() => {
-        class Child extends LightningElement {
-            render() {
-                return compileTemplate(`
-                    <template>
-                        <slot></slot>
-                    </template>
-                `);
+        beforeEach(() => {
+            class Child extends LightningElement {
+                constructor() {
+                    super();
+                    this._assignedElementsCount = -1;
+                    this._slotChangeCount = 0;
+                }
+                render() {
+                    return compileTemplate(`
+                        <template>
+                            <slot onslotchange={handleSlotChange}></slot>
+                        </template>
+                    `);
+                }
+                handleSlotChange(event) {
+                    this._slotChangeCount += 1;
+                    this._assignedElementsCount = event.assignedElements().length;
+                }
+                get assignedElementsCount() {
+                    return this._assignedElementsCount;
+                }
+                get slotChangeCount() {
+                    return this._slotChangeCount;
+                }
             }
-        }
+            Child.publicProps = {
+                assignedElementsCount: {
+                    config: 1, // readonly
+                },
+                slotChangeCount: {
+                    config: 1, // readonly
+                },
+            };
 
-        class Parent extends LightningElement {
-            things;
-            constructor() {
-                super();
-                this.things = ['foo'];
+            class Parent extends LightningElement {
+                constructor() {
+                    super();
+                    this.things = ['foo'];
+                }
+                render() {
+                    return compileTemplate(`
+                        <template>
+                            <x-child>
+                                <template for:each={things} for:item="thing">
+                                    <span key={thing}>{thing}</span>
+                                </template>
+                            </x-child>
+                        </template>
+                    `, {
+                        modules: { 'x-child': Child }
+                    });
+                }
+                setThings(things) {
+                    this.things = things;
+                }
             }
-            render() {
-                return compileTemplate(`
-                    <template>
-                        <x-child>
-                            <template for:each={things} for:item="thing">
-                                <span key={thing}>{thing}</span>
-                            </template>
-                        </x-child>
-                    </template>
-                `, {
-                    modules: { 'x-child': Child }
-                });
-            }
-            setThings(things) {
-                this.things = things;
-            }
-        }
-        Parent.publicMethods = ['setThings'];
-        Parent.track = { things: 1 };
+            Parent.publicMethods = ['setThings'];
+            Parent.track = { things: 1 };
 
-        element = createElement('x-parent', { is: Parent });
+            element = createElement('x-parent', { is: Parent });
+        });
+
+        it('should dispatch on initial render', () => {
+            document.body.appendChild(element);
+            const child: HTMLUnknownElement = getHostShadowRoot(element).querySelector('x-child');
+            expect(child.assignedElementsCount).toBe(1);
+            expect(child.slotChangeCount).toBe(1);
+        });
+
+        it('should dispatch when adding slotables', () => {
+            document.body.appendChild(element);
+            element.setThings(['foo', 'bar']);
+            const child: HTMLUnknownElement = getHostShadowRoot(element).querySelector('x-child');
+            expect(child.assignedElementsCount).toBe(2);
+            expect(child.slotChangeCount).toBe(2);
+        });
+
+        it('should dispatch when removing slotables', () => {
+            document.body.appendChild(element);
+            element.setThings([]);
+            const child: HTMLUnknownElement = getHostShadowRoot(element).querySelector('x-child');
+            expect(child.assignedElementsCount).toBe(0);
+            expect(child.slotChangeCount).toBe(2);
+        });
     });
 
-    it('should dispatch on initial render', () => {
-        expect.assertions(1);
+    describe('programmatic binding', () {
+        // Initialized before each test
+        let element;
 
-        const shadowRoot = getHostShadowRoot(element);
-        shadowRoot.addEventListener('slotchange', event => {
-            const slot = event.target as LightningSlotElement;
-            expect(slot.assignedElements().length).toBe(1);
+        beforeEach(() => {
+            class Child extends LightningElement {
+                render() {
+                    return compileTemplate(`
+                        <template>
+                            <slot></slot>
+                        </template>
+                    `);
+                }
+            }
+
+            class Parent extends LightningElement {
+                things;
+                constructor() {
+                    super();
+                    this.things = ['foo'];
+                }
+                render() {
+                    return compileTemplate(`
+                        <template>
+                            <x-child>
+                                <template for:each={things} for:item="thing">
+                                    <span key={thing}>{thing}</span>
+                                </template>
+                            </x-child>
+                        </template>
+                    `, {
+                        modules: { 'x-child': Child }
+                    });
+                }
+                setThings(things) {
+                    this.things = things;
+                }
+            }
+            Parent.publicMethods = ['setThings'];
+            Parent.track = { things: 1 };
+
+            element = createElement('x-parent', { is: Parent });
         });
-        document.body.appendChild(element);
-    });
 
-    it('should not be composed', () => {
-        expect.assertions(1);
+        it('should not be composed', () => {
+            expect.assertions(1);
 
-        element.addEventListener('slotchange', () => {
-            // Host element should not get the event
-            expect(false);
+            element.addEventListener('slotchange', () => {
+                // Host element should not get the event
+                expect(false);
+            });
+            document.body.appendChild(element);
+            expect(true);
         });
-        document.body.appendChild(element);
-        expect(true);
-    });
 
-    it('should dispatch when adding slotables', () => {
-        expect.assertions(1);
+        it('should dispatch when adding slotables', () => {
+            expect.assertions(1);
 
-        document.body.appendChild(element);
-        const shadowRoot = getHostShadowRoot(element);
-        shadowRoot.addEventListener('slotchange', () => {
-            const slot = event.target as LightningSlotElement;
-            expect(slot.assignedElements().length).toBe(2);
+            document.body.appendChild(element);
+            const child: HTMLUnknownElement = getHostShadowRoot(element).querySelector('x-child');
+            const shadowRoot = getHostShadowRoot(child);
+            shadowRoot.addEventListener('slotchange', () => {
+                const slot = event.target as LightningSlotElement;
+                expect(slot.assignedElements().length).toBe(2);
+            });
+            element.setThings(['foo', 'bar']);
         });
-        element.setThings(['foo', 'bar']);
-    });
 
-    it('should dispatch when removing slotables', () => {
-        expect.assertions(1);
+        it('should dispatch when removing slotables', () => {
+            expect.assertions(1);
 
-        document.body.appendChild(element);
-        const shadowRoot = getHostShadowRoot(element);
-        shadowRoot.addEventListener('slotchange', () => {
-            const slot = event.target as LightningSlotElement;
-            expect(slot.assignedElements().length).toBe(0);
+            document.body.appendChild(element);
+            const child: HTMLUnknownElement = getHostShadowRoot(element).querySelector('x-child');
+            const shadowRoot = getHostShadowRoot(child);
+            shadowRoot.addEventListener('slotchange', () => {
+                const slot = event.target as LightningSlotElement;
+                expect(slot.assignedElements().length).toBe(0);
+            });
+            element.setThings([]);
         });
-        element.setThings([]);
     });
 });
 
