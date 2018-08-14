f61000e758e51260f0f7dee9b3c06515ac2ba99c
Eugene Kashida <ekashida@gmail.com>
2018-08-13 17:15:36 -0700

    refactor: unit tests

--- packages/lwc-engine/src/polyfills/click-event-composed/__tests__/polyfill.spec.ts
+++ packages/lwc-engine/src/polyfills/click-event-composed/__tests__/polyfill.spec.ts
@@ -19,15 +19,15 @@ describe('click-event-composed polyfill', () => {
     `);
 
     it('should patch click events for listeners bound to the host element', () => {
-        expect.assertions(2);
+        expect.assertions(1);
 
         class Foo extends LightningElement {
             renderedCallback() {
                 this.addEventListener('click', event => {
-                    if (event instanceof MouseEvent) {
+                    const isCustomClick = event instanceof CustomEvent;
+                    if (!isCustomClick) {
                         return;
                     }
-                    expect(event instanceof CustomEvent).toBe(true);
                     expect(event.composed).toBe(true);
                 });
                 this.template.querySelector('button').click();
@@ -35,15 +35,17 @@ describe('click-event-composed polyfill', () => {
             render() {
                 return html;
             }
-            handleClick(event: MouseEvent) {
-                if (event instanceof MouseEvent) {
-                    // Stop native click since we expect it to be composed in most
-                    // browsers and substitute a non-composed version in its place.
-                    event.stopPropagation();
-                    const nonComposedClickEvent = new CustomEvent('click', { bubbles: true });
-                    const button = event.target;
-                    button.dispatchEvent(nonComposedClickEvent);
+            handleClick(event: Event) {
+                const isCustomClick = event instanceof CustomEvent;
+                if (isCustomClick) {
+                    return;
                 }
+                // Stop native click since we expect it to be composed in most
+                // browsers and substitute a non-composed version in its place.
+                event.stopPropagation();
+                const nonComposedClickEvent = new CustomEvent('click', { bubbles: true });
+                const button = event.target;
+                button.dispatchEvent(nonComposedClickEvent);
             }
         }
 
@@ -51,16 +53,16 @@ describe('click-event-composed polyfill', () => {
     });
 
     it('should not patch click events for listeners bound to the target element (known limitation)', () => {
-        expect.assertions(2);
+        expect.assertions(1);
 
         class Foo extends LightningElement {
             renderedCallback() {
                 const button = this.template.querySelector('button');
                 button.addEventListener('click', event => {
-                    if (event instanceof MouseEvent) {
+                    const isCustomClick = event instanceof CustomEvent;
+                    if (!isCustomClick) {
                         return;
                     }
-                    expect(event instanceof CustomEvent).toBe(true);
                     expect(event.composed).toBe(false);
                 });
                 button.click();
@@ -68,8 +70,9 @@ describe('click-event-composed polyfill', () => {
             render() {
                 return html;
             }
-            handleClick(event: MouseEvent) {
-                if (event instanceof MouseEvent) {
+            handleClick(event: Event) {
+                const isCustomClick = event instanceof CustomEvent;
+                if (!isCustomClick) {
                     // Stop native click since we expect it to be composed in most
                     // browsers and substitute a non-composed version in its place.
                     event.stopPropagation();
