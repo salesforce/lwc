98a58374418e3a6ce4fd510a7d1f969b054ed4d7
Alex <apapko@salesforce.com>
2018-08-17 11:19:45 -0700

    fix(compiler): remove comments during minifaction

--- packages/lwc-compiler/src/rollup-plugins/minify.ts
+++ packages/lwc-compiler/src/rollup-plugins/minify.ts
@@ -3,7 +3,9 @@ import minify from 'babel-preset-minify';
 
 import { BABEL_CONFIG_BASE } from '../babel-plugins';
 
-export const MINIFY_CONFIG: any = Object.assign({}, BABEL_CONFIG_BASE, {
+export const MINIFY_CONFIG: any = Object.assign({
+    comments: false,
+}, BABEL_CONFIG_BASE, {
     presets: [[minify, { guards: false, evaluate: false }]]
 });
 
