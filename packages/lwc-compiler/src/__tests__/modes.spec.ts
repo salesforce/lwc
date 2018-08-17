aad7b36813ad59cbc6d03fd95d6f1d56ea4fe125
Alex <apapko@salesforce.com>
2018-08-17 11:14:17 -0700

    fix: remove comments in minified bundled

---
+++ packages/lwc-compiler/src/__tests__/modes.spec.ts
@@ -0,0 +1,123 @@
+import { compile } from "../index";
+import { pretify, readFixture } from "./utils";
+
+const VALID_CONFIG = {
+    outputConfig: {
+        env: {},
+        minify: false,
+        compat: false,
+        format: "amd"
+    },
+    name: "node_env",
+    namespace: "x",
+    files: {
+        "node_env.js": readFixture(
+            "./node_env/node_env.js"
+        ),
+        "node_env.html": readFixture(
+            "./node_env/node_env.html"
+        )
+    },
+};
+
+describe('shape of compiled bundle in production', () => {
+    test('test bundle with NODE_ENV set to dev', async () => {
+        const customConfig = {
+            outputConfig: {
+                format: 'es',
+                env: {
+                    NODE_ENV: 'development',
+                },
+            },
+        };
+        const config = Object.assign({}, VALID_CONFIG, customConfig);
+        const { result: { code }} = await compile(config);
+        expect(pretify(code)).toBe(pretify(readFixture('expected-node-env-dev.js')));
+    });
+
+    test('test bundle with NODE_ENV set to production', async () => {
+        const customConfig = {
+            outputConfig: {
+                format: 'es',
+                env: {
+                    NODE_ENV: 'production',
+                },
+            },
+        };
+        const config = Object.assign({}, VALID_CONFIG, customConfig);
+        const { result: { code }} = await compile(config);
+        expect(pretify(code)).toBe(pretify(readFixture('expected-node-env-prod.js')));
+    });
+
+    test('test bundle for production in compat', async () => {
+        const customConfig = {
+            outputConfig: {
+                compat: true,
+                minify: true,
+            }
+        };
+        const config = Object.assign({}, VALID_CONFIG, customConfig);
+        const { result: { code }} = await compile(config);
+        expect(pretify(code)).toBe(pretify(readFixture('expected-prod_compat-mode.js')));
+    });
+
+    test('bundle in dev mode', async () => {
+        const customConfig = {
+            name: "class_and_template",
+            files: {
+                "class_and_template.js": readFixture(
+                    "./class_and_template/class_and_template.js"
+                ),
+                "class_and_template.html": readFixture(
+                    "./class_and_template/class_and_template.html"
+                ),
+            },
+        };
+        const config = Object.assign({}, VALID_CONFIG, customConfig);
+        const { result: {code} } = await compile(config);
+        expect(pretify(code)).toBe(pretify(readFixture('expected-dev-mode.js')));
+    });
+
+    test('bundle in prod mode', async () => {
+        const customConfig = {
+            name: "class_and_template",
+            files: {
+                "class_and_template.js": readFixture(
+                    "./class_and_template/class_and_template.js"
+                ),
+                "class_and_template.html": readFixture(
+                    "./class_and_template/class_and_template.html"
+                ),
+            },
+            outputConfig: {
+                minify: true,
+            }
+        };
+        const config = Object.assign({}, VALID_CONFIG, customConfig);
+        const { result: {code} } = await compile(config);
+        expect(pretify(code)).toBe(pretify(readFixture('expected-prod-mode.js')));
+    });
+
+    test('minified bundles do not contain comments', async () => {
+        const minifiedConfig = {
+            name: "comments",
+            files: {
+                "comments.js": readFixture(
+                    "./comments/comments.js"
+                ),
+                "comments.html": readFixture(
+                    "./comments/comments.html"
+                ),
+                "comments.css": readFixture(
+                    "./comments/comments.css"
+                )
+            },
+            outputConfig: {
+                minify: true
+            },
+        };
+        const config = Object.assign({}, VALID_CONFIG, minifiedConfig);
+        const { result: {code} } = await compile(config);
+        expect(pretify(code)).toBe(pretify(readFixture('expected-minify-no-comments.js')));
+    });
+});
