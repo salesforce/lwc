30f5f550b047113873b2bce184ac82320818e8f1
Alex <apapko@salesforce.com>
2018-08-23 11:19:29 -0700

    test(compiler): add transform test on oss compiler side

---
+++ packages/lwc-compiler/src/transformers/__tests__/transform-html-namespace.spec.ts
@@ -0,0 +1,76 @@
+import { transform } from '../../transformers/transformer';
+import { pretify } from '../../__tests__/utils';
+
+describe('transform', () => {
+    it('should apply namespace transformation to html file', async () => {
+        const actual = `
+            <template>
+                <c-foo></c-foo>
+            </template>
+        `;
+
+        const expected = `
+            import stylesheet from './foo.css'
+            import _namespaceFoo from \"namespace-foo\";
+            export default function tmpl($api, $cmp, $slotset, $ctx) {
+                const {
+                    c: api_custom_element
+                } = $api;
+                return [api_custom_element(\"namespace-foo\", _namespaceFoo, {
+                    key: 1
+                }, [])];
+            }
+            if (stylesheet) {
+                tmpl.hostToken = 'namespace-foo_foo-host';
+                tmpl.shadowToken = 'namespace-foo_foo';
+                const style = document.createElement('style');
+                style.type = 'text/css';
+                style.dataset.token = 'namespace-foo_foo'
+                style.textContent = stylesheet('namespace-foo_foo');
+                document.head.appendChild(style);
+            }
+        `;
+        const { code } = await transform(actual, 'foo.html', {
+            namespace: 'namespace',
+            name: 'foo',
+        });
+        expect(pretify(code)).toBe(pretify(expected));
+    });
+
+    // TODO: Seems like style injection doesn't default to 'c-' if namespace is not provided,
+    // will be handling this separately.
+    //
+    // it('should not apply namespace transformation if its value is not provided', async () => {
+    //     const actual = `
+    //         <template>
+    //             <c-foo></c-foo>
+    //         </template>
+    //     `;
+
+    //     const expected = `
+    //     import stylesheet from './foo.css'
+    //     import _cFoo from \"c-foo\";
+    //     export default function tmpl($api, $cmp, $slotset, $ctx) {
+    //         const {
+    //         c: api_custom_element
+    //         } = $api;
+    //         return [api_custom_element(\"c-foo\", _cFoo, {
+    //         key: 1
+    //         }, [])];
+    //     }
+    //     if (stylesheet) {
+    //         tmpl.hostToken = 'c-foo_foo-host';
+    //         tmpl.shadowToken = 'c-foo_foo';
+    //         const style = document.createElement('style');
+    //         style.type = 'text/css';
+    //         style.dataset.token = 'undefined-foo_foo'
+    //         style.textContent = stylesheet('c-foo_foo');
+    //         document.head.appendChild(style);
+    //     }
+    //     `;
+    //     const { code } = await transform(actual, 'foo.html', {
+    //         name: 'foo',
+    //     });
+    //     expect(pretify(code)).toBe(pretify(expected));
+    // });
+});
