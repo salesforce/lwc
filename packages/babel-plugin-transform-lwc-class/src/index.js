88d24636657436d0445faf8c4268548b2d1342be
Alex <apapko@salesforce.com>
2018-09-05 11:36:29 -0700

    feat: add unsused decorator error

--- packages/babel-plugin-transform-lwc-class/src/index.js
+++ packages/babel-plugin-transform-lwc-class/src/index.js
@@ -3,6 +3,8 @@ const metadata = require('./metadata');
 const component = require('./component');
 const decorators = require('./decorators');
 
+const { LWC_DECORATORS, LWC_PACKAGE_ALIAS } = require('./constants');
+
 /**
  * The transform is done in 2 passes:
  *    - First, apply in a single AST traversal the decorators and the component transformation.
@@ -11,7 +13,24 @@ const decorators = require('./decorators');
 module.exports = function LwcClassTransform(api) {
     const { merge: mergeVisitors } = api.traverse.visitors;
 
-    const { visitor: classPropertyVisitor } = classProperty(api, { loose: true });
+    const { visitor: classPropertyVisitor } = classProperty(api, {
+        loose: true,
+    });
+
+    const visitors = [
+        metadata(api, config),
+        namespace(api, config),
+        decorators(api, config),
+        component(api, config),
+        {
+            Program: {
+                exit(path, state) {
+                    const exitVisitors = mergeVisitors(classPropertyVisitor, unusedDecoratorVisitor)
+                    path.traverse(exitVisitors, state);
+                },
+            },
+        },
+    ];
 
     return {
         manipulateOptions(opts, parserOpts) {
@@ -19,20 +38,17 @@ module.exports = function LwcClassTransform(api) {
             parserOpts.plugins.push('classProperties');
             parserOpts.plugins.push('dynamicImport');
         },
-        visitor: mergeVisitors([
-            metadata(api),
-            decorators(api),
-            component(api),
-            {
-                Program: {
-                    exit(path, state) {
-                        path.traverse(
-                            classPropertyVisitor,
-                            state
-                        );
-                    }
-                }
-            },
-        ])
+        visitor: mergeVisitors(visitors),
+    };
+};
+
+function unusedDecoratorVisitor({ types: t }) {
+    return {
+        Decorator(path) {
+            const { name } = path.node.expression;
+            throw path.parentPath.buildCodeFrameError(
+                `Invalid "${name}" decorator usage. Supported decorators (${LWC_DECORATORS.join(', ')}) should be imported from "${LWC_PACKAGE_ALIAS}"`,
+            );
+        }
     }
 }
