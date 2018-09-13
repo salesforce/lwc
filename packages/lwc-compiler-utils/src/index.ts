2c0e47c0f754f15416b3364a0c03671f066ddb72
Alex <apapko@salesforce.com>
2018-09-12 21:06:19 -0700

    feat(packages): introduce compiel utils

---
+++ packages/lwc-compiler-utils/src/index.ts
@@ -0,0 +1,1 @@
+export { applyNamespaceAliasingFromSource } from './namespace-utils';
---
+++ packages/lwc-compiler-utils/src/namespace-utils.ts
@@ -0,0 +1,127 @@
+// TODO: THIS ENTIRE FILE WILL BE MOVED INTO A MODULE ( this is a duplicate from oss )
+const SALESFORCE_IMPORT_PREFIX = '@salesforce/';
+const DEFAULT_SALESFORCE_NAMESPACE = 'c';
+
+export function applyNamespaceAliasingFromSource(moduleName: string, namespaceMapping: {[name: string]: string}) {
+    let updatedModuleName = moduleName;
+    if (moduleName.startsWith(SALESFORCE_IMPORT_PREFIX)) {
+        const [prefix, type, value] = moduleName.split('/');
+        updatedModuleName = getSalesforceNamespacedModule(
+            value,
+            type,
+            namespaceMapping,
+        );
+    } else {
+        updatedModuleName = getStandardNamespacedModule(
+            moduleName,
+            namespaceMapping,
+        );
+    }
+
+    return updatedModuleName;
+}
+
+export function applyNamespaceAliasing(moduleName: string, moduleType: string, namespaceMapping: {[name: string]: string}) {
+    let updatedModuleName = moduleName;
+
+    if (moduleType && moduleType !== 'module') {
+        updatedModuleName = getSalesforceNamespacedModule(
+            moduleName,
+            moduleType,
+            namespaceMapping,
+        );
+    } else {
+        updatedModuleName = getStandardNamespacedModule(
+            moduleName,
+            namespaceMapping,
+        );
+    }
+    return updatedModuleName;
+}
+
+function getSalesforceNamespacedModule(value: string, type: string, namespaceMapping: {[name: string]: string}) {
+    // Escape early if no namespace mapping is provided
+    const targetSalesforceNamespace =
+        namespaceMapping[DEFAULT_SALESFORCE_NAMESPACE];
+    if (targetSalesforceNamespace === undefined) {
+        return value;
+    }
+
+    // early exit to support a case such as @salesforce/apex, where the value is not present
+    if (value === undefined) {
+        return value;
+    }
+
+    let updatedValue = value;
+    switch (type) {
+        // @salesforce/label/c.label1 -> @salesforce/label/namespace.label1
+        case 'label':
+            if (value.startsWith(`${DEFAULT_SALESFORCE_NAMESPACE}.`)) {
+                updatedValue = value.replace(
+                    `${DEFAULT_SALESFORCE_NAMESPACE}.`,
+                    `${targetSalesforceNamespace}.`,
+                );
+            }
+            break;
+
+        // @salesforce/resource-url/resource1 -> @salesforce/resource-url/namespace__resource1
+        case 'resource-url':
+            // Only prefix with the namespace if no other namespace is provided
+            if (!value.includes('__')) {
+                updatedValue = `${targetSalesforceNamespace}__${value}`;
+            }
+            break;
+
+        // @salesforce/apex/MyClass.methodA -> @salesforce/apex/acme.MyClass.methodA
+        case 'apex':
+            // We detect if the apex reference already contains the namespace by checking the number of parts in it's
+            // reference. If the namespace is present the value would have the following from <ns>.<class>.<method>
+            // otherwise it would be <class>.<method>.
+            const parts = value.split('.');
+            if (parts.length === 2) {
+                updatedValue = `${targetSalesforceNamespace}.${value}`;
+            }
+            break;
+
+        // @salesforce/schema/CustomObject1__c -> @salesforce/schema/acme__CustomObject1__c
+        // @salesforce/schema/Account.CustomField__c -> @salesforce/schema/Account.acme__CustomField__c
+        // @salesforce/schema/Account.Relationship__r.CustomField__c -> @salesforce/schema/Account.acme__Relationship__r.acme__CustomField__c
+        case 'schema':
+            // For each object, field and relationship that is part of the module name, if it is custom and has no
+            // namespace, we need to add the target namespace.
+            updatedValue = value
+                .split('.')
+                .map(part => {
+                    const isCustom = part.endsWith('__c') || part.endsWith('__r');
+                    const isNotNamespaced = part.split('__').length < 3;
+
+                    return isCustom && isNotNamespaced
+                        ? `${targetSalesforceNamespace}__${part}`
+                        : part;
+                })
+                .join('.');
+            break;
+
+        case 'componentTagName':
+            if (value.startsWith(`${DEFAULT_SALESFORCE_NAMESPACE}-`)) {
+                updatedValue = value.replace(
+                    `${DEFAULT_SALESFORCE_NAMESPACE}`,
+                    `${targetSalesforceNamespace}`,
+                );
+            }
+            break;
+    }
+
+    return updatedValue;
+}
+
+function getStandardNamespacedModule(moduleName: string, namespaceMapping: {[name: string]: string}) {
+    for (const [original, target] of Object.entries(namespaceMapping)) {
+        if (moduleName.startsWith(`${original}/`)) {
+            return moduleName.replace(`${original}/`, `${target}/`);
+        }
+    }
+
+    // If namespace remapping is not needed return the original module name.
+    return moduleName;
+}
