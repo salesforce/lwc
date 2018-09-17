485dd98d61282dcc9e1e508ab7ad595da24b2801
Caridy Patiño <caridy@gmail.com>
2018-09-17 12:09:18 -0400

    refactor(engine): fixes #633 to avoid leaking tagName in component (#643)
    * refactor(engine): fixes #633 to avoid leaking tagName in component
    
    * fix(engine): PR #643 review

--- packages/lwc-engine/src/framework/vm.ts
+++ packages/lwc-engine/src/framework/vm.ts
@@ -2,12 +2,12 @@ import assert from "../shared/assert";
 import { getComponentDef } from "./def";
 import { createComponent, linkComponent, renderComponent, clearReactiveListeners, ComponentConstructor, ErrorCallback, markComponentAsDirty } from "./component";
 import { patchChildren } from "./patch";
-import { isArray, ArrayPush, isUndefined, isNull, ArrayUnshift, ArraySlice, create, isTrue, isObject, keys, isFalse, defineProperty, isFunction } from "../shared/language";
+import { isArray, ArrayPush, isUndefined, isNull, ArrayUnshift, ArraySlice, create, isTrue, isObject, keys, isFalse, defineProperty, isFunction, StringToLowerCase } from "../shared/language";
 import { getInternalField } from "../shared/fields";
 import { ViewModelReflection, addCallbackToNextTick, EmptyObject, EmptyArray } from "./utils";
 import { invokeServiceHook, Services } from "./services";
 import { invokeComponentCallback } from "./invoker";
-import { parentElementGetter, ElementInnerHTMLSetter, ShadowRootInnerHTMLSetter } from "./dom-api";
+import { parentElementGetter, ElementInnerHTMLSetter, ShadowRootInnerHTMLSetter, elementTagNameGetter } from "./dom-api";
 
 import { VNodeData, VNodes } from "../3rdparty/snabbdom/types";
 import { Template } from "./template";
@@ -472,7 +472,9 @@ export function getErrorComponentStack(startingElement: HTMLElement): string {
     do {
         const currentVm: VM | undefined = getInternalField(elm, ViewModelReflection);
         if (!isUndefined(currentVm)) {
-            ArrayPush.call(wcStack, (currentVm.component as ComponentInterface).toString());
+            const tagName = elementTagNameGetter.call(elm);
+            const is = elm.getAttribute('is');
+            ArrayPush.call(wcStack, `<${StringToLowerCase.call(tagName)}${ is ? ' is="${is}' : '' }>`);
         }
         // TODO: bug #435 - shadowDOM will preventing this walking process, we
         // need to find a different way to find the right boundary
