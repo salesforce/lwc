7c09ed9bb00c5cb3d1497e856a0ee4f9b779cb53
Eugene Kashida <ekashida@gmail.com>
2018-09-19 10:55:28 -0700

    feat(engine): initial implementation of slotchange

--- packages/lwc-engine/src/faux-shadow/slot.ts
+++ packages/lwc-engine/src/faux-shadow/slot.ts
@@ -1,28 +1,69 @@
+import assert from "../shared/assert";
 import { defineProperties } from "../shared/language";
+import { addEventListener } from "./element";
 import {
-    addEventListener,
-    removeEventListener,
-} from "./element";
+    getInternalField,
+    setInternalField,
+    createFieldName,
+} from "../shared/fields";
 
-function addEventListenerPatchedValue(this: EventTarget, type: string, listener: EventListener, options?: boolean | AddEventListenerOptions) {
-    if (type === 'slotchange') {
-        // set up the mutation observer to observe and dispatch child nodes mutations
+// We can use a single observer without having to worry about leaking because
+// "Registered observers in a node’s registered observer list have a weak
+// reference to the node."
+// https://dom.spec.whatwg.org/#garbage-collection
+let observer;
+
+function initSlotObserver() {
+    // MutationObserver is not yet implemented in jsdom:
+    // https://github.com/jsdom/jsdom/issues/639
+    if (typeof MutationObserver === 'undefined') {
+        return observer = {
+            observe() {
+                assert.isTrue(
+                    process.env.NODE_ENV === 'test',
+                    'MutationObserver should not be mocked outside of the jest test environment'
+                );
+            },
+        };
     }
-    addEventListener.call(this as HTMLSlotElement, type, listener, options);
+    return new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+            if (process.env.NODE_ENV !== 'production') {
+                assert.isTrue(
+                    mutation.type === 'childList',
+                    `Invalid mutation type: ${
+                        mutation.type
+                    }. This mutation handler for slots should only handle "childList" mutations.`
+                );
+            }
+            mutation.target.dispatchEvent(
+                new CustomEvent('slotchange', { bubbles: true })
+            );
+        });
+    });
 }
 
-function removeEventListenerPatchedValue(this: EventTarget, type: string, listener: EventListener, options?: boolean | AddEventListenerOptions) {
-    removeEventListener.call(this as HTMLSlotElement, type, listener, options);
+const observerConfig = { childList: true };
+const SlotChangeKey = createFieldName('slotchange');
+
+function addEventListenerPatchedValue(this: EventTarget, type: string, listener: EventListener, options?: boolean | AddEventListenerOptions) {
+    if (type === 'slotchange' && !getInternalField(this, SlotChangeKey)) {
+        if (process.env.NODE_ENV === 'test') {
+            /* tslint:disable-next-line:no-console */
+            console.warn('The "slotchange" event is not supported in our jest test environment.');
+        }
+        setInternalField(this, SlotChangeKey, true);
+        if (!observer) {
+            observer = initSlotObserver();
+        }
+        observer.observe(this as Node, observerConfig);
+    }
+    addEventListener.call(this as HTMLSlotElement, type, listener, options);
 }
 
 const HTMLSlotElementPatchDescriptors: PropertyDescriptorMap = {
     addEventListener: {
         value: addEventListenerPatchedValue,
-        configurable: true,
-        enumerable: true,
-    },
-    removeEventListener: {
-        value: removeEventListenerPatchedValue,
         configurable: true,
         enumerable: true,
     },
