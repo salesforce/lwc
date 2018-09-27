5ee44d2f8c290eaf8b5d9dda53c167a840dc6dd7
Alex <apapko@salesforce.com>
2018-09-26 17:05:00 -0700

    wip: merge conflict

--- packages/babel-plugin-transform-lwc-class/src/index.js
+++ packages/babel-plugin-transform-lwc-class/src/index.js
@@ -8,39 +8,12 @@ const { LWC_DECORATORS, LWC_PACKAGE_ALIAS } = require('./constants');
 /**
  * The transform is done in 2 passes:
  *    - First, apply in a single AST traversal the decorators and the component transformation.
- *    - Then, in a second path transform class properties using the official babel plugin 'babel-plugin-transform-class-properties'.
+ *    - Then, in a second path transform class properties using the official babel plugin "babel-plugin-transform-class-properties".
  */
 module.exports = function LwcClassTransform(api) {
     const { merge: mergeVisitors } = api.traverse.visitors;
 
-    const visitors = [
-        metadata(api, config),
-        namespace(api, config),
-        decorators(api, config),
-        component(api, config),
-        {
-            Program: {
-                exit(path, state) {
-                    const existVisitors = mergeVisitors([
-                        classProperty(api, { loose: true }).visitor,
-                        {
-                            Decorator(path) {
-                                throw path.parentPath.buildCodeFrameError(
-                                    `Invalid '${
-                                        path.node.expression.name
-                                    }' decorator usage. Supported decorators (${LWC_DECORATORS.join(
-                                        ', '
-                                    )}) should be imported from '${LWC_PACKAGE_ALIAS}'`
-                                );
-                            }
-                        }
-                    ]);
-
-                    path.traverse(existVisitors, state);
-                }
-            }
-        }
-    ];
+    const { visitor: classPropertyVisitor } = classProperty(api, { loose: true });
 
     return {
         manipulateOptions(opts, parserOpts) {
@@ -48,6 +21,34 @@ module.exports = function LwcClassTransform(api) {
             parserOpts.plugins.push('classProperties');
             parserOpts.plugins.push('dynamicImport');
         },
-        visitor: mergeVisitors(visitors)
-    };
-};
+        visitor: mergeVisitors([
+            metadata(api),
+            decorators(api),
+            component(api),
+            {
+                Program: {
+                    exit(path, state) {
+                        const visitors = mergeVisitors([
+                            classProperty(api, { loose: true }).visitor,
+                            {
+                                Decorator(path) {
+                                    throw path.parentPath.buildCodeFrameError(
+                                        `Invalid '${
+                                            path.node.expression.name
+                                        }' decorator usage. Supported decorators (${LWC_DECORATORS.join(
+                                            ', '
+                                        )}) should be imported from '${LWC_PACKAGE_ALIAS}'`
+                                    );
+                                }
+                            }
+                        ])
+                        path.traverse(
+                            visitors,
+                            state
+                        );
+                    }
+                }
+            },
+        ])
+    }
+}
