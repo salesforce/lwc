648922ff090ebda440197d55e8e9cb8bc4e792cd
Kevin Venkiteswaran <kevin@v11n.com>
2018-09-28 15:01:57 -0700

    fix(jest-transformer): update tests to reflect namespace syntax (#685)

--- packages/lwc-jest-transformer/src/transforms/__tests__/resource-scoped-import.test.js
+++ packages/lwc-jest-transformer/src/transforms/__tests__/resource-scoped-import.test.js
@@ -4,40 +4,52 @@ const test = require('./utils/test-transform').test(
 
 describe('@salesforce/resourceUrl import', () => {
     test('does default transformation', `
-        import myResource from '@salesforce/resourceUrl/c.foo';
+        import myResource from '@salesforce/resourceUrl/foo';
     `, `
         let myResource;
 
         try {
-          myResource = require('@salesforce/resourceUrl/c.foo').default;
+          myResource = require('@salesforce/resourceUrl/foo').default;
         } catch (e) {
-          myResource = 'c.foo';
+          myResource = 'foo';
+        }
+    `);
+
+    test('does default transformation for namespaced values', `
+        import myResource from '@salesforce/resourceUrl/ns__foo';
+    `, `
+        let myResource;
+
+        try {
+          myResource = require('@salesforce/resourceUrl/ns__foo').default;
+        } catch (e) {
+          myResource = 'ns__foo';
         }
     `);
 
     test('allows non-@salesforce/resourceUrl named imports', `
         import { otherNamed } from './something-valid';
-        import myResource from '@salesforce/resourceUrl/c.foo';
+        import myResource from '@salesforce/resourceUrl/foo';
     `, `
         import { otherNamed } from './something-valid';
         let myResource;
 
         try {
-          myResource = require('@salesforce/resourceUrl/c.foo').default;
+          myResource = require('@salesforce/resourceUrl/foo').default;
         } catch (e) {
-          myResource = 'c.foo';
+          myResource = 'foo';
         }
     `);
 
     test('throws error if using named import', `
-        import { myResource } from '@salesforce/resourceUrl/c.foo';
-    `, undefined, 'Invalid import from @salesforce/resourceUrl/c.foo');
+        import { myResource } from '@salesforce/resourceUrl/foo';
+    `, undefined, 'Invalid import from @salesforce/resourceUrl/foo');
 
     test('throws error if renamed default imports', `
-        import { default as resource } from '@salesforce/resourceUrl/c.foo';
-    `, undefined, 'Invalid import from @salesforce/resourceUrl/c.foo');
+        import { default as resource } from '@salesforce/resourceUrl/foo';
+    `, undefined, 'Invalid import from @salesforce/resourceUrl/foo');
 
     test('throws error if renamed multiple default imports', `
-        import { default as resource, foo } from '@salesforce/resourceUrl/c.foo';
-    `, undefined, 'Invalid import from @salesforce/resourceUrl/c.foo');
+        import { default as resource, foo } from '@salesforce/resourceUrl/foo';
+    `, undefined, 'Invalid import from @salesforce/resourceUrl/foo');
 });
