0a82499f82bb887ac085098e99e9eda0877cc7bc
Jose David Rodriguez Velasco <jrodriguezvelasco@salesforce.com>
2018-10-02 12:44:46 -0700

    feat(rollup-plugin-lwc): allow sourcemap
    option when instantiating the rollup-plugin-lwc-compiler

--- packages/rollup-plugin-lwc-compiler/src/index.js
+++ packages/rollup-plugin-lwc-compiler/src/index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const compiler = require("lwc-compiler");
 const pluginUtils = require("rollup-pluginutils");
 const lwcResolver = require("lwc-module-resolver");
-const { getModuleQualifiedName, getLwcEnginePath } = require('./utils');
+const { getModuleQualifiedName } = require('./utils');
 const { DEFAULT_OPTIONS, DEFAULT_MODE } = require("./constants");
 
 module.exports = function rollupLwcCompiler(pluginOptions = {}) {
@@ -11,6 +11,7 @@ module.exports = function rollupLwcCompiler(pluginOptions = {}) {
     const filter = pluginUtils.createFilter(include, exclude);
     const mergedPluginOptions = Object.assign({}, DEFAULT_OPTIONS, pluginOptions);
     const { resolveFromPackages, resolveFromSource } = mergedPluginOptions;
+    const { sourcemap = false } = pluginOptions;
 
     // Closure to store the resolved modules
     let modulePaths = {};
@@ -63,7 +64,8 @@ module.exports = function rollupLwcCompiler(pluginOptions = {}) {
                 mode: DEFAULT_MODE, // Use always default mode since any other (prod or compat) will be resolved later
                 name: moduleRegistry.moduleName,
                 namespace: moduleRegistry.moduleNamespace,
-                moduleSpecifier: moduleRegistry.moduleSpecifier
+                moduleSpecifier: moduleRegistry.moduleSpecifier,
+                outputConfig: { sourcemap }
             });
 
             return { code, map };
