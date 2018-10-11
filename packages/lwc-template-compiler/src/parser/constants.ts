d36b53bd327848a2a9f393873165061afab27814
Diego Ferreiro Val <dval@salesforce.com>
2018-10-11 10:53:08 -0700

    fix: reformatting

--- packages/lwc-template-compiler/src/parser/constants.ts
+++ packages/lwc-template-compiler/src/parser/constants.ts
@@ -1,7 +1,11 @@
 export const EXPRESSION_RE = /(\{(?:.)+?\})/g;
 
 export const IF_RE = /^if:/;
-export const VALID_IF_MODIFIER = new Set(['true', 'false', 'strict-true']);
+export const VALID_IF_MODIFIER = new Set([
+    'true',
+    'false',
+    'strict-true',
+]);
 
 export const ITERATOR_RE = /^iterator\:.*$/;
 
@@ -20,7 +24,9 @@ const ID_REFERENCING_ATTRIBUTES = [
     'aria-owns',
     'for',
 ];
-export const STATIC_ATTRIBUTE_SET = new Set(['id', ...ID_REFERENCING_ATTRIBUTES]);
+export const STATIC_ATTRIBUTE_SET = new Set([
+    'id', ...ID_REFERENCING_ATTRIBUTES
+]);
 
 const ATTRIBUTE_NAME_CHAR = [
     ':A-Z_a-z\\u00C0-\\u00D6\\u00D8-\\u00F6\\u00F8-\\u02FF\\u0370-\\u037D\\u037F-\\u1FFF\\u200C-',
@@ -79,42 +85,13 @@ export const SVG_TAG_WHITELIST = new Set([
 ]);
 
 export const GLOBAL_ATTRIBUTE_SET = new Set([
-    'role',
-    'accesskey',
-    'class',
-    'contenteditable',
-    'contextmenu',
-    'dir',
-    'draggable',
-    'dropzone',
-    'hidden',
-    'id',
-    'itemprop',
-    'lang',
-    'slot',
-    'spellcheck',
-    'style',
-    'tabindex',
-    'title',
+    'role', 'accesskey', 'class', 'contenteditable', 'contextmenu', 'dir', 'draggable', 'dropzone', 'hidden',
+    'id', 'itemprop', 'lang', 'slot', 'spellcheck', 'style', 'tabindex', 'title',
 ]);
 
 export const VOID_ELEMENT_SET = new Set([
-    'area',
-    'base',
-    'br',
-    'col',
-    'embed',
-    'hr',
-    'img',
-    'input',
-    'keygen',
-    'link',
-    'menuitem',
-    'meta',
-    'param',
-    'source',
-    'track',
-    'wbr',
+    'area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'menuitem', 'meta', 'param',
+    'source', 'track', 'wbr',
 ]);
 
 export const DASHED_TAGNAME_ELEMENT_SET = new Set([
@@ -131,22 +108,22 @@ export const DASHED_TAGNAME_ELEMENT_SET = new Set([
 export const ATTRS_PROPS_TRANFORMS: { [name: string]: string } = {
     // These have to be quoted because aria- attributes below are quoted
     // Our linting doesn't like to mix quoted and un-quoted property names
-    accesskey: 'accessKey',
-    readonly: 'readOnly',
-    tabindex: 'tabIndex',
-    bgcolor: 'bgColor',
-    colspan: 'colSpan',
-    rowspan: 'rowSpan',
-    contenteditable: 'contentEditable',
-    crossorigin: 'crossOrigin',
-    datetime: 'dateTime',
-    formaction: 'formAction',
-    ismap: 'isMap',
-    maxlength: 'maxLength',
-    minlength: 'minLength',
-    novalidate: 'noValidate',
-    usemap: 'useMap',
-    for: 'htmlFor',
+    'accesskey': 'accessKey',
+    'readonly': 'readOnly',
+    'tabindex': 'tabIndex',
+    'bgcolor': 'bgColor',
+    'colspan': 'colSpan',
+    'rowspan': 'rowSpan',
+    'contenteditable': 'contentEditable',
+    'crossorigin': 'crossOrigin',
+    'datetime': 'dateTime',
+    'formaction': 'formAction',
+    'ismap': 'isMap',
+    'maxlength': 'maxLength',
+    'minlength': 'minLength',
+    'novalidate': 'noValidate',
+    'usemap': 'useMap',
+    'for': 'htmlFor',
 
     // According to the following list, there are 48 aria attributes (of which 2 are deprecated):
     // https://www.w3.org/TR/wai-aria-1.1/#x6-6-definitions-of-states-and-properties-all-aria-attributes
@@ -165,11 +142,11 @@ export const ATTRS_PROPS_TRANFORMS: { [name: string]: string } = {
     'aria-describedby': 'ariaDescribedBy',
     'aria-details': 'ariaDetails',
     'aria-disabled': 'ariaDisabled',
-    //  'aria-dropeffect': 'ariaDropEffect', /* Deprecated in ARIA 1.1 */
+//  'aria-dropeffect': 'ariaDropEffect', /* Deprecated in ARIA 1.1 */
     'aria-errormessage': 'ariaErrorMessage',
     'aria-expanded': 'ariaExpanded',
     'aria-flowto': 'ariaFlowTo',
-    //  'aria-grabbed': 'ariaGrabbed', /* Deprecated in ARIA 1.1 */
+//  'aria-grabbed': 'ariaGrabbed', /* Deprecated in ARIA 1.1 */
     'aria-haspopup': 'ariaHasPopup',
     'aria-hidden': 'ariaHidden',
     'aria-invalid': 'ariaInvalid',
@@ -212,214 +189,521 @@ export const HTML_TAG_BLACKLIST = new Set([
 ]);
 
 export const HTML_ATTRIBUTES_REVERSE_LOOKUP: { [attr: string]: string[] } = {
-    'xlink:href': ['use'],
-    role: [],
-    accept: ['form', 'input'],
-    'accept-charset': ['form'],
-    accesskey: [],
-    action: ['form'],
-    align: [
-        'applet',
-        'caption',
-        'col',
-        'colgroup',
-        'hr',
-        'iframe',
-        'img',
-        'table',
-        'tbody',
-        'td',
-        'tfoot',
-        'th',
-        'thead',
-        'tr',
-    ],
-    allowfullscreen: ['iframe'],
-    allowtransparency: [
-        'iframe', // Non standard
-    ],
-    alt: ['applet', 'area', 'img', 'input'],
-    async: ['script'],
-    autocomplete: ['form', 'input'],
-    autofocus: ['button', 'input', 'keygen', 'select', 'textarea'],
-    autoplay: ['audio', 'video'],
-    autosave: ['input'],
-    bgcolor: [
-        'body',
-        'col',
-        'colgroup',
-        'marquee',
-        'table',
-        'tbody',
-        'tfoot',
-        'td',
-        'th',
-        'tr',
-    ],
-    border: ['img', 'object', 'table'],
-    buffered: ['audio', 'video'],
-    challenge: ['keygen'],
-    charset: ['meta', 'script'],
-    checked: ['command', 'input'],
-    cite: ['blockquote', 'del', 'ins', 'q'],
-    class: [],
-    code: ['applet'],
-    codebase: ['applet'],
-    color: ['basefont', 'font', 'hr'],
-    cols: ['textarea'],
-    colspan: ['td', 'th'],
-    content: ['meta'],
-    contenteditable: [],
-    contextmenu: [],
-    controls: ['audio', 'video'],
-    coords: ['area'],
-    data: ['object'],
-    'data-*': [],
-    datetime: ['del', 'ins', 'time'],
-    default: ['track'],
-    defer: ['script'],
-    dir: [],
-    dirname: ['input', 'textarea'],
-    disabled: [
-        'button',
-        'command',
-        'fieldset',
-        'input',
-        'keygen',
-        'optgroup',
-        'option',
-        'select',
-        'textarea',
-    ],
-    download: ['a', 'area'],
-    draggable: [],
-    dropzone: [],
-    enctype: ['form'],
-    for: ['label', 'output'],
-    form: [
-        'button',
-        'fieldset',
-        'input',
-        'keygen',
-        'label',
-        'meter',
-        'object',
-        'output',
-        'progress',
-        'select',
-        'textarea',
-    ],
-    formaction: ['input', 'button'],
-    headers: ['td', 'th'],
-    height: ['canvas', 'embed', 'iframe', 'img', 'input', 'object', 'video'],
-    hidden: [],
-    high: ['meter'],
-    href: ['a', 'area', 'base', 'link'],
-    hreflang: ['a', 'area', 'link'],
-    'http-equiv': ['meta'],
-    icon: ['command'],
-    id: [],
-    integrity: ['link', 'script'],
-    ismap: ['img'],
-    itemprop: [],
-    keytype: ['keygen'],
-    kind: ['track'],
-    label: ['track'],
-    lang: [],
-    language: ['script'],
-    list: ['input'],
-    loop: ['audio', 'bgsound', 'marquee', 'video'],
-    low: ['meter'],
-    manifest: ['html'],
-    max: ['input', 'meter', 'progress'],
-    minlength: ['textarea', 'input'],
-    maxlength: ['input', 'textarea'],
-    media: ['a', 'area', 'link', 'source', 'style'],
-    method: ['form'],
-    min: ['input', 'meter'],
-    multiple: ['input', 'select'],
-    muted: ['video'],
-    name: [
-        'slot',
-        'button',
-        'form',
-        'fieldset',
-        'iframe',
-        'input',
-        'keygen',
-        'object',
-        'output',
-        'select',
-        'textarea',
-        'map',
-        'meta',
-        'param',
-    ],
-    novalidate: ['form'],
-    open: ['details'],
-    optimum: ['meter'],
-    pattern: ['input'],
-    ping: ['a', 'area'],
-    placeholder: ['input', 'textarea'],
-    poster: ['video'],
-    preload: ['audio', 'video'],
-    radiogroup: ['command'],
-    readonly: ['input', 'textarea'],
-    rel: ['a', 'area', 'link'],
-    required: ['input', 'select', 'textarea'],
-    reversed: ['ol'],
-    rows: ['textarea'],
-    rowspan: ['td', 'th'],
-    sandbox: ['iframe'],
-    scope: ['th'],
-    scoped: ['style'],
-    scrolling: [
-        'iframe', // Not supported in HTML5
-    ],
-    seamless: ['iframe'],
-    selected: ['option'],
-    shape: ['a', 'area'],
-    size: ['input', 'select'],
-    sizes: ['link', 'img', 'source'],
-    slot: [],
-    span: ['col', 'colgroup'],
-    spellcheck: [],
-    src: [
-        'audio',
-        'embed',
-        'iframe',
-        'img',
-        'input',
-        'script',
-        'source',
-        'track',
-        'video',
-    ],
-    srcdoc: ['iframe'],
-    srclang: ['track'],
-    srcset: ['img'],
-    start: ['ol'],
-    step: ['input'],
-    style: [],
-    summary: ['table'],
-    tabindex: [],
-    target: ['a', 'area', 'base', 'form'],
-    title: [],
-    type: [
-        'button',
-        'input',
-        'command',
-        'embed',
-        'object',
-        'script',
-        'source',
-        'style',
-        'menu',
-    ],
-    usemap: ['img', 'input', 'object'],
-    value: ['button', 'option', 'input', 'li', 'meter', 'progress', 'param'],
-    width: ['canvas', 'embed', 'iframe', 'img', 'input', 'object', 'video'],
-    wrap: ['textarea'],
+  'xlink:href': [
+    'use',
+  ],
+  'role': [],
+  'accept': [
+    'form',
+    'input',
+  ],
+  'accept-charset': [
+    'form',
+  ],
+  'accesskey': [
+
+  ],
+  'action': [
+    'form',
+  ],
+  'align': [
+    'applet',
+    'caption',
+    'col',
+    'colgroup',
+    'hr',
+    'iframe',
+    'img',
+    'table',
+    'tbody',
+    'td',
+    'tfoot',
+    'th',
+    'thead',
+    'tr',
+  ],
+  'allowfullscreen': [
+      'iframe',
+  ],
+  'allowtransparency': [
+      'iframe', // Non standard
+  ],
+  'alt': [
+    'applet',
+    'area',
+    'img',
+    'input',
+  ],
+  'async': [
+    'script',
+  ],
+  'autocomplete': [
+    'form',
+    'input',
+  ],
+  'autofocus': [
+    'button',
+    'input',
+    'keygen',
+    'select',
+    'textarea',
+  ],
+  'autoplay': [
+    'audio',
+    'video',
+  ],
+  'autosave': [
+    'input',
+  ],
+  'bgcolor': [
+    'body',
+    'col',
+    'colgroup',
+    'marquee',
+    'table',
+    'tbody',
+    'tfoot',
+    'td',
+    'th',
+    'tr',
+  ],
+  'border': [
+    'img',
+    'object',
+    'table',
+  ],
+  'buffered': [
+    'audio',
+    'video',
+  ],
+  'challenge': [
+    'keygen',
+  ],
+  'charset': [
+    'meta',
+    'script',
+  ],
+  'checked': [
+    'command',
+    'input',
+  ],
+  'cite': [
+    'blockquote',
+    'del',
+    'ins',
+    'q',
+  ],
+  'class': [
+
+  ],
+  'code': [
+    'applet',
+  ],
+  'codebase': [
+    'applet',
+  ],
+  'color': [
+    'basefont',
+    'font',
+    'hr',
+  ],
+  'cols': [
+    'textarea',
+  ],
+  'colspan': [
+    'td',
+    'th',
+  ],
+  'content': [
+    'meta',
+  ],
+  'contenteditable': [
+
+  ],
+  'contextmenu': [
+
+  ],
+  'controls': [
+    'audio',
+    'video',
+  ],
+  'coords': [
+    'area',
+  ],
+  'data': [
+    'object',
+  ],
+  'data-*': [
+
+  ],
+  'datetime': [
+    'del',
+    'ins',
+    'time',
+  ],
+  'default': [
+    'track',
+  ],
+  'defer': [
+    'script',
+  ],
+  'dir': [
+
+  ],
+  'dirname': [
+    'input',
+    'textarea',
+  ],
+  'disabled': [
+    'button',
+    'command',
+    'fieldset',
+    'input',
+    'keygen',
+    'optgroup',
+    'option',
+    'select',
+    'textarea',
+  ],
+  'download': [
+    'a',
+    'area',
+  ],
+  'draggable': [
+
+  ],
+  'dropzone': [
+
+  ],
+  'enctype': [
+    'form',
+  ],
+  'for': [
+    'label',
+    'output',
+  ],
+  'form': [
+    'button',
+    'fieldset',
+    'input',
+    'keygen',
+    'label',
+    'meter',
+    'object',
+    'output',
+    'progress',
+    'select',
+    'textarea',
+  ],
+  'formaction': [
+    'input',
+    'button',
+  ],
+  'headers': [
+    'td',
+    'th',
+  ],
+  'height': [
+    'canvas',
+    'embed',
+    'iframe',
+    'img',
+    'input',
+    'object',
+    'video',
+  ],
+  'hidden': [
+
+  ],
+  'high': [
+    'meter',
+  ],
+  'href': [
+    'a',
+    'area',
+    'base',
+    'link',
+  ],
+  'hreflang': [
+    'a',
+    'area',
+    'link',
+  ],
+  'http-equiv': [
+    'meta',
+  ],
+  'icon': [
+    'command',
+  ],
+  'id': [
+
+  ],
+  'integrity': [
+    'link',
+    'script',
+  ],
+  'ismap': [
+    'img',
+  ],
+  'itemprop': [
+
+  ],
+  'keytype': [
+    'keygen',
+  ],
+  'kind': [
+    'track',
+  ],
+  'label': [
+    'track',
+  ],
+  'lang': [
+
+  ],
+  'language': [
+    'script',
+  ],
+  'list': [
+    'input',
+  ],
+  'loop': [
+    'audio',
+    'bgsound',
+    'marquee',
+    'video',
+  ],
+  'low': [
+    'meter',
+  ],
+  'manifest': [
+    'html',
+  ],
+  'max': [
+    'input',
+    'meter',
+    'progress',
+  ],
+  'minlength': [
+    'textarea',
+    'input',
+  ],
+  'maxlength': [
+    'input',
+    'textarea',
+  ],
+  'media': [
+    'a',
+    'area',
+    'link',
+    'source',
+    'style',
+  ],
+  'method': [
+    'form',
+  ],
+  'min': [
+    'input',
+    'meter',
+  ],
+  'multiple': [
+    'input',
+    'select',
+  ],
+  'muted': [
+    'video',
+  ],
+  'name': [
+    'slot',
+    'button',
+    'form',
+    'fieldset',
+    'iframe',
+    'input',
+    'keygen',
+    'object',
+    'output',
+    'select',
+    'textarea',
+    'map',
+    'meta',
+    'param',
+  ],
+  'novalidate': [
+    'form',
+  ],
+  'open': [
+    'details',
+  ],
+  'optimum': [
+    'meter',
+  ],
+  'pattern': [
+    'input',
+  ],
+  'ping': [
+    'a',
+    'area',
+  ],
+  'placeholder': [
+    'input',
+    'textarea',
+  ],
+  'poster': [
+    'video',
+  ],
+  'preload': [
+    'audio',
+    'video',
+  ],
+  'radiogroup': [
+    'command',
+  ],
+  'readonly': [
+    'input',
+    'textarea',
+  ],
+  'rel': [
+    'a',
+    'area',
+    'link',
+  ],
+  'required': [
+    'input',
+    'select',
+    'textarea',
+  ],
+  'reversed': [
+    'ol',
+  ],
+  'rows': [
+    'textarea',
+  ],
+  'rowspan': [
+    'td',
+    'th',
+  ],
+  'sandbox': [
+    'iframe',
+  ],
+  'scope': [
+    'th',
+  ],
+  'scoped': [
+    'style',
+  ],
+  'scrolling': [
+      'iframe', // Not supported in HTML5
+  ],
+  'seamless': [
+    'iframe',
+  ],
+  'selected': [
+    'option',
+  ],
+  'shape': [
+    'a',
+    'area',
+  ],
+  'size': [
+    'input',
+    'select',
+  ],
+  'sizes': [
+    'link',
+    'img',
+    'source',
+  ],
+  'slot': [
+
+  ],
+  'span': [
+    'col',
+    'colgroup',
+  ],
+  'spellcheck': [
+
+  ],
+  'src': [
+    'audio',
+    'embed',
+    'iframe',
+    'img',
+    'input',
+    'script',
+    'source',
+    'track',
+    'video',
+  ],
+  'srcdoc': [
+    'iframe',
+  ],
+  'srclang': [
+    'track',
+  ],
+  'srcset': [
+    'img',
+  ],
+  'start': [
+    'ol',
+  ],
+  'step': [
+    'input',
+  ],
+  'style': [
+
+  ],
+  'summary': [
+    'table',
+  ],
+  'tabindex': [
+
+  ],
+  'target': [
+    'a',
+    'area',
+    'base',
+    'form',
+  ],
+  'title': [
+
+  ],
+  'type': [
+    'button',
+    'input',
+    'command',
+    'embed',
+    'object',
+    'script',
+    'source',
+    'style',
+    'menu',
+  ],
+  'usemap': [
+    'img',
+    'input',
+    'object',
+  ],
+  'value': [
+    'button',
+    'option',
+    'input',
+    'li',
+    'meter',
+    'progress',
+    'param',
+  ],
+  'width': [
+    'canvas',
+    'embed',
+    'iframe',
+    'img',
+    'input',
+    'object',
+    'video',
+  ],
+  'wrap': [
+    'textarea',
+  ],
 };
 
 export const HTML_NAMESPACE_URI = 'http://www.w3.org/1999/xhtml';
