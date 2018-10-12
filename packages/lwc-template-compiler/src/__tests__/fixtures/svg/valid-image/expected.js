996496a19c125506b68d587f94a1fab6fc5ae6e5
Bob Yao <bobbyyao@gmail.com>
2018-10-12 16:09:08 -0700

    test(fixtures): fix fixture testing; update svg tests (#733)

---
+++ packages/lwc-template-compiler/src/__tests__/fixtures/svg/valid-image/expected.js
@@ -0,0 +1,36 @@
+import { registerTemplate } from "lwc";
+
+function tmpl($api, $cmp, $slotset, $ctx) {
+  const { h: api_element } = $api;
+
+  return [
+    api_element(
+      "svg",
+      {
+        attrs: {
+          width: "200",
+          height: "200"
+        },
+        key: 2
+      },
+      [
+        api_element(
+          "image",
+          {
+            attrs: {
+              "xlink:href": "/foo.png",
+              x: "1",
+              y: "2",
+              height: "200",
+              width: "200"
+            },
+            key: 1
+          },
+          []
+        )
+      ]
+    )
+  ];
+}
+
+export default registerTemplate(tmpl);
