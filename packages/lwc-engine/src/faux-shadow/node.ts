38d2747bdb69dd1a1d062c52e93f6f366425f4e2
Caridy Patiño <caridy@gmail.com>
2018-10-15 22:02:12 -0400

    fix(engine): retargeting fix for non-signed elms

--- packages/lwc-engine/src/faux-shadow/node.ts
+++ packages/lwc-engine/src/faux-shadow/node.ts
@@ -112,7 +112,13 @@ const OwnerKey = '$$OwnerKey$$';
 const OwnKey = '$$OwnKey$$';
 
 export function getNodeOwnerKey(node: Node): number | undefined {
-    return node[OwnerKey];
+    let ownerKey;
+    // search for the first element with owner identity (just in case of manually inserted elements)
+    while (!isNull(node) && isUndefined((ownerKey = node[OwnerKey]))) {
+        node = parentNodeGetter.call(node);
+    }
+    // either we hit the wall, or the node is root element (which does not have an owner key)
+    return (isUndefined(ownerKey) || isNull(node)) ? undefined : ownerKey;
 }
 
 export function getNodeKey(node: Node): number | undefined {
