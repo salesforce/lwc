efa6a662a86622b14299310490b0940712bf54ca
Eugene Kashida <ekashida@gmail.com>
2018-10-15 19:36:56 -0700

    fix: retargeting works as expected for innerHTML template modifications (#739)
    * test: retargeting works as expected for innerHTML template modifications
    
    * fix(engine): retargeting fix for non-signed elms
    
    * fix(engine): small perf opt

--- packages/lwc-engine/src/faux-shadow/node.ts
+++ packages/lwc-engine/src/faux-shadow/node.ts
@@ -112,7 +112,12 @@ const OwnerKey = '$$OwnerKey$$';
 const OwnKey = '$$OwnKey$$';
 
 export function getNodeOwnerKey(node: Node): number | undefined {
-    return node[OwnerKey];
+    let ownerKey: number | undefined;
+    // search for the first element with owner identity (just in case of manually inserted elements)
+    while (!isNull(node) && isUndefined((ownerKey = node[OwnerKey]))) {
+        node = parentNodeGetter.call(node);
+    }
+    return ownerKey;
 }
 
 export function getNodeKey(node: Node): number | undefined {
