a9ae309051c722197c022d74874b8e08ec7a26f7
David Turissini <dturissini@dturissini-ltm.internal.salesforce.com>
2018-10-18 11:38:57 -0700

    fix(engine): linting

--- packages/lwc-engine/src/framework/main.ts
+++ packages/lwc-engine/src/framework/main.ts
@@ -5,7 +5,7 @@ export { BaseLightningElement as LightningElement } from "./base-lightning-eleme
 export { register } from "./services";
 export { unwrap } from "./membrane";
 export { registerTemplate } from "./template";
-export { isNodeCreatedByLWC } from "./utils";
+export { isNodeFromTemplate } from "./vm";
 
 // TODO: REMOVE THIS https://github.com/salesforce/lwc/issues/129
 export { dangerousObjectMutation } from "./membrane";
