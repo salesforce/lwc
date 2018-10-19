41f3c5fee77b28fc7d3a0b1eb924c51325f58cf7
David Turissini <dturissini@dturissini-ltm.internal.salesforce.com>
2018-10-19 15:40:30 -0700

    fix(engine): tests

---
+++ packages/lwc-integration/src/components/accessibility/test-delegates-focus-from-previous-sibling/test-delegates-focus-from-previous-sibling.spec.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+
+describe('Tabbing into custom element with delegates focus', () => {
+    const URL = 'http://localhost:4567/delegates-focus-from-previous-sibling';
+    let element;
+
+    before(() => {
+        browser.url(URL);
+    });
+
+    it('should apply focus to input in shadow', function () {
+        browser.keys(['Tab']);
+        browser.keys(['Tab']);
+
+        const activeFromDocument = browser.execute(() => {
+            return document.activeElement;
+        });
+        assert.equal(activeFromDocument.getTagName(), 'integration-delegates-focus-from-previous-sibling');
+        const activeFromShadow = browser.execute(() => {
+            return document.querySelector('integration-delegates-focus-from-previous-sibling').shadowRoot.activeElement;
+        });
+
+        assert.equal(activeFromShadow.getTagName(), 'integration-child');
+    });
+});
