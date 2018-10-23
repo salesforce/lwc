d813097a3eb965197cd23d0a0ab8d0e7fc5a8ed9
Cheng <cheng.li@salesforce.com>
2018-10-23 14:23:33 -0700

    Update quotes and tests

--- packages/lwc-jest-transformer/src/test/modules/example/i18n/__tests__/i18n.test.js
+++ packages/lwc-jest-transformer/src/test/modules/example/i18n/__tests__/i18n.test.js
@@ -2,7 +2,7 @@ import { createElement } from 'lwc';
 import I18n from 'example/i18n';
 
 jest.mock('@salesforce/i18n/dir', () => {
-    return { default: "value set in test" };
+    return { default: 'value set in test' };
 }, { virtual: true });
 
 afterEach(() => {
@@ -22,16 +22,16 @@ describe('example-i18n', () => {
     it('returns default label value as import path', () => {
         const element = createElement('example-i18n', { is: I18n });
         document.body.appendChild(element);
-        const targetElement = document.body.querySelector("p");
-        expect(targetElement.getAttribute('lang')).toBe("en");
-        expect(targetElement.textContent).toBe("M/d/yyyy");
+        const targetElement = element.shadowRoot.querySelector('p');
+        expect(targetElement.getAttribute('lang')).toBe('en');
+        expect(targetElement.textContent).toBe('M/d/yyyy');
     });
 
     it('returns value from mock defined in test file', () => {
         const element = createElement('example-i18n', { is: I18n });
         document.body.appendChild(element);
-        const value = document.body.querySelector("p").getAttribute('dir');
-        expect(value).toBe("value set in test");
+        const value = element.shadowRoot.querySelector('p').getAttribute('dir');
+        expect(value).toBe('value set in test');
     });
 
     it.each([
@@ -44,7 +44,7 @@ describe('example-i18n', () => {
         const values = createElement('example-i18n', { is: I18n }).getI18nValues();
         const key = id.split('.').pop();
         expect(values[key]).toBeDefined();
-        expect(values[key]).not.toEqual("");
+        expect(values[key]).not.toEqual('');
     });
 
 });
