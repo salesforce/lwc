1ab7f4a33304fa2c785434e03f1f506f7cea650a
Diego Ferreiro Val <elfogris@gmail.com>
2018-11-02 13:27:34 -0700

    feat: upgrade COMPAT to support splice (#797)
    * wip: add array native tests
    
    * fix: compat splice
    
    * fix: update yarn

---
+++ packages/lwc-integration/src/components/natives/test-array-primitives/array-primitives.spec.js
@@ -0,0 +1,46 @@
+const assert = require('assert');
+describe('Testing array primitives', () => {
+    const URL = 'http://localhost:4567/array-primitives';
+    let element;
+
+    before(() => {
+        browser.url(URL);
+    });
+
+    it('page load', () => {
+        const title = browser.getTitle();
+        assert.equal(title, 'array-primitives');
+    });
+
+    it('check initial state', function () {
+        const initialLength = 5;
+        const { value } = browser.execute(function () {
+            return document.querySelector('integration-array-primitives').shadowRoot.querySelectorAll('li').length
+        });
+
+        assert.equal(value, initialLength);
+    });
+
+    it('check splice reactivity', function () {
+        const splicedList = [ 'one', 'three', 'four', 'five' ];
+        const splicedLength = splicedList.length;
+        browser.execute(function () {
+            return document.querySelector('integration-array-primitives').shadowRoot.querySelector('.splice').click();
+        });
+
+        // Technically a microtask is not needed since selenium already does macrotasking
+
+        const { value } = browser.execute(function () {
+            var list = Array.prototype.slice.call(
+                document.querySelector('integration-array-primitives').shadowRoot.querySelectorAll('li')
+            );
+
+            return list.map(function (li) {
+                return li.textContent;
+            });
+        });
+
+        assert.equal(value.length, splicedLength);
+        assert.deepEqual(value, splicedList);
+    });
+});
