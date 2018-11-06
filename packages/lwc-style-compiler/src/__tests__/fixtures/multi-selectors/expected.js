922eb30e9dec6e7e7a75d491a1aa3d36fea67d96
Diego Ferreiro Val <dval@salesforce.com>
2018-11-05 16:37:02 -0800

    fix: css newline

--- packages/lwc-style-compiler/src/__tests__/fixtures/multi-selectors/expected.js
+++ packages/lwc-style-compiler/src/__tests__/fixtures/multi-selectors/expected.js
@@ -1,9 +1,7 @@
 function stylesheet(hostSelector, shadowSelector, nativeShadow) {
   return `
 h1${shadowSelector}, h2${shadowSelector} {}
-h1${shadowSelector},
-h2${shadowSelector}
-{}
+h1${shadowSelector},h2${shadowSelector}{}
 `;
 }
 export default [stylesheet];
--- packages/lwc-style-compiler/src/__tests__/inline.spec.ts
+++ packages/lwc-style-compiler/src/__tests__/inline.spec.ts
@@ -3,16 +3,19 @@ import { transform } from '../index';
 describe('playground test for debugging ', () => {
     it('examples', () => {
         const src = `
-        :host(.foo) {
-            color: var(--b);
+        :host:not(.selected):hover button,
+        :host button:focus {
+            color: #005fb2; /* $color-background-button-brand-hover */
+            transform: scale(1.2);
         }
-
 `;
         const { code } = transform(src, 'test', {
             customProperties: {
                 resolverModule: "custom-properties-resolver"
             }
         });
+
+        console.log(code);
 
         expect(code).toBeDefined();
     });
