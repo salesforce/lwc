c644c3b76c534042c1a41668b40e16afde9e77f1
Bob Yao <bobbyyao@gmail.com>
2018-11-05 20:31:46 -0800

    fix: css newline (#804)
    * fix: css newline & styles

--- packages/lwc-style-compiler/src/__tests__/inline.spec.ts
+++ packages/lwc-style-compiler/src/__tests__/inline.spec.ts
@@ -3,10 +3,11 @@ import { transform } from '../index';
 describe('playground test for debugging ', () => {
     it('examples', () => {
         const src = `
-        :host(.foo) {
-            color: var(--b);
+        :host:not(.selected):hover button,
+        :host button:focus {
+            color: #005fb2; /* $color-background-button-brand-hover */
+            transform: scale(1.2);
         }
-
 `;
         const { code } = transform(src, 'test', {
             customProperties: {
