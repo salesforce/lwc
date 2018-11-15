b6f2a5863eda5a15018a8a489649702e8e5c4fae
Diego Ferreiro Val <dval@salesforce.com>
2018-11-14 16:18:08 -0800

    fix: parse atrules correctly in CSS

--- packages/lwc-style-compiler/src/__tests__/inline.spec.ts
+++ packages/lwc-style-compiler/src/__tests__/inline.spec.ts
@@ -3,17 +3,20 @@ import { transform } from '../index';
 describe('playground test for debugging ', () => {
     it('examples', () => {
         const src = `
-        :host:not(.selected):hover button,
-        :host button:focus {
-            color: #005fb2; /* $color-background-button-brand-hover */
-            transform: scale(1.2);
+        @media screen and (max-width: 768px) {
+            :host {
+                width: calc(50% - 1rem);
+            }
         }
+
 `;
         const { code } = transform(src, 'test', {
             customProperties: {
                 resolverModule: "custom-properties-resolver"
             }
         });
+
+        console.log(code);
 
         expect(code).toBeDefined();
     });
