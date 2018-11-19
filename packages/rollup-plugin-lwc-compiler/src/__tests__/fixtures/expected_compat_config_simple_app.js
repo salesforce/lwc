faf18666521ccd1ba56f7386d21de96f823dddbe
Diego Ferreiro Val <dval@salesforce.com>
2018-11-19 14:55:34 -0800

    fix: testes

--- packages/rollup-plugin-lwc-compiler/src/__tests__/fixtures/expected_compat_config_simple_app.js
+++ packages/rollup-plugin-lwc-compiler/src/__tests__/fixtures/expected_compat_config_simple_app.js
@@ -1,5 +1,5 @@
-(function (lwc) {
-    'use strict';
+(function(lwc) {
+    "use strict";
 
     var __callKey1 = Proxy.callKey1;
 
@@ -13,7 +13,9 @@
 
     function _assertThisInitialized(self) {
       if (self === void 0) {
-        throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+        throw new ReferenceError(
+          "this hasn't been initialised - super() hasn't been called"
+        );
       }
 
       return self;
@@ -27,21 +29,26 @@
       return _assertThisInitialized(self);
     }
 
-    var __getKey = Proxy.getKey;
-
     function _getPrototypeOf(o) {
-      _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) {
-        return __getKey(o, "__proto__") || Object.getPrototypeOf(o);
-      };
+      _getPrototypeOf = Object.setPrototypeOf
+        ? Object.getPrototypeOf
+        : function _getPrototypeOf(o) {
+            return (
+              (o._ES5ProxyType ? o.get("__proto__") : o.__proto__) ||
+              Object.getPrototypeOf(o)
+            );
+          };
       return _getPrototypeOf(o);
     }
 
     function _setPrototypeOf(o, p) {
-      _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) {
-        __setKey(o, "__proto__", p);
+      _setPrototypeOf =
+        Object.setPrototypeOf ||
+        function _setPrototypeOf(o, p) {
+          __setKey(o, "__proto__", p);
 
-        return o;
-      };
+          return o;
+        };
 
       return _setPrototypeOf(o, p);
     }
@@ -51,13 +58,23 @@
         throw new TypeError("Super expression must either be null or a function");
       }
 
-      __setKey(subClass, "prototype", Object.create(superClass && __getKey(superClass, "prototype"), {
-        constructor: {
-          value: subClass,
-          writable: true,
-          configurable: true
-        }
-      }));
+      __setKey(
+        subClass,
+        "prototype",
+        Object.create(
+          superClass &&
+            (superClass._ES5ProxyType
+              ? superClass.get("prototype")
+              : superClass.prototype),
+          {
+            constructor: {
+              value: subClass,
+              writable: true,
+              configurable: true
+            }
+          }
+        )
+      );
 
       if (superClass) _setPrototypeOf(subClass, superClass);
     }
@@ -67,12 +84,17 @@
     var __concat = Proxy.concat;
 
     function tmpl($api, $cmp, $slotset, $ctx) {
-      var api_dynamic = __getKey($api, "d"),
-          api_element = __getKey($api, "h");
-
-      return [api_element("div", {
-        key: 2
-      }, [api_dynamic(__getKey($cmp, "x"))])];
+      var api_dynamic = $api._ES5ProxyType ? $api.get("d") : $api.d,
+        api_element = $api._ES5ProxyType ? $api.get("h") : $api.h;
+      return [
+        api_element(
+          "div",
+          {
+            key: 2
+          },
+          [api_dynamic($cmp._ES5ProxyType ? $cmp.get("x") : $cmp.x)]
+        )
+      ];
     }
 
     var _tmpl = lwc.registerTemplate(tmpl);
@@ -80,35 +102,50 @@
     __setKey(tmpl, "stylesheets", []);
 
     __setKey(tmpl, "stylesheetTokens", {
-        hostAttribute: "x-foo_foo-host",
-        shadowAttribute: "x-foo_foo"
+      hostAttribute: "x-foo_foo-host",
+      shadowAttribute: "x-foo_foo"
     });
 
     var Foo =
-    /*#__PURE__*/
-    function (_LightningElement) {
-      _inherits(Foo, _LightningElement);
+      /*#__PURE__*/
+      (function(_LightningElement) {
+        _inherits(Foo, _LightningElement);
 
-      function Foo() {
-        var _getPrototypeOf2;
+        function Foo() {
+          var _getPrototypeOf2, _getPrototypeOf3, _call;
 
-        var _this;
+          var _this;
 
-        _classCallCheck(this, Foo);
+          _classCallCheck(this, Foo);
 
-        for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-          __setKey(args, _key, arguments[_key]);
+          for (
+            var _len = arguments.length, args = new Array(_len), _key = 0;
+            _key < _len;
+            _key++
+          ) {
+            __setKey(args, _key, arguments[_key]);
+          }
+
+          _this = _possibleConstructorReturn(
+            this,
+            __callKey2(
+              ((_getPrototypeOf3 = _getPrototypeOf2 = _getPrototypeOf(Foo)),
+              (_call = _getPrototypeOf3._ES5ProxyType
+                ? _getPrototypeOf3.get("call")
+                : _getPrototypeOf3.call)),
+              "apply",
+              _getPrototypeOf2,
+              __concat([this], args)
+            )
+          );
+
+          __setKey(_this, "x", void 0);
+
+          return _this;
         }
 
-        _this = _possibleConstructorReturn(this, __callKey2(__getKey(_getPrototypeOf2 = _getPrototypeOf(Foo), "call"), "apply", _getPrototypeOf2, __concat([this], args)));
-
-        __setKey(_this, "x", void 0);
-
-        return _this;
-      }
-
-      return Foo;
-    }(lwc.LightningElement);
+        return Foo;
+      })(lwc.LightningElement);
 
     lwc.registerDecorators(Foo, {
       publicProps: {
@@ -123,20 +160,32 @@
     });
 
     function tmpl$1($api, $cmp, $slotset, $ctx) {
-      var api_custom_element = __getKey($api, "c"),
-          api_element = __getKey($api, "h");
-
-      return [api_element("div", {
-        classMap: {
-          "container": true
-        },
-        key: 2
-      }, [api_custom_element("x-foo", _xFoo, {
-        props: {
-          "x": "1"
-        },
-        key: 3
-      }, [])])];
+      var api_custom_element = $api._ES5ProxyType ? $api.get("c") : $api.c,
+        api_element = $api._ES5ProxyType ? $api.get("h") : $api.h;
+      return [
+        api_element(
+          "div",
+          {
+            classMap: {
+              container: true
+            },
+            key: 2
+          },
+          [
+            api_custom_element(
+              "x-foo",
+              _xFoo,
+              {
+                props: {
+                  x: "1"
+                },
+                key: 3
+              },
+              []
+            )
+          ]
+        )
+      ];
     }
 
     var _tmpl$1 = lwc.registerTemplate(tmpl$1);
@@ -144,40 +193,42 @@
     __setKey(tmpl$1, "stylesheets", []);
 
     __setKey(tmpl$1, "stylesheetTokens", {
-        hostAttribute: "x-app_app-host",
-        shadowAttribute: "x-app_app"
+      hostAttribute: "x-app_app-host",
+      shadowAttribute: "x-app_app"
     });
 
     var App =
-    /*#__PURE__*/
-    function (_LightningElement) {
-      _inherits(App, _LightningElement);
+      /*#__PURE__*/
+      (function(_LightningElement) {
+        _inherits(App, _LightningElement);
 
-      function App() {
-        var _this;
+        function App() {
+          var _this;
 
-        _classCallCheck(this, App);
+          _classCallCheck(this, App);
 
-        _this = _possibleConstructorReturn(this, __callKey1(_getPrototypeOf(App), "call", this));
+          _this = _possibleConstructorReturn(
+            this,
+            __callKey1(_getPrototypeOf(App), "call", this)
+          );
 
-        __setKey(_this, "list", []);
+          __setKey(_this, "list", []);
 
-        return _this;
-      }
+          return _this;
+        }
 
-      return App;
-    }(lwc.LightningElement);
+        return App;
+      })(lwc.LightningElement);
 
     var App$1 = lwc.registerComponent(App, {
       tmpl: _tmpl$1
     });
 
-    var container = __callKey1(document, "getElementById", 'main');
+    var container = __callKey1(document, "getElementById", "main");
 
-    var element = lwc.createElement('x-app', {
+    var element = lwc.createElement("x-app", {
       is: App$1
     });
 
     __callKey1(container, "appendChild", element);
-
-  }(Engine));
+  })(Engine);
