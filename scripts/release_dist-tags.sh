376baf08021c98b53f53f91e2acccee81870681c
Diego Ferreiro Val <dval@salesforce.com>
2018-11-26 22:13:00 -0800

    wip: upgrade script package rename

--- scripts/release_dist-tags.sh
+++ scripts/release_dist-tags.sh
@@ -9,14 +9,14 @@ LWC_VERSION="$1"
 TAG_NAME="$2"
 
 # Get the current tag
-CURRENT_TAG=$(npm dist-tags ls lwc-engine --registry='https://npm.lwcjs.org' | sed -n "s/.*${TAG_NAME}: \(.*\)/\1/p")
+CURRENT_TAG=$(npm dist-tags ls @lwc/engine --registry='https://npm.lwcjs.org' | sed -n "s/.*${TAG_NAME}: \(.*\)/\1/p")
 
 if [[ ${CURRENT_TAG} = ${LWC_VERSION} ]]; then
   echo "LWC version ${LWC_VERSION} already tagged to ${TAG_NAME}"
   exit 0;
 fi
 
-CMD_UPDATE_TAGS="lerna exec --ignore benchmark --ignore lwc-integration -- npm dist-tag add \$LERNA_PACKAGE_NAME@${LWC_VERSION} ${TAG_NAME} --registry='https://npm.lwcjs.org'"
+CMD_UPDATE_TAGS="lerna exec --ignore perf-benchmarks --ignore integration-tests -- npm dist-tag add \$LERNA_PACKAGE_NAME@${LWC_VERSION} ${TAG_NAME} --registry='https://npm.lwcjs.org'"
 
 echo $CMD_UPDATE_TAGS
 $CMD_UPDATE_TAGS
