9b0c4d43d51e57885885a1104a9b9f0bff856259
Jose David Rodriguez Velasco <jrodriguezvelasco@salesforce.com>
2018-12-14 10:00:33 -0800

    feat: no-production-assert rule
    We should strip asserts when compiling the framework code for production environment.
    For that the assertion code should be wrapped in: if (process.env.NODE_ENV === 'production') {}.
    
    This is the simple, but good enough implemetation for the code we have.

---
+++ packages/lwc-tslint-rules/src/rules/__tests__/noProductionAssertRule.spec.ts
@@ -0,0 +1,33 @@
+import { runLintRule } from 'test-utils';
+
+const rule = 'no-production-assert';
+
+describe('no-production-assert', () => {
+    it(`should fail when an assert is called and dont have the production check.`, () => {
+        const src = `assert.logger.logWarning('some message');`;
+        const result = runLintRule({src, rule});
+        expect(result.errorCount).toBe(1);
+    });
+
+    it(`should not fail when an assert is called.`, () => {
+        const src = `
+        if (process.env.NODE_ENV !== 'production') {
+            assert.logWarning('lolo');
+        }
+    `;
+        const result = runLintRule({src, rule});
+        expect(result.errorCount).toBe(0);
+    });
+
+    it(`should analize else and fail when an assert is called.`, () => {
+        const src = `
+        if (process.env.NODE_ENV !== 'production') {
+            assert.logWarning('this assert is fine');
+        } else {
+            assert.logWarning('this is leaking to prod');
+        }
+    `;
+        const result = runLintRule({src, rule});
+        expect(result.errorCount).toBe(1);
+    });
+});
