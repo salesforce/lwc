0da24e9a9ee55483e7ef56009bd3ef419df9487d
Trevor <tbliss@salesforce.com>
2018-12-14 15:23:02 -0800

    build: script to check headers

--- packages/integration-tests/scripts/wdio.sauce.conf.js
+++ packages/integration-tests/scripts/wdio.sauce.conf.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
 // force non-headless Chrome on Sauce Labs for debugability
 process.env.HEADLESS_CHROME=false;
 const merge = require('deepmerge');
