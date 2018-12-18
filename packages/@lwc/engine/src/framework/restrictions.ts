972feba77f6f99b5527b106a47730fbe398b79e6
Jose David Rodriguez Velasco <jrodriguezvelasco@salesforce.com>
2018-12-18 15:36:37 -0800

    fix: ignore existing no-production-assert failures
    Cause they are checked before calling.

--- packages/@lwc/engine/src/framework/restrictions.ts
+++ packages/@lwc/engine/src/framework/restrictions.ts
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
+/* tslint:disable:no-production-assert */
 import assert from "../shared/assert";
 import { defineProperties, getOwnPropertyNames, forEach, assign, isString, isUndefined, ArraySlice, toString, StringToLowerCase, setPrototypeOf, getPrototypeOf, getPropertyDescriptor } from "../shared/language";
 import { ComponentInterface } from "./component";
