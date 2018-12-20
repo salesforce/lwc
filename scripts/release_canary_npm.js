4571d00e0acf213a273f7ccb46896c263e278f50
Diego Ferreiro Val <dval@salesforce.com>
2018-12-20 15:02:30 -0800

    chore: minor adjustment to canary release

--- scripts/release_canary_npm.js
+++ scripts/release_canary_npm.js
@@ -11,35 +11,17 @@ const AWS = require('aws-sdk');
 const glob = require('glob');
 const { lookup } = require('mime-types');
 
-function getConfig() {
-    return {
-        accessKeyId: process.env.RELEASE_ACCESS_KEY_ID || process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.RELEASE_SECRET_ACCESS_KEY || process.env.AWS_SECRET_ACCESS_KEY,
-        region: process.env.RELEASE_REGION || process.env.AWS_REGION
-    };
-}
-
-const S3 = new AWS.S3({ ...getConfig() });
+const CONFIG = {
+    accessKeyId: process.env.RELEASE_ACCESS_KEY_ID || process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.RELEASE_SECRET_ACCESS_KEY || process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.RELEASE_REGION || process.env.AWS_REGION
+};
 const BUCKET = process.env.RELEASE_BUCKET_NAME || process.env.BUCKETEER_BUCKET_NAME || process.env.AWS_BUCKET_NAME;
 const TEN_YEARS = 1000 * 60 * 60 * 24 * 365 * 10;
+
+const S3 = new AWS.S3(CONFIG);
 const PREFIX = 'public';
 const HOST = `https://${BUCKET}.s3.amazonaws.com`;
-
-function listObjects(packageName) {
-    return new Promise(function (reject, resolve) {
-        S3.listObjectsV2({
-            Bucket: BUCKET,
-            Prefix: PREFIX + '/builds/' + (packageName || ''),
-            Delimiter: PREFIX + '/',
-        }, function (err, data) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(data);
-            }
-        });
-    });
-}
 
 function pushPackage({ sha, packageName, packageTar }) {
     return new Promise(function (resolve, reject) {
@@ -66,12 +48,6 @@ async function run() {
     const [sha, customPath] = process.argv.slice(2);
     const cwd = process.cwd();
     const absPath = path.resolve(cwd, customPath || 'packages');
-
-    if (sha === 'list') {
-        const list = await listObjects(customPath);
-        console.log(list);
-        return;
-    }
 
     if (!sha) {
         throw new Error('Pushing packages require a git sha commit to pin the package');
