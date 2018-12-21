a0aeb8fc483eda250255ac45da5f9358ffbdb517
Diego Ferreiro Val <elfogris@gmail.com>
2018-12-20 16:59:37 -0800

    chore: simplify release script (#903)
    * chore: minor adjustment to canary release
    
    * chore: update yarn
    
    * chore: add package_version
    
    * chore: fix release
    
    * chore: fix pkg
    
    * chore: fixing ci
    
    * chore: minor docs
    
    * chore: minor docs

--- scripts/release_canary_npm.js
+++ scripts/release_canary_npm.js
@@ -8,37 +8,33 @@
 const fs = require('fs');
 const path = require('path');
 const AWS = require('aws-sdk');
-const glob = require('glob');
+const execa = require('execa');
 const { lookup } = require('mime-types');
 
-function getConfig() {
-    return {
-        accessKeyId: process.env.RELEASE_ACCESS_KEY_ID || process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.RELEASE_SECRET_ACCESS_KEY || process.env.AWS_SECRET_ACCESS_KEY,
-        region: process.env.RELEASE_REGION || process.env.AWS_REGION
-    };
-}
+// To invoke this from the commandline you need the following to env vars to exist:
+//
+// RELEASE_BUCKET_NAME
+// RELEASE_SECRET_ACCESS_KEY
+// RELEASE_ACCESS_KEY_ID
+// RELEASE_REGION
+//
 
-const S3 = new AWS.S3({ ...getConfig() });
+const CONFIG = {
+    accessKeyId: process.env.RELEASE_ACCESS_KEY_ID || process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.RELEASE_SECRET_ACCESS_KEY || process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.RELEASE_REGION || process.env.AWS_REGION
+};
 const BUCKET = process.env.RELEASE_BUCKET_NAME || process.env.BUCKETEER_BUCKET_NAME || process.env.AWS_BUCKET_NAME;
 const TEN_YEARS = 1000 * 60 * 60 * 24 * 365 * 10;
+
+const S3 = new AWS.S3(CONFIG);
 const PREFIX = 'public';
 const HOST = `https://${BUCKET}.s3.amazonaws.com`;
 
-function listObjects(packageName) {
-    return new Promise(function (reject, resolve) {
-        S3.listObjectsV2({
-            Bucket: BUCKET,
-            Prefix: PREFIX + '/builds/' + (packageName || ''),
-            Delimiter: PREFIX + '/',
-        }, function (err, data) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(data);
-            }
-        });
-    });
+async function exec(command, args, options) {
+    console.log(`\n\tRunning: \`${command} ${args.join(' ')}\``);
+    const { stdout } = await execa(command, args, options);
+    return stdout;
 }
 
 function pushPackage({ sha, packageName, packageTar }) {
@@ -67,29 +63,37 @@ async function run() {
     const cwd = process.cwd();
     const absPath = path.resolve(cwd, customPath || 'packages');
 
-    if (sha === 'list') {
-        const list = await listObjects(customPath);
-        console.log(list);
-        return;
-    }
-
     if (!sha) {
         throw new Error('Pushing packages require a git sha commit to pin the package');
     }
 
-    const pkgs = glob.sync('*/*.tgz', { cwd: absPath });
+    const pkgs = fs.readdirSync(absPath);
 
     if (pkgs.length) {
         console.log('Creating package artifacts for SHA:', sha);
 
-        for (const pkg of pkgs) {
-            const [pkgName, ] = pkg.split(path.sep);
-            const { name } = require(path.join(absPath, pkgName, 'package.json'));
-            const fullPath = path.join(absPath, pkg);
+        for (const pkgName of pkgs) {
+            // Find package.json
+            const jsonPath = path.join(absPath, pkgName, 'package.json');
+            const pkgJson = require(jsonPath);
+
+            // Override package.json
+            const { name, version } = pkgJson;
+            const fullPath = path.join(absPath, pkgName);
+            pkgJson._originalversion = version;
+            pkgJson.version = `${version}-canary+${sha}`;
+            fs.writeFileSync(jsonPath, JSON.stringify(pkgJson, null, 2), { encoding: 'utf-8' });
+
+            // Generate tar artifact
+            const tar = await exec('npm', ['pack'], { cwd: fullPath });
+            const tarPath = path.join(fullPath, tar);
+
+            // Push package to S3
             process.stdout.write(`Pushing package: ${pkgName}...`);
-            const url = await pushPackage({ packageName: name, sha, packageTar : fullPath });
+            const url = await pushPackage({ packageName: name, sha, packageTar : tarPath });
             process.stdout.write(` [DONE]\n Uploaded to: ${HOST}/${url}\n`);
         }
+
     } else {
         console.log('No packages found');
     }
