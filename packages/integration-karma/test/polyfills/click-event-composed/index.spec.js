c6185814c13b160639c0fddcc967462d6195d3dd
Pierre-Marie Dartus <p.dartus@salesforce.com>
2019-01-15 12:02:06 +0100

    test: migrate click-event-composed to karma

---
+++ packages/integration-karma/test/polyfills/click-event-composed/index.spec.js
@@ -0,0 +1,35 @@
+import { createElement } from 'test-utils';
+
+import Test from 'x/test';
+
+it('should set the composed property to true when invoking click() on an element', () => {
+    let clickEvent;
+
+    const elm = document.createElement('div');
+    elm.addEventListener('click', evt => {
+        clickEvent = evt;
+    });
+
+    elm.click();
+
+    expect(clickEvent instanceof Event).toBe(true);
+    expect(clickEvent.composed).toBe(true);
+});
+
+it('should let the event bubble throws the shadow root when invoking click() on an element in the shadow', () => {
+    let clickEvent;
+
+    const elm = createElement('x-test', { is: Test });
+    document.body.appendChild(elm);
+    elm.addEventListener('click', evt => {
+        clickEvent = evt;
+    })
+
+    elm.shadowRoot.querySelector('div').click();
+
+    expect(clickEvent instanceof Event).toBe(true);
+    expect(clickEvent.composed).toBe(true);
+});
+
+
+// TODO: Need to migrate the 2 remaining tests. I don't understand why/what they are testing.
