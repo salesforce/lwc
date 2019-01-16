640185415548debbcda7e53963c085fff8e205d8
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2019-01-16 08:52:48 +0100

    test: Migrate event composed polyfill tests to karma (#966)
    * test: migrate event-composed-polyfill
    
    * test: make event-composed test as pending
    
    * test: migrate click-event-composed to karma
    
    * test: add custom-event-composed to karma
    
    * test: migrate focus-event-composed polyfill test to karma
    
    * test: disable failing click test for Safari
    
    * test: adapt tests for IE11

---
+++ packages/integration-karma/test/polyfills/click-event-composed/index.spec.js
@@ -0,0 +1,38 @@
+import { createElement } from 'test-utils';
+
+import Test from 'x/test';
+
+it('should set the composed property to true when invoking click() on an element', () => {
+    let clickEvent;
+
+    const elm = document.createElement('div');
+    document.body.appendChild(elm);
+
+    elm.addEventListener('click', evt => {
+        clickEvent = evt;
+    });
+
+    elm.click();
+
+    expect(clickEvent instanceof Event).toBe(true);
+    expect(clickEvent.composed).toBe(true);
+});
+
+// The composed-event-click-polyfill doesn't work when native Shadow DOM is enabled on Safari 12.0.0 (it has been fixed
+// with Safari 12.0.1). The polyfill only patches the event javascript wrapper and doesn't have any effect on how Webkit
+// make the event bubbles.
+// TODO: Enable this test again once Sauce Labs supports Safari version >= 12.0.0
+xit('should let the event bubble throws the shadow root when invoking click() on an element in the shadow', () => {
+    let clickEvent;
+
+    const elm = createElement('x-test', { is: Test });
+    document.body.appendChild(elm);
+    elm.addEventListener('click', evt => {
+        clickEvent = evt;
+    })
+
+    elm.shadowRoot.querySelector('div').click();
+
+    expect(clickEvent instanceof Event).toBe(true);
+    expect(clickEvent.composed).toBe(true);
+});
