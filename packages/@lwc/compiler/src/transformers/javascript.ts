9b75cbdedb299afb56c4394f34f184e5b35a1664
Alex <apapko@salesforce.com>
2019-01-15 16:40:26 -0800

    wip: remove metadata from bundle output

--- packages/@lwc/compiler/src/transformers/javascript.ts
+++ packages/@lwc/compiler/src/transformers/javascript.ts
@@ -11,13 +11,11 @@ import { normalizeToCompilerError, TransformerErrors } from "@lwc/errors";
 import { BABEL_CONFIG_BASE, BABEL_PLUGINS_BASE } from "../babel-plugins";
 import { NormalizedCompilerOptions } from "../compiler/options";
 import { FileTransformerResult } from "./transformer";
-import { MetadataCollector } from "../bundler/meta-collector";
 
 export default function(
     code: string,
     filename: string,
     options: NormalizedCompilerOptions,
-    metadataCollector?: MetadataCollector
 ): FileTransformerResult {
     const { isExplicitImport } = options;
     const config = Object.assign({}, BABEL_CONFIG_BASE, {
@@ -33,12 +31,8 @@ export default function(
         throw normalizeToCompilerError(TransformerErrors.JS_TRANSFORMER_ERROR, e, { filename });
     }
 
-    const metadata: lwcClassTransformPlugin.Metadata = (result as any)
-        .metadata;
-
     return {
         code: result.code,
         map: result.map,
-        metadata,
     };
 }
