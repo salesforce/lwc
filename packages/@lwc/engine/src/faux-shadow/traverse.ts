4931eec29e9c95ed6bd8d4c6e3766a67c785ff2f
Caridy Patiño <caridy@gmail.com>
2019-01-17 15:02:41 -0500

    fix(engine): fixes #973 to support cloneNode with ie11 devtool comment (#974)
    * fix(engine): fixes #973 to support cloneNode with ie11 devtool comment
    * fix(engine): fix for devtool logging of -bash on a root custom element
    * fix(engine): more guarding around isNodeSlotted for inserted nodes

--- packages/@lwc/engine/src/faux-shadow/traverse.ts
+++ packages/@lwc/engine/src/faux-shadow/traverse.ts
@@ -9,6 +9,7 @@ import {
     getNodeKey,
     getNodeNearestOwnerKey,
     PatchedNode,
+    getInternalChildNodes,
 } from "./node";
 import {
     childNodesGetter as nativeChildNodesGetter,
@@ -43,15 +44,16 @@ export function getNodeOwner(node: Node): HTMLElement | null {
     if (isUndefined(ownerKey)) {
         return null;
     }
+    let nodeOwner: Node | null = node;
     // At this point, node is a valid node with owner identity, now we need to find the owner node
     // search for a custom element with a VM that owns the first element with owner identity attached to it
-    while (!isNull(node) && (getNodeKey(node) !== ownerKey)) {
-        node = parentNodeGetter.call(node);
+    while (!isNull(nodeOwner) && (getNodeKey(nodeOwner) !== ownerKey)) {
+        nodeOwner = parentNodeGetter.call(nodeOwner);
     }
-    if (isNull(node)) {
+    if (isNull(nodeOwner)) {
         return null;
     }
-    return node as HTMLElement;
+    return nodeOwner as HTMLElement;
 }
 
 export function isSlotElement(elm: Element): boolean {
@@ -76,15 +78,15 @@ export function isNodeSlotted(host: Element, node: Node): boolean {
     }
     const hostKey = getNodeKey(host);
     // just in case the provided node is not an element
-    let currentElement: Element = node instanceof Element ? node : parentElementGetter.call(node);
+    let currentElement = node instanceof Element ? node : parentElementGetter.call(node);
     while (!isNull(currentElement) && currentElement !== host) {
         const elmOwnerKey = getNodeNearestOwnerKey(currentElement);
-        const parent: Element = parentElementGetter.call(currentElement);
+        const parent = parentElementGetter.call(currentElement);
         if (elmOwnerKey === hostKey) {
             // we have reached a host's node element, and only if
             // that element is an slot, then the node is considered slotted
             return isSlotElement(currentElement);
-        } else if (parent !== host && getNodeNearestOwnerKey(parent) !== elmOwnerKey) {
+        } else if (!isNull(parent) && parent !== host && getNodeNearestOwnerKey(parent) !== elmOwnerKey) {
             // we are crossing a boundary of some sort since the elm and its parent
             // have different owner key. for slotted elements, this is only possible
             // if the parent happens to be a slot that is not owned by the host
@@ -268,7 +270,7 @@ export function PatchedElement(elm: HTMLElement): HTMLElementConstructor {
             return createStaticNodeList(lightDomQuerySelectorAll(this, selectors));
         }
         get innerHTML(this: Element): string {
-            const { childNodes } = this;
+            const childNodes = getInternalChildNodes(this);
             let innerHTML = '';
             for (let i = 0, len = childNodes.length; i < len; i += 1) {
                 innerHTML += getOuterHTML(childNodes[i]);
