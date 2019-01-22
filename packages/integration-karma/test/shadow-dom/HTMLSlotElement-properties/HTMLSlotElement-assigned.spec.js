91d5a966fbadf4c7fb3eb393e3b12cefe763cc94
Pierre-Marie Dartus <p.dartus@salesforce.com>
2019-01-22 11:14:57 +0100

    test: migrate assignedNode and assignedElements tests

---
+++ packages/integration-karma/test/shadow-dom/HTMLSlotElement-properties/HTMLSlotElement-assigned.spec.js
@@ -0,0 +1,191 @@
+import { createElement } from 'test-utils';
+
+// import Distribute from 'x/distribute';
+import NoDirectChild from 'x/noDirectChild';
+
+import Basic from 'x/basic';
+import SlotsInSlots from 'x/slotsInSlots';
+import Complex from 'x/complex';
+
+function extractDataIds(root) {
+    const nodes = {};
+
+    function processElement(elm) {
+        if (elm.hasAttribute('data-id')) {
+            nodes[elm.getAttribute('data-id')] = elm;
+        }
+
+        if (elm.shadowRoot) {
+            Object.assign(nodes, extractShadowDataIds(elm.shadowRoot));
+        }
+    }
+
+    processElement(root);
+
+    const walker = document.createTreeWalker(root, NodeFilter.SHOW_ELEMENT);
+
+    let elm;
+    while (elm = walker.nextNode()){
+        processElement(elm);
+    }
+
+    return nodes;
+}
+
+function extractShadowDataIds(shadowRoot) {
+    const nodes = {};
+
+    // We can't use a TreeWalker directly on the ShadowRoot since with synthetic shadow the ShadowRoot is not an
+    // actual DOM nodes. So we need to iterate over the children manually and run the tree walker on each child.
+    for (const child of shadowRoot.childNodes) {
+        Object.assign(nodes, extractDataIds(child));
+    }
+
+    return nodes;
+}
+
+describe('ignore non direct host children', () => {
+    let elm;
+    let nodes;
+
+    beforeAll(() => {
+        elm = createElement('x-no-direct-child', { is: NoDirectChild });
+        document.body.appendChild(elm);
+
+        nodes = extractShadowDataIds(elm.shadowRoot);
+    });
+
+    it('assignedNodes', () => {
+        expect(nodes.default1.assignedNodes()).toEqual([
+            nodes.slotted3,
+        ]);
+        expect(nodes.slot1.assignedNodes()).toEqual([
+            nodes.slotted1,
+            nodes.slotted2,
+        ]);
+
+        expect(nodes.default1.assignedNodes({ flatten: true })).toEqual([
+            nodes.slotted3,
+        ]);
+        expect(nodes.slot1.assignedNodes({ flatten: true })).toEqual([
+            nodes.slotted1,
+            nodes.slotted2,
+        ]);
+    })
+
+    it('assignedElements', () => {
+        expect(nodes.default1.assignedElements()).toEqual([
+            nodes.slotted3,
+        ]);
+        expect(nodes.slot1.assignedElements()).toEqual([
+            nodes.slotted1,
+            nodes.slotted2,
+        ]);
+
+        expect(nodes.default1.assignedElements({ flatten: true })).toEqual([
+            nodes.slotted3,
+        ]);
+        expect(nodes.slot1.assignedElements({ flatten: true })).toEqual([
+            nodes.slotted1,
+            nodes.slotted2,
+        ]);
+    })
+});
+
+describe('fallback content basic', () => {
+    let elm;
+    let nodes;
+
+    beforeAll(() => {
+        elm = createElement('x-basic', { is: Basic });
+        document.body.appendChild(elm);
+
+        nodes = extractShadowDataIds(elm.shadowRoot);
+    });
+
+    it('assignedNodes', () => {
+        expect(nodes.default1.assignedNodes()).toEqual([]);
+        expect(nodes.slot1.assignedNodes()).toEqual([]);
+
+        expect(nodes.default1.assignedNodes({ flatten: true })).toEqual([
+            nodes.default1.firstChild,
+            nodes.fallback1,
+            nodes.default1.lastChild,
+        ]);
+        expect(nodes.slot1.assignedNodes({ flatten: true })).toEqual([]);
+    });
+
+    it('assignedElements', () => {
+        expect(nodes.default1.assignedElements()).toEqual([]);
+        expect(nodes.slot1.assignedElements()).toEqual([]);
+
+        expect(nodes.default1.assignedElements({ flatten: true })).toEqual([
+            nodes.fallback1,
+        ]);
+        expect(nodes.slot1.assignedElements({ flatten: true })).toEqual([]);
+    });
+});
+
+describe('fallback content slots in slots', () => {
+    let elm;
+    let nodes;
+
+    beforeAll(() => {
+        elm = createElement('x-slots-in-slots', { is: SlotsInSlots });
+        document.body.appendChild(elm);
+
+        nodes = extractShadowDataIds(elm.shadowRoot);
+    });
+
+    it('assignedNodes', () => {
+        expect(nodes.slot1.assignedNodes()).toEqual([]);
+        expect(nodes.slot2.assignedNodes()).toEqual([]);
+
+        expect(nodes.slot1.assignedNodes({ flatten: true })).toEqual([nodes.fallback1]);
+        expect(nodes.slot2.assignedNodes({ flatten: true })).toEqual([nodes.fallback1]);
+    });
+
+    it('assignedElements', () => {
+        expect(nodes.slot1.assignedElements()).toEqual([]);
+        expect(nodes.slot2.assignedElements()).toEqual([]);
+
+        expect(nodes.slot1.assignedElements({ flatten: true })).toEqual([nodes.fallback1]);
+        expect(nodes.slot2.assignedElements({ flatten: true })).toEqual([nodes.fallback1]);
+    });
+});
+
+describe('fallback content complex', () => {
+    let elm;
+    let nodes;
+
+    beforeAll(() => {
+        elm = createElement('x-complex', { is: Complex });
+        document.body.appendChild(elm);
+
+        nodes = extractShadowDataIds(elm.shadowRoot);
+    });
+
+    it('assignedNodes', () => {
+        expect(nodes.slot1.assignedNodes()).toEqual([nodes.slotted1]);
+        expect(nodes.slot2.assignedNodes()).toEqual([]);
+        expect(nodes.slot3.assignedNodes()).toEqual([nodes.slot2]);
+        expect(nodes.slot4.assignedNodes()).toEqual([]);
+
+        expect(nodes.slot1.assignedNodes({ flatten: true })).toEqual([nodes.slotted1]);
+        expect(nodes.slot2.assignedNodes({ flatten: true })).toEqual([nodes.slotted1, nodes.fallback2]);
+        expect(nodes.slot3.assignedNodes({ flatten: true })).toEqual([nodes.slotted1, nodes.fallback2]);
+        expect(nodes.slot4.assignedNodes({ flatten: true })).toEqual([nodes.slotted1, nodes.fallback2, nodes.fallback3]);
+    });
+
+    it('assignedElements', () => {
+        expect(nodes.slot1.assignedElements()).toEqual([nodes.slotted1]);
+        expect(nodes.slot2.assignedElements()).toEqual([]);
+        expect(nodes.slot3.assignedElements()).toEqual([nodes.slot2]);
+        expect(nodes.slot4.assignedElements()).toEqual([]);
+
+        expect(nodes.slot1.assignedElements({ flatten: true })).toEqual([nodes.slotted1]);
+        expect(nodes.slot2.assignedElements({ flatten: true })).toEqual([nodes.slotted1, nodes.fallback2]);
+        expect(nodes.slot3.assignedElements({ flatten: true })).toEqual([nodes.slotted1, nodes.fallback2]);
+        expect(nodes.slot4.assignedElements({ flatten: true })).toEqual([nodes.slotted1, nodes.fallback2, nodes.fallback3]);
+    });
+})
--- packages/integration-karma/test/shadow-dom/HTMLSlotElement-properties/HTMLSlotElement.name.spec.js
+++ packages/integration-karma/test/shadow-dom/HTMLSlotElement-properties/HTMLSlotElement.name.spec.js
@@ -1,21 +1,21 @@
 import { createElement } from 'test-utils';
 
-import Slots from 'x/slots';
+import Basic from 'x/basic';
 
 it('should return the slot name attribute in the DOM', () => {
-    const elm = createElement('x-test', { is: Slots });
+    const elm = createElement('x-basic', { is: Basic });
     document.body.appendChild(elm);
 
     const slots = elm.shadowRoot.querySelectorAll('slot');
     expect(slots[0].getAttribute('name')).toBe(null);
-    expect(slots[1].getAttribute('name')).toBe('foo');
+    expect(slots[1].getAttribute('name')).toBe('slot1');
 });
 
 it('should have property reflecting the attribute', () => {
-    const elm = createElement('x-test', { is: Slots });
+    const elm = createElement('x-basic', { is: Basic });
     document.body.appendChild(elm);
 
     const slots = elm.shadowRoot.querySelectorAll('slot');
     expect(slots[0].name).toBe('');
-    expect(slots[1].name).toBe('foo');
+    expect(slots[1].name).toBe('slot1');
 });
