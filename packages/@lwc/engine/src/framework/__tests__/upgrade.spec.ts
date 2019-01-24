f99cad0caa8c580ac91edf4880c9312816934681
Pierre-Marie Dartus <p.dartus@salesforce.com>
2019-01-24 11:17:20 +0100

    test: migrate unwrap test to Karma

--- packages/@lwc/engine/src/framework/__tests__/upgrade.spec.ts
+++ packages/@lwc/engine/src/framework/__tests__/upgrade.spec.ts
@@ -9,45 +9,6 @@ import { compileTemplate } from 'test-utils';
 
 describe('upgrade', () => {
     describe('#createElement()', () => {
-        it('should allow access to profixied default values for public props', () => {
-            const x = [1, 2, 3], y = { foo: 1 };
-            type MyComponentElement = HTMLElement & {
-                x: any,
-                y: any;
-            };
-            const def = class MyComponent extends LightningElement {
-                x: any;
-                y: any;
-                constructor() {
-                    super();
-                    this.x = x;
-                    this.y = y;
-                }
-
-                static publicProps = {
-                    x: true,
-                    y: true
-                };
-            };
-            const elm: MyComponentElement = createElement('x-foo', { is: def }) as MyComponentElement;
-            expect(x).toEqual(elm.x);
-            expect(y).toEqual(elm.y);
-            expect(elm.x).not.toBe(x);
-            expect(elm.y).not.toBe(y);
-        });
-
-        it('should proxify any value before setting a property', () => {
-            const def = class MyComponent extends LightningElement {};
-            def.publicProps = { x: 1 };
-            const elm = createElement('x-foo', { is: def });
-            const o = { foo: 1 };
-            elm.x = o;
-            expect(o).toEqual(elm.x);
-            expect(elm.x).not.toBe(o);
-        });
-
-    });
-
     describe('patches for Node.', () => {
         beforeEach(() => {
             document.body.innerHTML = '';
