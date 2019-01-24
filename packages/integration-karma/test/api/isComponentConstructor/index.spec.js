34814b52f1b166fd066545aec0143a68d6097d56
Pierre-Marie Dartus <p.dartus@salesforce.com>
2019-01-24 10:40:09 +0100

    test: initial test migration

---
+++ packages/integration-karma/test/api/isComponentConstructor/index.spec.js
@@ -0,0 +1,30 @@
+import { LightningElement, isComponentConstructor } from 'lwc';
+
+function testInvalidComponentConstructor(name, ctor) {
+    it(`return false for ${name}`, () => {
+        // TODO: #933 - isComponentConstructor throws when not passing a proper constructor instead of returning a
+        // boolean.
+        expect(() => isComponentConstructor(ctor)).toThrowError(
+            /Invalid prototype chain for \w+, you must extend LightningElement./
+        );
+    });
+}
+
+// TODO: #932 - fix because currently throwing `Cannot read property 'name' of null`
+// testInvalidComponentConstructor('null', null);
+// testInvalidComponentConstructor('undefined', undefined);
+
+testInvalidComponentConstructor('string', 'component');
+testInvalidComponentConstructor('object', {});
+testInvalidComponentConstructor('object', class Foo {});
+
+it('should return true if when passing a class inheriting from LightningElement', () => {
+    class Component extends LightningElement {}
+    expect(isComponentConstructor(Component)).toBe(true);
+});
+
+it('should return true if when passing a class with LightningElement in it\'s prototype chain', () => {
+    class Base extends LightningElement {}
+    class Component extends Base {}
+    expect(isComponentConstructor(Component)).toBe(true);
+});
