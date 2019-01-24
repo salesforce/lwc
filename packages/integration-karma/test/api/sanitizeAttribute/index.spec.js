7fa3b76481cd5ca0025b8b3a5cc46dbf1386d9b2
Pierre-Marie Dartus <p.dartus@salesforce.com>
2019-01-24 11:48:40 +0100

    test: add test for sanitizeAttribute

---
+++ packages/integration-karma/test/api/sanitizeAttribute/index.spec.js
@@ -0,0 +1,41 @@
+import { createElement } from 'test-utils';
+
+import Xlink from 'x/xlink';
+
+let originalSanitizeAttribute = Engine.sanitizeAttribute;
+afterEach(() => {
+    // Reset original sanitizer after each test.
+    Engine.sanitizeAttribute = originalSanitizeAttribute;
+});
+
+it('uses the original passthrough sanitizer when not overridden', () => {
+    const elm = createElement('x-link', { is: Xlink });
+    document.body.appendChild(elm);
+
+    const use = elm.shadowRoot.querySelector('use');
+    expect(use.getAttribute('xlink:href')).toBe('/foo');
+});
+
+it('receives the right parameters', () => {
+    spyOn(Engine, 'sanitizeAttribute');
+
+    const elm = createElement('x-link', { is: Xlink });
+    document.body.appendChild(elm);
+
+    expect(Engine.sanitizeAttribute).toHaveBeenCalledWith(
+        'use',
+        'http://www.w3.org/2000/svg',
+        'xlink:href',
+        '/foo',
+    );
+});
+
+it('replace the original attribute value with the returned value', () => {
+    Engine.sanitizeAttribute = () => '/bar';
+
+    const elm = createElement('x-link', { is: Xlink });
+    document.body.appendChild(elm);
+
+    const use = elm.shadowRoot.querySelector('use');
+    expect(use.getAttribute('xlink:href')).toBe('/bar');
+});
