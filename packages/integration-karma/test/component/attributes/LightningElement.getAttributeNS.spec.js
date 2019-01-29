19e7b022049be6ee2180eabe4636ac477b633807
Pierre-Marie Dartus <p.dartus@salesforce.com>
2019-01-29 18:41:41 +0100

    test: add attributes tests

--- packages/integration-karma/test/component/attributes/LightningElement.getAttributeNS.spec.js
+++ packages/integration-karma/test/component/attributes/LightningElement.getAttributeNS.spec.js
@@ -1,17 +1,19 @@
 import { LightningElement, api } from 'lwc';
 import { createElement } from 'test-utils';
 
+const TEST_NS = 'http://www.salesforce.com/2019/lwc';
+
 class GetAttributeNS extends LightningElement {
     @api
-    getComponentAttribute(...args) {
+    getComponentAttributeNS(...args) {
         return this.getAttributeNS(...args);
     }
 }
 
-function testInvalidProperty(type, name) {
+function testInvalidProperty(type, ns, name) {
     it(`should return null when passing an invalid attribute name ${type}`, () => {
         const elm = createElement('x-get-attribute', { is: GetAttributeNS });
-        expect(elm.getComponentAttribute(name)).toBe(null);
+        expect(elm.getComponentAttributeNS(ns, name)).toBe(null);
     });
 }
 
@@ -19,26 +21,26 @@ describe('LightningElement.getAttributeNS', () => {
     it('throws if no argument is passed', () => {
         const elm = createElement('x-get-attribute', { is: GetAttributeNS });
         expect(() => {
-            elm.getComponentAttribute();
-        }).toBe(null);
+            elm.getComponentAttributeNS();
+        }).toThrowError(TypeError);
     });
 
-    testInvalidProperty('null', null);
-    testInvalidProperty('undefined', undefined);
-    testInvalidProperty('empty string', '');
-    testInvalidProperty('number', 1);
+    it('throws if when missing the attribute name', () => {
+        const elm = createElement('x-get-attribute', { is: GetAttributeNS });
+        expect(() => {
+            elm.getComponentAttributeNS(TEST_NS);
+        }).toThrowError(TypeError);
+    });
+
+    testInvalidProperty('null', TEST_NS, null);
+    testInvalidProperty('undefined', TEST_NS, undefined);
+    testInvalidProperty('empty string', TEST_NS, '');
+    testInvalidProperty('number', TEST_NS, 1);
 
     it('should return attribute on the host element', () => {
         const elm = createElement('x-get-attribute', { is: GetAttributeNS });
-        elm.setAttribute('foo', 'bar');
+        elm.setAttributeNS(TEST_NS, 'foo', 'bar');
 
-        expect(elm.getComponentAttribute('foo')).toBe('bar');
-    });
-
-    it('should normalize the attribute name to lowercase', () => {
-        const elm = createElement('x-get-attribute', { is: GetAttributeNS });
-        elm.setAttribute('foo', 'bar');
-
-        expect(elm.getComponentAttribute('FOO')).toBe('bar');
+        expect(elm.getComponentAttributeNS(TEST_NS, 'foo')).toBe('bar');
     });
 });
