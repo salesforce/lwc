82fc87fdf10b80934e0bef1d59c4a5abe0fe478f
Pierre-Marie Dartus <p.dartus@salesforce.com>
2019-01-30 08:34:37 +0100

    test: add constructor related tests

---
+++ packages/integration-karma/test/component/LightningElement/index.spec.js
@@ -0,0 +1,29 @@
+import { LightningElement } from 'lwc';
+import { createElement } from 'test-utils';
+
+it('should throw when trying to invoke the constructor manually', () => {
+    expect(() => {
+        new LightningElement();
+    }).toThrowError(ReferenceError);
+
+    expect(() => {
+        class Test extends LightningElement {}
+        new Test();
+    }).toThrowError(ReferenceError);
+});
+
+// TODO: Open an issue for this
+xit('should have no property enumerable on the component instance', () => {
+    let enumerableProperties = [];
+
+    class Test extends LightningElement {
+        connectedCallback() {
+            enumerableProperties = Object.keys(this);
+        }
+    }
+
+    const elm = createElement('x-test', { is: Test });
+    document.body.appendChild(elm);
+
+    expect(enumerableProperties).toEqual([]);
+});
