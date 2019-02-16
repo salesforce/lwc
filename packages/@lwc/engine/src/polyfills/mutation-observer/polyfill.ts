471d6bc0bcc27996dc43ceae7b4bec17ed7c2552
Ravi <ravi.jayaramappa@salesforce.com>
2019-02-15 18:46:58 -0800

    fix: address PR feedback

--- packages/@lwc/engine/src/polyfills/mutation-observer/polyfill.ts
+++ packages/@lwc/engine/src/polyfills/mutation-observer/polyfill.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-import { ArrayIndexOf, defineProperty, isUndefined, defineProperties } from '../../shared/language';
+import { ArrayIndexOf, ArrayReduce, ArrayPush, create, defineProperty, defineProperties, forEach, isUndefined } from '../../shared/language';
 import { getNodeNearestOwnerKey, getNodeKey } from '../../faux-shadow/node';
 import { SyntheticShadowRoot } from '../../faux-shadow/shadow-root';
 
@@ -21,7 +21,7 @@ const wrapperLookupField = '$$lwcObserverCallbackWrapper$$';
  */
 function retargetMutationRecord(originalRecord: MutationRecord): MutationRecord {
     const { addedNodes, removedNodes, target, type} = originalRecord;
-    const retargetedRecord = Object.create(MutationRecord.prototype);
+    const retargetedRecord = create(MutationRecord.prototype);
     defineProperties(retargetedRecord, {
         addedNodes: {
             get() { return addedNodes; },
@@ -48,50 +48,54 @@ function retargetMutationRecord(originalRecord: MutationRecord): MutationRecord 
 }
 
 /**
- * Some basic expectations to understand the filtering logic.
- * 1. A single MutationObserver can observe multiple targets
- * 2. A single callback can be user by multiple MutationObservers to handle mutations occuring in different parts of the document
- *
  * This function first gathers the OwnerKey of all the targets observed by the MutationObserver instance.
  * Next, process each MutationRecord to determine if the mutation occured in the same shadow tree as
  * one of the targets being observed.
+ * 
+ * The key filtering logic is to match the observed target node's ownerKey/ownKey with the ownerKey of the nodes in the
+ * MutationRecord.
+ * The ownerKey of the rootnode will be undefined. Similarly ownerkey of nodes outside the shadow will be undefined.
  * @param {MutationRecords[]} mutations
  * @param {MutationObserver} observer
  */
 function filterMutationRecords(mutations: MutationRecord[], observer: MutationObserver): MutationRecord[] {
     const observedTargets = observer[observedTargetsField];
     const observedTargetOwnerKeys: Array<(number|undefined)> = [];
-    observedTargets.forEach((node: Node) => {
+    forEach.call(observedTargets, (node: Node) => {
         // If the observed target is a shadowRoot, the ownerkey of the shadow tree will be fetched using the host
         const observedTargetOwnerKey = node instanceof SyntheticShadowRoot
             ? getNodeKey((node as ShadowRoot).host)
             : getNodeNearestOwnerKey(node);
-        observedTargetOwnerKeys.push(observedTargetOwnerKey);
+        ArrayPush.call(observedTargetOwnerKeys, observedTargetOwnerKey);
     });
-    return mutations.map((record: MutationRecord) => {
-        const { target , addedNodes, removedNodes, type } = record;
-        // If the mutations affected a lwc host element or its shadow,
-        // because LWC uses synthetic shadow, target will be the host
-        if (type === 'childList' && getNodeKey(target)) {
-            const sampleNode: Node = addedNodes.length > 0 ? addedNodes[0] : removedNodes[0];
-            const sampleNodeOwnerKey = getNodeNearestOwnerKey(sampleNode);
-            // Is node being added/removed to a subtree that is being observed
-            if (ArrayIndexOf.call(observedTargetOwnerKeys, sampleNodeOwnerKey) !== -1) {
-                // If the target was being observed, the return record as-is
-                if (observedTargets.indexOf(target) !== -1) {
-                    return record;
-                } else { // else, must be observing the shadowRoot
-                    return retargetMutationRecord(record);
+    return ArrayReduce.call(
+        mutations,
+        (filteredSet, record: MutationRecord) => {
+            const { target , addedNodes, removedNodes, type } = record;
+            // If the mutations affected a lwc host element or its shadow,
+            // because LWC uses synthetic shadow, target will be the host
+            if (type === 'childList' && !isUndefined(getNodeKey(target))) {
+                // Optimization: Peek in and test one node to decide if the MutationRecord qualifies
+                // The remaining nodes in this MutationRecord will have the same ownerKey
+                const sampleNode: Node = addedNodes.length > 0 ? addedNodes[0] : removedNodes[0];
+                const sampleNodeOwnerKey = getNodeNearestOwnerKey(sampleNode);
+                // Is node being added/removed to a subtree that is being observed
+                if (ArrayIndexOf.call(observedTargetOwnerKeys, sampleNodeOwnerKey) !== -1) {
+                    // If the target was being observed, the return record as-is
+                    if (observedTargets.indexOf(target) !== -1) {
+                        ArrayPush.call(filteredSet, record);
+                    } else { // else, must be observing the shadowRoot
+                        ArrayPush.call(filteredSet, retargetMutationRecord(record));
+                    }
+                }
+            } else {
+                const mutationInScope = ArrayIndexOf.call(observedTargetOwnerKeys, getNodeNearestOwnerKey(target)) !== -1;
+                if (mutationInScope) {
+                    ArrayPush.call(filteredSet, record);
                 }
             }
-        } else {
-            const mutationInScope = ArrayIndexOf.call(observedTargetOwnerKeys, getNodeNearestOwnerKey(target)) !== -1;
-            if (mutationInScope) {
-                return record;
-            }
-        }
-        return undefined;
-    }).filter((recordOrUndefined) => !isUndefined(recordOrUndefined)) as MutationRecord[];
+            return filteredSet;
+        }, []) as MutationRecord[];
 }
 
 function getWrappedCallback(callback: MutationCallback): MutationCallback {
@@ -140,7 +144,7 @@ function patchedDisconnect(this: MutationObserver): void {
 function patchedObserve(this: MutationObserver, target: Node, options?: MutationObserverInit): void {
     // If the observer was created by the patched constructor, this field should be defined. Adding a guard for extra safety
     if (!isUndefined(this[observedTargetsField])) {
-        this[observedTargetsField].push(target);
+        ArrayPush.call(this[observedTargetsField], target);
     }
     // If the target is a SyntheticShadowRoot, observer the host since the shadowRoot is an empty documentFragment
     if (target instanceof SyntheticShadowRoot) {
