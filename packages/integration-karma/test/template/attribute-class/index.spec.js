2f7514ae78b1419d47ce84e4deab4d3e2ab1343e
Pierre-Marie Dartus <p.dartus@salesforce.com>
2019-02-18 11:58:20 +0100

    test: migrate dynamic properties

---
+++ packages/integration-karma/test/template/attribute-class/index.spec.js
@@ -0,0 +1,50 @@
+import { createElement } from 'test-utils';
+
+import Dynamic from 'x/dynamic';
+
+fdescribe('dynamic class attribute', () => {
+    function createDynamicClass(className) {
+        const elm = createElement('x-dynamic', { is: Dynamic });
+        elm.dynamicClass = className;
+        document.body.appendChild(elm);
+
+        return {
+            host: elm,
+            target: elm.shadowRoot.querySelector('div')
+        };
+    }
+
+    it('simple', () => {
+        const { target } = createDynamicClass('foo');
+        expect(target.className).toBe('foo');
+    });
+
+    it('multiple classes', () => {
+        const { target } = createDynamicClass('foo bar');
+        expect(target.className).toBe('foo bar');
+    });
+
+    it('inconsistent spacing', () => {
+        const { target } = createDynamicClass('  foo   bar baz');
+        expect(target.className).toBe('foo bar baz');
+    });
+
+    it('partial replacement', () => {
+        const { host, target } = createDynamicClass('foo bar');
+
+        host.dynamicClass = 'bar baz';
+        return Promise.resolve().then(() => {
+            expect(target.className).toBe('bar baz');
+        });
+    });
+
+    it('full replacement', () => {
+        const { host, target } = createDynamicClass('foo bar');
+        expect(target.className).toBe('foo bar');
+
+        host.dynamicClass = 'baz buz';
+        return Promise.resolve().then(() => {
+            expect(target.className).toBe('baz buz');
+        });
+    });
+});
