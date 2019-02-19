cfd383842575bd61f7274d890a681b18279c23c7
Pierre-Marie Dartus <p.dartus@salesforce.com>
2019-02-19 10:29:40 +0100

    chore: remove inline eslint config in wire scripts

--- packages/@lwc/wire-service/scripts/rollup/rollup.config.umd.dev.js
+++ packages/@lwc/wire-service/scripts/rollup/rollup.config.umd.dev.js
@@ -4,8 +4,6 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-/* eslint-env node */
-
 const path = require('path');
 const rollupReplacePlugin = require('rollup-plugin-replace');
 const typescript = require('rollup-plugin-typescript');
--- packages/@lwc/wire-service/scripts/rollup/rollup.config.umd.prod.js
+++ packages/@lwc/wire-service/scripts/rollup/rollup.config.umd.prod.js
@@ -4,8 +4,6 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-/* eslint-env node */
-
 const path = require('path');
 const babel = require("@babel/core");
 const minify = require("babel-preset-minify");
