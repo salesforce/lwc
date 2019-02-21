556e6254c3cf8b7e75732e0ff6ebdd208bb8e07b
Pierre-Marie Dartus <p.dartus@salesforce.com>
2019-02-21 15:01:48 +0100

    test: fix IE11 getBoundingClientRect test failure

--- packages/integration-karma/test/component/LightningElement.getBoundingClientRect/index.spec.js
+++ packages/integration-karma/test/component/LightningElement.getBoundingClientRect/index.spec.js
@@ -3,14 +3,30 @@ import { createElement } from 'test-utils';
 import Test from 'x/test';
 import ConstructorInvocation from 'x/constructorInvocation';
 
-it('should throw when accessing during construction', () => {
-    expect(() => {
-        createElement('x-constructor-invocation', { is: ConstructorInvocation });
-    }).toThrowErrorDev(
-        Error,
-        /Assert Violation: this\.getBoundingClientRect\(\) should not be called during the construction of the custom element for <x-constructor-invocation> because the element is not yet in the DOM, instead, you can use it in one of the available life-cycle hooks\./
-    );
-});
+// IE11 doesn't support calling Element.getBoundingClientRect is the element is not attached to the DOM. We need to
+// disable the test specifically on IE11 because it would fail when running in prod compat mode.
+// https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/106812/
+const supportCallingGetBoundingClientRect = (() => {
+    const elm = document.createElement('div');
+
+    try {
+        elm.getBoundingClientRect();
+        return true;
+    } catch (error) {
+        return false;
+    }
+})();
+
+if (supportCallingGetBoundingClientRect) {
+    it('should throw when accessing during construction', () => {
+        expect(() => {
+            createElement('x-constructor-invocation', { is: ConstructorInvocation });
+        }).toThrowErrorDev(
+            Error,
+            /Assert Violation: this\.getBoundingClientRect\(\) should not be called during the construction of the custom element for <x-constructor-invocation> because the element is not yet in the DOM, instead, you can use it in one of the available life-cycle hooks\./
+        );
+    });
+}
 
 it('should return the host element dimensions', () => {
     const elm = createElement('x-test', { is: Test });
