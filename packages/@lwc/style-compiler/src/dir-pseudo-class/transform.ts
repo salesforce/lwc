c6081b6fccd75184392f2a65f990f577d127f141
Pierre-Marie Dartus <p.dartus@salesforce.com>
2019-03-07 16:41:54 +0100

    chore: add missing headers

--- packages/@lwc/style-compiler/src/dir-pseudo-class/transform.ts
+++ packages/@lwc/style-compiler/src/dir-pseudo-class/transform.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
 import { attribute, combinator, Root, Selector, isCombinator } from 'postcss-selector-parser';
 
 import isDirPseudoClass from '../utils/is-dir-pseudo-class';
--- packages/@lwc/style-compiler/src/utils/is-dir-pseudo-class.ts
+++ packages/@lwc/style-compiler/src/utils/is-dir-pseudo-class.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
 import { isPseudoClass, Node, Pseudo } from 'postcss-selector-parser';
 
 export default function isDir(node: Node): node is Pseudo {
