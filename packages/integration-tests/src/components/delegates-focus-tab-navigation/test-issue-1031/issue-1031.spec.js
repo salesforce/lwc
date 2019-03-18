d0f659bc28166737cf073ac08040219d3ce36c37
Eugene Kashida <ekashida@salesforce.com>
2019-03-17 23:51:25 -0700

    refactor(engine): prevent focus events when engine manages focus

--- packages/integration-tests/src/components/delegates-focus-tab-navigation/test-issue-1031/issue-1031.spec.js
+++ packages/integration-tests/src/components/delegates-focus-tab-navigation/test-issue-1031/issue-1031.spec.js
@@ -7,13 +7,13 @@
 const assert = require('assert');
 const URL = 'http://localhost:4567/issue-1031';
 
-// Enable after fixing W-5936969
-describe.skip('Tab navigation when tabindex -1', () => {
+describe('issue #1031', () => {
     before(() => {
         browser.url(URL);
     });
 
     it('should skip child shadow when tabbing after dynamically updating parent tabindex from 0 to -1', function() {
+        browser.click('.initialize'); // init tabindex to 0
         browser.click('.first-outside');
         browser.keys(['Tab']); // host element
         browser.keys(['Tab']); // second outside input
@@ -28,6 +28,7 @@ describe.skip('Tab navigation when tabindex -1', () => {
     });
 
     it('should skip child shadow when shift-tabbing after dynamically updating parent tabindex from 0 to -1', function() {
+        browser.click('.initialize'); // init tabindex to 0
         browser.click('.second-outside');
         browser.keys(['Shift', 'Tab', 'Shift']); // host element
         browser.keys(['Shift', 'Tab', 'Shift']); // first outside input
