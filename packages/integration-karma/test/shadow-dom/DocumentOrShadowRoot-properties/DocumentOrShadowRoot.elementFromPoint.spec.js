c5f7b4e7f321bbf2f9d13dbb3d4590731a52ab0d
Eugene Kashida <ekashida@salesforce.com>
2019-03-21 11:12:27 -0700

    chore: review feedback

--- packages/integration-karma/test/shadow-dom/DocumentOrShadowRoot-properties/DocumentOrShadowRoot.elementFromPoint.spec.js
+++ packages/integration-karma/test/shadow-dom/DocumentOrShadowRoot-properties/DocumentOrShadowRoot.elementFromPoint.spec.js
@@ -2,16 +2,17 @@ import { createElement } from 'test-utils';
 
 import ElementFromPoint from 'x/elementFromPoint';
 
-it('should return the external host element', () => {
-    const elm = createElement('x-element-from-point', { is: ElementFromPoint });
-    document.body.appendChild(elm);
+let elm;
 
+beforeAll(() => {
+    elm = createElement('x-element-from-point', { is: ElementFromPoint });
+    document.body.appendChild(elm);
+});
+
+it('should return the external host element', () => {
     expect(document.elementFromPoint(5, 5)).toBe(elm);
 });
 
 it('should return the internal div element', () => {
-    const elm = createElement('x-element-from-point', { is: ElementFromPoint });
-    document.body.appendChild(elm);
-
     expect(elm.shadowRoot.elementFromPoint(5, 5)).toBe(elm.shadowRoot.querySelector('div'));
 });
