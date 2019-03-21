f50f512babd07a4f34a5ea51f9f425fec855abbb
Eugene Kashida <ekashida@salesforce.com>
2019-03-20 21:33:11 -0700

    test: fix failing test

--- packages/integration-karma/test/shadow-dom/DocumentOrShadowRoot-properties/DocumentOrShadowRoot.elementFromPoint.spec.js
+++ packages/integration-karma/test/shadow-dom/DocumentOrShadowRoot-properties/DocumentOrShadowRoot.elementFromPoint.spec.js
@@ -6,20 +6,12 @@ it('should return the external host element', () => {
     const elm = createElement('x-element-from-point', { is: ElementFromPoint });
     document.body.appendChild(elm);
 
-    const { left, right, top, bottom } = elm.getBoundingClientRect();
-    const x = (left + right) / 2;
-    const y = (top + bottom) / 2;
-
-    expect(document.elementFromPoint(x, y)).toBe(elm);
+    expect(document.elementFromPoint(5, 5)).toBe(elm);
 });
 
 it('should return the internal div element', () => {
     const elm = createElement('x-element-from-point', { is: ElementFromPoint });
     document.body.appendChild(elm);
 
-    const { left, right, top, bottom } = elm.getBoundingClientRect();
-    const x = (left + right) / 2;
-    const y = (top + bottom) / 2;
-
-    expect(elm.shadowRoot.elementFromPoint(x, y)).toBe(elm.shadowRoot.querySelector('div'));
+    expect(elm.shadowRoot.elementFromPoint(5, 5)).toBe(elm.shadowRoot.querySelector('div'));
 });
