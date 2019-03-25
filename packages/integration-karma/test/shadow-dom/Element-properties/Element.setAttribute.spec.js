c591e9cd19bfbe170c611f463b5d58ef7c0a1077
Ravi <ravi.jayaramappa@salesforce.com>
2019-03-25 10:56:42 -0700

    test: migrate misc traverse tests there is no more traverse proxy, but the test cases are still useful

---
+++ packages/integration-karma/test/shadow-dom/Element-properties/Element.setAttribute.spec.js
@@ -0,0 +1,11 @@
+import { createElement } from 'test-utils';
+
+import TestWithDiv from 'x/testWithDiv';
+
+it('should allow setting attributes manually', () => {
+    const elm = createElement('x-foo', { is: TestWithDiv });
+    document.body.appendChild(elm);
+    const shadowRoot = elm.shadowRoot;
+    shadowRoot.querySelector('div').setAttribute('id', 'something');
+    expect(shadowRoot.querySelector('div').id).toBe('something');
+});
