031a0548329ad89aba1e529f2f2d00916ccf94ef
Pierre-Marie Dartus <p.dartus@salesforce.com>
2019-03-26 16:29:50 +0100

    chore: add scroll top to avoid test flakiness

--- packages/integration-karma/test/shadow-dom/DocumentOrShadowRoot-properties/DocumentOrShadowRoot.elementFromPoint.spec.js
+++ packages/integration-karma/test/shadow-dom/DocumentOrShadowRoot-properties/DocumentOrShadowRoot.elementFromPoint.spec.js
@@ -3,10 +3,12 @@ import { createElement } from 'test-utils';
 import ElementFromPoint from 'x/elementFromPoint';
 
 let elm;
-
 beforeAll(() => {
     elm = createElement('x-element-from-point', { is: ElementFromPoint });
     document.body.appendChild(elm);
+
+    // Scroll to page top to have the element in the viewport for elementFromPoint to run as expected.
+    document.documentElement.scrollTop = 0;
 });
 
 it('should return the external host element', () => {
