8e6756116f393ddb40473d8da071c2307e140fa3
Ravi <ravi.jayaramappa@salesforce.com>
2019-04-01 18:24:31 -0700

    test: moar tests to karma

--- packages/integration-karma/test/natives/misc/enumerable-properties.spec.js
+++ packages/integration-karma/test/natives/misc/enumerable-properties.spec.js
@@ -1,7 +1,5 @@
-import { LightningElement } from 'lwc';
-
-export default class EnumerablePropertyLeak extends LightningElement {
-    get objectEnumerableProperties() {
+describe('enumerable properties', () => {
+    it('should not leak any properties on Object', () => {
         const properties = [];
 
         const obj = { x: 'x', y: 'y' };
@@ -9,17 +7,16 @@ export default class EnumerablePropertyLeak extends LightningElement {
             properties.push(property);
         }
 
-        return properties;
-    }
+        expect(properties).toEqual(['x', 'y']);
+    });
 
-    get arrayEnumerableProperties() {
+    it('should not leak any properties on Array', () => {
         const properties = [];
 
         const array = ['x', 'y'];
         for (const property in array) {
             properties.push(property);
         }
-
-        return properties;
-    }
-}
+        expect(properties).toEqual(['0', '1']);
+    });
+});
