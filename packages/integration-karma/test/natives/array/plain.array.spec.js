7cd23ecd7d346d7f24a86d3eec58aab901a7c22f
Ravi <ravi.jayaramappa@salesforce.com>
2019-04-02 15:24:34 -0700

    test: more tests to karma

--- packages/integration-karma/test/natives/array/plain.array.spec.js
+++ packages/integration-karma/test/natives/array/plain.array.spec.js
@@ -1,42 +1,12 @@
-import { createElement } from 'test-utils';
-import XPlainArray from 'x/plainArray';
-
 describe('Plain array methods', () => {
-    let shadowRoot;
-    beforeAll(() => {
-        const elm = createElement('x-plain-array', { is: XPlainArray });
-        document.body.appendChild(elm);
-        shadowRoot = elm.shadowRoot;
+    it('should get expected items correctly after push', function() {
+        const array = [1, 2];
+        array.push(3, 4);
+        expect(array).toEqual([1, 2, 3, 4]);
     });
-
-    it('should display unshifted items correctly', function() {
-        const elements = shadowRoot.querySelectorAll('.push-list-plain li');
-        expect(elements[0].textContent).toBe('1');
-        expect(elements[1].textContent).toBe('2');
-        expect(elements[2].textContent).toBe('3');
-        expect(elements[3].textContent).toBe('4');
-    });
-
-    it('should display pushed items correctly', function() {
-        const elements = shadowRoot.querySelectorAll('.push-list li');
-        expect(elements[0].textContent).toBe('first');
-        expect(elements[1].textContent).toBe('second');
-        expect(elements[2].textContent).toBe('proxy');
-        expect(elements[3].textContent).toBe('fourth');
-    });
-
     it('should display concat items correctly', function() {
-        const elements = shadowRoot.querySelectorAll('.concat-list-plain li');
-        expect(elements[0].textContent).toBe('1');
-        expect(elements[1].textContent).toBe('2');
-        expect(elements[2].textContent).toBe('3');
-        expect(elements[3].textContent).toBe('4');
-    });
-
-    it('should display concat items correctly', function() {
-        const elements = shadowRoot.querySelectorAll('.concat-list-proxy li');
-        expect(elements[0].textContent).toBe('first');
-        expect(elements[1].textContent).toBe('second');
-        expect(elements[2].textContent).toBe('proxy');
+        let array = [1, 2];
+        array = array.concat([3, 4]);
+        expect(array).toEqual([1, 2, 3, 4]);
     });
 });
