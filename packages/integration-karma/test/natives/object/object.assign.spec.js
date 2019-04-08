07a1d4c90e12fb2118b577505423bb8a05a0506d
Ravi <ravi.jayaramappa@salesforce.com>
2019-04-08 11:32:18 -0700

    test: address pr feedback

--- packages/integration-karma/test/natives/object/object.assign.spec.js
+++ packages/integration-karma/test/natives/object/object.assign.spec.js
@@ -1,4 +1,4 @@
-describe('Issue 828 - Object assign', () => {
+describe('Object assign', () => {
     it('should return copy of object on using object.assign', function() {
         const assign = { inner: 'foo' };
         const obj = Object.assign({}, assign);
