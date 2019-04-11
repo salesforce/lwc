80e9fa8fc3752d812d25b6e89720a16711a417e5
Ravi <ravi.jayaramappa@salesforce.com>
2019-04-11 15:01:00 -0700

    test: remove redundant tests these tests have been ported over to karma in #1166

--- packages/integration-tests/src/components/lifecycle/test-error-boundary-suite/integration/childConnectedThrow/childConnectedThrow.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class ChildConnectedThrow extends LightningElement {
-    connectedCallback() {
-        throw new Error('Child threw in connectedCallback');
-    }
-}
