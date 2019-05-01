398e9d407df666ec41ed4ba3e71678f63a7204fa
Jose David Rodriguez Velasco <jodarove@gmail.com>
2019-05-01 16:55:06 -0700

    Revert "fix(compiler): enable strict mode (#1175)" (#1203)
    * Revert "fix(compiler): enable strict mode (#1175)"
    
    This partially reverts commit a4298242c1a8ec9266b809898a7ce92d43ff1aa5.
    
    cherrypicks 85688a55a79c930d0571b62b3fabc6aefa8396ed
    * chore: update peer dependencies to .38

--- packages/@lwc/compiler/src/__tests__/regression.spec.ts
+++ packages/@lwc/compiler/src/__tests__/regression.spec.ts
@@ -10,7 +10,7 @@ import { readFixture, pretify } from './utils';
 describe('regression test', () => {
     it('#743 - Object rest spread throwing', async () => {
         const actual = `const base = { foo: true }; const res = { ...base, bar: false };`;
-        const expected = `define('x/foo', function () { 'use strict';
+        const expected = `define('x/foo', function () {
             function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
         function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
