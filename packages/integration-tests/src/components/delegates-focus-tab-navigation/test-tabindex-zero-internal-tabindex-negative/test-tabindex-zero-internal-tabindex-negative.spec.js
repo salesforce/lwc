d5752b64ff11eaf5e9e66fb66bcb23af5142019c
Eugene Kashida <ekashida@gmail.com>
2019-05-08 07:54:25 +0900

    fix: prevent tabbing into subtrees with tabindex -1 (#1206)
    * test: adjust tests to reproduce issue
    
    * refactor: tabIndex setter comment
    
    * refactor: keyboard focus handling relative to current context
    
    * test: failing test for internal negative tabindex
    
    * fix: correct tabbing when negative tabindex in zero tabindex
    
    * chore: remove .only
    
    * chore: use getOwnerDocument instead of ownerDocumentGetter
    
    * chore: refactor test to use blocking selenium api

---
+++ packages/integration-tests/src/components/delegates-focus-tab-navigation/test-tabindex-zero-internal-tabindex-negative/test-tabindex-zero-internal-tabindex-negative.spec.js
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+const assert = require('assert');
+const URL = 'http://localhost:4567/tabindex-zero-internal-tabindex-negative';
+
+describe('Tab navigation when tabindex 0', () => {
+    beforeEach(() => {
+        browser.url(URL);
+    });
+
+    it('should skip internal elements contained by a negative tabindex subtree when delegating focus (forward)', function() {
+        const firstInput = browser.execute(function() {
+            return document
+                .querySelector('integration-tabindex-zero-internal-tabindex-negative')
+                .shadowRoot.querySelector('.first');
+        });
+        firstInput.click();
+
+        browser.keys(['Tab']);
+
+        var tagName = browser.execute(function() {
+            var container = document.activeElement;
+            var parent = container.shadowRoot.activeElement;
+            var input = parent.shadowRoot.activeElement;
+            return input.tagName;
+        }).value;
+
+        assert.equal(tagName, 'INPUT');
+    });
+
+    it('should skip internal elements contained by a negative tabindex subtree when delegating focus (backward)', function() {
+        const lastInput = browser.execute(function() {
+            return document
+                .querySelector('integration-tabindex-zero-internal-tabindex-negative')
+                .shadowRoot.querySelector('.last');
+        });
+        lastInput.click();
+
+        browser.keys(['Shift', 'Tab', 'Shift']);
+
+        var tagName = browser.execute(function() {
+            var container = document.activeElement;
+            var parent = container.shadowRoot.activeElement;
+            var input = parent.shadowRoot.activeElement;
+            return input.tagName;
+        }).value;
+
+        assert.equal(tagName, 'INPUT');
+    });
+});
