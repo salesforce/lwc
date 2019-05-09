ee640f129855375c21fc605f221cd328f56c3cc8
Jose David Rodriguez Velasco <jodarove@gmail.com>
2019-05-08 17:04:34 -0700

    test(integration-karma): migrate misc tests from integration-test (#1212)
    * test(integration-karma): migrate non-composed-events-on-custom-element
    
    * test(integration-karma): migrate events/slotted-native-element
    
    * test(integration-karma): migrate events/window-event-listiner
    
    * test(integration-karma): roolback migrate window-event-listener
    
    * test(integration-karma): migrate rendering/duplicate-text-rendering
    
    * test(integration-karma): migrate rendering/form-tag
    
    * test(integration-karma): migrate rendering/nested-state
    
    * test(integration-karma): migrate rendering/null-logging

---
+++ packages/integration-karma/test/events/slotted-event-target/index.spec.js
@@ -0,0 +1,39 @@
+import { createElement } from 'test-utils';
+import Container from 'x/container';
+
+describe('Event target in slot elements', () => {
+    it('should receive event with correct target in slotted custom element', function() {
+        const elm = createElement('x-container', { is: Container });
+        document.body.appendChild(elm);
+
+        const child = elm.shadowRoot.querySelector('x-child');
+        child.click();
+
+        return Promise.resolve().then(() => {
+            const elementWithResult = elm.shadowRoot
+                .querySelector('x-parent')
+                .shadowRoot.querySelector('.correct-event-target');
+
+            expect(elementWithResult).not.toBeNull();
+            expect(elementWithResult.innerText).toBe('Event target is correct');
+        });
+    });
+
+    it('should receive event with correct target for slotted native element', function() {
+        const elm = createElement('x-container', { is: Container });
+        elm.isNativeElement = true;
+        document.body.appendChild(elm);
+
+        const trigger = elm.shadowRoot.querySelector('p');
+        trigger.click();
+
+        return Promise.resolve().then(() => {
+            const elementWithResult = elm.shadowRoot
+                .querySelector('x-slotted-native-element')
+                .shadowRoot.querySelector('.correct-event-target');
+
+            expect(elementWithResult).not.toBeNull();
+            expect(elementWithResult.innerText).toBe('Event target is correct');
+        });
+    });
+});
