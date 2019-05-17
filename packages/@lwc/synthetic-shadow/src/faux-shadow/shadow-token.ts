9894c9326b28d39a0c6f04f2da2878a006b88ac2
Caridy Patiño <caridy@gmail.com>
2019-05-17 15:41:19 -0400

    refactor(sythectic-shadow): making shadowToken and domManual properties

---
+++ packages/@lwc/synthetic-shadow/src/faux-shadow/shadow-token.ts
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+import { isUndefined, defineProperty } from '../shared/language';
+import { setAttribute, removeAttribute } from '../env/element';
+
+const ShadowTokenPrivateKey = '$$ShadowTokenKey$$';
+
+/**
+ * Patching Element.prototype.$shadowToken$ to make elements a portal:
+ *
+ *  - we use a property to allow engines to set the a custom attribute that should be
+ *    placed into the element to sandbox the css rules defined for the template.
+ *
+ *  - this custom attribute must be unique.
+ *
+ **/
+defineProperty(Element.prototype, '$shadowToken$', {
+    set(this: Element, shadowToken: string | undefined) {
+        const oldShadowToken = this[ShadowTokenPrivateKey];
+        if (!isUndefined(oldShadowToken) && oldShadowToken !== shadowToken) {
+            removeAttribute.call(this, oldShadowToken);
+        }
+        if (!isUndefined(shadowToken)) {
+            setAttribute.call(this, shadowToken, '');
+        }
+        this[ShadowTokenPrivateKey] = shadowToken;
+    },
+    get(this: Element): string | undefined {
+        return this[ShadowTokenPrivateKey];
+    },
+});
