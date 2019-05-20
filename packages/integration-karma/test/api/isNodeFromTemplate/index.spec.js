7febfe4faa9dc343e1603f9e51cf97f259322372
Ravi <ravi.jayaramappa@salesforce.com>
2019-05-20 15:52:19 -0700

    test: tests for lwc:dom="manual" behavior

--- packages/integration-karma/test/api/isNodeFromTemplate/index.spec.js
+++ packages/integration-karma/test/api/isNodeFromTemplate/index.spec.js
@@ -41,12 +41,33 @@ it('should return true on elements rendered from the template', () => {
     expect(isNodeFromTemplate(div)).toBe(true);
 });
 
-it('should return true on elements manually inserted in the DOM inside a an element with lwc:dom=manual', () => {
+it('should return true on elements manually inserted in the DOM inside an element with lwc:dom="manual"', () => {
     const elm = createElement('x-test', { is: Test });
     document.body.appendChild(elm);
 
     const div = document.createElement('div');
     elm.shadowRoot.querySelector('div').appendChild(div);
 
-    expect(isNodeFromTemplate(div)).toBe(true);
+    return new Promise(resolve => {
+        setTimeout(resolve);
+    }).then(() => {
+        expect(isNodeFromTemplate(div)).toBe(true);
+    });
+});
+
+// TODO: cpatino: RRH relies on this behavior
+// if isNodeFromTemplate() returns true, locker will prevent traversing to such elements from document
+xit('should return false on elements manually inserted in the DOM inside an element NOT marked with lwc:dom="manual"', () => {
+    const elm = createElement('x-test', { is: Test });
+    document.body.appendChild(elm);
+    spyOn(console, 'error'); // ignore warning about manipulating node without lwc:dom="manual"
+
+    const span = document.createElement('span');
+    elm.shadowRoot.querySelector('h2').appendChild(span);
+
+    return new Promise(resolve => {
+        setTimeout(resolve);
+    }).then(() => {
+        expect(isNodeFromTemplate(span)).toBe(false);
+    });
 });
