a4483f270a316f16182b75c8371086d290c4656d
Diego Ferreiro Val <dval@salesforce.com>
2019-05-26 16:58:52 -0700

    feat: add synthetic runtime module

--- packages/@lwc/synthetic-shadow/scripts/rollup/rollup.config.es-and-cjs.js
+++ packages/@lwc/synthetic-shadow/scripts/rollup/rollup.config.es-and-cjs.js
@@ -7,6 +7,7 @@
 const typescript = require('typescript');
 const path = require('path');
 const rollupTypescript = require('rollup-plugin-typescript');
+const rollupCleanup = require('rollup-plugin-cleanup');
 const { version } = require('../../package.json');
 const { generateTargetName } = require('./util');
 
@@ -17,9 +18,16 @@ const modulesDirectory = path.resolve(__dirname, '../../dist/modules');
 const banner = `/* proxy-compat-disable */`;
 const footer = `/** version: ${version} */`;
 
-function rollupConfig(config) {
-    const { format, target } = config;
+function wrapModule() {
+    return {
+        renderChunk(code) {
+            return `${banner}\nexport default function enableSyntheticShadow() {\n${code}\n}`;
+        },
+    };
+}
 
+function rollupConfig(config) {
+    const { format, target, wrap } = config;
     const targetName = generateTargetName(config);
     const targetDirectory = (format === 'es' ? modulesDirectory : commonJSDirectory) + `/${target}`;
 
@@ -32,12 +40,19 @@ function rollupConfig(config) {
             banner,
             footer,
         },
-        plugins: [rollupTypescript({ target, typescript })],
+        plugins: [
+            wrap && wrapModule(),
+            rollupTypescript({ target, typescript }),
+            rollupCleanup({ comments: 'none', extensions: ['js', 'ts'], sourcemap: false }),
+        ].filter(Boolean),
     };
 }
 
 module.exports = [
-    rollupConfig({ format: 'es', target: 'es2017', test: true }),
+    // Wrap encloses de module into a function so it can be conditionally enabled at runtime
+    // This is very useful for testing.
+    rollupConfig({ format: 'es', target: 'es2017', wrap: true }),
+
     rollupConfig({ format: 'es', target: 'es2017' }),
     rollupConfig({ format: 'cjs', target: 'es2017' }),
     rollupConfig({ format: 'cjs', target: 'es5' }),
