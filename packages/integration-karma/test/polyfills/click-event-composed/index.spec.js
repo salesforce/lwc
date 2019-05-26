e51a43d358d4a8fdef7f61ccb8299d2b11ba582d
Eugene Kashida <ekashida@gmail.com>
2019-05-25 19:37:56 -0700

    chore: issue for todo in click-event-composed

--- packages/integration-karma/test/polyfills/click-event-composed/index.spec.js
+++ packages/integration-karma/test/polyfills/click-event-composed/index.spec.js
@@ -15,7 +15,7 @@ it('should set the composed property to true when invoking click() on an element
     elm.click();
 
     expect(clickEvent instanceof Event).toBe(true);
-    // TODO: #XXX - remove this condition again once Sauce Labs supports Safari version >= 12.0.0
+    // TODO: #1278 - remove this condition again once Sauce Labs supports Safari version >= 12.0.0
     if (!process.env.NATIVE_SHADOW) {
         expect(clickEvent.composed).toBe(true);
     }
@@ -24,7 +24,7 @@ it('should set the composed property to true when invoking click() on an element
 // The composed-event-click-polyfill doesn't work when native Shadow DOM is enabled on Safari 12.0.0 (it has been fixed
 // with Safari 12.0.1). The polyfill only patches the event javascript wrapper and doesn't have any effect on how Webkit
 // make the event bubbles.
-// TODO: #XXX - Enable this test again once Sauce Labs supports Safari version >= 12.0.0
+// TODO: #1278 - Enable this test again once Sauce Labs supports Safari version >= 12.0.0
 xit('should let the event bubble throws the shadow root when invoking click() on an element in the shadow', () => {
     let clickEvent;
 
