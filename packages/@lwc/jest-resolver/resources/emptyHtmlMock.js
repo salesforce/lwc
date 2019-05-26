6d1d761e80891935c9ddfe0a6b7fbda341f8fa88
Diego Ferreiro Val <dval@salesforce.com>
2019-05-26 11:10:10 -0700

    chore: moving test packages to another repo

--- packages/@lwc/jest-resolver/resources/emptyHtmlMock.js
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2018, salesforce.com, inc.
- * All rights reserved.
- * SPDX-License-Identifier: MIT
- * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
- */
-/* js file returned by lwc-jest-resolver. return falsey to ignore HTML */
-module.exports = undefined;
