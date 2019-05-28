6fc3b23ef433b61965086dd01baa353eab81c4ce
Diego Ferreiro Val <dval@salesforce.com>
2019-05-28 12:51:02 -0700

    fix: synthetic-shadow in Best (#1310)
    * fix: synthetic-shadow in Best
    
    * fix: add header

--- packages/perf-benchmarks/src/__benchmarks__/benchmark-table-component/tablecmp-append-1k.benchmark.js
+++ packages/perf-benchmarks/src/__benchmarks__/benchmark-table-component/tablecmp-append-1k.benchmark.js
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-import '@lwc/synthetic-shadow';
+
 import { createElement } from 'lwc';
 import Table from 'benchmark/tableComponent';
 
