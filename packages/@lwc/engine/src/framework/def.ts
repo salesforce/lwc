4f9bee1fd79328dfa04674746619038c1eff9c26
Eugene Kashida <ekashida@gmail.com>
2019-05-27 20:32:50 -0700

    chore: link issues to todos in def.ts

--- packages/@lwc/engine/src/framework/def.ts
+++ packages/@lwc/engine/src/framework/def.ts
@@ -103,8 +103,7 @@ function createComponentDef(
 
     let decoratorsMeta = getDecoratorsRegisteredMeta(Ctor);
 
-    // TODO: #XXX - eventually, the compiler should do this call directly, but we will also
-    // have to fix all our tests, which are using this declaration manually.
+    // TODO: #1295 - refactor tests that are using this declaration manually
     if (isUndefined(decoratorsMeta)) {
         registerDecorators(Ctor, {
             publicMethods: getOwnValue(Ctor, 'publicMethods'),
@@ -224,9 +223,7 @@ export function getComponentDef(Ctor: any, subclassComponentName?: string): Comp
 
         let meta = getComponentRegisteredMeta(Ctor);
         if (isUndefined(meta)) {
-            // TODO: #XXX - remove this workaround:
-            // this is temporary until
-            // all tests are updated to call registerComponent:
+            // TODO: #1295 - remove this workaround after refactoring tests
             meta = {
                 template: undefined,
                 name: Ctor.name,
