a14b2d5c3408df88ea138ccbe13a762aa57010e7
Eugene Kashida <ekashida@gmail.com>
2019-05-27 19:59:03 -0700

    chore: link issue to todo readonly.ts

--- packages/@lwc/engine/src/framework/decorators/readonly.ts
+++ packages/@lwc/engine/src/framework/decorators/readonly.ts
@@ -10,7 +10,7 @@ import { reactiveMembrane } from '../membrane';
 // when used with exactly one argument, we assume it is a function invocation.
 export default function readonly(obj: any): any {
     if (process.env.NODE_ENV !== 'production') {
-        // TODO: #XXX - enable the usage of this function as @readonly decorator
+        // TODO: #1292 - Remove the readonly decorator
         if (arguments.length !== 1) {
             assert.fail(
                 '@readonly cannot be used as a decorator just yet, use it as a function with one argument to produce a readonly version of the provided value.'
