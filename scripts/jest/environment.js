d2d797e4b75ca3cb5f92639ced4277ca78a814f0
Ravi Jayaramappa <ravi.jayaramappa@salesforce.com>
2019-05-28 14:50:41 -0700

    fix: cache patched types and propagate for each test suite

--- scripts/jest/environment.js
+++ scripts/jest/environment.js
@@ -12,6 +12,8 @@ const polyfillFile = require.resolve('@lwc/synthetic-shadow');
 const polyfillSource = fs.readFileSync(polyfillFile, 'utf-8');
 const polyfillScript = vm.createScript(polyfillSource, '@lwc/synthetic-shadow');
 
+let ShadowRootPatched;
+let HTMLSlotElementPatched;
 /** Synthetic Shadow Polyfill should be installed only once per worker */
 function _syntheticShadow(env) {
     if (env.dom) {
@@ -20,6 +22,9 @@ function _syntheticShadow(env) {
         _syntheticShadow = function _syntheticShadow() {
             /* noop */
         };
+        // Cache the patched types and reuse
+        ShadowRootPatched = env.dom.window.ShadowRoot;
+        HTMLSlotElementPatched = env.dom.window.HTMLSlotElement;
     }
 }
 
@@ -29,9 +34,12 @@ module.exports = class JSDOMEnvironmentWithSyntheticShadow extends JSDOMEnvironm
 
         // Running the synthetic-shadow polyfill
         _syntheticShadow(this);
-
         // Instrinsics that should be transferred to each environment
-        this.global.ShadowRoot = this.dom.window.ShadowRoot;
-        this.global.HTMLSlotElement = this.dom.window.HTMLSlotElement;
+        // Reuse the cached types since _syntheticShadow() becomes a noop after the first run
+        // JSDOM reuses prototypes across the workers, but recreats the global(window obj) for each test suite
+        // Hence, _syntheticShadow() is run once to patch all the prototypes and becomes a noop after.
+        // So the follow block will propagated the patched types to the global object
+        this.global.ShadowRoot = ShadowRootPatched;
+        this.global.HTMLSlotElement = HTMLSlotElementPatched;
     }
 };
