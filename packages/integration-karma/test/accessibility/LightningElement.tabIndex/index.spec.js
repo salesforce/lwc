e654f59e574dc85370a209d57102f47f7a38900b
Eugene Kashida <ekashida@gmail.com>
2019-06-01 01:55:32 -0700

    chore: disable tabIndex test

--- packages/integration-karma/test/accessibility/LightningElement.tabIndex/index.spec.js
+++ packages/integration-karma/test/accessibility/LightningElement.tabIndex/index.spec.js
@@ -3,10 +3,11 @@ import { createElement } from 'lwc';
 import DelegatesFocusTrue from 'x/delegatesFocusTrue';
 import DelegatesFocusFalse from 'x/delegatesFocusFalse';
 
-it('should have a default tabIndex value of 0 when delegatesFocus=true', () => {
+// TODO: #1330 - Enable this test after Chrome fixes this bug
+xit('should have a default tabIndex value of -1 when delegatesFocus=true', () => {
     const elm = createElement('x-tabindex', { is: DelegatesFocusTrue });
     document.body.appendChild(elm);
-    expect(elm.tabIndex).toBe(0);
+    expect(elm.tabIndex).toBe(-1);
 });
 
 it('should have a default tabIndex value of -1 when delegatesFocus=false', () => {
