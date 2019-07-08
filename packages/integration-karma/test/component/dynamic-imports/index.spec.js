30c0027df29a8d4231d8662967a762c2c7677f68
Caridy Patiño <caridy@gmail.com>
2019-07-07 22:37:33 -0400

    fix(engine): fixing tests for PR #1397

--- packages/integration-karma/test/component/dynamic-imports/index.spec.js
+++ packages/integration-karma/test/component/dynamic-imports/index.spec.js
@@ -1,21 +1,99 @@
 import { createElement } from 'lwc';
 import Container from 'x/dynamic';
 import DynamicCtor from 'x/ctor';
+import AlterCtor from 'x/alter';
 import { registerForLoad, clearRegister } from 'test-utils';
 
 beforeEach(() => {
     clearRegister();
 });
 
+function waitTwoMicroTasks(fn) {
+    return Promise.resolve().then(() => {
+        return Promise.resolve().then(() => {
+            return fn();
+        });
+    });
+}
+
 it('should call the loader', () => {
+    // note, using `x-` prefix instead of `x/` because these are
+    // handled by `registerForLoad`
     registerForLoad('x-ctor', DynamicCtor);
+    registerForLoad('x-alter', AlterCtor);
 
     const elm = createElement('x-dynamic', { is: Container });
     document.body.appendChild(elm);
 
     return Promise.resolve().then(() => {
-        const span = elm.shadowRoot.querySelector('span');
-        expect(span).not.toBeNull();
-        expect(span.textContent).toBe('ctor_html');
+        const child = elm.shadowRoot.querySelector('x-ctor');
+        expect(child).toBeNull();
+        // first rendered with ctor set to undefined (nothing)
+        elm.enableCtor();
+        return waitTwoMicroTasks(() => {
+            // second rendered with ctor set to x-ctor
+            const ctorElm = elm.shadowRoot.querySelector('x-ctor');
+            expect(ctorElm).not.toBeNull();
+            const span = ctorElm.shadowRoot.querySelector('span');
+            expect(span).not.toBeNull();
+            expect(span.textContent).toBe('ctor_html');
+            elm.enableAlter();
+            return waitTwoMicroTasks(() => {
+                // third rendered with ctor set to x-alter
+                const alterElm = elm.shadowRoot.querySelector('x-ctor');
+                expect(alterElm).not.toBeNull();
+                const span = alterElm.shadowRoot.querySelector('span');
+                expect(span).not.toBeNull();
+                expect(span.textContent).toBe('alter_html');
+                elm.disableAll();
+                return waitTwoMicroTasks(() => {
+                    // third rendered with ctor set to null (nothing)
+                    const child = elm.shadowRoot.querySelector('x-ctor');
+                    expect(child).toBeNull();
+                });
+            });
+        });
+    });
+});
+
+it('should not reuse DOM elements', () => {
+    registerForLoad('x-ctor', DynamicCtor);
+    registerForLoad('x-alter', AlterCtor);
+
+    const elm = createElement('x-dynamic', { is: Container });
+    elm.enableCtor();
+    document.body.appendChild(elm);
+
+    return waitTwoMicroTasks(() => {
+        const childElm = elm.shadowRoot.querySelector('x-ctor');
+        expect(childElm).not.toBeNull();
+        elm.enableAlter();
+        return waitTwoMicroTasks(() => {
+            const alterElm = elm.shadowRoot.querySelector('x-ctor');
+            expect(alterElm).not.toBe(childElm);
+        });
+    });
+});
+
+it('should not cache DOM elements', () => {
+    registerForLoad('x-ctor', DynamicCtor);
+    registerForLoad('x-alter', AlterCtor);
+
+    const elm = createElement('x-dynamic', { is: Container });
+    elm.enableCtor();
+    document.body.appendChild(elm);
+
+    // from ctor to alter back to ctor, new elements should be created
+    return waitTwoMicroTasks(() => {
+        const childElm = elm.shadowRoot.querySelector('x-ctor');
+        expect(childElm).not.toBeNull();
+        elm.enableAlter();
+        return waitTwoMicroTasks(() => {
+            elm.enableCtor();
+            return waitTwoMicroTasks(() => {
+                const secondCtorElm = elm.shadowRoot.querySelector('x-ctor');
+                expect(secondCtorElm).not.toBe(childElm);
+            });
+        });
     });
 });
