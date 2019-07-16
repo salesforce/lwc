b68edf52c5390537f056f16e11f3cfe8d9111df5
Jose David Rodriguez Velasco <jodarove@gmail.com>
2019-07-16 09:58:26 -0700

    fix: simplify test

--- packages/integration-karma/test/rendering/disconnecting-root-vm/index.spec.js
+++ packages/integration-karma/test/rendering/disconnecting-root-vm/index.spec.js
@@ -2,33 +2,19 @@ import { createElement } from 'lwc';
 import ParentCmp from 'x/parent';
 
 describe('disconnecting root vm', () => {
-    // unhandledrejection is not supported in ie11 or firefox. In those browsers we will get false positives
-    // but we will get the failure in chrome and safari.
-    // We dont use the error handler because karma does not triggers it.
-    if (!process.env.COMPAT) {
-        it('should not throw an error when disconnecting an already disconnected child vm', function(done) {
-            let unhandledErrorEvent = null;
-            const unhandledErrorListener = event => {
-                event.preventDefault();
-                unhandledErrorEvent = event;
-            };
+    it('should not throw an error when disconnecting an already disconnected child vm', function(done) {
+        const elm = createElement('x-parent', { is: ParentCmp });
+        elm.labels = ['label 1', 'label 2'];
+        document.body.appendChild(elm);
 
-            window.addEventListener('unhandledrejection', unhandledErrorListener);
+        return Promise.resolve().then(() => {
+            elm.labels = [];
+            document.body.removeChild(elm);
 
-            const elm = createElement('x-parent', { is: ParentCmp });
-            elm.labels = ['label 1', 'label 2'];
-            document.body.appendChild(elm);
-
-            return Promise.resolve().then(() => {
-                elm.labels = [];
-                document.body.removeChild(elm);
-
-                setTimeout(() => {
-                    expect(unhandledErrorEvent).toBe(null);
-                    window.removeEventListener('unhandledrejection', unhandledErrorListener);
-                    done();
-                });
+            setTimeout(() => {
+                expect(elm.getError()).toBe(null);
+                done();
             });
         });
-    }
+    });
 });
