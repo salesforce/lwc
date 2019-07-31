9facfaf71278b58b38773073a9adcdbcc0cda451
Ravi Jayaramappa <ravi.jayaramappa@salesforce.com>
2019-07-31 14:30:26 -0700

    fix: use non-cached Node.prototype methods in synthetic-shadow

--- packages/@lwc/synthetic-shadow/src/faux-shadow/shadow-root.ts
+++ packages/@lwc/synthetic-shadow/src/faux-shadow/shadow-root.ts
@@ -33,10 +33,6 @@ import {
     parentElementGetter,
     textContextSetter,
     isConnected,
-    removeChild,
-    insertBefore,
-    replaceChild,
-    appendChild,
 } from '../env/node';
 import { createStaticHTMLCollection } from '../shared/static-html-collection';
 import { getOuterHTML } from '../3rdparty/polymer/outer-html';
@@ -267,7 +263,7 @@ const NodePatchDescriptors = {
             newChild: T,
             refChild: Node | null
         ): T {
-            insertBefore.call(getHost(this), newChild, refChild);
+            Node.prototype.insertBefore.call(getHost(this), newChild, refChild);
             return newChild;
         },
     },
@@ -276,7 +272,7 @@ const NodePatchDescriptors = {
         enumerable: true,
         configurable: true,
         value<T extends Node>(this: SyntheticShadowRootInterface, oldChild: T): T {
-            removeChild.call(getHost(this), oldChild);
+            Node.prototype.removeChild.call(getHost(this), oldChild);
             return oldChild;
         },
     },
@@ -285,7 +281,7 @@ const NodePatchDescriptors = {
         enumerable: true,
         configurable: true,
         value<T extends Node>(this: SyntheticShadowRootInterface, newChild: T): T {
-            appendChild.call(getHost(this), newChild);
+            Node.prototype.appendChild.call(getHost(this), newChild);
             return newChild;
         },
     },
@@ -294,7 +290,7 @@ const NodePatchDescriptors = {
         enumerable: true,
         configurable: true,
         value<T extends Node>(this: SyntheticShadowRootInterface, newChild: Node, oldChild: T): T {
-            replaceChild.call(getHost(this), newChild, oldChild);
+            Node.prototype.replaceChild.call(getHost(this), newChild, oldChild);
             return oldChild;
         },
     },
