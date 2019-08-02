a6503b2e001bd1d87542897de4d2394d371ba7e3
Eugene Kashida <ekashida@gmail.com>
2019-08-01 20:01:49 -0700

    fix: account for clicks on form element labels when delegating focus
    * test: reproduce the issue
    
    * fix: account for clicks on form element labels
    
    * chore: review feedback
    
    (cherry picked from commit d5e4be376cbf8f8673bc164fe7bd8ac62e546ab2)

---
+++ packages/integration-tests/src/components/accessibility/test-delegates-focus-non-focusable-click-target/delegates-focus-non-focusable-click-target.spec.js
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+const assert = require('assert');
+
+describe('focus delegation when clicking on form element label', () => {
+    const URL = 'http://localhost:4567/delegates-focus-non-focusable-click-target';
+
+    beforeEach(() => {
+        browser.url(URL);
+    });
+
+    it('should apply focus to element associated with label when relatedTarget is null', function() {
+        const label = browser.execute(function() {
+            return document
+                .querySelector('integration-delegates-focus-non-focusable-click-target')
+                .shadowRoot.querySelector('integration-input')
+                .shadowRoot.querySelector('label');
+        });
+        label.click();
+
+        const inputClassName = browser.execute(function() {
+            const container = document.activeElement;
+            const integrationInput = container.shadowRoot.activeElement;
+            const inputClassName = integrationInput.shadowRoot.activeElement.className;
+            return inputClassName;
+        }).value;
+
+        assert.strictEqual(inputClassName, 'internal');
+    });
+
+    it('should apply focus to element associated with label when relatedTarget is non-null', function() {
+        const headInput = browser.execute(function() {
+            return document
+                .querySelector('integration-delegates-focus-non-focusable-click-target')
+                .shadowRoot.querySelector('.head');
+        });
+        // Focus on input so that relatedTarget will be non-null
+        headInput.click();
+
+        const label = browser.execute(function() {
+            return document
+                .querySelector('integration-delegates-focus-non-focusable-click-target')
+                .shadowRoot.querySelector('integration-input')
+                .shadowRoot.querySelector('label');
+        });
+        label.click();
+
+        const inputClassName = browser.execute(function() {
+            const container = document.activeElement;
+            const integrationInput = container.shadowRoot.activeElement;
+            const inputClassName = integrationInput.shadowRoot.activeElement.className;
+            return inputClassName;
+        }).value;
+
+        assert.strictEqual(inputClassName, 'internal');
+    });
+});
---
+++ packages/integration-tests/src/components/accessibility/test-delegates-focus-non-focusable-click-target/integration/input/input.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+
+export default class FancyInput extends LightningElement {
+    static delegatesFocus = true;
+}
