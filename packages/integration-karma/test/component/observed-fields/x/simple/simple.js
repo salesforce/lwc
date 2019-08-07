2174a2b293fcc1e27b921a0fdc0854c55e9c8d82
Jose David Rodriguez Velasco <jodarove@gmail.com>
2019-08-07 12:10:53 -0700

    feat: tests for observed fields

---
+++ packages/integration-karma/test/component/observed-fields/x/simple/simple.js
@@ -0,0 +1,32 @@
+import { LightningElement, api, track } from 'lwc';
+
+class BaseKlass extends LightningElement {
+    inheritedValue = 'initial';
+}
+
+export default class Simple extends BaseKlass {
+    @track trackedField;
+    simpleValue = 'initial';
+    complexValue = {
+        name: 'foo',
+        lastName: 'bar',
+    };
+
+    constructor() {
+        super();
+        this.expandoField = 'initial';
+    }
+
+    @api setValue(field, value) {
+        this[field] = value;
+    }
+
+    @api getValue(field) {
+        return this[field];
+    }
+
+    @api mutateComplexValue() {
+        this.complexValue.name = 'mutated name';
+        this.complexValue.lastName = 'mutated lastName';
+    }
+}
