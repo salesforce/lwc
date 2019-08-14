d9cc8f1810bfbab75f6050923239a6f48b3e27c4
Eugene Kashida <ekashida@gmail.com>
2019-08-14 15:29:47 -0700

    test: add test cases for invoking focus on host element

---
+++ packages/integration-tests/src/components/accessibility/test-delegates-focus-focus-method-on-host-element/delegates-focus-focus-method-on-host-element.spec.js
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+const assert = require('assert');
+
+const URL = 'http://localhost:4567/delegates-focus-focus-method-on-host-element';
+
+describe('Invoking the focus method of a host element', () => {
+    beforeEach(() => {
+        browser.url(URL);
+    });
+
+    it('should apply focus to the host element (tabindex -1)', function() {
+        // Click the top input to give the focus event's relatedTarget a
+        // non-null value so that we enter the code path that we want to test.
+        browser
+            .execute(function() {
+                return document
+                    .querySelector('integration-delegates-focus-focus-method-on-host-element')
+                    .shadowRoot.querySelector('input');
+            })
+            .click();
+
+        browser.execute(function() {
+            document
+                .querySelector('integration-delegates-focus-focus-method-on-host-element')
+                .shadowRoot.querySelector('integration-button.negative')
+                .focus();
+        });
+
+        const className = browser.execute(function() {
+            var container = document.querySelector(
+                'integration-delegates-focus-focus-method-on-host-element'
+            );
+            var activeElement = container.shadowRoot.activeElement;
+            return activeElement.className;
+        }).value;
+
+        assert.equal(className, 'negative');
+    });
+
+    it('should apply focus to the host element (tabindex 0)', function() {
+        // Click the top input to give the focus event's relatedTarget a
+        // non-null value so that we enter the code path that we want to test.
+        browser
+            .execute(function() {
+                return document
+                    .querySelector('integration-delegates-focus-focus-method-on-host-element')
+                    .shadowRoot.querySelector('input');
+            })
+            .click();
+
+        browser.execute(function() {
+            document
+                .querySelector('integration-delegates-focus-focus-method-on-host-element')
+                .shadowRoot.querySelector('integration-button.zero')
+                .focus();
+        });
+
+        const className = browser.execute(function() {
+            var container = document.querySelector(
+                'integration-delegates-focus-focus-method-on-host-element'
+            );
+            var activeElement = container.shadowRoot.querySelector('integration-button.zero');
+            return activeElement.className;
+        }).value;
+
+        assert.equal(className, 'zero');
+    });
+
+    it('should apply focus to the host element (no tabindex)', function() {
+        // Click the top input to give the focus event's relatedTarget a
+        // non-null value so that we enter the code path that we want to test.
+        browser
+            .execute(function() {
+                return document
+                    .querySelector('integration-delegates-focus-focus-method-on-host-element')
+                    .shadowRoot.querySelector('input');
+            })
+            .click();
+
+        browser.execute(function() {
+            document
+                .querySelector('integration-delegates-focus-focus-method-on-host-element')
+                .shadowRoot.querySelector('integration-button.none')
+                .focus();
+        });
+
+        const className = browser.execute(function() {
+            var container = document.querySelector(
+                'integration-delegates-focus-focus-method-on-host-element'
+            );
+            var activeElement = container.shadowRoot.querySelector('integration-button.none');
+            return activeElement.className;
+        }).value;
+
+        assert.equal(className, 'none');
+    });
+});
