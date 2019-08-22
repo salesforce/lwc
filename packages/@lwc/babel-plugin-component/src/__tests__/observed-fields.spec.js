81a6e484a0808b9b171dd9df733df248d25f57ab
Jose David Rodriguez Velasco <jodarove@gmail.com>
2019-08-21 21:34:49 -0700

    feat: add wire config as function (#1455)
    * feat: add wire config as function
    
    * fix: replace multiple conditionals with logical expression
    
    * fix: failing tests
    
    * fix: perf improvement based on review
    
    * fix: prettier
    
    * fix: remove unnecessary assigment in config function
    
    * fix: lint
    
    * fix: allow return null value in config props
    
    * wip: extract arg name (host) as constant
    
    * wip: rename wire config argument to $cmp
    
    - use jasmine.addMatchers in beforeAll instead beforeEach
    - mix 2 regex in 1

--- packages/@lwc/babel-plugin-component/src/__tests__/observed-fields.spec.js
+++ packages/@lwc/babel-plugin-component/src/__tests__/observed-fields.spec.js
@@ -60,7 +60,10 @@ describe('observed fields', () => {
                   publicMethods: ["someMethod"],
                   wire: {
                     wiredProp: {
-                      adapter: createElement
+                      adapter: createElement,
+                      config: function($cmp) {
+                        return {};
+                      }
                     }
                   },
                   track: {
@@ -171,7 +174,10 @@ describe('observed fields', () => {
                     publicMethods: ["someMethod"],
                     wire: {
                       wiredProp: {
-                        adapter: createElement
+                        adapter: createElement,
+                        config: function($cmp) {
+                          return {};
+                        }
                       }
                     },
                     track: {
