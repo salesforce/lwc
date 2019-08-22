06ef81e6bd58495e1f706ad537ecd0ee2c5482b5
Eugene Kashida <ekashida@salesforce.com>
2019-08-21 20:15:34 -0700

    chore: use local babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    presets: [
-        "@babel/preset-typescript"
-    ],
-    plugins: [
-        ["@babel/plugin-proposal-class-properties", { "loose": true }],
-        "@babel/transform-modules-commonjs",
-    ]
-}
+    babelrcRoots: ['./packages/*'],
+};
