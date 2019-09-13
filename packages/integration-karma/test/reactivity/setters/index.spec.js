1724c49cf6cf550da126c5f54e848ecc716bc4e7
Eugene Kashida <ekashida@salesforce.com>
2019-09-12 20:28:57 -0700

    test: enable reactive setter flag

--- packages/integration-karma/test/reactivity/setters/index.spec.js
+++ packages/integration-karma/test/reactivity/setters/index.spec.js
@@ -1,8 +1,10 @@
-import { createElement } from 'test-utils';
+import { createElement, setFeatureFlagForTest } from 'lwc';
 
 import Parent from 'x/parent';
 
 describe('Reactivity for setters', () => {
+    setFeatureFlagForTest('ENABLE_REACTIVE_SETTER', true);
+
     it('should not alter rendering if they are not invoked', () => {
         const elm = createElement('x-test', { is: Parent });
         document.body.appendChild(elm);
