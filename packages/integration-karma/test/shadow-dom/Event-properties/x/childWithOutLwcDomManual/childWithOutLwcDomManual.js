4a8007adb32a6d5a97e4490f967b456095a7c969
Ravi Jayaramappa <ravi.jayaramappa@salesforce.com>
2019-09-16 18:26:52 -0700

    test: update tests

--- packages/integration-karma/test/shadow-dom/Event-properties/x/childWithOutLwcDomManual/childWithOutLwcDomManual.js
+++ packages/integration-karma/test/shadow-dom/Event-properties/x/childWithOutLwcDomManual/childWithOutLwcDomManual.js
@@ -2,7 +2,7 @@ import { LightningElement } from 'lwc';
 
 export default class Child extends LightningElement {
     renderedCallback() {
-        const portal = this.template.querySelector('div');
-        portal.appendChild(document.createElement('span'));
+        const container = this.template.querySelector('div');
+        container.appendChild(document.createElement('span'));
     }
 }
--- packages/integration-karma/test/shadow-dom/Event-properties/x/parentWithDynamicChild/parentWithDynamicChild.js
+++ packages/integration-karma/test/shadow-dom/Event-properties/x/parentWithDynamicChild/parentWithDynamicChild.js
@@ -1,6 +1,6 @@
 import { LightningElement, api } from 'lwc';
 
-export default class ParentWithDomManualChild extends LightningElement {
+export default class ParentWithDynamicChild extends LightningElement {
     @api
     eventListener;
 
