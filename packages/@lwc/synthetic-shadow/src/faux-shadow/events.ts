67276e8792f8c0e436efb0b4b74035f8ba5094bd
Ravi Jayaramappa <ravi.jayaramappa@salesforce.com>
2019-09-16 18:40:27 -0700

    fix: add todo

--- packages/@lwc/synthetic-shadow/src/faux-shadow/events.ts
+++ packages/@lwc/synthetic-shadow/src/faux-shadow/events.ts
@@ -67,6 +67,7 @@ function targetGetter(this: Event): EventTarget | null {
     // Handle cases where the currentTarget is null (for async events),
     // and when an event has been added to Window
     if (!(originalCurrentTarget instanceof Node)) {
+        // TODO: issue #1511 - Special escape hatch to support legacy behavior. Should be fixed.
         // If the event's target is being accessed async and originalTarget is not a keyed element, do not retarget
         if (isNull(originalCurrentTarget) && isUndefined(getNodeOwnerKey(originalTarget as Node))) {
             return originalTarget;
