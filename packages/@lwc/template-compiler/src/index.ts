80015f1cb1b93dfa89a3fff23dbb14dcc0ff4874
Eugene Kashida <ekashida@salesforce.com>
2019-09-27 14:38:45 +0900

    Revert "feat(template-compiler): binding ast parser (#1498)"
    This reverts commit 1d77e5ec8e1423513129a26a3841cbcab4c7bc9d.

--- packages/@lwc/template-compiler/src/index.ts
+++ packages/@lwc/template-compiler/src/index.ts
@@ -23,19 +23,13 @@ import generate from './codegen';
 import { TemplateCompileResult, TemplateParseResult } from './shared/types';
 import { TEMPLATE_MODULES_PARAMETER } from './shared/constants';
 
-import bindingTransform, { BindingParseResult } from './transform/binding';
-
 export { IRElement } from './shared/types';
 export { Config } from './config';
 
-export function parse(source: string, config?: Config): BindingParseResult | TemplateParseResult {
+export function parse(source: string, config?: Config): TemplateParseResult {
     const options = mergeConfig(config || {});
     const state = new State(source, options);
-    const parsed = parseTemplate(source, state);
-    if (options.experimentalDataBindingAST) {
-        return bindingTransform(parsed);
-    }
-    return parsed;
+    return parseTemplate(source, state);
 }
 
 export default function compile(source: string, config: Config): TemplateCompileResult {
