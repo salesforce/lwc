94dc583b1083dca85eba261859ad5f7ef7e09c3d
Eugene Kashida <ekashida@salesforce.com>
2019-10-02 21:51:12 +0900

    feat(features): validation to prevent usage of undefined flags

--- packages/@lwc/features/src/runtime.ts
+++ packages/@lwc/features/src/runtime.ts
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-import { FeatureFlagLookup, FeatureFlagValue } from './flags';
-import { assert, create, isFalse, isTrue } from '@lwc/shared';
+import features, { FeatureFlagLookup, FeatureFlagValue } from './flags';
+import { assert, create, isFalse, isTrue, isUndefined } from '@lwc/shared';
 
 const runtimeFlags: FeatureFlagLookup = create(null);
 
@@ -14,8 +14,13 @@ const runtimeFlags: FeatureFlagLookup = create(null);
 function setFeatureFlag(name: string, value: FeatureFlagValue) {
     assert.invariant(
         isTrue(value) || isFalse(value),
-        `Runtime feature flags must be set to a boolean value but received a(n) ${typeof value} value for the '${name}' flag.`
+        `Invalid ${typeof value} value specified for the "${name}" flag. Runtime feature flags can only be set to a boolean value.`
     );
+    if (isUndefined(features[name])) {
+        console.warn(
+            `LWC feature flag "${name}" is undefined. Possible reasons are that 1) it was misspelled or 2) it was removed from the @lwc/features package.`
+        );
+    }
     runtimeFlags[name] = value;
 }
 
@@ -23,6 +28,11 @@ function setFeatureFlag(name: string, value: FeatureFlagValue) {
 // add a check to make sure it is not invoked in production.
 function setFeatureFlagForTest(name: string, value: FeatureFlagValue) {
     if (process.env.NODE_ENV !== 'production') {
+        if (isUndefined(features[name])) {
+            throw new Error(
+                `Feature flag "${name}" is undefined. Possible reasons are that 1) it was misspelled or 2) it was removed from the @lwc/features package.`
+            );
+        }
         return setFeatureFlag(name, value);
     }
 }
