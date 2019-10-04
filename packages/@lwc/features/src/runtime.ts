47e5f517f0576395e38868fb12293eab7c4d8b0f
Eugene Kashida <ekashida@salesforce.com>
2019-10-05 05:46:43 +0900

    chore: relax flag validation to not throw in production

--- packages/@lwc/features/src/runtime.ts
+++ packages/@lwc/features/src/runtime.ts
@@ -5,17 +5,22 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
 import features, { FeatureFlagLookup, FeatureFlagValue } from './flags';
-import { assert, create, isFalse, isTrue, isUndefined } from '@lwc/shared';
+import { create, isFalse, isTrue, isUndefined } from '@lwc/shared';
 
 const runtimeFlags: FeatureFlagLookup = create(null);
 
 // This function is not whitelisted for use within components and is meant for
 // configuring runtime feature flags during app initialization.
 function setFeatureFlag(name: string, value: FeatureFlagValue) {
-    assert.invariant(
-        isTrue(value) || isFalse(value),
-        `Invalid ${typeof value} value specified for the "${name}" flag. Runtime feature flags can only be set to a boolean value.`
-    );
+    const isBoolean = isTrue(value) || isFalse(value);
+    if (!isBoolean) {
+        const message = `Invalid ${typeof value} value specified for the "${name}" flag. Runtime feature flags can only be set to a boolean value.`;
+        if (process.env.NODE_ENV === 'production') {
+            console.error(message);
+        } else {
+            throw new TypeError(message);
+        }
+    }
     if (isUndefined(features[name])) {
         console.warn(
             `LWC feature flag "${name}" is undefined. Possible reasons are that 1) it was misspelled or 2) it was removed from the @lwc/features package.`
