afe836d7fd00934827dfb450e92dd42e4632c435
Ravi Jayaramappa <ravi.jayaramappa@salesforce.com>
2019-10-15 17:14:01 -0700

    chore: allow a registry to be specified for release:ci:npm target (#1580)

--- scripts/release_npm.sh
+++ scripts/release_npm.sh
@@ -8,17 +8,23 @@
 # Normalize params
 PACKAGE_VERSION="$1"
 TAG_NAME="$2"
+# Optional registry param
+REGISTRY="$3"
 
 if [ -z "${PACKAGE_VERSION}" ]; then
-        echo "You must specify a version to release"
-        echo "Comand options: semver tagName (ex: 0.12.1 next)"
-        exit 1;
+  echo "You must specify a version to release"
+  echo "Comand options: semver tagName (ex: 0.12.1 next)"
+  exit 1;
 fi
 
 if [ -z "${TAG_NAME}" ]; then
-        echo "You must specify a tagname"
-        echo "Comand options: semver tagName (ex: 0.12.1 next)"
-        exit 1;
+  echo "You must specify a tagname"
+  echo "Comand options: semver tagName (ex: 0.12.1 next)"
+  exit 1;
+fi
+
+if [ -n "${REGISTRY}" ]; then
+  REGISTRY = "--registry ${REGISTRY}"
 fi
 
 TAG="--npm-tag $TAG_NAME"
@@ -33,7 +39,7 @@ fi
 
 # Command to push the packages
 CMD_UPDATE_VERSION="lerna version ${PACKAGE_VERSION} --yes --exact --force-publish --no-git-tag-version --no-push"
-CMD_PUBLISH_PACKAGES="lerna publish ${PACKAGE_VERSION} ${TAG} --yes --exact --force-publish --no-git-tag-version --no-push --no-verify-access --no-verify-registry"
+CMD_PUBLISH_PACKAGES="lerna publish ${PACKAGE_VERSION} ${TAG} --yes --exact --force-publish --no-git-tag-version --no-push ${REGISTRY} --no-verify-access --no-verify-registry"
 
 # Publish the packages to npm. Note that lerna cleans the working tree after this as of 3.0.4, so we need to reapply version
 # https://github.com/lerna/lerna/blob/3cbeeabcb443d9415bb86c4539652b85cd7b4025/commands/publish/index.js#L354-L363
