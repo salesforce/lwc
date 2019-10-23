3419acdf91d784b5f1cb21bf99530582a7042d40
Jose David Rodriguez <jodarove@gmail.com>
2019-10-23 14:14:02 -0700

    fix: refactor tests

--- packages/integration-karma/test/dom-manual/moving-node-to-document/index.spec.js
+++ packages/integration-karma/test/dom-manual/moving-node-to-document/index.spec.js
@@ -1,79 +1,79 @@
 import { createElement } from 'lwc';
 import Container from 'x/container';
 
+function createTestContainer(classForSpanInPortal) {
+    const elm = createElement('x-container', { is: Container });
+    document.body.appendChild(elm);
+    const spanInPortal = document.createElement('span');
+    if (classForSpanInPortal) {
+        spanInPortal.classList.add(classForSpanInPortal);
+    }
+    const portalElement = elm.shadowRoot.querySelector('div');
+
+    return {
+        portalElement,
+        spanInPortal,
+    };
+}
+
 describe('Moving elements from inside lwc:dom=manual', () => {
     it('should return correct parentNode', () => {
-        const elm = createElement('x-container', { is: Container });
-        document.body.appendChild(elm);
-        const span = document.createElement('span');
-        const div = elm.shadowRoot.querySelector('div');
-        div.appendChild(span);
+        const { portalElement, spanInPortal } = createTestContainer();
+        portalElement.appendChild(spanInPortal);
 
         return Promise.resolve().then(() => {
-            document.body.appendChild(span);
+            document.body.appendChild(spanInPortal);
 
             // There is a drawback: The mutation observer is async, therefore if we access it immediately,
             // it will still have an owner key.
             return Promise.resolve().then(() => {
-                expect(span.parentNode).toBe(document.body);
+                expect(spanInPortal.parentNode).toBe(document.body);
             });
         });
     });
 
     it('should return correct results in querySelector', () => {
-        const elm = createElement('x-container', { is: Container });
-        document.body.appendChild(elm);
-        const span = document.createElement('span');
-        span.classList.add('qs-lwc-dom-manual');
-        const div = elm.shadowRoot.querySelector('div');
-        div.appendChild(span);
+        const { portalElement, spanInPortal } = createTestContainer('qs-lwc-dom-manual');
+        portalElement.appendChild(spanInPortal);
 
         return Promise.resolve().then(() => {
-            document.body.appendChild(span);
+            document.body.appendChild(spanInPortal);
 
             // There is a drawback: The mutation observer is async, therefore if we access it immediately,
             // it will still have an owner key.
             return Promise.resolve().then(() => {
-                expect(document.querySelector('.qs-lwc-dom-manual')).toBe(span);
+                expect(document.querySelector('.qs-lwc-dom-manual')).toBe(spanInPortal);
             });
         });
     });
 
     it('should return correct results in querySelectorAll', () => {
-        const elm = createElement('x-container', { is: Container });
-        document.body.appendChild(elm);
-        const span = document.createElement('span');
-        span.classList.add('qs-all-lwc-dom-manual');
-        const div = elm.shadowRoot.querySelector('div');
-        div.appendChild(span);
+        const { portalElement, spanInPortal } = createTestContainer('qs-all-lwc-dom-manual');
+        portalElement.appendChild(spanInPortal);
 
         return Promise.resolve().then(() => {
-            document.body.appendChild(span);
+            document.body.appendChild(spanInPortal);
 
             // There is a drawback: The mutation observer is async, therefore if we access it immediately,
             // it will still have an owner key.
             return Promise.resolve().then(() => {
-                expect(document.querySelectorAll('.qs-all-lwc-dom-manual')[0]).toBe(span);
+                expect(document.querySelectorAll('.qs-all-lwc-dom-manual')[0]).toBe(spanInPortal);
             });
         });
     });
 
     it('should return correct results in querySelector when node is added/removed on the same tick', () => {
-        const elm = createElement('x-container', { is: Container });
-        document.body.appendChild(elm);
-        const span = document.createElement('span');
-        span.classList.add('qs-mm-lwc-dom-manual');
-        const div = elm.shadowRoot.querySelector('div');
-        div.appendChild(span);
-        div.removeChild(span);
+        const { portalElement, spanInPortal } = createTestContainer('qs-mm-lwc-dom-manual');
+        portalElement.appendChild(spanInPortal);
+        portalElement.removeChild(spanInPortal);
 
         return Promise.resolve().then(() => {
-            document.body.appendChild(span);
+            document.body.appendChild(spanInPortal);
 
             // There is a drawback: The mutation observer is async, therefore if we access it immediately,
             // it will still have an owner key.
             return Promise.resolve().then(() => {
-                expect(document.querySelector('.qs-mm-lwc-dom-manual')).toBe(span);
+                expect(document.querySelector('.qs-mm-lwc-dom-manual')).toBe(spanInPortal);
             });
         });
     });
