ac9e624d64f700ad4e8cc0c1ee7b2c6468977db4
Diego Ferreiro Val <dval@salesforce.com>
2019-11-08 13:40:31 -0800

    Update packages/@lwc/module-resolver/src/__tests__/multi-version.spec.ts
    Co-Authored-By: Pierre-Marie Dartus <dartus.pierremarie@gmail.com>

--- packages/@lwc/module-resolver/src/__tests__/multi-version.spec.ts
+++ packages/@lwc/module-resolver/src/__tests__/multi-version.spec.ts
@@ -22,7 +22,7 @@ describe('resolve modules', () => {
             .filter(m => m.specifier === 'ui/button')
             .sort((a, b) => a.scope.length - b.scope.length);
         const [btn1, btn2] = buttons;
-        expect(btn1.scope !== btn2.scope).toBeTruthy();
+        expect(btn1.scope).not.toBe(btn2.scope);
         expect(btn1.scope).toMatch(/multi-version$/); // endsWith('multi-version');
         expect(btn2.scope).toMatch(/fancy-components$/); // endsWith('fancy-components');
     });
