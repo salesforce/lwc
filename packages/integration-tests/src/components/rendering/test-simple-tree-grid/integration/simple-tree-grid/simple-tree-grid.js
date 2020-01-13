41a6378803edb624afd64b0f120d228879bcfe53
Pierre-Marie Dartus <p.dartus@salesforce.com>
2020-01-13 19:20:10 +0100

    test(karma): Move eligible webdriver tests to karma (#1669)

--- packages/integration-tests/src/components/rendering/test-simple-tree-grid/integration/simple-tree-grid/simple-tree-grid.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, track } from 'lwc';
-import data from './hardcoded-data';
-
-export default class TreeContainer extends LightningElement {
-    @track
-    myData = data;
-
-    toggleAll() {}
-}
