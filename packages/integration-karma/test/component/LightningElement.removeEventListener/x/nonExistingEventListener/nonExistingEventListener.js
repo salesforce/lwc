0fc593761785db560998dd152e15aa3977815a7c
Ravi Jayaramappa <ravi.jayaramappa@salesforce.com>
2020-01-13 16:14:06 -0800

    test: remove test for removing non-existent listener on custom element

--- packages/integration-karma/test/component/LightningElement.removeEventListener/x/nonExistingEventListener/nonExistingEventListener.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class NonExistingEventListener extends LightningElement {
-    connectedCallback() {
-        this.removeEventListener('click', () => {});
-    }
-}
