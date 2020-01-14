d5b9f40df3b948321429cec8e573c1f9c879e8c5
Ravi Jayaramappa <ravi.jayaramappa@salesforce.com>
2020-01-13 16:32:12 -0800

    test: enable tests that depend on latest safari
    fixes #1278

--- packages/integration-karma/test/shadow-dom/Event-properties/Event.target.spec.js
+++ packages/integration-karma/test/shadow-dom/Event-properties/Event.target.spec.js
@@ -4,22 +4,16 @@ import XEventHandlingParent from 'x/eventHandlingParent';
 import XDocumentEventListener from 'x/documentEventListener';
 import XParentWithDynamicChild from 'x/parentWithDynamicChild';
 
-// The composed-event-click-polyfill doesn't work when native Shadow DOM is enabled on Safari 12.0.0 (it has been fixed
-// with Safari 12.0.1). The polyfill only patches the event javascript wrapper and doesn't have any effect on how Webkit
-// make the event bubbles.
-// TODO [#1278]: Enable this test again once Sauce Labs supports Safari version >= 12.0.0
-if (!process.env.NATIVE_SHADOW) {
-    it('Async event target should be root node', function() {
-        const elm = createElement('x-async-event-target', { is: XAsyncEventTarget });
-        document.body.appendChild(elm);
-        const triggerElm = elm.shadowRoot.querySelector('x-child');
-        triggerElm.click();
-        expect(elm.syncTargetIsCorrect).toBe(true);
-        return new Promise(setTimeout).then(() => {
-            expect(elm.asyncTargetIsCorrect).toBe(true);
-        });
+it('Async event target should be root node', function() {
+    const elm = createElement('x-async-event-target', { is: XAsyncEventTarget });
+    document.body.appendChild(elm);
+    const triggerElm = elm.shadowRoot.querySelector('x-child');
+    triggerElm.click();
+    expect(elm.syncTargetIsCorrect).toBe(true);
+    return new Promise(setTimeout).then(() => {
+        expect(elm.asyncTargetIsCorrect).toBe(true);
     });
-}
+});
 
 it('parent should receive composed event with correct target', function() {
     const elm = createElement('x-parent', { is: XEventHandlingParent });
@@ -32,30 +26,24 @@ it('parent should receive composed event with correct target', function() {
     });
 });
 
-// The composed-event-click-polyfill doesn't work when native Shadow DOM is enabled on Safari 12.0.0 (it has been fixed
-// with Safari 12.0.1). The polyfill only patches the event javascript wrapper and doesn't have any effect on how Webkit
-// make the event bubbles.
-// TODO [#1278]: Enable this test again once Sauce Labs supports Safari version >= 12.0.0
-if (!process.env.NATIVE_SHADOW) {
-    describe('event.target on document event listener', () => {
-        let actual;
-        const listener = evt => {
-            actual = evt.target.tagName.toLowerCase();
-        };
-        beforeAll(() => {
-            document.addEventListener('click', listener);
-        });
-        afterAll(() => {
-            document.removeEventListener('click', listener);
-        });
-        it('should return correct target', function() {
-            const elm = createElement('x-document-event-listener', { is: XDocumentEventListener });
-            document.body.appendChild(elm);
-            elm.shadowRoot.querySelector('button').click();
-            expect(actual).toBe('x-document-event-listener');
-        });
+describe('event.target on document event listener', () => {
+    let actual;
+    const listener = evt => {
+        actual = evt.target.tagName.toLowerCase();
+    };
+    beforeAll(() => {
+        document.addEventListener('click', listener);
     });
-}
+    afterAll(() => {
+        document.removeEventListener('click', listener);
+    });
+    it('should return correct target', function() {
+        const elm = createElement('x-document-event-listener', { is: XDocumentEventListener });
+        document.body.appendChild(elm);
+        elm.shadowRoot.querySelector('button').click();
+        expect(actual).toBe('x-document-event-listener');
+    });
+});
 
 // legacy usecases
 describe('should not retarget event', () => {
