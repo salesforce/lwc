7762b7d842baac10b0c979a8768e38927837c69a
Ravi Jayaramappa <ravi.jayaramappa@salesforce.com>
2020-01-20 10:30:43 -0800

    fix: use regex for error message variants

--- packages/integration-karma/test/template/directive-for-each/index.spec.js
+++ packages/integration-karma/test/template/directive-for-each/index.spec.js
@@ -64,7 +64,7 @@ it('should throw an error when the passing a non iterable', () => {
 
     // TODO [#1283]: Improve this error message. The vm should not be exposed and the message is not helpful.
     expect(() => document.body.appendChild(elm)).toThrowError(
-        /Invalid template iteration for value `\[object Object\]` in \[object:vm undefined \(\d+\)\]. It must be an array-like object and not `null` nor `undefined`.|Invalid template iteration for value `\[object ProxyObject\]` in \[object:vm undefined \(\d+\)\]. It must be an array-like object and not `null` nor `undefined`./
+        /Invalid template iteration for value `\[object (ProxyObject|Object)\]` in \[object:vm undefined \(\d+\)\]. It must be an array-like object and not `null` nor `undefined`./
     );
 });
 
