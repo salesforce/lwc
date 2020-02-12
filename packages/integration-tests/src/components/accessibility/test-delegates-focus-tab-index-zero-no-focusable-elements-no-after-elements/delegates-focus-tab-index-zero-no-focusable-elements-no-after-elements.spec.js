ff0683012002672a46202b855da2c052435397c1
Eugene Kashida <ekashida@salesforce.com>
2020-02-12 02:08:22 -0800

    test: fix test-issue-1031 and refactor it while we're at it

--- packages/integration-tests/src/components/accessibility/test-delegates-focus-tab-index-zero-no-focusable-elements-no-after-elements/delegates-focus-tab-index-zero-no-focusable-elements-no-after-elements.spec.js
+++ packages/integration-tests/src/components/accessibility/test-delegates-focus-tab-index-zero-no-focusable-elements-no-after-elements/delegates-focus-tab-index-zero-no-focusable-elements-no-after-elements.spec.js
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
+const assert = require('assert');
+
 describe('Delegate focus with tabindex 0, no tabbable elements, and no tabbable elements after', () => {
     const URL = '/delegates-focus-tab-index-zero-no-focusable-elements-no-after-elements';
 
@@ -15,15 +17,10 @@ describe('Delegate focus with tabindex 0, no tabbable elements, and no tabbable 
         browser.keys(['Tab']);
         browser.keys(['Tab']);
 
-        browser.waitUntil(
-            () => {
-                const active = browser.$(function() {
-                    return document.activeElement;
-                });
-                return active.getTagName().toLowerCase() === 'body';
-            },
-            undefined,
-            'It should focus the body'
-        );
+        const tagName = browser.execute(function() {
+            return document.activeElement.tagName;
+        });
+
+        assert.strictEqual(tagName, 'BODY');
     });
 });
