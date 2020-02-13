53de5d321e71930476b3cd3cb4256dc702912440
Eugene Kashida <ekashida@salesforce.com>
2020-02-12 19:47:47 -0800

    test: basic invocation case

---
+++ packages/integration-tests/src/components/delegates-focus-programmatic/test-basic-invocation/basic-invocation.spec.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+const assert = require('assert');
+
+const URL = '/basic-invocation';
+
+describe('basic invocation', () => {
+    beforeEach(() => {
+        browser.url(URL);
+    });
+
+    it('should focus on the first programmatically focusable element', function() {
+        const button = browser.$(function() {
+            return document
+                .querySelector('integration-basic-invocation')
+                .shadowRoot.querySelector('button');
+        });
+        button.click();
+        const className = browser.execute(function() {
+            const container = document.activeElement;
+            const child = container.shadowRoot.activeElement;
+            return child.shadowRoot.activeElement.className;
+        });
+        assert.equal(className, 'internal-input');
+    });
+});
