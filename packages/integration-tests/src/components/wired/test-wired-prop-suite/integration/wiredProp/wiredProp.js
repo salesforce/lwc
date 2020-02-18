e69bd24529d2a349382daeef24c8c13d07476c99
Jose David Rodriguez Velasco <jodarove@gmail.com>
2020-02-18 10:39:15 -0800

    wip: repro in ie11

--- packages/integration-tests/src/components/wired/test-wired-prop-suite/integration/wiredProp/wiredProp.js
+++ packages/integration-tests/src/components/wired/test-wired-prop-suite/integration/wiredProp/wiredProp.js
@@ -1,5 +1,6 @@
-import { LightningElement, api, wire } from 'lwc';
-import { getTodo } from 'todo';
+import { LightningElement, api, track, wire } from 'lwc';
+import { getTodo, echoAdapter } from 'todo';
+
 export default class WiredProp extends LightningElement {
     @api todoId;
 
@@ -9,4 +10,19 @@ export default class WiredProp extends LightningElement {
     get error() {
         return 'Error loading data: ' + this.todo.error.message;
     }
+
+    firstParamValue = 'first-param-value';
+    secondParamValue = 'second-param-value';
+
+    @wire(echoAdapter, { firstParam: '$firstParamValue', secondParam: '$secondParamValue' })
+    firstWire;
+
+    @wire(echoAdapter, {
+        firstParam: '$firstWire.firstParam',
+        secondParam: '$firstWire.secondParam',
+    })
+    setSecondWire(newValue) {
+        debugger;
+        console.log(newValue);
+    };
 }
