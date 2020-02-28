0d16177cade23b1575dcce7a7094517ad21ebd68
Jose David Rodriguez Velasco <jodarove@gmail.com>
2020-02-28 11:16:06 -0800

    fix: disable dependent adapter test in compat

--- packages/integration-karma/test/wire/reactive-params.spec.js
+++ packages/integration-karma/test/wire/reactive-params.spec.js
@@ -3,16 +3,18 @@ import { createElement } from 'lwc';
 import CascadeWiredProps from 'x/cascadeWiredProps';
 
 describe('@wire reactive parameters', () => {
-    it('should provide complete configuration to dependent adapter', done => {
-        const elm = createElement('x-cascade-wire', { is: CascadeWiredProps });
-        elm.addEventListener('dependantwirevalue', evt => {
-            const secondWireValue = evt.detail.providedValue;
+    if (process.env.COMPAT !== true) {
+        it('should provide complete configuration to dependent adapter', done => {
+            const elm = createElement('x-cascade-wire', { is: CascadeWiredProps });
+            elm.addEventListener('dependantwirevalue', evt => {
+                const secondWireValue = evt.detail.providedValue;
 
-            expect(secondWireValue.firstParam).toBe('first-param-value');
-            expect(secondWireValue.secondParam).toBe('second-param-value');
-            done();
+                expect(secondWireValue.firstParam).toBe('first-param-value');
+                expect(secondWireValue.secondParam).toBe('second-param-value');
+                done();
+            });
+
+            document.body.appendChild(elm);
         });
-
-        document.body.appendChild(elm);
-    });
+    }
 });
