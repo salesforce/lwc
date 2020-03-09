5720104cd2c4987525baf513c17ed6717daebe3c
Eugene Kashida <ekashida@salesforce.com>
2020-03-09 13:52:05 -0700

    chore: review feedback

--- packages/@lwc/synthetic-shadow/src/polyfills/clipboard-event-composed/main.ts
+++ packages/@lwc/synthetic-shadow/src/polyfills/clipboard-event-composed/main.ts
@@ -4,15 +4,17 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
+import { assign, create, defineProperties } from '@lwc/shared';
+
 if (typeof ClipboardEvent !== 'undefined') {
-    const isComposedType = Object.assign(Object.create(null), {
+    const isComposedType = assign(create(null), {
         copy: 1,
         cut: 1,
         paste: 1,
     });
 
     // Patch the prototype to override the composed property on user-agent dispatched events
-    Object.defineProperties(ClipboardEvent.prototype, {
+    defineProperties(ClipboardEvent.prototype, {
         composed: {
             get() {
                 const { type } = this;
