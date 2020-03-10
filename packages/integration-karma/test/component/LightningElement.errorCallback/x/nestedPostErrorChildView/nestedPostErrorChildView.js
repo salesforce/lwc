49d31fb1921594b41b8ef3fa10d93a4c666be481
Pierre-Marie Dartus <p.dartus@salesforce.com>
2020-03-10 10:03:07 +0100

    test(karma): Error boundary tests cleanup (#1720)
    (cherry picked from commit 9b23ba300be98c0307321e0a41ed9cb69f4e4a0d)

--- packages/integration-karma/test/component/LightningElement.errorCallback/x/nestedPostErrorChildView/nestedPostErrorChildView.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class PostErrorChildView extends LightningElement {
-    renderedCallback() {
-        throw new Error('Boundary Alternative Child Offender Throws');
-    }
-}
