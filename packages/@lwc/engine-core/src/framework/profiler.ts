9e3d7cc69b99eada0062ec750afc83f00f8ed9fd
Tariq Rafique <mrafique@salesforce.com>
2020-03-20 10:38:09 -0700

    feat: Profiler Support

---
+++ packages/@lwc/engine-core/src/framework/profiler.ts
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+import { startMeasure, endMeasure, MeasurementPhase } from './performance-timing';
+import { VM } from './vm';
+
+function noop(_opId: number, _phase: number, _cmpName: string, _vm_idx: number) {}
+
+let logOperation = noop;
+
+export const enum OperationId {
+    constructor = 0,
+    render = 1,
+    patch = 2,
+    connectedCallback = 3,
+    renderedCallback = 4,
+    disconnectedCallback = 5,
+    errorCallback = 6,
+}
+
+const enum Phase {
+    Start = 0,
+    Stop = 1,
+}
+
+const opIdToMeasurementPhaseMappingArray: MeasurementPhase[] = [
+    'constructor',
+    'render',
+    'patch',
+    'connectedCallback',
+    'renderedCallback',
+    'disconnectedCallback',
+    'errorCallback',
+];
+
+let profilerEnabled = false;
+let logMarks = false;
+let bufferLogging = false;
+
+if (process.env.NODE_ENV !== 'production') {
+    profilerEnabled = true;
+    logMarks = true;
+    bufferLogging = false;
+}
+
+const profilerStateCallbacks: ((arg0: boolean) => void)[] = [];
+
+function trackProfilerState(callback: (arg0: boolean) => void) {
+    callback(profilerEnabled);
+    profilerStateCallbacks.push(callback);
+}
+
+function logOperationStart(opId: OperationId, vm: VM) {
+    if (logMarks) {
+        startMeasure(opIdToMeasurementPhaseMappingArray[opId], vm);
+    }
+    if (bufferLogging) {
+        const cmpName = getComponentName(vm);
+        logOperation(opId, Phase.Start, cmpName, vm.idx);
+    }
+}
+
+function logOperationEnd(opId: OperationId, vm: VM) {
+    if (logMarks) {
+        endMeasure(opIdToMeasurementPhaseMappingArray[opId], vm);
+    }
+    if (bufferLogging) {
+        const cmpName = getComponentName(vm);
+        logOperation(opId, Phase.Stop, cmpName, vm.idx);
+    }
+}
+
+function getComponentName(vm: VM) {
+    try {
+        return vm.elm.tagName;
+    } catch (error) {
+        return 'invalid-tag-name';
+    }
+}
+
+function enableProfiler() {
+    profilerEnabled = true;
+    bufferLogging = true;
+    notifyProfilerStateChange();
+}
+
+function disableProfiler() {
+    if (process.env.NODE_ENV !== 'production') {
+        // in non-prod mode we want to keep logging marks
+        profilerEnabled = true;
+        logMarks = true;
+        bufferLogging = false;
+    } else {
+        profilerEnabled = false;
+        bufferLogging = false;
+        logMarks = false;
+    }
+    notifyProfilerStateChange();
+}
+
+function notifyProfilerStateChange() {
+    for (let i = 0; i < profilerStateCallbacks.length; i++) {
+        profilerStateCallbacks[i](profilerEnabled);
+    }
+}
+
+function attachBuffer(
+    bufferCallback: (_opId: number, _phase: number, _cmpName: string, _vm_idx: number) => void
+) {
+    logOperation = bufferCallback;
+    bufferLogging = true;
+}
+
+function detachBuffer() {
+    const currentLogOperation = logOperation;
+    logOperation = noop;
+    bufferLogging = false;
+    return currentLogOperation;
+}
+
+const profilerControl = {
+    enableProfiler,
+    disableProfiler,
+    attachBuffer,
+    detachBuffer,
+};
+
+export { logOperationStart, logOperationEnd, trackProfilerState, profilerControl };
---
+++ packages/integration-karma/test/profiler/sanity/profiler.spec.js
@@ -0,0 +1,102 @@
+import { createElement } from 'lwc';
+import Container from 'x/container';
+
+describe('Profiler Sanity Test', () => {
+    afterEach(() => {
+        LWC.__unstable__ProfilerControl.detachBuffer();
+        LWC.__unstable__ProfilerControl.disableProfiler();
+    });
+
+    const X_CONTAINER = 'X-CONTAINER';
+    const X_ERROR_CHILD = 'X-ERROR-CHILD';
+    const X_ITEM = 'X-ITEM';
+
+    const OperationId = {
+        constructor: 0,
+        render: 1,
+        patch: 2,
+        connectedCallback: 3,
+        renderedCallback: 4,
+        disconnectedCallback: 5,
+        errorCallback: 6,
+    };
+
+    const Phase = {
+        Start: 0,
+        Stop: 1,
+    };
+
+    async function generateContainer() {
+        const elm = createElement(X_CONTAINER.toLowerCase(), { is: Container });
+        document.body.appendChild(elm);
+        await Promise.resolve();
+        return elm;
+    }
+
+    function enableProfilerAndRegisterBuffer() {
+        const profilerControl = LWC.__unstable__ProfilerControl;
+        const events = [];
+        profilerControl.enableProfiler();
+        profilerControl.attachBuffer((opId, phase, name) => {
+            events.push({ opId, phase, name });
+        });
+        return events;
+    }
+
+    function matchEventsOfTypeFor(opId, name, profilerEvents) {
+        const filteredEvents = profilerEvents.filter((e) => e.name === name && e.opId === opId);
+        const expectedEvents = [
+            { opId, phase: Phase.Start, name },
+            { opId, phase: Phase.Stop, name },
+        ];
+        expect(filteredEvents).toEqual(expectedEvents);
+    }
+
+    it('container first render without activating list', async () => {
+        const profilerEvents = enableProfilerAndRegisterBuffer();
+        await generateContainer();
+
+        matchEventsOfTypeFor(OperationId.constructor, X_CONTAINER, profilerEvents);
+        matchEventsOfTypeFor(OperationId.render, X_CONTAINER, profilerEvents);
+        matchEventsOfTypeFor(OperationId.patch, X_CONTAINER, profilerEvents);
+        matchEventsOfTypeFor(OperationId.connectedCallback, X_CONTAINER, profilerEvents);
+        matchEventsOfTypeFor(OperationId.renderedCallback, X_CONTAINER, profilerEvents);
+    });
+
+    it('activate children in iteration in container', async () => {
+        const elm = await generateContainer();
+        const profilerEvents = enableProfilerAndRegisterBuffer();
+        const activateListButton = elm.shadowRoot.querySelector('.profiler-renderList');
+
+        activateListButton.click();
+        await Promise.resolve();
+
+        // the parent container will have a patch and render event
+        matchEventsOfTypeFor(OperationId.patch, X_CONTAINER, profilerEvents);
+        matchEventsOfTypeFor(OperationId.render, X_CONTAINER, profilerEvents);
+        // the child item will have the usual lifecycle
+        matchEventsOfTypeFor(OperationId.constructor, X_ITEM, profilerEvents);
+        matchEventsOfTypeFor(OperationId.render, X_ITEM, profilerEvents);
+        matchEventsOfTypeFor(OperationId.patch, X_ITEM, profilerEvents);
+    });
+
+    it('error callback counted properly', async () => {
+        const elm = await generateContainer();
+        const profilerEvents = enableProfilerAndRegisterBuffer();
+        const errorButton = elm.shadowRoot
+            .querySelector(X_ERROR_CHILD.toLowerCase())
+            .shadowRoot.querySelector('button');
+
+        try {
+            errorButton.click();
+        } catch (e) {
+            // do nothing
+        }
+
+        const expectedEvents = [
+            { opId: OperationId.errorCallback, phase: Phase.Start, name: X_ERROR_CHILD },
+            { opId: OperationId.errorCallback, phase: Phase.Stop, name: X_ERROR_CHILD },
+        ];
+        expect(profilerEvents).toEqual(expectedEvents);
+    });
+});
