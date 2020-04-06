93377fa5cf51955ccde945580c7511bb1e21f99b
Jose David Rodriguez Velasco <jodarove@gmail.com>
2020-04-06 15:16:55 -0700

    feat(compiler): Provide config fn for wire config. (#1806)

--- packages/integration-karma/test/api/getComponentDef/x/wirePropertiesInheritance/base.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement, wire } from 'lwc';
-import wireAdapter from 'x/wireAdapter';
-
-export default class Base extends LightningElement {
-    @wire(wireAdapter, { parent: true }) parentProp;
-    @wire(wireAdapter, { parent: true }) overriddenInChild;
-}
