23c4bf5638f26c67993c61ef6569c77aec0784cc
Eugene Kashida <ekashida@salesforce.com>
2020-04-10 02:54:12 -0700

    test: update test for dispatchEvent type error

--- packages/integration-karma/test/component/LightningElement.dispatchEvent/index.spec.js
+++ packages/integration-karma/test/component/LightningElement.dispatchEvent/index.spec.js
@@ -29,11 +29,7 @@ it('should throw an error if the parameter is not an instance of Event', () => {
 
     expect(() => {
         elm.dispatch('event');
-    }).toThrowError(
-        Error,
-        // Error message covers chrome, firefox, Safari, IE11, Safari 10.0.0
-        /Failed to execute 'dispatchEvent' on 'EventTarget': parameter 1 is not of type 'Event'|Argument 1 \('event'\) to EventTarget.dispatchEvent must be an instance of Event|Argument 1 of EventTarget\.dispatchEvent is not an object|Invalid argument|Type error/
-    );
+    }).toThrowError(TypeError);
 });
 
 it('should throw when event is dispatched during construction', function() {
