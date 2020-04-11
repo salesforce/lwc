91f735a0a7df9574e08308b6d6b2ce6a35d0b280
Jose David Rodriguez Velasco <jodarove@gmail.com>
2020-04-10 17:04:29 -0700

    fix: Wire service call config when all params are undefined
    From the prevous wire protocol (summer20 branch). When all the params in the config of a wire adapter are undefined, the config event should not be triggered on the wire adapter.

--- packages/integration-karma/test/wire/reactive-params.spec.js
+++ packages/integration-karma/test/wire/reactive-params.spec.js
@@ -1,22 +1,57 @@
 import { createElement } from 'lwc';
 
 import CascadeWiredProps from 'x/cascadeWiredProps';
+import SimpleWiredProps from 'x/simpleWiredProps';
 
 describe('@wire reactive parameters', () => {
-    if (process.env.COMPAT !== true) {
-        // this functionality does not works in compat due the fact that the wire-service can't extract
-        // the values of an ie11 proxy object.
-        it('should provide complete configuration to dependent adapter', done => {
-            const elm = createElement('x-cascade-wire', { is: CascadeWiredProps });
-            elm.addEventListener('dependantwirevalue', evt => {
-                const secondWireValue = evt.detail.providedValue;
+    it('should provide complete configuration to dependent adapter', done => {
+        const elm = createElement('x-cascade-wire', { is: CascadeWiredProps });
+        elm.addEventListener('dependantwirevalue', evt => {
+            const secondWireValue = evt.detail.providedValue;
 
-                expect(secondWireValue.firstParam).toBe('first-param-value');
-                expect(secondWireValue.secondParam).toBe('second-param-value');
-                done();
-            });
-
-            document.body.appendChild(elm);
+            expect(secondWireValue.firstParam).toBe('first-param-value');
+            expect(secondWireValue.secondParam).toBe('second-param-value');
+            done();
         });
-    }
+
+        document.body.appendChild(elm);
+    });
+
+    it('should not call update when all params are undefined params', done => {
+        const elm = createElement('x-simple-wire', { is: SimpleWiredProps });
+        let wireConfigCalled = false;
+
+        elm.addEventListener('adapterupdate', () => {
+            wireConfigCalled = true;
+        });
+
+        // elm.setSimpleWireConfig('jose');
+        document.body.appendChild(elm);
+
+        setTimeout(() => {
+            expect(wireConfigCalled).toBe(false);
+            done();
+        }, 1);
+    });
+
+    it('should call update when at least one of initial undefined params change', done => {
+        const elm = createElement('x-simple-wire', { is: SimpleWiredProps });
+        let wireConfigCalled = false;
+
+        elm.addEventListener('adapterupdate', () => {
+            wireConfigCalled = true;
+        });
+
+        document.body.appendChild(elm);
+
+        setTimeout(() => {
+            expect(wireConfigCalled).toBe(false);
+            elm.setSimpleWireConfig('test-value');
+
+            setTimeout(() => {
+                expect(wireConfigCalled).toBe(true);
+                done();
+            }, 1);
+        }, 1);
+    });
 });
