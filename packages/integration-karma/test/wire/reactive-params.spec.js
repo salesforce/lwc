b391a6202fef7aa46911fe1e61a9afea39275ea3
Jose David Rodriguez Velasco <jodarove@gmail.com>
2020-04-13 20:30:55 -0700

    fix(wire-service): config with all params being undefined (#1823)
    fixes an issue introduced on #1459 that calls the update method on a wire adapter, even when initially all the params of the config are undefined. The fix is only for the legacy adapters that are registered with the wire-service.

--- packages/integration-karma/test/wire/reactive-params.spec.js
+++ packages/integration-karma/test/wire/reactive-params.spec.js
@@ -3,20 +3,16 @@ import { createElement } from 'lwc';
 import CascadeWiredProps from 'x/cascadeWiredProps';
 
 describe('@wire reactive parameters', () => {
-    if (process.env.COMPAT !== true) {
-        // this functionality does not works in compat due the fact that the wire-service can't extract
-        // the values of an ie11 proxy object.
-        it('should provide complete configuration to dependent adapter', done => {
-            const elm = createElement('x-cascade-wire', { is: CascadeWiredProps });
-            elm.addEventListener('dependantwirevalue', evt => {
-                const secondWireValue = evt.detail.providedValue;
+    it('should provide complete configuration to dependent adapter', done => {
+        const elm = createElement('x-cascade-wire', { is: CascadeWiredProps });
+        elm.addEventListener('dependantwirevalue', evt => {
+            const secondWireValue = evt.detail.providedValue;
 
-                expect(secondWireValue.firstParam).toBe('first-param-value');
-                expect(secondWireValue.secondParam).toBe('second-param-value');
-                done();
-            });
-
-            document.body.appendChild(elm);
+            expect(secondWireValue.firstParam).toBe('first-param-value');
+            expect(secondWireValue.secondParam).toBe('second-param-value');
+            done();
         });
-    }
+
+        document.body.appendChild(elm);
+    });
 });
