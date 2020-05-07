ed6919cae2ad6e98f7f720a6e7b4fe98bd3fa008
Pierre-Marie Dartus <p.dartus@salesforce.com>
2020-05-07 16:14:09 +0200

    fix: add missing headers

--- packages/@lwc/shared/src/aria.ts
+++ packages/@lwc/shared/src/aria.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+
 // Global Aria and Role Properties derived from ARIA and Role Attributes.
 // https://wicg.github.io/aom/spec/aria-reflection.html
 export const AriaPropertyNames = [
