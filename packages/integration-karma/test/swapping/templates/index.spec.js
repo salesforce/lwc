87311e621c482a1311761975966b3f7f2e41994d
Caridy Patiño <caridy@gmail.com>
2020-05-13 15:02:36 -0400

    refactor(engine): adding tests

---
+++ packages/integration-karma/test/swapping/templates/index.spec.js
@@ -0,0 +1,36 @@
+import { createElement } from 'lwc';
+
+// TODO [#1869]: getting the global API from global LWC in tests until it is allowed in compiler
+const { registerTemplateSwap } = LWC;
+
+import Simple from 'base/simple';
+import Advanced from 'base/advanced';
+import { first, second } from 'base/views';
+
+const simpleBaseTemplate = Simple.baseTemplate;
+const advancedBaseTemplate = Advanced.baseTemplate;
+
+describe('template swapping', () => {
+    it('should work with components with implicit template definition', () => {
+        const elm = createElement('x-simple', { is: Simple });
+        document.body.appendChild(elm);
+        expect(elm.shadowRoot.firstChild.outerHTML).toBe('<p class="simple">simple</p>');
+        registerTemplateSwap(simpleBaseTemplate, first);
+        return Promise.resolve().then(() => {
+            expect(elm.shadowRoot.firstChild.outerHTML).toBe('<p class="first">first</p>');
+            registerTemplateSwap(first, second);
+            return Promise.resolve().then(() => {
+                expect(elm.shadowRoot.firstChild.outerHTML).toBe('<p class="second">second</p>');
+            });
+        });
+    });
+    it('should work with components with explict template definition', () => {
+        const elm = createElement('x-advanced', { is: Advanced });
+        document.body.appendChild(elm);
+        expect(elm.shadowRoot.firstChild.outerHTML).toBe('<p class="advanced">advanced</p>');
+        registerTemplateSwap(advancedBaseTemplate, second);
+        return Promise.resolve().then(() => {
+            expect(elm.shadowRoot.firstChild.outerHTML).toBe('<p class="second">second</p>');
+        });
+    });
+});
