40c4846e87f1394d68efaf1c4c4e5d871495d365
Eugene Kashida <ekashida@salesforce.com>
2020-05-12 19:58:11 -0700

    fix: error message validation

--- packages/@lwc/engine/src/__tests__/events.spec.ts
+++ packages/@lwc/engine/src/__tests__/events.spec.ts
@@ -4,8 +4,9 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-import { createElement, LightningElement, registerDecorators } from '../';
+import { createElement } from '@lwc/engine-dom';
 import { compileTemplate } from 'test-utils';
+import { LightningElement, registerDecorators } from '../';
 
 describe('events', () => {
     describe('bookkeeping', () => {
