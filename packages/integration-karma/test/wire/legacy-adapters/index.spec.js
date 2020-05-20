907083fef6d0771875f584c78ca72d42a84a2b63
Jose David Rodriguez Velasco <jodarove@gmail.com>
2020-05-20 10:26:43 -0700

    fix(wire-service): legacy adapters consecutive config calls with same config

--- packages/integration-karma/test/wire/legacy-adapters/index.spec.js
+++ packages/integration-karma/test/wire/legacy-adapters/index.spec.js
@@ -2,6 +2,7 @@ import { createElement } from 'lwc';
 
 import StaticWiredProps from 'x/staticWiredProps';
 import DynamicWiredProps from 'x/dynamicWiredProps';
+import SameConfigCase from 'x/sameConfigCase';
 
 describe('legacy wire adapters (register call)', () => {
     describe('with static config', () => {
@@ -102,6 +103,64 @@ describe('legacy wire adapters (register call)', () => {
                 const calls = elm.mixedAllParamsUndefinedCalls;
                 expect(calls.length).toBe(0);
                 done();
+            }, 0);
+        });
+
+        // This following 2 test cases can occur in two scenarios:
+        // 1. Because a config param depends on multiple values, some of them change but generates the same config value
+        // 2. An issue presented today with the @api decorator, in which whenever the api property is set,
+        //    it will force the reactivity system to notify it as a mutation on the component.
+        //
+        // With the current wire protocol:
+        // Case 1) is valid for new wire adapters and should be handled at the adapter level.
+        // Case 2) is a loophole in the engine today, but it can be closed without the risk of compromising existing components.
+        //
+        // With the previous wire protocol:
+        // 1) was not possible to reproduce, a config param could only depend on one property from the component, and since
+        //    now is possible, existing adapters may behave incorrectly.
+        // 2) was handled at the wire-protocol level and existing adapters may behave incorrectly.
+        it('should not call config when the generated config is the same as the last one (case 1)', (done) => {
+            const elm = createElement('x-same-config', { is: SameConfigCase });
+            elm.a = 3;
+            elm.b = 2;
+
+            setTimeout(() => {
+                const firstResult = elm.resultMultipleDependenciesForConfig;
+                expect(firstResult.sum).toBe(5);
+
+                elm.a = 1;
+                elm.b = 4;
+
+                setTimeout(() => {
+                    const secondResult = elm.resultMultipleDependenciesForConfig;
+
+                    // Based on the RFC: every time that `adapter.update()` is invoked, a new config object will
+                    // be provided as a first argument, no identity is preserved in this case.
+                    expect(firstResult).toBe(secondResult);
+
+                    done();
+                });
+            });
+        });
+
+        it('should not call config when the generated config is the same as the last one (case 2)', (done) => {
+            const elm = createElement('x-same-config', { is: SameConfigCase });
+            elm.a = 3;
+
+            setTimeout(() => {
+                const firstResult = elm.resultApiValueDependency;
+                expect(firstResult.a).toBe(3);
+
+                // setting same api value
+                elm.a = 3;
+
+                setTimeout(() => {
+                    const secondResult = elm.resultApiValueDependency;
+
+                    expect(firstResult).toBe(secondResult);
+
+                    done();
+                }, 0);
             }, 0);
         });
     });
