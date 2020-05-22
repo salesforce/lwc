4446fe8b031607b864d7472914eaf3125d8bacfd
Eugene Kashida <ekashida@salesforce.com>
2020-05-21 18:49:51 -0700

    test: more

--- packages/@lwc/babel-plugin-component/src/__tests__/dedupe-imports.spec.js
+++ packages/@lwc/babel-plugin-component/src/__tests__/dedupe-imports.spec.js
@@ -62,13 +62,29 @@ describe('deduping imports', () => {
         `   import { baz } from 'foo';
             import buzz, { foo, bar } from 'foo';
             import * as Foo from 'foo';
-            
+
         `,
         {
             output: {
                 code: `
                 import buzz, { baz, foo, bar } from 'foo';
                 import * as Foo from 'foo';
+                `,
+            },
+        }
+    );
+
+    pluginTest(
+        'should handle both default and importing the entire contents for a module',
+        `
+            import defaultExport, * as name from 'foo';
+            import { baz } from 'foo';
+        `,
+        {
+            output: {
+                code: `
+                import defaultExport, * as name from 'foo';
+                import { baz } from 'foo';
                 `,
             },
         }
