d1916625a4593d0cc33b08e2e13b7666079a7fab
Pierre-Marie Dartus <p.dartus@salesforce.com>
2020-06-04 15:26:43 +0200

    refactor(engine): remove template null check

--- packages/@lwc/engine/src/framework/template.ts
+++ packages/@lwc/engine/src/framework/template.ts
@@ -134,7 +134,7 @@ export function evaluateTemplate(vm: VM, html: Template): Array<VNode | null> {
                     }
 
                     // Check that the template was built by the compiler.
-                    if (isNull(html) || !isTemplateRegistered(html)) {
+                    if (!isTemplateRegistered(html)) {
                         throw new TypeError(
                             `Invalid template returned by the render() method on ${vm}. It must return an imported template (e.g.: \`import html from "./${
                                 vm.def.name
