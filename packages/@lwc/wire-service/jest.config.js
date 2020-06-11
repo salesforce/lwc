9c23b2cfe3a3482b237c7e8b0cb6b40963c6ff05
Pierre-Marie Dartus <p.dartus@salesforce.com>
2020-06-11 12:01:45 +0200

    chore(wire-service): cleanup package structure

--- packages/@lwc/wire-service/jest.config.js
+++ packages/@lwc/wire-service/jest.config.js
@@ -10,13 +10,4 @@ module.exports = {
     ...BASE_CONFIG,
     displayName: 'lwc-wire-service',
     roots: ['<rootDir>/src'],
-
-    // Override global threshold for the package. As we increase the test coverage we should increase
-    // the threshold as well.
-    coverageThreshold: {
-        global: {
-            ...BASE_CONFIG.coverageThreshold.global,
-            branches: 60,
-        },
-    },
 };
