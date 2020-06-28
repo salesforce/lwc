773333bdffa25cf0bac4888e758561405f63ed60
Ravi Jayaramappa <ravi.jayaramappa@salesforce.com>
2020-06-28 16:54:44 -0700

    test: test for lwc:dynamic

---
+++ packages/@lwc/engine-server/src/__tests__/fixtures/lwc-dynamic/modules/x/lwc-dynamic/lwc-dynamic.js
@@ -0,0 +1,13 @@
+import { LightningElement, track } from 'lwc';
+export default class DynamicCtor extends LightningElement {
+    @track customCtor;
+
+    connectedCallback() {
+        this.loadCtor();
+    }
+
+    async loadCtor() {
+        const ctor = await import("x/test");
+        this.customCtor = ctor.default;
+    }
+}
