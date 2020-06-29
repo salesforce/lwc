67e230ec8d93a173b7ebdba0fab5bc84c06db22b
Pierre-Marie Dartus <p.dartus@salesforce.com>
2020-06-29 16:08:12 +0200

    perf: add SSR table component benchmark

---
+++ packages/perf-benchmarks/src/__benchmarks__/engine-server/tablecmp-render-10k.benchmark.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+
+import { renderComponent } from '@lwc/engine-server';
+
+import Table from 'benchmark/tableComponent';
+import Store from 'benchmark/store';
+
+benchmark(`benchmark-table-component/render/10k`, () => {
+    run(() => {
+        const store = new Store();
+        store.runLots();
+
+        renderComponent('benchmark-table', Table, {
+            rows: store.data,
+        });
+    });
+});
