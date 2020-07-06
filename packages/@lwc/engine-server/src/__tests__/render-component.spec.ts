f2dbd273d3d5b34f4b21559a5a343c2fc92d15bd
Pierre-Marie Dartus <p.dartus@salesforce.com>
2020-07-06 16:15:30 +0200

    chore: add missing header

--- packages/@lwc/engine-server/src/__tests__/render-component.spec.ts
+++ packages/@lwc/engine-server/src/__tests__/render-component.spec.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+
 import { renderComponent, LightningElement } from '../index';
 
 class Test extends LightningElement {}
