5f9b2c2aced2f005cb9700bad7b6215f013f63d7
Pierre-Marie Dartus <p.dartus@salesforce.com>
2020-07-09 13:22:49 +0200

    chore(engine-server): add missing header

--- packages/@lwc/engine-server/src/utils/html-escape.ts
+++ packages/@lwc/engine-server/src/utils/html-escape.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+
 const ESCAPED_CHARS: { [char: string]: string } = {
     '"': '&quot;',
     "'": '&#x27;',
