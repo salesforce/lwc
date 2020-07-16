c4dcc14cb401c2f89a1d284924fc650eb7a72f3c
Jose David Rodriguez Velasco <jodarove@gmail.com>
2020-07-15 22:11:53 -0700

    fix: handle spellcheck in firefox

--- packages/@lwc/template-compiler/src/codegen/index.ts
+++ packages/@lwc/template-compiler/src/codegen/index.ts
@@ -440,7 +440,10 @@ function transform(root: IRNode, codeGen: CodeGen): t.Expression {
             case IRAttributeType.Boolean: {
                 // A boolean value used in an attribute should always generate .setAttribute(attr.name, ''),
                 // regardless if is a boolean attribute or not.
-                return isUsedAsAttribute ? t.stringLiteral('') : t.booleanLiteral(attr.value);
+                // Note: in Firefox, elm.setAttribute('spellcheck', ''), sets the attribute, but the property value is false.
+                return isUsedAsAttribute && attr.name !== 'spellcheck'
+                    ? t.stringLiteral('')
+                    : t.booleanLiteral(attr.value);
             }
         }
     }
