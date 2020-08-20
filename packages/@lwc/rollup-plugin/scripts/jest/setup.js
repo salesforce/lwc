7ba76198c1e8abf3502be826d529fdca8ef61393
Pierre-Marie Dartus <p.dartus@salesforce.com>
2020-08-20 17:38:40 +0200

    chore: update missing headers

--- packages/@lwc/rollup-plugin/scripts/jest/setup.js
+++ packages/@lwc/rollup-plugin/scripts/jest/setup.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+
 // Increase timeout for all the rollup plugin tests. Depending on the environment, the default 5
 // seconds might not be enough for the test to complete.
 jest.setTimeout(20000);
