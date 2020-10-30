2342e57de70a312825a6b951714e24e51033d817
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2020-10-30 05:59:03 -0300

    chore(lwc): Add typings to ShadowRoot (#2067)

--- packages/lwc/types.d.ts
+++ packages/lwc/types.d.ts
@@ -126,6 +126,20 @@ declare module 'lwc' {
         compareDocumentPosition(otherNode: Node): number;
         contains(otherNode: Node): boolean;
         elementFromPoint(x: number, y: number): Element | null;
+        querySelector<K extends keyof HTMLElementTagNameMap>(
+            selectors: K
+        ): HTMLElementTagNameMap[K] | null;
+        querySelector<K extends keyof SVGElementTagNameMap>(
+            selectors: K
+        ): SVGElementTagNameMap[K] | null;
+        querySelector<E extends Element = Element>(selectors: string): E | null;
+        querySelectorAll<K extends keyof HTMLElementTagNameMap>(
+            selectors: K
+        ): NodeListOf<HTMLElementTagNameMap[K]>;
+        querySelectorAll<K extends keyof SVGElementTagNameMap>(
+            selectors: K
+        ): NodeListOf<SVGElementTagNameMap[K]>;
+        querySelectorAll<E extends Element = Element>(selectors: string): NodeListOf<E>;
     }
 
     /**
