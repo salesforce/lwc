9e9b0f455888fe957bf5501210652154b3a688f6
Eugene Kashida <ekashida@gmail.com>
2020-11-02 12:00:12 -0500

    build: fix release branch restriction for npm publish (#2074)

--- scripts/release/publish.js
+++ scripts/release/publish.js
@@ -24,25 +24,38 @@ const ARGS = [
 
 const { stderr, stdin, stdout } = process;
 
-const RELEASE_BRANCH_RE = /^(master|((winter|spring|summer)\d+))$/;
-
 try {
     if (!isCI) {
         console.error('This script is only meant to run in CI.');
         process.exit(1);
     }
 
-    // `git branch --show-current` is only available as of git@2.22.0
-    const currentBranch = execa.sync('git', ['rev-parse', '--abbrev-ref', 'HEAD'], { shell: true })
-        .stdout;
-    if (!RELEASE_BRANCH_RE.test(currentBranch)) {
-        console.error(
-            `Invalid branch: "${currentBranch}". This script is only meant to run in a release branch.`
-        );
+    // We can't use a straightforward git command like `git branch --show-current` because we've
+    // checked out a git tag and we're in a detached HEAD state. The following logic attempts to
+    // verify that this tag points to a commit contained by a release branch.
+
+    // All branches that contain the current commit. Since we only deal with release commits, the
+    // only scenario where this might be an issue is if we try to publish a release commit from
+    // master after CLCO because the commit will show up in two branches.
+    const branches = execa.commandSync('git branch --all --contains').stdout;
+
+    // Restrict the regex to remote branches to avoid assumptions about local branches.
+    const REMOTE_RELEASE_BRANCH_RE = /origin\/(master|((winter|spring|summer)\d+))/;
+
+    const result = REMOTE_RELEASE_BRANCH_RE.exec(branches);
+    if (result === null) {
+        const tag = execa.commandSync('git tag --points-at HEAD').stdout;
+        console.error(`The commit referenced by "${tag}" is not contained by any release branch.`);
         process.exit(1);
     }
 
-    ARGS.push('--dist-tag', currentBranch === 'master' ? 'next' : currentBranch);
+    const [, releaseBranch] = result;
+    const distTag = releaseBranch === 'master' ? 'next' : releaseBranch;
+    ARGS.push('--dist-tag', distTag);
+
+    console.log(
+        `Attempting to release from branch "${releaseBranch}" using dist-tag "${distTag}".`
+    );
 
     execa.sync('lerna', ARGS, {
         // Prioritize locally installed binaries (node_modules/.bin)
