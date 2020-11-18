42724f6e63a9893eb473581fb63213b0867d7313
Pierre-Marie Dartus <p.dartus@salesforce.com>
2020-11-18 15:38:57 +0100

    refactor: remove unnecessary code

--- packages/@lwc/shared/src/html-attributes.ts
+++ packages/@lwc/shared/src/html-attributes.ts
@@ -4,11 +4,12 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-import { AriaAttrNameToPropNameMap, AriaPropNameToAttrNameMap } from './aria';
-import { keys, hasOwnProperty, StringReplace } from './language';
+import { AriaPropNameToAttrNameMap } from './aria';
+import { hasOwnProperty, StringReplace } from './language';
 
 /**
- * Maps boolean attribute name to supported tags: 'boolean attr name' => Set of allowed tag names that supports them.
+ * Maps boolean attribute name to supported tags: 'boolean attr name' => Set of allowed tag names
+ * that supports them.
  */
 const BOOLEAN_ATTRIBUTES = new Map([
     ['autofocus', new Set(['button', 'input', 'keygen', 'select', 'textarea'])],
@@ -72,46 +73,24 @@ export function isGlobalHtmlAttribute(attrName: string): boolean {
     return GLOBAL_ATTRIBUTE.has(attrName);
 }
 
-const HTML_ATTRIBUTES_TO_PROPERTY: Record<string, string> = {
-    accesskey: 'accessKey',
-    readonly: 'readOnly',
-    tabindex: 'tabIndex',
-    bgcolor: 'bgColor',
-    colspan: 'colSpan',
-    rowspan: 'rowSpan',
-    contenteditable: 'contentEditable',
-    crossorigin: 'crossOrigin',
-    datetime: 'dateTime',
-    formaction: 'formAction',
-    ismap: 'isMap',
-    maxlength: 'maxLength',
-    minlength: 'minLength',
-    novalidate: 'noValidate',
-    usemap: 'useMap',
-    for: 'htmlFor',
+const HTML_PROPERTIES_TO_ATTRIBUTE: Record<string, string> = {
+    accessKey: 'accesskey',
+    readOnly: 'readonly',
+    tabIndex: 'tabindex',
+    bgColor: 'bgcolor',
+    colSpan: 'colspan',
+    rowSpan: 'rowspan',
+    contentEditable: 'contenteditable',
+    crossOrigin: 'crossorigin',
+    dateTime: 'datetime',
+    formAction: 'formaction',
+    isMap: 'ismap',
+    maxLength: 'maxlength',
+    minLength: 'minlength',
+    noValidate: 'novalidate',
+    useMap: 'usemap',
+    htmlFor: 'for',
 };
-const HTML_PROPERTIES_TO_ATTRIBUTE: Record<string, string> = {};
-
-keys(HTML_ATTRIBUTES_TO_PROPERTY).forEach((attrName) => {
-    const propName = HTML_ATTRIBUTES_TO_PROPERTY[attrName];
-    HTML_PROPERTIES_TO_ATTRIBUTE[propName] = attrName;
-});
-
-const CAMEL_REGEX = /-([a-z])/g;
-
-export function htmlAttributeToProperty(attrName: string): string {
-    if (attrName in AriaAttrNameToPropNameMap) {
-        return AriaAttrNameToPropNameMap[attrName];
-    }
-
-    if (!hasOwnProperty.call(HTML_ATTRIBUTES_TO_PROPERTY, attrName)) {
-        HTML_ATTRIBUTES_TO_PROPERTY[attrName] = StringReplace.call(attrName, CAMEL_REGEX, (match) =>
-            match[1].toUpperCase()
-        );
-    }
-
-    return HTML_ATTRIBUTES_TO_PROPERTY[attrName]!;
-}
 
 const CAPS_REGEX = /[A-Z]/g;
 
