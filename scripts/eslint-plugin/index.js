bdae32fcea0c2abd6abdde44ad3b5a114d21af0c
Josh Kovitz <jkovitz@salesforce.com>
2020-11-25 10:50:02 -0800

    fix: naming and inclusion

--- scripts/eslint-plugin/index.js
+++ scripts/eslint-plugin/index.js
@@ -9,11 +9,13 @@
 const noExtendGlobalConstructor = require('./rules/no-extend-global-constructor');
 const noProductionAssert = require('./rules/no-production-assert');
 const noInvalidTodo = require('./rules/no-invalid-todo');
+const noUppercaseAfterUnderscore = require('./rules/no-uppercase-after-underscore');
 
 module.exports = {
     rules: {
         'no-extend-global-constructor': noExtendGlobalConstructor,
         'no-production-assert': noProductionAssert,
         'no-invalid-todo': noInvalidTodo,
+        'no-uppercase-after-underscore': noUppercaseAfterUnderscore
     },
 };
