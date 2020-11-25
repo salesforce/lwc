20ecfeb1aeb6159cd7f7736df46e76af9be42256
Josh Kovitz <jkovitz@salesforce.com>
2020-11-25 10:59:24 -0800

    fix: formatting

--- scripts/eslint-plugin/rules/no-uppercase-after-underscore.js
+++ scripts/eslint-plugin/rules/no-uppercase-after-underscore.js
@@ -5,11 +5,11 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
 /*
-* We prohibit attribute names that contain combination of hyphen and underscores characters.
-* We need to ensure that public properties aren't created in this format, as uppercase characters
-* will be translated to '-{character}' and users aren't likely to understand or expect this behaviour
-* https://developer.salesforce.com/docs/component-library/documentation/en/lwc/lwc.js_props_names
-*/
+ * We prohibit attribute names that contain combination of hyphen and underscores characters.
+ * We need to ensure that public properties aren't created in this format, as uppercase characters
+ * will be translated to '-{character}' and users aren't likely to understand or expect this behaviour
+ * https://developer.salesforce.com/docs/component-library/documentation/en/lwc/lwc.js_props_names
+ */
 
 const INVALID_PROPERTY_NAME_REGEX = /_[A-Z]/g;
 
@@ -17,17 +17,22 @@ module.exports = {
     meta: {
         type: 'problem',
         docs: {
-            description: 'Disallow uppercase characters following an underscore in property names'
-        }
+            description: 'Disallow uppercase characters following an underscore in property names',
+        },
     },
 
     create(context) {
         return {
             Identifier(node) {
                 if (node.name.match(INVALID_PROPERTY_NAME_REGEX)) {
-                    context.report(node, `Avoid uppercase characters following an underscore in property names: "${node.name.match(INVALID_PROPERTY_NAME_REGEX)[0]}"`);
+                    context.report(
+                        node,
+                        `Avoid uppercase characters following an underscore in property names: "${
+                            node.name.match(INVALID_PROPERTY_NAME_REGEX)[0]
+                        }"`
+                    );
                 }
-            }
-        }
-    }
-}
+            },
+        };
+    },
+};
