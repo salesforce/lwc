2d05d2f796715fd26e9ac343e6a1e32a8c57e502
Eugene Kashida <ekashida@gmail.com>
2020-12-19 02:51:03 -0800

    chore: improve test names

--- packages/integration-karma/test/shadow-dom/Event-properties/Event.currentTarget.spec.js
+++ packages/integration-karma/test/shadow-dom/Event-properties/Event.currentTarget.spec.js
@@ -2,17 +2,19 @@ import { createElement } from 'lwc';
 
 import Container from 'x/container';
 
-it('should be null when accessed asynchronously', function (done) {
-    const container = createElement('x-container', { is: Container });
-    document.body.appendChild(container);
+describe('Event.currentTarget', () => {
+    it('should be null when accessed asynchronously', function (done) {
+        const container = createElement('x-container', { is: Container });
+        document.body.appendChild(container);
 
-    container.addEventListener('test', (event) => {
-        expect(event.currentTarget).toEqual(container);
-        setTimeout(() => {
-            expect(event.currentTarget).toBeNull();
-            done();
+        container.addEventListener('test', (event) => {
+            expect(event.currentTarget).toEqual(container);
+            setTimeout(() => {
+                expect(event.currentTarget).toBeNull();
+                done();
+            });
         });
+        const div = container.shadowRoot.querySelector('div');
+        div.dispatchEvent(new CustomEvent('test', { bubbles: true, composed: true }));
     });
-    const div = container.shadowRoot.querySelector('div');
-    div.dispatchEvent(new CustomEvent('test', { bubbles: true, composed: true }));
 });
