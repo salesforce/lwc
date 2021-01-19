02d795d751958820b61f78650da86ac6ecdc6bb3
Eugene Kashida <ekashida@gmail.com>
2021-01-19 02:26:52 -0800

    test: refactor

--- packages/integration-karma/test/shadow-dom/Event-properties/Event.composedPath.spec.js
+++ packages/integration-karma/test/shadow-dom/Event-properties/Event.composedPath.spec.js
@@ -3,32 +3,30 @@ import { createElement } from 'lwc';
 import Container from 'x/container';
 
 describe('Event.composedPath', () => {
-    it('should return an empty array when asynchronously invoked', function (done) {
+    it('should return an empty array when asynchronously invoked', () => {
         const container = createElement('x-container', { is: Container });
         document.body.appendChild(container);
 
+        let _event;
         container.addEventListener('test', (event) => {
-            expect(event.composedPath()).toHaveSize(7);
-            setTimeout(() => {
-                expect(event.composedPath()).toHaveSize(0);
-                done();
-            });
+            _event = event;
         });
+
         const div = container.shadowRoot.querySelector('div');
         div.dispatchEvent(new CustomEvent('test', { bubbles: true, composed: true }));
+
+        expect(_event.composedPath()).toHaveSize(0);
     });
 
-    it('should not throw when invoked on an event with a target that is not an instance of Node', function (done) {
+    it('should not throw when invoked on an event with a target that is not an instance of Node', (done) => {
         const req = new XMLHttpRequest();
-        req.addEventListener('readystatechange', (event) => {
-            // Chrome and Safari return an empty array but Firefox returns an array containing a
-            // single instance of XMLHttpRequest.
+        req.addEventListener('test', (event) => {
+            // Not looking at return value because browsers have different implementations.
             expect(() => {
                 event.composedPath();
             }).not.toThrowError();
             done();
         });
-        req.open('GET', 'http://localhost');
-        req.send();
+        req.dispatchEvent(new CustomEvent('test'));
     });
 });
