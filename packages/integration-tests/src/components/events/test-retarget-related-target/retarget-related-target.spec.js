62e244b378904e8c93cda6e1d42d763f31c046a9
Eugene Kashida <ekashida@gmail.com>
2021-01-20 10:55:02 -0800

    test: disable in ie11 due to its quirks

--- packages/integration-tests/src/components/events/test-retarget-related-target/retarget-related-target.spec.js
+++ packages/integration-tests/src/components/events/test-retarget-related-target/retarget-related-target.spec.js
@@ -8,30 +8,33 @@ const assert = require('assert');
 
 const URL = '/retarget-related-target';
 
-describe('Retarget relatedTarget', () => {
-    before(async () => {
-        await browser.url(URL);
+if (process.env.COMPAT === 'false') {
+    describe('Retarget relatedTarget', () => {
+        before(async () => {
+            await browser.url(URL);
+        });
+
+        it('should retarget relatedTarget for MouseEvent', async () => {
+            const first = await browser.shadowDeep$(
+                'integration-retarget-related-target',
+                'integration-child.first',
+                'input'
+            );
+            const second = await browser.shadowDeep$(
+                'integration-retarget-related-target',
+                'integration-child.second',
+                'input'
+            );
+            const indicator = await browser.shadowDeep$(
+                'integration-retarget-related-target',
+                '.related-target-class-name'
+            );
+
+            await first.moveTo();
+            await second.moveTo();
+            await first.moveTo();
+
+            assert.strictEqual(await indicator.getText(), 'undefined, first, second');
+        });
     });
-
-    it('should retarget relatedTarget for MouseEvent', async () => {
-        const first = await browser.shadowDeep$(
-            'integration-retarget-related-target',
-            'integration-child.first',
-            'input'
-        );
-        const second = await browser.shadowDeep$(
-            'integration-retarget-related-target',
-            'integration-child.second',
-            'input'
-        );
-        const indicator = await browser.shadowDeep$(
-            'integration-retarget-related-target',
-            '.related-target-class-name'
-        );
-
-        await first.moveTo();
-        await second.moveTo();
-
-        assert.strictEqual(await indicator.getText(), 'undefined, first');
-    });
-});
+}
