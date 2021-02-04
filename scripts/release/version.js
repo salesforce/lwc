9ab6591b18e90c8a063991a57085f39ede7d7635
Pierre-Marie Dartus <p.dartus@salesforce.com>
2021-02-04 15:19:46 +0100

    chore: add back --force-publish flag

--- scripts/release/version.js
+++ scripts/release/version.js
@@ -14,6 +14,9 @@ const ARGS = [
     'version',
     // Use exact version number without using semver notation (i.e., ~ and ^)
     '--exact',
+    // Skip lerna change detection and force all the package to be re-published
+    '--force-publish',
+    // Skip git commit and tag push
     '--no-push',
 ];
 
