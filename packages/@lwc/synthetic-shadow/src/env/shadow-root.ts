40d0f678436ce33119f73db5e6a755dcc5c81541
Eugene Kashida <ekashida@gmail.com>
2021-02-08 01:43:13 -0800

    fix(synthetic-shadow): native shadow root support for pathComposer() (#2228)

---
+++ packages/@lwc/synthetic-shadow/src/env/shadow-root.ts
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+import { isNull } from '@lwc/shared';
+
+let NativeShadowRoot: any = null;
+if (typeof ShadowRoot !== 'undefined') {
+    NativeShadowRoot = ShadowRoot;
+}
+
+export const isInstanceOfNativeShadowRoot: (node: any) => boolean = isNull(NativeShadowRoot)
+    ? () => false
+    : (node) => node instanceof NativeShadowRoot;
