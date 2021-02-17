2000ca15a0ec6fe0bf1ff2bc2f30078202cf19c3
Eugene Kashida <ekashida@gmail.com>
2021-02-17 00:51:54 -0800

    refactor(babel-plugin-component): move component registration transform out of post-process (#2221)

--- packages/@lwc/babel-plugin-component/src/component.js
+++ packages/@lwc/babel-plugin-component/src/component.js
@@ -4,13 +4,69 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-const { generateError, getEngineImportSpecifiers } = require('./utils');
-const { LWC_PACKAGE_EXPORTS, LWC_SUPPORTED_APIS } = require('./constants');
+const { basename, extname } = require('path');
+
+const moduleImports = require('@babel/helper-module-imports');
 const { LWCClassErrors } = require('@lwc/errors');
 
-module.exports = function () {
+const { LWC_PACKAGE_ALIAS, LWC_SUPPORTED_APIS, REGISTER_COMPONENT_ID } = require('./constants');
+const { generateError, getEngineImportSpecifiers } = require('./utils');
+
+const TEMPLATE_KEY = 'tmpl';
+
+function getBaseName(classPath) {
+    const ext = extname(classPath);
+    return basename(classPath, ext);
+}
+
+function importDefaultTemplate(path, state) {
+    const { filename } = state.file.opts;
+    const componentName = getBaseName(filename);
+    return moduleImports.addDefault(path, `./${componentName}.html`, {
+        nameHint: TEMPLATE_KEY,
+    });
+}
+
+function needsComponentRegistration(path) {
+    return (
+        (path.isIdentifier() && path.node.name !== 'undefined' && path.node.name !== 'null') ||
+        // path.isMemberExpression() || // this will probably yield more false positives than anything else
+        path.isCallExpression() ||
+        path.isClassDeclaration() ||
+        path.isConditionalExpression()
+    );
+}
+
+module.exports = function ({ types: t }) {
+    function createRegisterComponent(declarationPath, state) {
+        const registerComponentId = moduleImports.addNamed(
+            declarationPath,
+            REGISTER_COMPONENT_ID,
+            LWC_PACKAGE_ALIAS
+        );
+        const templateIdentifier = importDefaultTemplate(declarationPath, state);
+        const statementPath = declarationPath.getStatementParent();
+        let node = declarationPath.node;
+
+        if (declarationPath.isClassDeclaration()) {
+            const hasIdentifier = t.isIdentifier(node.id);
+            if (hasIdentifier) {
+                statementPath.insertBefore(node);
+                node = node.id;
+            } else {
+                // if it does not have an id, we can treat it as a ClassExpression
+                t.toExpression(node);
+            }
+        }
+
+        return t.callExpression(registerComponentId, [
+            node,
+            t.objectExpression([t.objectProperty(t.identifier(TEMPLATE_KEY), templateIdentifier)]),
+        ]);
+    }
+
     return {
-        Program(path, state) {
+        Program(path) {
             const engineImportSpecifiers = getEngineImportSpecifiers(path);
 
             // validate internal api imports
@@ -22,11 +78,16 @@ module.exports = function () {
                     });
                 }
             });
+        },
 
-            // Store on state local identifiers referencing engine base component
-            state.componentBaseClassImports = engineImportSpecifiers
-                .filter(({ name }) => name === LWC_PACKAGE_EXPORTS.BASE_COMPONENT)
-                .map(({ path }) => path.get('local'));
+        ExportDefaultDeclaration(path, state) {
+            const implicitResolution = !state.opts.isExplicitImport;
+            if (implicitResolution) {
+                const declaration = path.get('declaration');
+                if (needsComponentRegistration(declaration)) {
+                    declaration.replaceWith(createRegisterComponent(declaration, state));
+                }
+            }
         },
     };
 };
--- packages/@lwc/babel-plugin-component/src/post-process/transform.js
+++ packages/@lwc/babel-plugin-component/src/post-process/transform.js
@@ -10,13 +10,10 @@
  * of all transformed decorators.
  */
 
-const { basename, extname } = require('path');
 const moduleImports = require('@babel/helper-module-imports');
+const { REGISTER_DECORATORS_ID } = require('../constants');
 const { isLWCNode } = require('../utils');
 const LWC_POST_PROCCESED = Symbol();
-
-const REGISTER_DECORATORS_ID = 'registerDecorators';
-const REGISTER_COMPONENT_ID = 'registerComponent';
 
 module.exports = function postProcess({ types: t }) {
     function collectDecoratedProperties(body) {
@@ -84,63 +81,7 @@ module.exports = function postProcess({ types: t }) {
         return t.callExpression(id, [klass, t.objectExpression(props)]);
     }
 
-    function getBaseName({ file }) {
-        const classPath = file.opts.filename;
-        const ext = extname(classPath);
-        return basename(classPath, ext);
-    }
-
-    function importDefaultTemplate(path, state) {
-        const componentName = getBaseName(state);
-        return moduleImports.addDefault(path, `./${componentName}.html`, {
-            nameHint: 'tmpl',
-        });
-    }
-
-    function createRegisterComponent(declarationPath, state) {
-        const id = moduleImports.addNamed(declarationPath, REGISTER_COMPONENT_ID, 'lwc');
-        const templateIdentifier = importDefaultTemplate(declarationPath, state);
-        const statementPath = declarationPath.getStatementParent();
-        let node = declarationPath.node;
-
-        if (declarationPath.isClassDeclaration()) {
-            const hasIdentifier = t.isIdentifier(node.id);
-            if (hasIdentifier) {
-                statementPath.insertBefore(node);
-                node = node.id;
-            } else {
-                // if it does not have an id, we can treat it as a ClassExpression
-                node.type = 'ClassExpression';
-            }
-        }
-
-        return t.callExpression(id, [
-            node,
-            t.objectExpression([t.objectProperty(t.identifier('tmpl'), templateIdentifier)]),
-        ]);
-    }
-
-    function needsComponentRegistration(path) {
-        return (
-            (path.isIdentifier() && path.node.name !== 'undefined' && path.node.name !== 'null') ||
-            // path.isMemberExpression() || // this will probably yield more false positives than anything else
-            path.isCallExpression() ||
-            path.isClassDeclaration() ||
-            path.isConditionalExpression()
-        );
-    }
-
     return {
-        // Register component
-        ExportDefaultDeclaration(path, state) {
-            const implicitResolution = !state.opts.isExplicitImport;
-            if (implicitResolution) {
-                const declaration = path.get('declaration');
-                if (needsComponentRegistration(declaration)) {
-                    declaration.replaceWith(createRegisterComponent(declaration, state));
-                }
-            }
-        },
         // Decorator collector for class expressions
         ClassExpression(path) {
             const { node } = path;
