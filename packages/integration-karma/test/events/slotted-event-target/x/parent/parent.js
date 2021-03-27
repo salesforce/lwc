39d8721b9640682f423e54175388290e594fd1c7
Eugene Kashida <ekashida@gmail.com>
2021-03-27 10:42:10 -0700

    chore: remove redundant slotted-event-target

--- packages/integration-karma/test/events/slotted-event-target/x/parent/parent.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement, track } from 'lwc';
-
-export default class XParent extends LightningElement {
-    @track eventTargetIsCorrectTag = false;
-    handleClick(evt) {
-        this.eventTargetIsCorrectTag = evt.target.tagName === 'X-CHILD';
-    }
-}
--- packages/integration-karma/test/events/slotted-event-target/x/slottedNativeElement/slottedNativeElement.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement, track } from 'lwc';
-
-export default class SlottedNativeElement extends LightningElement {
-    @track eventTargetIsPTag = false;
-    handleClick(evt) {
-        this.eventTargetIsPTag = evt.target.tagName === 'P';
-    }
-}
