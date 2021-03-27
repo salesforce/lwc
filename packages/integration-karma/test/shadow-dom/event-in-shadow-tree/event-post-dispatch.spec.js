049646d917dae4e28a2a736fb861ddeee5fefcd6
Eugene Kashida <ekashida@gmail.com>
2021-03-27 04:31:18 -0700

    test: enable event-post-dispatch suite
    - enable after retargeting bug fix
    - enable composed:false event target null tests (fixed in safari: https://bugs.webkit.org/show_bug.cgi?id=218638)
    - replace component with x-container
    - remove nested shadow tests because this suite is not about retargeting

--- packages/integration-karma/test/shadow-dom/event-in-shadow-tree/event-post-dispatch.spec.js
+++ packages/integration-karma/test/shadow-dom/event-in-shadow-tree/event-post-dispatch.spec.js
@@ -4,151 +4,85 @@
 import { createElement } from 'lwc';
 import { extractDataIds } from 'test-utils';
 
-import ShadowTree from 'x/shadowTree';
-import NestedShadowTree from 'x/nestedShadowTree';
+import Container from 'x/container';
 
-function createShadowTree(parentNode) {
-    const elm = createElement('x-shadow-tree', { is: ShadowTree });
-    elm.setAttribute('data-id', 'x-shadow-tree');
-    parentNode.appendChild(elm);
-
-    return extractDataIds(elm);
+function assertEventStateReset(event) {
+    expect(event.eventPhase).toBe(0);
+    expect(event.currentTarget).toBe(null);
+    expect(event.composedPath().length).toBe(0);
 }
 
-function assertEventStateReset(evt) {
-    expect(evt.eventPhase).toBe(0);
-    expect(evt.currentTarget).toBe(null);
-    expect(evt.composedPath().length).toBe(0);
+function createComponent() {
+    const element = createElement('x-container', { is: Container });
+    element.setAttribute('data-id', 'x-container');
+    document.body.appendChild(element);
+    return extractDataIds(element);
 }
 
-function createNestedShadowTree(parentNode) {
-    const elm = createElement('x-nested-shadow-tree', { is: NestedShadowTree });
-    elm.setAttribute('data-id', 'x-nested-shadow-tree');
-    parentNode.appendChild(elm);
+describe('single shadow boundary', () => {
+    describe('native element', () => {
+        it('{ bubbles: true, composed: true }', () => {
+            const nodes = createComponent();
+            const event = new CustomEvent('test', { bubbles: true, composed: true });
+            nodes.container_div.dispatchEvent(event);
 
-    return extractDataIds(elm);
-}
-
-// TODO [#1129]: Invoking dispatchEvent on nodes rendered by the template doesn't respect retargetting
-if (process.env.NATIVE_SHADOW) {
-    describe('Event properties post dispatch on node in a shadow tree', () => {
-        let nodes;
-        beforeAll(() => {
-            nodes = createShadowTree(document.body);
+            assertEventStateReset(event);
+            expect(event.target).toBe(nodes['x-container']);
         });
 
-        it('element (composed: true)', () => {
-            const evt = new CustomEvent('test', { composed: true, bubbles: true });
-            nodes.span.dispatchEvent(evt);
-            assertEventStateReset(evt);
-            expect(evt.target).toBe(nodes['x-shadow-tree']);
-        });
+        it('{ bubbles: true, composed: false }', () => {
+            const nodes = createComponent();
+            const event = new CustomEvent('test', { bubbles: true, composed: false });
+            nodes.container_div.dispatchEvent(event);
 
-        // WebKit bug - https://bugs.webkit.org/show_bug.cgi?id=206374
-        // In Safari, the event target is not null.
-        xit('element (composed: false)', () => {
-            const evt = new CustomEvent('test', { bubbles: true });
-            nodes.span.dispatchEvent(evt);
-
-            assertEventStateReset(evt);
-            expect(evt.target).toBe(null);
-        });
-
-        it('element added via lwc:dom="manual" (composed: true)', () => {
-            const evt = new CustomEvent('test', { composed: true, bubbles: true });
-            nodes['span-manual'].dispatchEvent(evt);
-
-            assertEventStateReset(evt);
-            expect(evt.target).toBe(nodes['x-shadow-tree']);
-        });
-
-        // WebKit bug - https://bugs.webkit.org/show_bug.cgi?id=206374
-        // In Safari, the event target is not null.
-        xit('element added via lwc:dom="manual" (composed: false)', () => {
-            const evt = new CustomEvent('test', { bubbles: true });
-            nodes['span-manual'].dispatchEvent(evt);
-
-            assertEventStateReset(evt);
-            expect(evt.target).toBe(null);
-        });
-
-        it('component (composed: true)', () => {
-            const evt = new CustomEvent('test', { composed: true, bubbles: true });
-            nodes['x-shadow-tree'].dispatchEventComponent(evt);
-
-            expect(evt.target).toBe(nodes['x-shadow-tree']);
-            assertEventStateReset(evt);
-        });
-
-        // WebKit bug - https://bugs.webkit.org/show_bug.cgi?id=206374
-        // In Safari, the event target is not null.
-        xit('component (composed: false)', () => {
-            const evt = new CustomEvent('test', { bubbles: true });
-            nodes['x-shadow-tree'].dispatchEventComponent(evt);
-
-            assertEventStateReset(evt);
-            expect(evt.target).toBe(nodes['x-shadow-tree']);
+            assertEventStateReset(event);
+            expect(event.target).toBe(null);
         });
     });
 
-    describe('Event properties post dispatch on node in a nested shadow tree', () => {
-        let nodes;
-        beforeAll(() => {
-            nodes = createNestedShadowTree(document.body);
+    describe('lwc:dom="manual" element', () => {
+        it('{ bubbles: true, composed: true }', () => {
+            const nodes = createComponent();
+            const event = new CustomEvent('test', { bubbles: true, composed: true });
+            nodes.container_span_manual.dispatchEvent(event);
+
+            // lwc:dom=manual is async due to MutationObserver
+            return Promise.resolve().then(() => {
+                assertEventStateReset(event);
+                expect(event.target).toBe(nodes['x-container']);
+            });
         });
 
-        it('element (composed: true)', () => {
-            const evt = new CustomEvent('test', { composed: true, bubbles: true });
-            nodes.span.dispatchEvent(evt);
+        it('{ bubbles: true, composed: false }', () => {
+            const nodes = createComponent();
+            const event = new CustomEvent('test', { bubbles: true, composed: false });
+            nodes.container_span_manual.dispatchEvent(event);
 
-            assertEventStateReset(evt);
-            expect(evt.target).toBe(nodes['x-nested-shadow-tree']);
-        });
-
-        // WebKit bug - https://bugs.webkit.org/show_bug.cgi?id=206374
-        // In Safari, the event target is not null.
-        xit('element (composed: false)', () => {
-            const evt = new CustomEvent('test', { bubbles: true });
-            nodes.span.dispatchEvent(evt);
-
-            assertEventStateReset(evt);
-            expect(evt.target).toBe(null);
-        });
-
-        it('element added via lwc:dom="manual" (composed: true)', () => {
-            const evt = new CustomEvent('test', { composed: true, bubbles: true });
-            nodes['span-manual'].dispatchEvent(evt);
-
-            expect(evt.target).toBe(nodes['x-nested-shadow-tree']);
-            assertEventStateReset(evt);
-        });
-
-        // WebKit bug - https://bugs.webkit.org/show_bug.cgi?id=206374
-        // In Safari, the event target is not null.
-        xit('element added via lwc:dom="manual" (composed: false)', () => {
-            const evt = new CustomEvent('test', { bubbles: true });
-            nodes['span-manual'].dispatchEvent(evt);
-
-            assertEventStateReset(evt);
-            expect(evt.target).toBe(null);
-        });
-
-        it('component (composed: true)', () => {
-            const evt = new CustomEvent('test', { composed: true, bubbles: true });
-            nodes['x-shadow-tree'].dispatchEventComponent(evt);
-
-            assertEventStateReset(evt);
-            expect(evt.target).toBe(nodes['x-nested-shadow-tree']);
-        });
-
-        // WebKit bug - https://bugs.webkit.org/show_bug.cgi?id=206374
-        // In Safari, the event target is not null.
-        xit('component (composed: false)', () => {
-            const evt = new CustomEvent('test', { bubbles: true });
-            nodes['x-shadow-tree'].dispatchEventComponent(evt);
-
-            assertEventStateReset(evt);
-            expect(evt.target).toBe(null);
+            // lwc:dom=manual is async due to MutationObserver
+            return Promise.resolve().then(() => {
+                assertEventStateReset(event);
+                expect(event.target).toBe(null);
+            });
         });
     });
-}
+
+    describe('component', () => {
+        it('{ bubbles: true, composed: true }', () => {
+            const nodes = createComponent();
+            const event = new CustomEvent('test', { bubbles: true, composed: true });
+            nodes['x-container'].dispatchEventComponent(event);
+
+            assertEventStateReset(event);
+            expect(event.target).toBe(nodes['x-container']);
+        });
+
+        it('{ bubbles: true, composed: false }', () => {
+            const nodes = createComponent();
+            const event = new CustomEvent('test', { bubbles: true, composed: false });
+            nodes['x-container'].dispatchEventComponent(event);
+
+            assertEventStateReset(event);
+            expect(event.target).toBe(nodes['x-container']);
+        });
+    });
+});
--- packages/integration-karma/test/shadow-dom/event-in-shadow-tree/x/container/container.js
+++ packages/integration-karma/test/shadow-dom/event-in-shadow-tree/x/container/container.js
@@ -1,3 +1,15 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class extends LightningElement {}
+export default class extends LightningElement {
+    @api
+    dispatchEventComponent(event) {
+        this.dispatchEvent(event);
+    }
+
+    renderedCallback() {
+        const spanManual = document.createElement('span');
+        spanManual.setAttribute('data-id', 'container_span_manual');
+        const span = this.template.querySelector('[data-id="container_span"]');
+        span.appendChild(spanManual);
+    }
+}
