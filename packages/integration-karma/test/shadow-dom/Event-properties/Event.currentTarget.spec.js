940a93341d3e13a4ae6378c15ecb164cb30cac79
Eugene Kashida <ekashida@gmail.com>
2021-03-31 06:53:38 -0700

    fix(synthetic-shadow): correct currentTarget in shadow root listeners (#2287)

--- packages/integration-karma/test/shadow-dom/Event-properties/Event.currentTarget.spec.js
+++ packages/integration-karma/test/shadow-dom/Event-properties/Event.currentTarget.spec.js
@@ -17,4 +17,30 @@ describe('Event.currentTarget', () => {
         const div = container.shadowRoot.querySelector('div');
         div.dispatchEvent(new CustomEvent('test', { bubbles: true, composed: true }));
     });
+
+    it('should reference the host element', (done) => {
+        const container = createElement('x-container', { is: Container });
+        document.body.appendChild(container);
+
+        container.addEventListener('test', (event) => {
+            expect(event.currentTarget).toEqual(container);
+            done();
+        });
+
+        const child = container.shadowRoot.querySelector('x-child');
+        child.dispatchEvent(new CustomEvent('test', { bubbles: true, composed: true }));
+    });
+
+    it('should reference the shadow root', (done) => {
+        const container = createElement('x-container', { is: Container });
+        document.body.appendChild(container);
+
+        container.shadowRoot.addEventListener('test', (event) => {
+            expect(event.currentTarget).toEqual(container.shadowRoot);
+            done();
+        });
+
+        const child = container.shadowRoot.querySelector('x-child');
+        child.dispatchEvent(new CustomEvent('test', { bubbles: true, composed: true }));
+    });
 });
