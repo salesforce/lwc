d1acc12dbeb16ab64b2a93ba715982aef37f8fd5
Nolan Lawson <nolan@nolanlawson.com>
2021-04-20 09:11:05 -0700

    fix: update packages/@lwc/compiler/src/transformers/javascript.ts
    Co-authored-by: Abdulsattar Mohammed <abdulsattarmohammed@salesforce.com>

--- packages/@lwc/compiler/src/transformers/javascript.ts
+++ packages/@lwc/compiler/src/transformers/javascript.ts
@@ -33,7 +33,7 @@ export default function scriptTransform(
             configFile: false,
             plugins: [
                 [lwcClassTransformPlugin, { isExplicitImport, dynamicImports }],
-                // Loose mode is only required for light DOM, due to the static field (`shadow`).
+                // Strict (not `loose`) mode is only required for light DOM, due to the static field (`shadow`).
                 // It may introduce some side effects or perf impact to use strict mode, so
                 // we should only use strict mode when light DOM is enabled.
                 [babelClassPropertiesPlugin, { loose: !options.experimentalLightDOMComponent }],
