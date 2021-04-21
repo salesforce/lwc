0b69d3adcc994b059cb28163a9a0aafc0584624a
Mohammed Abdul Sattar <abdulsattarmohammed@salesforce.com>
2021-04-21 16:04:34 +0530

    test: fix flags not being set properly when run in parallel

--- packages/integration-karma/test/light-dom/basic/index.spec.js
+++ packages/integration-karma/test/light-dom/basic/index.spec.js
@@ -2,23 +2,20 @@ import { createElement, setFeatureFlagForTest } from 'lwc';
 
 import Test from 'x/test';
 describe('Basic Light DOM', () => {
-    beforeEach(() => {
-        setFeatureFlagForTest('ENABLE_LIGHT_DOM_COMPONENTS', true);
-    });
-    afterEach(() => {
-        setFeatureFlagForTest('ENABLE_LIGHT_DOM_COMPONENTS', false);
-    });
     it('should render properly', () => {
+        setFeatureFlagForTest('ENABLE_LIGHT_DOM_COMPONENTS', true);
         const elm = createElement('x-test', { is: Test });
         document.body.appendChild(elm);
 
         expect(elm.shadowRoot).toBeNull();
         expect(elm.firstChild.innerText).toEqual('Hello, Light DOM');
+        setFeatureFlagForTest('ENABLE_LIGHT_DOM_COMPONENTS', false);
     });
 });
 
-describe('Basic Light DOM when flag is disabled', () => {
-    it('should render properly', () => {
+describe('when flag is disabled', () => {
+    it('should render to Shadow DOM', () => {
+        setFeatureFlagForTest('ENABLE_LIGHT_DOM_COMPONENTS', false);
         const elm = createElement('x-test', { is: Test });
         document.body.appendChild(elm);
 
