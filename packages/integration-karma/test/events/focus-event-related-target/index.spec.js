1e18bfe2d0a91e5a3ed7d1532093499f25b56e78
Nolan Lawson <nlawson@salesforce.com>
2021-04-22 12:08:54 -0700

    test: add note about test which may fail locally

--- packages/integration-karma/test/events/focus-event-related-target/index.spec.js
+++ packages/integration-karma/test/events/focus-event-related-target/index.spec.js
@@ -1,7 +1,11 @@
 import { createElement } from 'lwc';
 import Container from 'x/container';
 
-it('should retarget relatedTarget', function () {
+// Note that focusin events are not fired in Chrome and Firefox if the browser content window is not focused.
+// Test for yourself: https://bl.ocks.org/nolanlawson/raw/d8ddc518041e9bf12498b8b50b39df95/
+// So this test may "flap" during local development if your browser window isn't focused (e.g. you're focused on
+// the DevTools instead).
+it('should retarget relatedTarget (note this test may fail if the browser window is not focused)', function () {
     const elm = createElement('x-container', { is: Container });
     document.body.appendChild(elm);
 
