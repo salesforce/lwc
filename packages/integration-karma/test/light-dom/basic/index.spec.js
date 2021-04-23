30ee43a2d3656617e12b3f024040699ed7ac33d9
Mohammed Abdul Sattar <abdulsattarmohammed@salesforce.com>
2021-04-23 17:54:44 +0530

    test: add test for null template

--- packages/integration-karma/test/light-dom/basic/index.spec.js
+++ packages/integration-karma/test/light-dom/basic/index.spec.js
@@ -1,6 +1,7 @@
-import { createElement, setFeatureFlagForTest } from 'lwc';
+import { createElement, setFeatureFlagForTest, LightningElement } from 'lwc';
 
 import Test from 'x/test';
+
 describe('Basic Light DOM', () => {
     it('should render properly', () => {
         setFeatureFlagForTest('ENABLE_LIGHT_DOM_COMPONENTS', true);
@@ -14,5 +15,26 @@ describe('Basic Light DOM', () => {
     it('should render to Shadow DOM when feature is disabled', () => {
         setFeatureFlagForTest('ENABLE_LIGHT_DOM_COMPONENTS', false);
         expect(() => createElement('x-test', { is: Test })).toThrowError();
+    });
+
+    it('should return null for template', () => {
+        setFeatureFlagForTest('ENABLE_LIGHT_DOM_COMPONENTS', true);
+
+        const spy = spyOn(console, 'error');
+        let template;
+        class TemplateTest extends LightningElement {
+            static shadow = false;
+            connectedCallback() {
+                template = this.template;
+            }
+        }
+
+        const elm = createElement('x-test', { is: TemplateTest });
+        document.body.appendChild(elm);
+        expect(template).toBeNull();
+        expect(spy.calls.mostRecent().args[0].message).toEqual(
+            '[LWC error]: Template returns null in components with no shadow. Since there is no shadow, all the operations can be performed on `this` itself. e.g. instead of `this.template.querySelector`, use `this.querySelector`.'
+        );
+        setFeatureFlagForTest('ENABLE_LIGHT_DOM_COMPONENTS', false);
     });
 });
