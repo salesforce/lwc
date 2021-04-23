0c60995c3d3938602791e2f19818c263644378b5
Nolan Lawson <nlawson@salesforce.com>
2021-04-23 11:51:20 -0700

    test: move to karma, check boolean values

--- packages/integration-karma/test/light-dom/shadow-getter/index.spec.js
+++ packages/integration-karma/test/light-dom/shadow-getter/index.spec.js
@@ -1,7 +1,8 @@
-import { createElement, setFeatureFlagForTest } from 'lwc';
+import { createElement, setFeatureFlagForTest, LightningElement } from 'lwc';
 
 import Test from 'x/test';
-describe('Basic Light DOM', () => {
+
+describe('Light DOM shadow getter', () => {
     it('should render properly', () => {
         setFeatureFlagForTest('ENABLE_LIGHT_DOM_COMPONENTS', true);
         const elm = createElement('x-test', { is: Test });
@@ -10,5 +11,19 @@ describe('Basic Light DOM', () => {
         expect(elm.shadowRoot).toBeNull();
         expect(elm.firstChild.innerText).toEqual('Hello, Light DOM');
         setFeatureFlagForTest('ENABLE_LIGHT_DOM_COMPONENTS', false);
+    });
+
+    it('shadow should be true for LightningElement', () => {
+        expect(LightningElement.shadow).toEqual(true);
+    });
+
+    it('shadow should not override LightningElement.shadow', () => {
+        class Base extends LightningElement {}
+        Base.shadow = false;
+        class Test extends Base {}
+        Test.shadow = true;
+        expect(LightningElement.shadow).toEqual(true);
+        expect(Base.shadow).toEqual(false);
+        expect(Test.shadow).toEqual(true);
     });
 });
