7f21f3455dc7ce7f08d41191251c2fc581c473a8
Nolan Lawson <nlawson@salesforce.com>
2021-04-27 16:19:38 -0700

    fix: fix license header

--- packages/@lwc/engine-dom/src/stylesheets.ts
+++ packages/@lwc/engine-dom/src/stylesheets.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+
 import { create, isUndefined } from '@lwc/shared';
 
 const elementsToStyleContents = new WeakMap<Element, { [content: string]: true } | undefined>();
