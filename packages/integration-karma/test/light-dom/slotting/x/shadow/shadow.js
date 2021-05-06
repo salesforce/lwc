1026d07d052edf9d236d52a7a890538a637d5318
Mohammed Abdul Sattar <abdulsattarmohammed@salesforce.com>
2021-05-06 19:33:53 +0530

    test: remove unused component

--- packages/integration-karma/test/light-dom/slotting/x/shadow/shadow.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class Shadow extends LightningElement {
-    connectedCallback() {
-        this.template.host.appendChild(document.createElement('div'));
-    }
-}
