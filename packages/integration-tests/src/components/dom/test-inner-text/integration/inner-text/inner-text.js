f1d496bad24ea50165008aa048ae56356a524c6a
Jose David Rodriguez Velasco <jodarove@gmail.com>
2021-05-14 14:04:35 -0700

    fix: remove unnecessary api methods

--- packages/integration-tests/src/components/dom/test-inner-text/integration/inner-text/inner-text.js
+++ packages/integration-tests/src/components/dom/test-inner-text/integration/inner-text/inner-text.js
@@ -1,17 +1,7 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement } from 'lwc';
 
 export default class InnerText extends LightningElement {
     _innerText = null;
-
-    @api
-    getTypedText() {
-        return this.template.querySelector('input').value;
-    }
-
-    @api
-    getInnerText() {
-        return this._innerText;
-    }
 
     computeInputInnerText(evt) {
         this._innerText = evt.target.innerText;
