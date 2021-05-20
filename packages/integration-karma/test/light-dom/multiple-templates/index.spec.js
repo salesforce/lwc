6c1e7398bc96a23f6e0816e9db004b94bde89296
Nolan Lawson <nolan@nolanlawson.com>
2021-05-20 09:37:22 -0700

    fix: update packages/integration-karma/test/light-dom/multiple-templates/index.spec.js
    Co-authored-by: Pierre-Marie Dartus <p.dartus@salesforce.com>

--- packages/integration-karma/test/light-dom/multiple-templates/index.spec.js
+++ packages/integration-karma/test/light-dom/multiple-templates/index.spec.js
@@ -21,7 +21,7 @@ describe('multiple templates', () => {
         expect(getComputedStyle(element.querySelector('div')).marginLeft).toEqual('0px');
         element.querySelector('div').setAttribute('foo', '');
         element.next();
-        return new Promise((resolve) => requestAnimationFrame(() => resolve())).then(() => {
+        return Promise.resolve().then(() => {
             expect(element.querySelector('div').textContent).toEqual('b');
             expect(getComputedStyle(element.querySelector('div')).color).toEqual('rgb(0, 0, 0)');
             expect(getComputedStyle(element.querySelector('div')).marginLeft).toEqual('10px');
