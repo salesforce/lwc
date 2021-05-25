acbe3f960f8de8c3257a70e069a5c2c3888bafdf
Eugene Kashida <ekashida@gmail.com>
2021-05-24 20:47:48 -0700

    test: add static idref scenario

--- packages/integration-karma/test/synthetic-shadow/scoped-id/multiple-idrefs.spec.js
+++ packages/integration-karma/test/synthetic-shadow/scoped-id/multiple-idrefs.spec.js
@@ -2,16 +2,30 @@ import { createElement } from 'lwc';
 
 import Multiple from 'x/multipleIdrefs';
 
-it('should handle multiple idrefs', () => {
+it('should handle multiple idrefs when set dynamically', () => {
     const elm = createElement('x-idref', { is: Multiple });
     document.body.appendChild(elm);
 
     const aomori = elm.shadowRoot.querySelector('.aomori');
     const hokkaido = elm.shadowRoot.querySelector('.hokkaido');
-    const input = elm.shadowRoot.querySelector('input');
+    const input = elm.shadowRoot.querySelector('.dynamic');
 
     expect(aomori.id).toMatch(/^aomori-\w+/);
     expect(hokkaido.id).toMatch(/^hokkaido-\w+/);
     expect(input.ariaLabelledBy).toContain(aomori.id);
     expect(input.ariaLabelledBy).toContain(hokkaido.id);
+});
+
+it('should handle multiple idrefs when set statically', () => {
+    const elm = createElement('x-idref', { is: Multiple });
+    document.body.appendChild(elm);
+
+    const aomori = elm.shadowRoot.querySelector('.aomori');
+    const iwate = elm.shadowRoot.querySelector('.iwate');
+    const input = elm.shadowRoot.querySelector('.static');
+
+    expect(aomori.id).toMatch(/^aomori-\w+/);
+    expect(iwate.id).toMatch(/^iwate-\w+/);
+    expect(input.ariaLabelledBy).toContain(aomori.id);
+    expect(input.ariaLabelledBy).toContain(iwate.id);
 });
