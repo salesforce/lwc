78d2f01fbe64245727ae841623b3f75b1e25a905
Mohammed Abdul Sattar <abdulsattarmohammed@salesforce.com>
2021-06-01 14:03:47 +0530

    test: add test and simplify another

--- packages/integration-karma/test/light-dom/slotting/index.spec.js
+++ packages/integration-karma/test/light-dom/slotting/index.spec.js
@@ -6,6 +6,7 @@ import DynamicChildren from 'x/dynamicChildren';
 import LightConsumer from 'x/lightConsumer';
 import ShadowConsumer from 'x/shadowConsumer';
 import ConditionalSlot from 'x/conditionalSlot';
+import ConditionalSlotted from 'x/conditionalSlotted';
 
 function createTestElement(tag, component) {
     const elm = createElement(tag, { is: component });
@@ -21,58 +22,39 @@ describe('Slotting', () => {
     afterEach(() => {
         setFeatureFlagForTest('ENABLE_LIGHT_DOM_COMPONENTS', false);
     });
-    // eslint-disable-next-line jest/no-focused-tests
     it('should render properly', () => {
         const nodes = createTestElement('x-default-slot', BasicSlot);
 
         expect(Array.from(nodes['x-container'].childNodes)).toEqual([
-            nodes['container-upper-slot-top'],
             nodes['upper-text'],
-            nodes['container-upper-slot-bottom'],
-            nodes['container-default-slot-top'],
             nodes['default-text'],
-            nodes['container-default-slot-bottom'],
-            nodes['container-lower-slot-top'],
             nodes['lower-text'],
-            nodes['container-lower-slot-bottom'],
         ]);
     });
 
     it('should render dynamic children', async () => {
         const nodes = createTestElement('x-dynamic-children', DynamicChildren);
         expect(Array.from(nodes['x-light-container'].childNodes)).toEqual([
-            nodes['container-upper-slot-top'],
             nodes['container-upper-slot-default'],
-            nodes['container-upper-slot-bottom'],
-            nodes['container-default-slot-top'],
             nodes['1'],
             nodes['2'],
             nodes['3'],
             nodes['4'],
             nodes['5'],
-            nodes['container-default-slot-bottom'],
-            nodes['container-lower-slot-top'],
             nodes['container-lower-slot-default'],
-            nodes['container-lower-slot-bottom'],
         ]);
 
         nodes.button.click();
         await Promise.resolve();
 
         expect(Array.from(nodes['x-light-container'].childNodes)).toEqual([
-            nodes['container-upper-slot-top'],
             nodes['container-upper-slot-default'],
-            nodes['container-upper-slot-bottom'],
-            nodes['container-default-slot-top'],
             nodes['5'],
             nodes['4'],
             nodes['3'],
             nodes['2'],
             nodes['1'],
-            nodes['container-default-slot-bottom'],
-            nodes['container-lower-slot-top'],
             nodes['container-lower-slot-default'],
-            nodes['container-lower-slot-bottom'],
         ]);
     });
 
@@ -89,7 +71,7 @@ describe('Slotting', () => {
         const nodes = createTestElement('x-shadow-consumer', ShadowConsumer);
 
         expect(nodes['x-shadow-consumer'].shadowRoot.innerHTML).toEqual(
-            '<x-light-container><p data-id="container-upper-slot-top">Upper slot top</p><p data-id="container-upper-slot-default">Upper slot default</p><p data-id="container-upper-slot-bottom">Default slot bottom</p><p data-id="container-default-slot-top">Default slot top</p><p data-id="shadow-consumer-text">Hello from Shadow DOM</p><p data-id="container-default-slot-bottom">Default slot bottom</p><p data-id="container-lower-slot-top">Lower slot top</p><p data-id="container-lower-slot-default">Lower slot default</p><p data-id="container-lower-slot-bottom">Lower slot bottom</p></x-light-container>'
+            '<x-light-container><p data-id="container-upper-slot-default">Upper slot default</p><p data-id="shadow-consumer-text">Hello from Shadow DOM</p><p data-id="container-lower-slot-default">Lower slot default</p></x-light-container>'
         );
     });
 
@@ -100,5 +82,18 @@ describe('Slotting', () => {
         nodes.button.click();
         await Promise.resolve();
         expect(Array.from(elm.childNodes)).toEqual([nodes.button]);
+    });
+
+    it('removes slotted content properly', async () => {
+        const nodes = createTestElement('x-conditional-slotted', ConditionalSlotted);
+        const elm = nodes['x-conditional-slotted'];
+        expect(elm.innerHTML).toEqual(
+            '<x-conditional-slot data-id="conditional-slot"><p data-id="slotted-text">Slotted content</p><button data-id="button">Toggle</button></x-conditional-slot>'
+        );
+        nodes.button.click();
+        await Promise.resolve();
+        expect(elm.innerHTML).toEqual(
+            '<x-conditional-slot data-id="conditional-slot"><button data-id="button">Toggle</button></x-conditional-slot>'
+        );
     });
 });
