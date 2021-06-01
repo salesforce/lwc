80fa7db9ab40093a8e62c28b8d489624498e7c3e
Mohammed Abdul Sattar <abdulsattarmohammed@salesforce.com>
2021-06-01 10:28:32 +0530

    test: add template compiler fixture

--- packages/@lwc/template-compiler/src/__tests__/fixtures/directive-render-mode/template/expected.js
+++ packages/@lwc/template-compiler/src/__tests__/fixtures/directive-render-mode/template/expected.js
@@ -1,7 +1,7 @@
 import { registerTemplate } from "lwc";
 function tmpl($api, $cmp, $slotset, $ctx) {
-  const { t: api_text, h: api_element } = $api;
-  return [
+  const { t: api_text, h: api_element, s: api_slot, f: api_flatten } = $api;
+  return api_flatten([
     api_element(
       "p",
       {
@@ -9,8 +9,28 @@ function tmpl($api, $cmp, $slotset, $ctx) {
       },
       [api_text("Root")]
     ),
-  ];
+    api_slot(
+      "",
+      {
+        key: 1,
+      },
+      [api_text("Default")],
+      $slotset
+    ),
+    api_slot(
+      "named",
+      {
+        attrs: {
+          name: "named",
+        },
+        key: 2,
+      },
+      [api_text("Named")],
+      $slotset
+    ),
+  ]);
 }
 export default registerTemplate(tmpl);
+tmpl.slots = ["", "named"];
 tmpl.stylesheets = [];
 tmpl.renderMode = "light";
