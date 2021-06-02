f5c1deb63d8a08350c4d563214395dc1b060f96b
Jose David Rodriguez Velasco <jodarove@gmail.com>
2021-06-02 09:30:14 -0700

    fix(engine-core): resolve circular dependencies of module (#2349)
    * fix(engine-core): resolve circular dependencies of module
    
    * Update packages/@lwc/engine-core/src/shared/circular-module-dependencies.ts
    
    Co-authored-by: Pierre-Marie Dartus <p.dartus@salesforce.com>
    
    * Update packages/@lwc/engine-core/src/shared/circular-module-dependencies.ts
    
    Co-authored-by: Pierre-Marie Dartus <p.dartus@salesforce.com>
    
    Co-authored-by: Pierre-Marie Dartus <p.dartus@salesforce.com>

--- packages/integration-karma/test/integrations/aura/index.spec.js
+++ packages/integration-karma/test/integrations/aura/index.spec.js
@@ -10,3 +10,14 @@ it('should support component class marked to be resolved as circular reference',
         .shadowRoot.querySelector('.child');
     expect(actual).toBeDefined();
 });
+
+it('should resolve component class of module when it is marked as circular reference', () => {
+    const elm = createElement('x-parent', { is: Parent });
+    elm.renderCircularModule = true;
+    document.body.appendChild(elm);
+    // Verifying that shadow tree was created to ensure the component class was successfully processed
+    const actual = elm.shadowRoot
+        .querySelector('x-child-module-marked-as-circular')
+        .shadowRoot.querySelector('.child');
+    expect(actual).toBeDefined();
+});
