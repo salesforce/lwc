1cfc01879aa13e1099372c5dbcf18fe3526f674c
Eugene Kashida <ekashida@gmail.com>
2021-06-03 19:40:24 -0700

    test: only non-compat

--- packages/integration-karma/test/mixed-shadow-mode/scoped-ids/scoped-ids.spec.js
+++ packages/integration-karma/test/mixed-shadow-mode/scoped-ids/scoped-ids.spec.js
@@ -1,27 +1,7 @@
 import { createElement } from 'lwc';
 import Test from 'x/test';
 
-if (process.env.COMPAT) {
-    it('should fallback to id-mangling (static)', () => {
-        const elm = createElement('x-test', { is: Test });
-        document.body.appendChild(elm);
-        const div = elm.shadowRoot.querySelector('.shizuoka');
-        const input = elm.shadowRoot.querySelector('.idref-static');
-
-        expect(div.id).toMatch(/^shizuoka-\w+$/);
-        expect(input.ariaDescribedBy).toContain(div.id);
-    });
-
-    it('should fallback to id-mangling (dynamic)', () => {
-        const elm = createElement('x-test', { is: Test });
-        document.body.appendChild(elm);
-        const div = elm.shadowRoot.querySelector('.yamanashi');
-        const input = elm.shadowRoot.querySelector('.idref-dynamic');
-
-        expect(div.id).toMatch(/^yamanashi-\w+$/);
-        expect(input.ariaLabelledBy).toContain(div.id);
-    });
-} else {
+if (!process.env.COMPAT) {
     it('should entrust id scoping to native shadow (static)', () => {
         const elm = createElement('x-test', { is: Test });
         document.body.appendChild(elm);
