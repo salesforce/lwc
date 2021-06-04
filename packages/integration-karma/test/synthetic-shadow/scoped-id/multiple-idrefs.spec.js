c1506469a2557148a5d95530ef197c529dd230d1
Eugene Kashida <ekashida@gmail.com>
2021-06-03 17:50:48 -0700

    test: native mode id/href assertions

--- packages/integration-karma/test/synthetic-shadow/scoped-id/multiple-idrefs.spec.js
+++ packages/integration-karma/test/synthetic-shadow/scoped-id/multiple-idrefs.spec.js
@@ -10,8 +10,14 @@ it('should handle multiple idrefs when set dynamically', () => {
     const hokkaido = elm.shadowRoot.querySelector('.hokkaido');
     const input = elm.shadowRoot.querySelector('.dynamic');
 
-    expect(aomori.id).toMatch(/^aomori-\w+/);
-    expect(hokkaido.id).toMatch(/^hokkaido-\w+/);
+    if (process.env.NATIVE_SHADOW) {
+        expect(aomori.id).toMatch(/^aomori$/);
+        expect(hokkaido.id).toMatch(/^hokkaido$/);
+    } else {
+        expect(aomori.id).toMatch(/^aomori-\w+/);
+        expect(hokkaido.id).toMatch(/^hokkaido-\w+/);
+    }
+
     expect(input.ariaLabelledBy).toContain(aomori.id);
     expect(input.ariaLabelledBy).toContain(hokkaido.id);
 });
@@ -24,8 +30,14 @@ it('should handle multiple idrefs when set statically', () => {
     const iwate = elm.shadowRoot.querySelector('.iwate');
     const input = elm.shadowRoot.querySelector('.static');
 
-    expect(aomori.id).toMatch(/^aomori-\w+/);
-    expect(iwate.id).toMatch(/^iwate-\w+/);
+    if (process.env.NATIVE_SHADOW) {
+        expect(aomori.id).toMatch(/^aomori$/);
+        expect(iwate.id).toMatch(/^iwate$/);
+    } else {
+        expect(aomori.id).toMatch(/^aomori-\w+/);
+        expect(iwate.id).toMatch(/^iwate-\w+/);
+    }
+
     expect(input.ariaLabelledBy).toContain(aomori.id);
     expect(input.ariaLabelledBy).toContain(iwate.id);
 });
