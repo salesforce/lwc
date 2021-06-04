b71bac16e7380f1aae358e21eaf36506586cb612
Eugene Kashida <ekashida@gmail.com>
2021-06-03 19:21:46 -0700

    test: preferNativeShadow component fallback to synthetic

--- packages/integration-karma/test/mixed-shadow-mode/scoped-ids/scoped-ids.spec.js
+++ packages/integration-karma/test/mixed-shadow-mode/scoped-ids/scoped-ids.spec.js
@@ -1,30 +1,52 @@
 import { createElement } from 'lwc';
 import Test from 'x/test';
 
-it('should entrust id scoping to native shadow (static)', () => {
-    const elm = createElement('x-test', { is: Test });
-    document.body.appendChild(elm);
-    const div = elm.shadowRoot.querySelector('#shizuoka');
-    expect(div).not.toBeNull();
-});
+if (process.env.COMPAT) {
+    it('should fallback to id-mangling (static)', () => {
+        const elm = createElement('x-test', { is: Test });
+        document.body.appendChild(elm);
+        const div = elm.shadowRoot.querySelector('.shizuoka');
+        const input = elm.shadowRoot.querySelector('.idref-static');
 
-it('should entrust id scoping to native shadow (dynamic)', () => {
-    const elm = createElement('x-test', { is: Test });
-    document.body.appendChild(elm);
-    const div = elm.shadowRoot.querySelector('#yamanashi');
-    expect(div).not.toBeNull();
-});
+        expect(div.id).toMatch(/^shizuoka-\w+$/);
+        expect(input.ariaDescribedBy).toContain(div.id);
+    });
 
-it('should entrust idref scoping to native shadow (static)', () => {
-    const elm = createElement('x-test', { is: Test });
-    document.body.appendChild(elm);
-    const input = elm.shadowRoot.querySelector('[aria-describedby="shizuoka yamanashi"]');
-    expect(input).not.toBeNull();
-});
+    it('should fallback to id-mangling (dynamic)', () => {
+        const elm = createElement('x-test', { is: Test });
+        document.body.appendChild(elm);
+        const div = elm.shadowRoot.querySelector('.yamanashi');
+        const input = elm.shadowRoot.querySelector('.idref-dynamic');
 
-it('should entrust idref scoping to native shadow (dynamic)', () => {
-    const elm = createElement('x-test', { is: Test });
-    document.body.appendChild(elm);
-    const input = elm.shadowRoot.querySelector('[aria-labelledby="yamanashi shizuoka"]');
-    expect(input).not.toBeNull();
-});
+        expect(div.id).toMatch(/^yamanashi-\w+$/);
+        expect(input.ariaLabelledBy).toContain(div.id);
+    });
+} else {
+    it('should entrust id scoping to native shadow (static)', () => {
+        const elm = createElement('x-test', { is: Test });
+        document.body.appendChild(elm);
+        const div = elm.shadowRoot.querySelector('#shizuoka');
+        expect(div).not.toBeNull();
+    });
+
+    it('should entrust id scoping to native shadow (dynamic)', () => {
+        const elm = createElement('x-test', { is: Test });
+        document.body.appendChild(elm);
+        const div = elm.shadowRoot.querySelector('#yamanashi');
+        expect(div).not.toBeNull();
+    });
+
+    it('should entrust idref scoping to native shadow (static)', () => {
+        const elm = createElement('x-test', { is: Test });
+        document.body.appendChild(elm);
+        const input = elm.shadowRoot.querySelector('[aria-describedby="shizuoka yamanashi"]');
+        expect(input).not.toBeNull();
+    });
+
+    it('should entrust idref scoping to native shadow (dynamic)', () => {
+        const elm = createElement('x-test', { is: Test });
+        document.body.appendChild(elm);
+        const input = elm.shadowRoot.querySelector('[aria-labelledby="yamanashi shizuoka"]');
+        expect(input).not.toBeNull();
+    });
+}
