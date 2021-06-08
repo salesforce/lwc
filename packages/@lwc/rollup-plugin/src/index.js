01471cc66e0223815815efc960a80f4eb8e6ff39
Pierre-Marie Dartus <p.dartus@salesforce.com>
2021-06-08 18:39:52 +0200

    refactor(rollup-plugin): simplify rollup plugin code

--- packages/@lwc/rollup-plugin/src/index.js
+++ packages/@lwc/rollup-plugin/src/index.js
@@ -10,8 +10,11 @@ const pluginUtils = require('@rollup/pluginutils');
 const compiler = require('@lwc/compiler');
 const { resolveModule } = require('@lwc/module-resolver');
 
-const { getModuleQualifiedName } = require('./utils');
-const { DEFAULT_OPTIONS, DEFAULT_MODE, DEFAULT_MODULES } = require('./constants');
+const DEFAULT_MODULES = [
+    { npm: '@lwc/engine-dom' },
+    { npm: '@lwc/synthetic-shadow' },
+    { npm: '@lwc/wire-service' },
+];
 
 const IMPLICIT_DEFAULT_HTML_PATH = '@lwc/resources/empty_html.js';
 const EMPTY_IMPLICIT_HTML_CONTENT = 'export default void 0';
@@ -25,25 +28,19 @@ function isImplicitHTMLImport(importee, importer) {
     );
 }
 
-function isMixingJsAndTs(importerExt, importeeExt) {
-    return (
-        (importerExt === '.js' && importeeExt === '.ts') ||
-        (importerExt === '.ts' && importeeExt === '.js')
-    );
-}
-
 module.exports = function rollupLwcCompiler(pluginOptions = {}) {
     const { include, exclude } = pluginOptions;
     const filter = pluginUtils.createFilter(include, exclude);
-    const mergedPluginOptions = Object.assign({}, DEFAULT_OPTIONS, pluginOptions);
 
     let customResolvedModules;
     let customRootDir;
 
     return {
         name: 'rollup-plugin-lwc-compiler',
+
         options({ input }) {
-            const { modules: userModules = [], rootDir } = mergedPluginOptions;
+            const { modules: userModules = [], rootDir } = pluginOptions;
+
             customRootDir = rootDir ? path.resolve(rootDir) : path.dirname(path.resolve(input));
             customResolvedModules = [...userModules, ...DEFAULT_MODULES, { dir: customRootDir }];
         },
@@ -53,13 +50,6 @@ module.exports = function rollupLwcCompiler(pluginOptions = {}) {
             if (importee.startsWith('.') && importer) {
                 const importerExt = path.extname(importer);
                 const ext = path.extname(importee) || importerExt;
-
-                // we don't currently support mixing .js and .ts
-                if (isMixingJsAndTs(importerExt, ext)) {
-                    throw new Error(
-                        `Importing a ${ext} file into a ${importerExt} is not supported`
-                    );
-                }
 
                 const normalizedPath = path.resolve(path.dirname(importer), importee);
                 const absPath = pluginUtils.addExtension(normalizedPath, ext);
@@ -96,23 +86,24 @@ module.exports = function rollupLwcCompiler(pluginOptions = {}) {
             }
         },
 
-        async transform(src, id) {
+        transform(src, id) {
             // Filter user-land config and lwc import
             if (!filter(id)) {
                 return;
             }
-            // If we don't find the moduleId, just resolve the module name/namespace
-            const moduleEntry = getModuleQualifiedName(id, mergedPluginOptions);
 
-            const { code, map } = await compiler.transform(src, id, {
-                mode: DEFAULT_MODE, // Use always default mode since any other (prod or compat) will be resolved later
-                name: moduleEntry.moduleName,
-                namespace: moduleEntry.moduleNamespace,
-                moduleSpecifier: moduleEntry.moduleSpecifier,
-                outputConfig: { sourcemap: mergedPluginOptions.sourcemap },
-                stylesheetConfig: mergedPluginOptions.stylesheetConfig,
-                experimentalDynamicComponent: mergedPluginOptions.experimentalDynamicComponent,
-                preserveHtmlComments: mergedPluginOptions.preserveHtmlComments,
+            // Extract module name and namespace from file path
+            const idParts = path.dirname(id).split(path.sep);
+            const name = idParts.pop();
+            const namespace = idParts.pop();
+
+            const { code, map } = compiler.transformSync(src, id, {
+                name,
+                namespace,
+                outputConfig: { sourcemap: pluginOptions.sourcemap },
+                stylesheetConfig: pluginOptions.stylesheetConfig,
+                experimentalDynamicComponent: pluginOptions.experimentalDynamicComponent,
+                preserveHtmlComments: pluginOptions.preserveHtmlComments,
             });
 
             return { code, map };
