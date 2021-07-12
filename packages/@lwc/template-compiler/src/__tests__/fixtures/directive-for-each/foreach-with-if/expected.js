79e979f872d994ed9fde0381fef5b5bd99265d53
Ravi Jayaramappa <ravi.jayaramappa@salesforce.com>
2021-07-12 02:15:10 -0700

    test: gold file compiled template when node has if and for:each (#2408)

---
+++ packages/@lwc/template-compiler/src/__tests__/fixtures/directive-for-each/foreach-with-if/expected.js
@@ -0,0 +1,31 @@
+import { registerTemplate } from "lwc";
+function tmpl($api, $cmp, $slotset, $ctx) {
+  const {
+    k: api_key,
+    t: api_text,
+    d: api_dynamic,
+    h: api_element,
+    i: api_iterator,
+  } = $api;
+  return [
+    api_element(
+      "section",
+      {
+        key: 0,
+      },
+      api_iterator($cmp.items, function (item) {
+        return $cmp.showItems
+          ? api_element(
+              "p",
+              {
+                key: api_key(1, item.id),
+              },
+              [api_text("1"), api_dynamic(item)]
+            )
+          : null;
+      })
+    ),
+  ];
+}
+export default registerTemplate(tmpl);
+tmpl.stylesheets = [];
