4ceec5e3fdd6006667d48523769aff7dcebe4245
Pierre-Marie Dartus <p.dartus@salesforce.com>
2021-07-19 17:49:40 +0200

    chore: add examples of failing tests

--- packages/integration-karma/test/light-dom/synthetic-shadow/index.spec.js
+++ packages/integration-karma/test/light-dom/synthetic-shadow/index.spec.js
@@ -42,23 +42,37 @@ describe('Light DOM + Synthetic Shadow DOM', () => {
             expect(nodes.p.parentElement).toEqual(nodes.consumer);
             expect(nodes.consumer.parentElement).toEqual(elm);
         });
+        // TODO [#2424]: Fails because the <p> doesn't have an owner key.
         xit('getRootNode', () => {
             expect(nodes.p.getRootNode()).toEqual(document);
             expect(nodes.consumer.getRootNode()).toEqual(document);
         });
-        it('textContent', () => {
+        // TODO [#2425]: Incorrect serialization
+        xit('textContent', () => {
+            expect(nodes.p.textContent).toEqual('I am an assigned element.');
             expect(nodes.consumer.textContent).toEqual(
                 'I am an assigned element.I am an assigned text.'
             );
+            expect(elm.textContent).toEqual('I am an assigned element.I am an assigned text.');
         });
+        // TODO [#2425]: Incorrect serialization
         xit('innerHTML', () => {
+            expect(nodes.p.innerHTML).toEqual('I am an assigned element.');
             expect(nodes.consumer.innerHTML).toEqual(
                 '<p data-id="p">I am an assigned element.</p>I am an assigned text.'
             );
+            expect(elm.innerHTML).toEqual(
+                '<x-consumer data-id="consumer"><p data-id="p">I am an assigned element.</p>I am an assigned text.</x-consumer>'
+            );
         });
-        it('outerHTML', () => {
+        // TODO [#2425]: Incorrect serialization
+        xit('outerHTML', () => {
+            expect(nodes.p.outerHTML).toEqual('<p data-id="p">I am an assigned element.</p>');
             expect(nodes.consumer.outerHTML).toEqual(
                 '<x-consumer data-id="consumer"><p data-id="p">I am an assigned element.</p>I am an assigned text.</x-consumer>'
+            );
+            expect(elm.outerHTML).toEqual(
+                '<x-light-container><x-consumer data-id="consumer"><p data-id="p">I am an assigned element.</p>I am an assigned text.</x-consumer></x-light-container>'
             );
         });
     });
@@ -98,18 +112,30 @@ describe('Light DOM + Synthetic Shadow DOM', () => {
             expect(nodes.consumer.getRootNode()).toEqual(elm.shadowRoot);
         });
         it('textContent', () => {
+            expect(nodes.p.textContent).toEqual('I am an assigned element.');
             expect(nodes.consumer.textContent).toEqual(
+                'I am an assigned element.I am an assigned text.'
+            );
+            expect(nodes['light-container'].textContent).toEqual(
                 'I am an assigned element.I am an assigned text.'
             );
         });
         it('innerHTML', () => {
+            expect(nodes.p.innerHTML).toEqual('I am an assigned element.');
             expect(nodes.consumer.innerHTML).toEqual(
                 '<p data-id="p">I am an assigned element.</p>I am an assigned text.'
             );
+            expect(nodes['light-container'].innerHTML).toEqual(
+                '<x-consumer data-id="consumer"><p data-id="p">I am an assigned element.</p>I am an assigned text.</x-consumer>'
+            );
         });
         it('outerHTML', () => {
+            expect(nodes.p.outerHTML).toEqual('<p data-id="p">I am an assigned element.</p>');
             expect(nodes.consumer.outerHTML).toEqual(
                 '<x-consumer data-id="consumer"><p data-id="p">I am an assigned element.</p>I am an assigned text.</x-consumer>'
+            );
+            expect(nodes['light-container'].outerHTML).toEqual(
+                '<x-light-container data-id="light-container"><x-consumer data-id="consumer"><p data-id="p">I am an assigned element.</p>I am an assigned text.</x-consumer></x-light-container>'
             );
         });
     });
