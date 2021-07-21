4d330571868b449eac5093f456d38101ae87ad61
Nolan Lawson <nlawson@salesforce.com>
2021-07-21 14:58:13 -0700

    test: fix code coverage

--- packages/@lwc/features/src/__tests__/flags.spec.ts
+++ packages/@lwc/features/src/__tests__/flags.spec.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-const { default: pluginTester } = require('babel-plugin-tester');
+import pluginTester from 'babel-plugin-tester';
 const plugin = require('../babel-plugin');
 
 const babelOptions = {
