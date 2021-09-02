a819c1c134724fd47449992d8da017f235dd98e4
Eugene Kashida <ekashida@gmail.com>
2021-09-02 11:22:29 -0700

    test: repro

--- packages/integration-karma/test/native-shadow/Event-methods/Event.composedPath.spec.js
+++ packages/integration-karma/test/native-shadow/Event-methods/Event.composedPath.spec.js
@@ -1,4 +1,69 @@
 if (process.env.COMPAT !== true) {
+    describe('[W-9846457]', () => {
+        let nativeParent;
+        let nativeChild;
+        const noop = () => {};
+
+        beforeEach(() => {
+            nativeParent = document.createElement('x-native-parent');
+            nativeParent.attachShadow({ mode: 'open' });
+            nativeChild = document.createElement('x-native-child');
+            nativeChild.attachShadow({ mode: 'open' });
+
+            nativeParent.shadowRoot.appendChild(nativeChild);
+            document.body.appendChild(nativeParent);
+
+            // Internally computes composed path and adds to cache
+            document.addEventListener('test', noop, true);
+        });
+
+        afterEach(() => {
+            nativeParent = null;
+            nativeChild = null;
+            document.removeEventListener('test', noop, true);
+        });
+
+        it('should handle composed bubbling events (nested child)', (done) => {
+            nativeChild.addEventListener('test', (event) => {
+                expect(event.composedPath()).toEqual([
+                    nativeChild.shadowRoot,
+                    nativeChild,
+                    nativeParent.shadowRoot,
+                    nativeParent,
+                    document.body,
+                    document.documentElement,
+                    document,
+                    window,
+                ]);
+                done();
+            });
+
+            nativeChild.shadowRoot.dispatchEvent(
+                new CustomEvent('test', { composed: true, bubbles: true })
+            );
+        });
+
+        it('should handle composed bubbling events (root parent)', (done) => {
+            nativeParent.addEventListener('test', (event) => {
+                expect(event.composedPath()).toEqual([
+                    nativeChild.shadowRoot,
+                    nativeChild,
+                    nativeParent.shadowRoot,
+                    nativeParent,
+                    document.body,
+                    document.documentElement,
+                    document,
+                    window,
+                ]);
+                done();
+            });
+
+            nativeChild.shadowRoot.dispatchEvent(
+                new CustomEvent('test', { composed: true, bubbles: true })
+            );
+        });
+    });
+
     describe('Event.composedPath() method', () => {
         describe('dispatched on shadow root', () => {
             it('{bubbles: true, composed: true}', (done) => {
