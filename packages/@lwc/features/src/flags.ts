8eaa6ac9138cacde3bafd73123b5fd5fa5a9705e
Pierre-Marie Dartus <p.dartus@salesforce.com>
2021-09-09 18:59:26 +0200

    refactor(features): Restructure code and add missing documentation (#2488)

--- packages/@lwc/features/src/flags.ts
+++ packages/@lwc/features/src/flags.ts
@@ -4,21 +4,17 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-export type FeatureFlagValue = boolean | null;
-export type FeatureFlagLookup = { [name: string]: FeatureFlagValue };
+import { create, keys, defineProperty, isUndefined, isBoolean, globalThis } from '@lwc/shared';
+import { FeatureFlagMap, FeatureFlagName, FeatureFlagValue } from './types';
 
-const featureFlagLookup: FeatureFlagLookup = {
+const features: FeatureFlagMap = {
     ENABLE_REACTIVE_SETTER: null,
     ENABLE_HMR: null,
-    // Flag to toggle on/off the enforcement of innerText/outerText shadow dom semantic in elements when using synthetic shadow.
-    // Note: Once active, elements outside the lwc boundary are controlled by the ENABLE_ELEMENT_PATCH flag.
     ENABLE_INNER_OUTER_TEXT_PATCH: null,
-    // Flags to toggle on/off the enforcement of shadow dom semantic in element/node outside lwc boundary when using synthetic shadow.
     ENABLE_ELEMENT_PATCH: null,
     ENABLE_NODE_LIST_PATCH: null,
     ENABLE_HTML_COLLECTIONS_PATCH: null,
     ENABLE_NODE_PATCH: null,
-    // Disables the fix for #2121 where non-composed events are visible outside of their shadow root.
     ENABLE_NON_COMPOSED_EVENTS_LEAKAGE: null,
     ENABLE_LIGHT_DOM_COMPONENTS: null,
     ENABLE_MIXED_SHADOW_MODE: null,
@@ -26,6 +22,63 @@ const featureFlagLookup: FeatureFlagLookup = {
     // is connected (instead of next tick). It only affects wire configurations that depend on component values.
     ENABLE_WIRE_SYNC_EMIT: null,
 };
-export default featureFlagLookup;
 
-export { runtimeFlags, setFeatureFlag, setFeatureFlagForTest } from './runtime';
+if (!globalThis.lwcRuntimeFlags) {
+    Object.defineProperty(globalThis, 'lwcRuntimeFlags', { value: create(null) });
+}
+
+export const runtimeFlags: Partial<FeatureFlagMap> = globalThis.lwcRuntimeFlags;
+
+/**
+ * Set the value at runtime of a given feature flag. This method only be invoked once per feature
+ * flag. It is meant to be used during the app initialization.
+ */
+export function setFeatureFlag(name: FeatureFlagName, value: FeatureFlagValue): void {
+    if (!isBoolean(value)) {
+        const message = `Failed to set the value "${value}" for the runtime feature flag "${name}". Runtime feature flags can only be set to a boolean value.`;
+        if (process.env.NODE_ENV !== 'production') {
+            throw new TypeError(message);
+        } else {
+            // eslint-disable-next-line no-console
+            console.error(message);
+            return;
+        }
+    }
+    if (isUndefined(features[name])) {
+        const availableFlags = keys(features)
+            .map((name) => `"${name}"`)
+            .join(', ');
+        // eslint-disable-next-line no-console
+        console.warn(
+            `Failed to set the value "${value}" for the runtime feature flag "${name}" because it is undefined. Available flags: ${availableFlags}.`
+        );
+        return;
+    }
+    if (process.env.NODE_ENV !== 'production') {
+        // Allow the same flag to be set more than once outside of production to enable testing
+        runtimeFlags[name] = value;
+    } else {
+        // Disallow the same flag to be set more than once in production
+        const runtimeValue = runtimeFlags[name];
+        if (!isUndefined(runtimeValue)) {
+            // eslint-disable-next-line no-console
+            console.error(
+                `Failed to set the value "${value}" for the runtime feature flag "${name}". "${name}" has already been set with the value "${runtimeValue}".`
+            );
+            return;
+        }
+        defineProperty(runtimeFlags, name, { value });
+    }
+}
+
+/**
+ * Set the value at runtime of a given feature flag. This method should only be used for testing
+ * purposes. It is a no-op when invoked in production mode.
+ */
+export function setFeatureFlagForTest(name: FeatureFlagName, value: FeatureFlagValue): void {
+    if (process.env.NODE_ENV !== 'production') {
+        setFeatureFlag(name, value);
+    }
+}
+
+export default features;
