158b81b7d137f4d159cbe5ab052a886739c5190e
Pierre-Marie Dartus <p.dartus@salesforce.com>
2021-09-14 11:36:20 +0200

    chore: remove duplicate wire integration tests

--- packages/integration-tests/src/shared/templates.js
+++ packages/integration-tests/src/shared/templates.js
@@ -11,16 +11,6 @@ exports.app = function (cmpName) {
     `;
 };
 
-exports.todoApp = function (cmpName) {
-    return `
-        import { createElement } from 'lwc';
-        import Cmp from 'integration/${cmpName}';
-
-        var element = createElement('integration-${cmpName}', { is: Cmp });
-        document.body.appendChild(element);
-    `;
-};
-
 const COMPAT = `
     <script src="../../shared/downgrade.js"></script>
     <script src="../../shared/polyfills.js"></script>
@@ -48,27 +38,6 @@ exports.html = function (cmpName, isCompat) {
                 ${isCompat ? COMPAT : ''}
                 ${SHADOW_POLYFILL}
                 <script src="../../shared/engine.js"></script>
-                <script src="./${cmpName}.js"></script>
-            </body>
-        </html>
-    `;
-};
-
-exports.wireServiceHtml = function (cmpName, isCompat) {
-    return `
-        <html>
-            <head>
-                <title>${cmpName}</title>
-            </head>
-            <body>
-                <script>
-                    window.process = { env: { NODE_ENV: "development" } };
-                </script>
-                ${isCompat ? COMPAT : ''}
-                ${SHADOW_POLYFILL}
-                <script src="../../shared/engine.js"></script>
-                <script src="../../shared/todo.js"></script>
-                <script src="../../shared/wire.js"></script>
                 <script src="./${cmpName}.js"></script>
             </body>
         </html>
