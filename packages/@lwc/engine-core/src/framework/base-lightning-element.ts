658ba25e489c314d380706a13b098b96d5f788dd
Nolan Lawson <nolan@nolanlawson.com>
2021-10-25 11:20:14 -0700

    fix(engine-core): fix LightningElement configurable/enumerable props (#2530)
    * fix(engine-core): fix LightningElement configurable/enumerable props
    
    * fix: add writable:true, add tests for writable/configurable
    
    * fix: remove configurable:true as it is useless
    
    * Revert "fix: remove configurable:true as it is useless"
    
    This reverts commit 9aaf6674b7207a967299097281a1a447e27b2219.

--- packages/@lwc/engine-core/src/framework/base-lightning-element.ts
+++ packages/@lwc/engine-core/src/framework/base-lightning-element.ts
@@ -530,6 +530,8 @@ for (const [elementProp, rendererMethod] of childGetters) {
 
             return renderer[rendererMethod](elm);
         },
+        configurable: true,
+        enumerable: true,
     };
 }
 
@@ -553,6 +555,9 @@ for (const queryMethod of queryMethods) {
 
             return renderer[queryMethod](elm, arg);
         },
+        configurable: true,
+        enumerable: true,
+        writable: true,
     };
 }
 
---
+++ packages/integration-karma/test/component/properties/index.spec.js
@@ -0,0 +1,154 @@
+import { createElement } from 'lwc';
+
+import Component from 'x/component';
+
+// This list can grow as we add more properties to the base LightningElement
+const expectedEnumerableProps = [
+    'accessKey',
+    'accessKeyLabel',
+    'addEventListener',
+    'ariaActiveDescendant',
+    'ariaAtomic',
+    'ariaAutoComplete',
+    'ariaBusy',
+    'ariaChecked',
+    'ariaColCount',
+    'ariaColIndex',
+    'ariaColSpan',
+    'ariaControls',
+    'ariaCurrent',
+    'ariaDescribedBy',
+    'ariaDetails',
+    'ariaDisabled',
+    'ariaErrorMessage',
+    'ariaExpanded',
+    'ariaFlowTo',
+    'ariaHasPopup',
+    'ariaHidden',
+    'ariaInvalid',
+    'ariaKeyShortcuts',
+    'ariaLabel',
+    'ariaLabelledBy',
+    'ariaLevel',
+    'ariaLive',
+    'ariaModal',
+    'ariaMultiLine',
+    'ariaMultiSelectable',
+    'ariaOrientation',
+    'ariaOwns',
+    'ariaPlaceholder',
+    'ariaPosInSet',
+    'ariaPressed',
+    'ariaReadOnly',
+    'ariaRelevant',
+    'ariaRequired',
+    'ariaRoleDescription',
+    'ariaRowCount',
+    'ariaRowIndex',
+    'ariaRowSpan',
+    'ariaSelected',
+    'ariaSetSize',
+    'ariaSort',
+    'ariaValueMax',
+    'ariaValueMin',
+    'ariaValueNow',
+    'ariaValueText',
+    'childNodes',
+    'children',
+    'classList',
+    'className',
+    'contentEditable',
+    'dataset',
+    'dir',
+    'dispatchEvent',
+    'draggable',
+    'dropzone',
+    'firstChild',
+    'firstElementChild',
+    'getAttribute',
+    'getAttributeNS',
+    'getBoundingClientRect',
+    'getElementsByClassName',
+    'getElementsByTagName',
+    'hasAttribute',
+    'hasAttributeNS',
+    'hidden',
+    'id',
+    'inputMode',
+    'isConnected',
+    'isContentEditable',
+    'lang',
+    'lastChild',
+    'lastElementChild',
+    'offsetHeight',
+    'offsetLeft',
+    'offsetParent',
+    'offsetTop',
+    'offsetWidth',
+    'querySelector',
+    'querySelectorAll',
+    'removeAttribute',
+    'removeAttributeNS',
+    'removeEventListener',
+    'render',
+    'role',
+    'setAttribute',
+    'setAttributeNS',
+    'shadowRoot',
+    'slot',
+    'spellcheck',
+    'style',
+    'tabIndex',
+    'template',
+    'title',
+    'toString',
+    'translate',
+];
+
+const expectedEnumerableAndWritableProps = [
+    'addEventListener',
+    'dispatchEvent',
+    'getAttribute',
+    'getAttributeNS',
+    'getBoundingClientRect',
+    'getElementsByClassName',
+    'getElementsByTagName',
+    'hasAttribute',
+    'hasAttributeNS',
+    'querySelector',
+    'querySelectorAll',
+    'removeAttribute',
+    'removeAttributeNS',
+    'removeEventListener',
+    'render',
+    'setAttribute',
+    'setAttributeNS',
+    'toString',
+];
+
+describe('properties', () => {
+    // IE11 and old Safari are buggy, return 'constructor' along with the other properties
+    if (!process.env.COMPAT) {
+        let elm;
+
+        beforeEach(() => {
+            elm = createElement('x-component', { is: Component });
+            document.body.appendChild(elm);
+        });
+
+        it('has expected enumerable properties', () => {
+            const props = elm.getEnumerableProps();
+            expect(props).toEqual(expectedEnumerableProps);
+        });
+
+        it('has expected writable properties', () => {
+            const props = elm.getEnumerableAndWritableProps();
+            expect(props).toEqual(expectedEnumerableAndWritableProps);
+        });
+
+        it('has expected configurable properties', () => {
+            const props = elm.getEnumerableAndConfigurableProps();
+            expect(props).toEqual([]);
+        });
+    }
+});
