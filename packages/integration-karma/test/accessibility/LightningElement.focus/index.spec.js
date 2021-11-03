d8a48321f04647327db6750c8eead4461aff782c
Nolan Lawson <nlawson@salesforce.com>
2021-11-03 07:54:31 -0700

    test: fix test for Firefox 94 delegatesFocus
    Fixes #2565

--- packages/integration-karma/test/accessibility/LightningElement.focus/index.spec.js
+++ packages/integration-karma/test/accessibility/LightningElement.focus/index.spec.js
@@ -4,12 +4,11 @@ import DelegatesFocusTrue from 'x/delegatesFocusTrue';
 import DelegatesFocusFalse from 'x/delegatesFocusFalse';
 import Container from 'x/container';
 
-beforeEach(() => {
-    document.body.focus();
-});
+describe('LightningElement.focus', () => {
+    beforeEach(() => {
+        document.body.focus();
+    });
 
-// TODO [#985]: Firefox does not implement delegatesFocus
-if (!process.env.NATIVE_SHADOW) {
     describe('host.focus() when { delegatesFocus: true }', () => {
         it('should focus the host element', () => {
             const elm = createElement('x-focus', { is: DelegatesFocusTrue });
@@ -28,70 +27,73 @@ if (!process.env.NATIVE_SHADOW) {
             expect(elm.shadowRoot.activeElement).toEqual(input);
         });
     });
-}
 
-it('should not focus the first internally focusable element (delegatesFocus=false)', () => {
-    const elm = createElement('x-focus', { is: DelegatesFocusFalse });
-    document.body.appendChild(elm);
-
-    elm.focus();
-    expect(elm.shadowRoot.activeElement).toBeNull();
-});
-
-it('should focus the host element (delegatesFocus=false, tabIndex=-1)', () => {
-    const container = createElement('x-container', { is: Container });
-    document.body.appendChild(container);
-
-    const elm = container.shadowRoot.querySelector('x-delegates-focus-false[tabindex="-1"]');
-    elm.focus();
-    expect(container.shadowRoot.activeElement).toBe(elm);
-    expect(elm.shadowRoot.activeElement).toBeNull();
-});
-
-it('should focus the host element (delegatesFocus=false, tabIndex=0)', () => {
-    const container = createElement('x-container', { is: Container });
-    document.body.appendChild(container);
-
-    const elm = container.shadowRoot.querySelector('x-delegates-focus-false[tabindex="0"]');
-    elm.focus();
-    expect(container.shadowRoot.activeElement).toBe(elm);
-    expect(elm.shadowRoot.activeElement).toBeNull();
-});
-
-// Tests that apply regardless of whether focus is being delegated
-function runFocusTests({ delegatesFocus }) {
-    const category = `(delegatesFocus=${delegatesFocus})`;
-    const Ctor = delegatesFocus ? DelegatesFocusTrue : DelegatesFocusFalse;
-
-    it(`should focus the internal element when invoking the focus method directly on the internal element ${category}`, () => {
-        const elm = createElement('x-focus', { is: Ctor });
+    it('should not focus the first internally focusable element (delegatesFocus=false)', () => {
+        const elm = createElement('x-focus', { is: DelegatesFocusFalse });
         document.body.appendChild(elm);
 
-        const input = elm.shadowRoot.querySelector('.second');
-        input.focus();
-        expect(elm.shadowRoot.activeElement).toBe(input);
-    });
-
-    it(`should blur the internal element when invoking the blur method directly on the internal element ${category}`, () => {
-        const elm = createElement('x-focus', { is: Ctor });
-        document.body.appendChild(elm);
-
-        const input = elm.shadowRoot.querySelector('.second');
-        input.focus();
-        input.blur();
+        elm.focus();
         expect(elm.shadowRoot.activeElement).toBeNull();
     });
 
-    it(`should not move focus if an internal element is already focused ${category}`, () => {
-        const elm = createElement('x-focus', { is: Ctor });
-        document.body.appendChild(elm);
+    it('should focus the host element (delegatesFocus=false, tabIndex=-1)', () => {
+        const container = createElement('x-container', { is: Container });
+        document.body.appendChild(container);
 
-        const input = elm.shadowRoot.querySelector('.second');
-        input.focus();
+        const elm = container.shadowRoot.querySelector('x-delegates-focus-false[tabindex="-1"]');
         elm.focus();
-        expect(elm.shadowRoot.activeElement).toBe(input);
+        expect(container.shadowRoot.activeElement).toBe(elm);
+        expect(elm.shadowRoot.activeElement).toBeNull();
     });
-}
 
-runFocusTests({ delegatesFocus: true });
-runFocusTests({ delegatesFocus: false });
+    it('should focus the host element (delegatesFocus=false, tabIndex=0)', () => {
+        const container = createElement('x-container', { is: Container });
+        document.body.appendChild(container);
+
+        const elm = container.shadowRoot.querySelector('x-delegates-focus-false[tabindex="0"]');
+        elm.focus();
+        expect(container.shadowRoot.activeElement).toBe(elm);
+        expect(elm.shadowRoot.activeElement).toBeNull();
+    });
+
+    // Tests that apply regardless of whether focus is being delegated
+    function runFocusTests({ delegatesFocus }) {
+        const category = `(delegatesFocus=${delegatesFocus})`;
+        const Ctor = delegatesFocus ? DelegatesFocusTrue : DelegatesFocusFalse;
+
+        it(`should focus the internal element when invoking the focus method directly on the internal element ${category}`, () => {
+            const elm = createElement('x-focus', { is: Ctor });
+            document.body.appendChild(elm);
+
+            const input = elm.shadowRoot.querySelector('.second');
+            input.focus();
+            expect(elm.shadowRoot.activeElement).toBe(input);
+        });
+
+        it(`should blur the internal element when invoking the blur method directly on the internal element ${category}`, () => {
+            const elm = createElement('x-focus', { is: Ctor });
+            document.body.appendChild(elm);
+
+            const input = elm.shadowRoot.querySelector('.second');
+            input.focus();
+            input.blur();
+            expect(elm.shadowRoot.activeElement).toBeNull();
+        });
+
+        // TODO [#2566]: Firefox behaves differently from Chrome/Safari in this test
+        if (!(process.env.NATIVE_SHADOW && delegatesFocus && window.mozInnerScreenX)) {
+            it(`should not move focus if an internal element is already focused ${category}`, () => {
+                const elm = createElement('x-focus', { is: Ctor });
+                document.body.appendChild(elm);
+
+                const input = elm.shadowRoot.querySelector('.second');
+                input.focus();
+                elm.focus();
+                expect(elm.shadowRoot.activeElement).toBe(input);
+            });
+        }
+    }
+
+    runFocusTests({ delegatesFocus: true });
+    runFocusTests({ delegatesFocus: false });
+});
