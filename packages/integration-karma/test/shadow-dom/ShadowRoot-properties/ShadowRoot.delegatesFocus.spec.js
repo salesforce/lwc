30a2bb57f5790243645a443ce6db949e45f93942
Nolan Lawson <nlawson@salesforce.com>
2021-11-03 14:16:37 -0700

    fix: revert "test: enable ShadowRoot.delegatesFocus.spec.js"
    This reverts commit 1b5a837aedce6245f0f4b6398cbf89e39b3eea0e.

--- packages/integration-karma/test/shadow-dom/ShadowRoot-properties/ShadowRoot.delegatesFocus.spec.js
+++ packages/integration-karma/test/shadow-dom/ShadowRoot-properties/ShadowRoot.delegatesFocus.spec.js
@@ -2,22 +2,25 @@ import { LightningElement } from 'lwc';
 import { createElement } from 'lwc';
 
 describe('ShadowRoot.delegatesFocus', () => {
-    it('ShadowRoot.delegatesFocus should be false by default', () => {
-        class NoDelegatesFocus extends LightningElement {}
+    // TODO [#985]: delegatedFocus is only implemented the native ShadowRoot by Blink
+    if (!process.env.NATIVE_SHADOW) {
+        it('ShadowRoot.delegatesFocus should be false by default', () => {
+            class NoDelegatesFocus extends LightningElement {}
 
-        const elm = createElement('x-test', { is: NoDelegatesFocus });
-        document.body.appendChild(elm);
+            const elm = createElement('x-test', { is: NoDelegatesFocus });
+            document.body.appendChild(elm);
 
-        expect(elm.shadowRoot.delegatesFocus).toBe(false);
-    });
-    it('ShadowRoot.delegatesFocus should be true if class has delegatesFocus static property set to true', () => {
-        class DelegatesFocus extends LightningElement {
-            static delegatesFocus = true;
-        }
+            expect(elm.shadowRoot.delegatesFocus).toBe(false);
+        });
+        it('ShadowRoot.delegatesFocus should be true if class has delegatesFocus static property set to true', () => {
+            class DelegatesFocus extends LightningElement {
+                static delegatesFocus = true;
+            }
 
-        const elm = createElement('x-test', { is: DelegatesFocus });
-        document.body.appendChild(elm);
+            const elm = createElement('x-test', { is: DelegatesFocus });
+            document.body.appendChild(elm);
 
-        expect(elm.shadowRoot.delegatesFocus).toBe(true);
-    });
+            expect(elm.shadowRoot.delegatesFocus).toBe(true);
+        });
+    }
 });
