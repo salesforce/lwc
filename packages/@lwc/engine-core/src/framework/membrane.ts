2fd18e4c0e368f252621f2286921b2d5bd99fa81
Pierre-Marie Dartus <p.dartus@salesforce.com>
2021-11-22 14:22:43 +0100

    chore: upgrade observable-membrane to 2.0.0

--- packages/@lwc/engine-core/src/framework/membrane.ts
+++ packages/@lwc/engine-core/src/framework/membrane.ts
@@ -4,19 +4,14 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-import ObservableMembrane from 'observable-membrane';
+import { ObservableMembrane } from 'observable-membrane';
 import { valueObserved, valueMutated } from './mutation-tracker';
 
 export const lockerLivePropertyKey = Symbol.for('@@lockerLiveValue');
 
-function valueDistortion(value: any) {
-    return value;
-}
-
 export const reactiveMembrane = new ObservableMembrane({
     valueObserved,
     valueMutated,
-    valueDistortion,
     tagPropertyKey: lockerLivePropertyKey,
 });
 
@@ -25,11 +20,4 @@ export const reactiveMembrane = new ObservableMembrane({
  * works for observable membrane objects. This API is subject to
  * change or being removed.
  */
-export const unwrap = function (value: any): any {
-    const unwrapped = reactiveMembrane.unwrapProxy(value);
-    if (unwrapped !== value) {
-        // if value is a proxy, unwrap to access original value and apply distortion
-        return valueDistortion(unwrapped);
-    }
-    return value;
-};
+export const unwrap = reactiveMembrane.unwrapProxy;
