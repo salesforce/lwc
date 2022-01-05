ebc6606a1e77768b88c9010a90568d626c7ebee7
James Tu <j.tu@salesforce.com>
2022-01-05 14:17:15 -0800

    fix: use last prop value for duplicate props

--- packages/@lwc/template-compiler/src/parser/index.ts
+++ packages/@lwc/template-compiler/src/parser/index.ts
@@ -42,6 +42,7 @@ import {
     LWCDirectiveRenderMode,
     LWCDirectiveDomMode,
     If,
+    Property,
 } from '../shared/types';
 
 import ParserCtx from './parser';
@@ -835,6 +836,7 @@ function parseSlot(
 function applyAttributes(ctx: ParserCtx, parsedAttr: ParsedAttribute, element: BaseElement): void {
     const { name: tag } = element;
     const attributes = parsedAttr.getAttributes();
+    const props: Map<string, Property> = new Map();
 
     for (const attr of attributes) {
         const { name } = attr;
@@ -901,11 +903,13 @@ function applyAttributes(ctx: ParserCtx, parsedAttr: ParsedAttribute, element: B
             element.attributes.push(attr);
         } else {
             const propName = attributeToPropertyName(name);
-            element.properties.push(ast.property(propName, attr.name, attr.value, attr.location));
+            props.set(propName, ast.property(propName, name, attr.value, attr.location));
 
             parsedAttr.pick(name);
         }
     }
+
+    element.properties.push(...props.values());
 }
 
 function validateRoot(ctx: ParserCtx, parsedAttr: ParsedAttribute, root: Root): void {
