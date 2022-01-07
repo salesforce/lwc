2c7854f0e767e28e61678565566d8c13f191cf1a
Jose David Rodriguez Velasco <jodarove@gmail.com>
2022-01-07 15:28:04 -0500

    fix: test about scoped styles

--- packages/integration-karma/test-hydration/light-dom/scoped-styles/can-scope-shadow-dom-styles/index.spec.js
+++ packages/integration-karma/test-hydration/light-dom/scoped-styles/can-scope-shadow-dom-styles/index.spec.js
@@ -1,12 +1,14 @@
 export default {
     test(target) {
-        // @todo: this failure may have to do with SSR, need to check.
         expect(getComputedStyle(target).backgroundColor).toEqual('rgb(0, 0, 255)');
         expect(getComputedStyle(target.shadowRoot.querySelector('div')).color).toEqual(
             'rgb(255, 0, 0)'
         );
-        expect(
-            getComputedStyle(target.shadowRoot.querySelector('x-light-child div')).color
-        ).not.toEqual('rgb(255, 0, 0)');
+
+        return Promise.resolve().then(() => {
+            expect(
+                getComputedStyle(target.shadowRoot.querySelector('x-light-child div')).color
+            ).toEqual('rgb(255, 0, 0)');
+        });
     },
 };
