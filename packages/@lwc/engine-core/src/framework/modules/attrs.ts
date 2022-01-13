088b4331fa1f8be680c589d6ebe392cda587853e
Pierre-Marie Dartus <p.dartus@salesforce.com>
2022-01-13 14:20:50 +0100

    refactor: avoid old data reassignment

--- packages/@lwc/engine-core/src/framework/modules/attrs.ts
+++ packages/@lwc/engine-core/src/framework/modules/attrs.ts
@@ -17,40 +17,30 @@ const xmlNS = 'http://www.w3.org/XML/1998/namespace';
 const ColonCharCode = 58;
 
 export function patchAttributes(oldVnode: VElement | null, vnode: VElement) {
-    const {
-        data: { attrs },
-    } = vnode;
-
+    const { attrs } = vnode.data;
     if (isUndefined(attrs)) {
         return;
     }
 
-    let oldAttrs = isNull(oldVnode) ? undefined : oldVnode.data.attrs;
-
+    const oldAttrs = isNull(oldVnode) ? EmptyObject : oldVnode.data.attrs;
     if (oldAttrs === attrs) {
         return;
     }
 
     if (process.env.NODE_ENV !== 'production') {
         assert.invariant(
-            isUndefined(oldAttrs) || keys(oldAttrs).join(',') === keys(attrs).join(','),
+            oldAttrs === EmptyObject || keys(oldAttrs).join(',') === keys(attrs).join(','),
             `vnode.data.attrs cannot change shape.`
         );
     }
 
-    const elm = vnode.elm!;
-
-    let key: string;
-    oldAttrs = isUndefined(oldAttrs) ? EmptyObject : oldAttrs;
-
-    // update modified attributes, add new attributes
-    // this routine is only useful for data-* attributes in all kind of elements
-    // and aria-* in standard elements (custom elements will use props for these)
-    for (key in attrs) {
+    const { elm } = vnode;
+    for (const key in attrs) {
         const cur = attrs[key];
-        const old = (oldAttrs as any)[key];
+        const old = oldAttrs[key];
+
         if (old !== cur) {
-            unlockAttribute(elm, key);
+            unlockAttribute(elm!, key);
             if (StringCharCodeAt.call(key, 3) === ColonCharCode) {
                 // Assume xml namespace
                 setAttribute(elm, key, cur as string, xmlNS);
@@ -62,7 +52,7 @@ export function patchAttributes(oldVnode: VElement | null, vnode: VElement) {
             } else {
                 setAttribute(elm, key, cur as string);
             }
-            lockAttribute(elm, key);
+            lockAttribute(elm!, key);
         }
     }
 }
--- packages/@lwc/engine-core/src/framework/modules/props.ts
+++ packages/@lwc/engine-core/src/framework/modules/props.ts
@@ -9,6 +9,8 @@ import { assert, isNull, isUndefined, keys } from '@lwc/shared';
 import { getProperty, setProperty } from '../../renderer';
 import { VElement } from '../../3rdparty/snabbdom/types';
 
+import { EmptyObject } from '../utils';
+
 function isLiveBindingProp(sel: string, key: string): boolean {
     // For properties with live bindings, we read values from the DOM element
     // instead of relying on internally tracked values.
@@ -16,33 +18,34 @@ function isLiveBindingProp(sel: string, key: string): boolean {
 }
 
 export function patchProps(oldVnode: VElement | null, vnode: VElement) {
-    const props = vnode.data.props;
+    const { props } = vnode.data;
     if (isUndefined(props)) {
         return;
     }
 
-    const oldProps = isNull(oldVnode) ? undefined : oldVnode.data.props;
+    const oldProps = isNull(oldVnode) ? EmptyObject : oldVnode.data.props;
     if (oldProps === props) {
         return;
     }
 
     if (process.env.NODE_ENV !== 'production') {
         assert.invariant(
-            isUndefined(oldProps) || keys(oldProps).join(',') === keys(props).join(','),
+            oldProps === EmptyObject || keys(oldProps).join(',') === keys(props).join(','),
             'vnode.data.props cannot change shape.'
         );
     }
 
-    const isFirstPatch = isUndefined(oldProps);
+    const isFirstPatch = isNull(oldVnode);
     const { elm, sel } = vnode;
 
     for (const key in props) {
         const cur = props[key];
 
-        // if it is the first time this element is patched, or the current value is different to the previous value...
+        // Set the property if it's the first time is is patched or if the previous property is
+        // different than the one previously set.
         if (
             isFirstPatch ||
-            cur !== (isLiveBindingProp(sel, key) ? getProperty(elm!, key) : (oldProps as any)[key])
+            cur !== (isLiveBindingProp(sel, key) ? getProperty(elm!, key) : oldProps[key])
         ) {
             setProperty(elm!, key, cur);
         }
