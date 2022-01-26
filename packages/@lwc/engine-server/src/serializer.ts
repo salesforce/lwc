6bf4ab8482bec4eeaefdd1ae97ddedf144c24f92
Jose David Rodriguez Velasco <jodarove@gmail.com>
2022-01-26 08:07:45 -0500

    fix(hydration): empty interpolated text mistmatch

--- packages/@lwc/engine-server/src/serializer.ts
+++ packages/@lwc/engine-server/src/serializer.ts
@@ -23,7 +23,7 @@ function serializeChildNodes(children: HostChildNode[]): string {
         .map((child): string => {
             switch (child.type) {
                 case HostNodeType.Text:
-                    return htmlEscape(child.value);
+                    return child.value === '' ? '\u200C' : htmlEscape(child.value);
                 case HostNodeType.Comment:
                     return `<!--${htmlEscape(child.value)}-->`;
                 case HostNodeType.Raw:
