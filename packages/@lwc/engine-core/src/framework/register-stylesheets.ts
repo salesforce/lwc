a372c7f7c89f029c74e261a8217cf740c3677228
Nolan Lawson <nlawson@salesforce.com>
2022-02-25 09:05:59 -0800

    fix: fix array logic

--- packages/@lwc/engine-core/src/framework/register-stylesheets.ts
+++ packages/@lwc/engine-core/src/framework/register-stylesheets.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-import { isUndefined } from '@lwc/shared';
+import { ArrayPush, isUndefined } from '@lwc/shared';
 import { Template } from './template';
 import { TemplateStylesheetFactories } from './stylesheet';
 import { flattenStylesheets } from './utils';
@@ -13,12 +13,15 @@ import { checkVersionMismatch } from './check-version-mismatch';
 export function registerStylesheets(
     tmpl: Template,
     stylesheetToken: string,
-    ...stylesheets: Array<TemplateStylesheetFactories | undefined>
+    ...stylesheetLists: Array<TemplateStylesheetFactories | undefined>
 ) {
     tmpl.stylesheetToken = stylesheetToken;
-    tmpl.stylesheets = stylesheets.filter(
-        (stylesheet) => !isUndefined(stylesheet)
-    ) as TemplateStylesheetFactories;
+    tmpl.stylesheets = [];
+    for (const stylesheets of stylesheetLists) {
+        if (!isUndefined(stylesheets)) {
+            ArrayPush.apply(tmpl.stylesheets, stylesheets);
+        }
+    }
     if (process.env.NODE_ENV !== 'production') {
         for (const stylesheet of flattenStylesheets(tmpl.stylesheets)) {
             checkVersionMismatch(stylesheet, 'stylesheet');
