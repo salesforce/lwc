fbd83151fe60c9567dfaabc7efe6a0ec9b657fc7
Pierre-Marie Dartus <p.dartus@salesforce.com>
2022-03-28 09:39:08 +0200

    test: Add `connectedCallback` invocation order test (#2764)

--- packages/integration-karma/test/rendering/callback-invocation-order/x/toggleParent/toggleParent.js
+++ packages/integration-karma/test/rendering/callback-invocation-order/x/toggleParent/toggleParent.js
@@ -1,20 +1,11 @@
 import { LightningElement } from 'lwc';
 
 export default class ToggleParent extends LightningElement {
-    constructor() {
-        super();
-        window.timingBuffer.push('parent:constructor');
-    }
-
     connectedCallback() {
         window.timingBuffer.push('parent:connectedCallback');
     }
 
     disconnectedCallback() {
         window.timingBuffer.push('parent:disconnectedCallback');
-    }
-
-    renderedCallback() {
-        window.timingBuffer.push('parent:renderedCallback');
     }
 }
