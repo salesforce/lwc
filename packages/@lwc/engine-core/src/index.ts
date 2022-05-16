d9bdaa5fdc26a69bab90ec017e76b770de0d8d8d
Nolan Lawson <nolan@nolanlawson.com>
2022-05-16 12:56:01 -0700

    fix(engine-dom): make feature flags work (#2812)
    * fix(engine-dom): make feature flags work
    
    Fixes #2811
    
    * fix: license headers
    
    * test: fix jest tests
    
    * test: fix test
    
    * test: fix test
    
    * fix: use Eugene's technique instead
    
    * Revert "fix: use Eugene's technique instead"
    
    This reverts commit 72afdc050129e7491d7a96ff30bc32e08b1f9f0b.
    
    * fix: use Eugene's technique instead
    
    * fix: revert unnecessary test change
    
    * fix: revert, use the elaborate test instead
    
    * fix: fix feature flags in engine-server as well

--- packages/@lwc/engine-core/src/index.ts
+++ packages/@lwc/engine-core/src/index.ts
@@ -5,4 +5,7 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
 
+// Tests
+import './testFeatureFlag';
+
 export * from './framework/main';
---
+++ packages/@lwc/engine-core/src/testFeatureFlag.ts
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+
+import features from '@lwc/features';
+
+if (process.env.NODE_ENV !== 'production' && typeof window !== 'undefined') {
+    window.addEventListener('test-dummy-flag', () => {
+        let hasFlag = false;
+        if (features.DUMMY_TEST_FLAG) {
+            hasFlag = true;
+        }
+
+        window.dispatchEvent(
+            new CustomEvent('has-dummy-flag', {
+                detail: {
+                    package: '@lwc/engine-core',
+                    hasFlag,
+                },
+            })
+        );
+    });
+}
