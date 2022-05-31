4d5b57ee33ad3254cb7bd9d5e3393a8d0a23f781
Nolan Lawson <nlawson@salesforce.com>
2022-05-31 15:35:34 -0700

    test: on second thought, bring test back

--- packages/integration-karma/test/static-content/index.spec.js
+++ packages/integration-karma/test/static-content/index.spec.js
@@ -1,6 +1,38 @@
-import { createElement } from 'lwc';
+import { createElement, setFeatureFlagForTest } from 'lwc';
+import Container from './x/container/container';
 import MultipleStyles from './x/multipleStyles/multipleStyles';
 import SvgNs from './x/svgNs/svgNs';
+
+// In compat mode, the component will always render in synthetic mode with the scope attribute
+if (!process.env.NATIVE_SHADOW && !process.env.COMPAT) {
+    describe('Mixed mode for static content', () => {
+        beforeEach(() => {
+            setFeatureFlagForTest('ENABLE_MIXED_SHADOW_MODE', true);
+        });
+        afterEach(() => {
+            setFeatureFlagForTest('ENABLE_MIXED_SHADOW_MODE', false);
+        });
+
+        ['native', 'synthetic'].forEach((firstRenderMode) => {
+            it(`should set the tokens for synthetic shadow when it renders first in ${firstRenderMode}`, () => {
+                const elm = createElement('x-container', { is: Container });
+                elm.syntheticFirst = firstRenderMode === 'synthetic';
+                document.body.appendChild(elm);
+
+                const syntheticMode = elm.shadowRoot
+                    .querySelector('x-component')
+                    .shadowRoot.querySelector('div');
+                const nativeMode = elm.shadowRoot
+                    .querySelector('x-native')
+                    .shadowRoot.querySelector('x-component')
+                    .shadowRoot.querySelector('div');
+
+                expect(syntheticMode.hasAttribute('x-component_component')).toBe(true);
+                expect(nativeMode.hasAttribute('x-component_component')).toBe(false);
+            });
+        });
+    });
+}
 
 describe('static content when stylesheets change', () => {
     it('should reflect correct token for scoped styles', () => {
