518ab2e959cbcd9eb4406fb8db18dbf4a525d4b2
Dale Bustad <dale@divmain.com>
2022-06-03 16:15:28 -0700

    chore: release v2.14.1 (#2866) @W-7258582 (#2867)
    * fix: only remove slot children in synthetic shadow (#2843)
    
    * fix: only remove slot children in synthetic shadow
    
    * fix: use case block
    
    * fix: only add version mismatch test code in karma (#2852)
    
    * test(integration-karma): ensure constructable stylesheets are re-used (#2844)
    
    * test(integration-karma): ensure constructable stylesheets are re-used
    
    * test: add test for shared style
    
    * chore(nucleus): remove more downstreams (#2855)
    
    * chore(nucleus): remove another downstream (#2857)
    
    * docs: fix typo in template compiler readme (#2848)
    
    * docs: fix typo in template compiler readme
    
    * docs: rewording usage of lwc dynamic directive
    
    Co-authored-by: Eugene Kashida <ekashida@gmail.com>
    
    Co-authored-by: Eugene Kashida <ekashida@gmail.com>
    
    * test(integration-karma): update ACT components (#2862)
    
    * fix(engine-core): revert "optimize computation of transitive shadow mode" (#2859)
    
    * fix(engine-core): correctly compute shadowMode
    
    * fix: fix comment
    
    * fix: update packages/integration-karma/test/mixed-shadow-mode/dual-component/index.spec.js
    
    Co-authored-by: Eugene Kashida <ekashida@gmail.com>
    
    * fix: fix another one
    
    * fix: improve tests
    
    * fix: improve tests
    
    * fix: improve test
    
    * fix: revert
    
    * Revert "refactor(engine): optimize computation of transitive shadow mode (#2803)"
    
    This reverts commit 95ce4c313f6e970991216fca0b5140b9c8ab5336.
    
    Co-authored-by: Eugene Kashida <ekashida@gmail.com>
    
    * chore: release v2.14.1 (#2866)
    
    Co-authored-by: Nolan Lawson <nolan@nolanlawson.com>
    Co-authored-by: Mohan Raj Rajamanickam <1509984+mohanraj-r@users.noreply.github.com>
    Co-authored-by: Eugene Kashida <ekashida@gmail.com>
    Co-authored-by: Ravi Jayaramappa <ravi.jayaramappa@salesforce.com>

--- packages/@lwc/engine-core/src/framework/rendering.ts
+++ packages/@lwc/engine-core/src/framework/rendering.ts
@@ -284,11 +284,15 @@ function unmount(vnode: VNode, parent: ParentNode, doRemove: boolean = false) {
         removeNode(elm!, parent);
     }
 
-    const removeChildren = sel === 'slot'; // slot content is removed to trigger slotchange event when removing slot
     switch (type) {
-        case VNodeType.Element:
+        case VNodeType.Element: {
+            // Slot content is removed to trigger slotchange event when removing slot.
+            // Only required for synthetic shadow.
+            const removeChildren =
+                sel === 'slot' && vnode.owner.shadowMode === ShadowMode.Synthetic;
             unmountVNodes(vnode.children, elm as ParentNode, removeChildren);
             break;
+        }
 
         case VNodeType.CustomElement: {
             const { vm } = vnode;
---
+++ packages/integration-karma/test/rendering/stylesheets/x/sharing2/sharing2.html
@@ -0,0 +1,3 @@
+<template>
+  <h1>hello</h1>
+</template>
