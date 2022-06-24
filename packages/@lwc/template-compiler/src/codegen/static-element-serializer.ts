4df1e8c1ee555de5be301023dc4334d606f840cb
Nolan Lawson <nlawson@salesforce.com>
2022-06-24 15:31:00 -0700

    fix: simplify serialization code

--- packages/@lwc/template-compiler/src/codegen/static-element-serializer.ts
+++ packages/@lwc/template-compiler/src/codegen/static-element-serializer.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-import { htmlEscape, isVoidElement, HTML_NAMESPACE } from '@lwc/shared';
+import { htmlEscape, isVoidElement } from '@lwc/shared';
 import { ChildNode, Element, Literal } from '../shared/types';
 import { isElement, isText, isComment } from '../shared/ast';
 
@@ -97,24 +97,14 @@ function serializeChildren(
 }
 
 export function serializeStaticElement(element: Element, preserveComments: boolean): string {
-    const { name: tagName, namespace } = element;
+    const { name: tagName } = element;
 
-    let html = `<${tagName}${serializeAttrs(element)}`;
+    let html = `<${tagName}${serializeAttrs(element)}>`;
 
-    const childrenHtml = serializeChildren(element.children, tagName, preserveComments);
+    html += serializeChildren(element.children, tagName, preserveComments);
 
-    // In the HTML namespace, some elements are "void", i.e. self-closing, and we want
-    // to serialize those without the unnecessary "/" character. For other namespaces,
-    // we need an explicit end tag (e.g. </foo>).
-    if (namespace === undefined || namespace === HTML_NAMESPACE) {
-        // undefined implies HTML
-        html += `>`;
-        if (!isVoidElement(tagName)) {
-            html += `${childrenHtml}</${tagName}>`;
-        }
-    } else {
-        // non-html namespace, e.g. svg or MathML
-        html += `>${childrenHtml}</${tagName}>`;
+    if (!isVoidElement(tagName)) {
+        html += `</${tagName}>`;
     }
 
     return html;
