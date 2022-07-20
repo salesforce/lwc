3b47377a7c88c6a7c1a1312da5763652bec7782b
Jason Ye <jye@salesforce.com>
2022-07-20 10:52:07 -0700

    fix: add tests using props from template

---
+++ packages/@lwc/engine-server/src/__tests__/fixtures/overwrite-properties-template/modules/x/overwrite-properties-template/overwrite-properties-template.js
@@ -0,0 +1,12 @@
+import { LightningElement, api } from 'lwc';
+
+export default class Cmp extends LightningElement {
+    name = 'my name';
+    namespace = 'my namespace';
+    type = 'my type';
+    parent = 'my parent';
+    value = 'my value';
+    shadowRoot = 'my shadow root';
+    attributes = 'my attributes';
+    eventListeners = 'my event listeners';
+}
