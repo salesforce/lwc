ded0331332c77a1573d45a85b4b9ec5e514b848e
James Tu <jmsjtu@gmail.com>
2022-07-27 22:56:35 -0700

    ci: add initial versioning script

--- scripts/release/README.md
+++ scripts/release/README.md
@@ -11,20 +11,4 @@ The release commit is generated locally, along with a git tag.
 point to commits that are not guaranteed to exist at a later time due to squashed merges or
 deleted branches.
 
-## `yarn release:publish:ci`
-
-This script publishes packages to NPM. It restricts usage to a CI context to ensure that all
-tests are run as part of the process. It also restricts usage to release branches. A release
-branch is defined as a branch that matches one of the following regular expressions:
-
--   /^master\$/
--   /^winter\d+\$/
--   /^spring\d+\$/
--   /^summer\d+\$/
-
-This script is triggered when a git tag that matches the regular expression /^v.\*/ is pushed to
-Github.
-
-The npm dist-tag used depends on the current branch. If the current branch is `master` then the
-dist-tag is `next`. For all other release branches (e.g., `winter21`), the branch name is used as
-the dist-tag.
+## add something for nucleus deploy how it works
--- scripts/release/version.mjs
+++ scripts/release/version.mjs
@@ -7,32 +7,105 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
 
-import { execaSync } from 'execa';
+import child_process from 'child_process';
+import fs from 'fs';
+import path from 'path';
+import { execaCommandSync } from 'execa';
+import semver from 'semver';
+import inquirer from 'inquirer';
 
-const ARGS = [
-    // https://github.com/lerna/lerna/tree/master/commands/version
-    'version',
-    // Use exact version number without using semver notation (i.e., ~ and ^)
-    '--exact',
-    // Skip lerna change detection and force version number updates across all packages
-    '--force-publish',
-    // Skip git commit and tag push
-    '--no-push',
-    // Don't add a git tag
-    '--no-git-tag-version',
-];
+const newVersion = await promptVersion();
+updatePackages(newVersion);
 
-const { stderr, stdin, stdout } = process;
+async function promptVersion() {
+    const tag = execaCommandSync('git describe --tags --abbrev=0 HEAD').stdout;
 
-try {
-    execaSync('lerna', ARGS, {
-        // Prioritize locally installed binaries (node_modules/.bin)
-        preferLocal: true,
-        stderr,
-        stdin,
-        stdout,
-    });
-} catch (ex) {
-    console.error(ex);
-    process.exit(1);
+    const patch = semver.inc(tag, 'patch');
+    const minor = semver.inc(tag, 'minor');
+    const major = semver.inc(tag, 'major');
+
+    const { prompt: promptAnswer } = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'prompt',
+            messageL: `Select a new version, currently ${tag}`,
+            choices: [
+                { value: patch, name: `Patch (${patch})` },
+                { value: minor, name: `Minor (${minor})` },
+                { value: major, name: `Major (${major})` },
+                { value: 'CUSTOM', name: 'Custom Version' },
+            ],
+            pageSize: 4,
+        },
+    ]);
+
+    if (promptAnswer === 'CUSTOM') {
+        const answer = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'input',
+                message: 'Enter a custom version',
+                filter: semver.valid,
+                validate: (v) => v !== null || 'Must be a valid semver version',
+            },
+        ]);
+
+        return answer.input;
+    }
+
+    return promptAnswer;
+}
+
+function updatePackages(newVersion) {
+    try {
+        const output = child_process.execSync('yarn --silent workspaces info');
+        const workspacesInfo = Object.values(JSON.parse(output));
+
+        // validate there are no mismatching dependencies
+        const hasMismatchedWorkspaceDependencies = workspacesInfo.includes(
+            ({ mismatchedWorkspaceDependencies }) => mismatchedWorkspaceDependencies.length
+        );
+        if (hasMismatchedWorkspaceDependencies) {
+            const misMatchedDependencies = workspacesInfo.map(
+                ({ location, mismatchedWorkspaceDependencies }) =>
+                    `${location}: ${mismatchedWorkspaceDependencies.join(', ')}`
+            );
+            throw new Error(`There are missmatching workspace dependencies, please resolve before bumping the versions:
+        ${misMatchedDependencies.join('\n')}
+        `);
+        }
+
+        const updatedPackages = workspacesInfo.map(({ location, workspaceDependencies }) => {
+            const packageJsonLocation = `${path.resolve(
+                path.dirname('..'),
+                location
+            )}/package.json`;
+            const packageJson = JSON.parse(fs.readFileSync(packageJsonLocation, 'utf-8'));
+            // Look for different types of dependencies ex: dependencies, devDependencies, peerDependencies
+            const packageDependencyTypes = Object.keys(packageJson).filter((type) =>
+                type.match(/.*[dD]ependencies.*/g)
+            );
+
+            packageJson.version = newVersion;
+
+            for (const dependencyType of packageDependencyTypes) {
+                for (const dependencyName of workspaceDependencies) {
+                    const currentVersion = packageJson[dependencyType][dependencyName];
+                    if (currentVersion !== newVersion) {
+                        packageJson[dependencyType][dependencyName] = newVersion;
+                    }
+                }
+            }
+
+            return [packageJsonLocation, JSON.stringify(packageJson, null, 4)];
+        });
+
+        // validate all the dependencies match again here
+        for (const [location, serializedJson] of updatedPackages) {
+            fs.writeFileSync(location, serializedJson);
+        }
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
 }
