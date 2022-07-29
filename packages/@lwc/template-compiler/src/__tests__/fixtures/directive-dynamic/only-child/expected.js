b4a423ab755d449f63e1837f863fa95fbfb2e5e0
Pierre-Marie Dartus <p.dartus@salesforce.com>
2022-07-29 16:41:16 +0200

    perf: optimize component with  directive

--- packages/@lwc/template-compiler/src/__tests__/fixtures/directive-dynamic/only-child/expected.js
+++ packages/@lwc/template-compiler/src/__tests__/fixtures/directive-dynamic/only-child/expected.js
@@ -3,10 +3,8 @@ const stc0 = {
   key: 0,
 };
 function tmpl($api, $cmp, $slotset, $ctx) {
-  const { dc: api_dynamic_component, f: api_flatten } = $api;
-  return api_flatten([
-    api_dynamic_component("x-foo", $cmp.trackedProp.foo, stc0),
-  ]);
+  const { dc: api_dynamic_component } = $api;
+  return [api_dynamic_component("x-foo", $cmp.trackedProp.foo, stc0)];
   /*LWC compiler vX.X.X*/
 }
 export default registerTemplate(tmpl);
