4d2f8ab32abe5bb6a703aedf0b107fb6d0c37435
James Tu <jmsjtu@gmail.com>
2022-08-02 17:59:26 -0700

    test: update unit tests

--- packages/@lwc/template-compiler/src/__tests__/fixtures/parsing-errors/invalid-template-attribute/single-attribute-with-children/expected.js
+++
@@ -1,8 +0,0 @@
-import { registerTemplate } from "lwc";
-function tmpl($api, $cmp, $slotset, $ctx) {
-  const { t: api_text } = $api;
-  return [api_text("Hello")];
-  /*LWC compiler vX.X.X*/
-}
-export default registerTemplate(tmpl);
-tmpl.stylesheets = [];
