9b61cd549960ec6a08c6410ec95f6a44066d16ce
Nolan Lawson <nolan@nolanlawson.com>
2022-08-03 14:38:45 -0700

    fix(babel-plugin-component): default/star imports should not throw (#2978)

---
+++ packages/@lwc/babel-plugin-component/src/__tests__/fixtures/component/import-engine-star/expected.js
@@ -0,0 +1,8 @@
+import _tmpl from "./test.html";
+import { registerComponent as _registerComponent } from "lwc";
+import * as lwc from 'lwc';
+export default _registerComponent(class extends lwc.LightningElement {
+  /*LWC compiler vX.X.X*/
+}, {
+  tmpl: _tmpl
+});
