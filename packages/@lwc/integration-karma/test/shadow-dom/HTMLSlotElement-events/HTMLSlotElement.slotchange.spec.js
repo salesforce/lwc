4b13d0ed9ff0e23b5808ed533b86ed83ced10522
Nolan Lawson <nlawson@salesforce.com>
2022-08-05 11:50:18 -0700

    test: undo unnecessary change

--- packages/@lwc/integration-karma/test/shadow-dom/HTMLSlotElement-events/HTMLSlotElement.slotchange.spec.js
+++ packages/@lwc/integration-karma/test/shadow-dom/HTMLSlotElement-events/HTMLSlotElement.slotchange.spec.js
@@ -11,70 +11,68 @@ function waitForSlotChange() {
 let parent;
 let child;
 
-describe('slotchange', () => {
-    beforeEach(async () => {
-        parent = createElement('x-parent', { is: Parent });
-        document.body.appendChild(parent);
-        child = parent.shadowRoot.querySelector('x-child');
+beforeEach(async () => {
+    parent = createElement('x-parent', { is: Parent });
+    document.body.appendChild(parent);
+    child = parent.shadowRoot.querySelector('x-child');
 
-        await waitForSlotChange();
-    });
+    await waitForSlotChange();
+});
 
-    it('should fire slotchange on initial render', () => {
+it('should fire slotchange on initial render', () => {
+    expect(child.getSlotChangeCount()).toBe(1);
+});
+
+it('should fire non-composed slotchange', () => {
+    expect(parent.getSlotChangeCount()).toBe(0);
+});
+
+it('should fire slotchange on add', () => {
+    child.setSlotChangeCount(0);
+    parent.add();
+
+    return waitForSlotChange().then(() => {
         expect(child.getSlotChangeCount()).toBe(1);
     });
+});
 
-    it('should fire non-composed slotchange', () => {
-        expect(parent.getSlotChangeCount()).toBe(0);
+it('should fire slotchange on remove', () => {
+    child.setSlotChangeCount(0);
+    parent.clear();
+
+    return waitForSlotChange().then(() => {
+        expect(child.getSlotChangeCount()).toBe(1);
+    });
+});
+
+it('should fire slotchange on replace', () => {
+    child.setSlotChangeCount(0);
+    parent.replace();
+
+    return waitForSlotChange().then(() => {
+        expect(child.getSlotChangeCount()).toBe(1);
+    });
+});
+
+it('should fire slotchange when slot is removed', () => {
+    child.setSlotChangeCount(0);
+    child.removeSlot();
+
+    return waitForSlotChange().then(() => {
+        expect(child.getSlotChangeCount()).toBe(1);
+    });
+});
+
+it('should fire slotchange when listener added programmatically', () => {
+    let count = 0;
+
+    child.shadowRoot.querySelector('slot').addEventListener('slotchange', () => {
+        count += 1;
     });
 
-    it('should fire slotchange on add', () => {
-        child.setSlotChangeCount(0);
-        parent.add();
+    parent.add();
 
-        return waitForSlotChange().then(() => {
-            expect(child.getSlotChangeCount()).toBe(1);
-        });
-    });
-
-    it('should fire slotchange on remove', () => {
-        child.setSlotChangeCount(0);
-        parent.clear();
-
-        return waitForSlotChange().then(() => {
-            expect(child.getSlotChangeCount()).toBe(1);
-        });
-    });
-
-    it('should fire slotchange on replace', () => {
-        child.setSlotChangeCount(0);
-        parent.replace();
-
-        return waitForSlotChange().then(() => {
-            expect(child.getSlotChangeCount()).toBe(1);
-        });
-    });
-
-    it('should fire slotchange when slot is removed', () => {
-        child.setSlotChangeCount(0);
-        child.removeSlot();
-
-        return waitForSlotChange().then(() => {
-            expect(child.getSlotChangeCount()).toBe(1);
-        });
-    });
-
-    it('should fire slotchange when listener added programmatically', () => {
-        let count = 0;
-
-        child.shadowRoot.querySelector('slot').addEventListener('slotchange', () => {
-            count += 1;
-        });
-
-        parent.add();
-
-        return waitForSlotChange().then(() => {
-            expect(count).toBe(1);
-        });
+    return waitForSlotChange().then(() => {
+        expect(count).toBe(1);
     });
 });
