5b42fa0937564a1dd1298e5d3caa09a747047adc
Jason Ye <jye@salesforce.com>
2022-09-07 10:05:19 -0700

    fix: add comment

--- packages/@lwc/template-compiler/src/codegen/index.ts
+++ packages/@lwc/template-compiler/src/codegen/index.ts
@@ -202,6 +202,7 @@ function transform(codeGen: CodeGen): t.Expression {
     }
 
     /**
+     * Transforms an IfBlock or ElseifBlock along with both its direct descendants and its 'else' descendants.
      *
      * @param conditionalParentBlock The IfBlock or ElseifBlock to transform into a conditional expression
      * @returns A conditional expression representing the full conditional tree with conditionalParentBlock as the root node
