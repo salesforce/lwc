4dd6713872345aefaf8634c9f846532ef93f4dea
James Tu <j.tu@salesforce.com>
2022-09-09 17:20:54 -0700

    fix(template-compiler): add deprecated void elements back (#3037)

---
+++ packages/@lwc/template-compiler/src/__tests__/fixtures/regression/deprecated-void-elements/param/expected.js
@@ -0,0 +1,9 @@
+import { parseFragment, registerTemplate } from "lwc";
+const $fragment1 = parseFragment`<param name="hello" value="world"${3}>`;
+function tmpl($api, $cmp, $slotset, $ctx) {
+  const { st: api_static_fragment } = $api;
+  return [api_static_fragment($fragment1(), 1)];
+  /*LWC compiler vX.X.X*/
+}
+export default registerTemplate(tmpl);
+tmpl.stylesheets = [];
