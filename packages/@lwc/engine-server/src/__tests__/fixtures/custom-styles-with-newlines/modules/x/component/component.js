d7a4c7c3cb12f67a19204a307b122f670fe2e941
Dale Bustad <dale@divmain.com>
2022-09-11 22:17:28 -0700

    test: styles with newlines

---
+++ packages/@lwc/engine-server/src/__tests__/fixtures/custom-styles-with-newlines/modules/x/component/component.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+
+export default class Component extends LightningElement {
+    color = 'blue';
+    margin = 8;
+
+    get customStyles() {
+        debugger;
+        return `
+            color: ${this.color};
+            margin: ${this.margin}px;
+        `;
+    }
+}
