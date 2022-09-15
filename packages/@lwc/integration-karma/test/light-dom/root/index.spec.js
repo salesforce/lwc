8fceda932e807dd920df8095daa62e701d72d87d
Mohammed Abdul Sattar <abdulsattarmohammed@salesforce.com>
2022-09-15 13:17:45 +0530

    chore: disable test for native shadow

--- packages/@lwc/integration-karma/test/light-dom/root/index.spec.js
+++ packages/@lwc/integration-karma/test/light-dom/root/index.spec.js
@@ -42,22 +42,24 @@ describe('root light element', () => {
     afterEach(() => {
         setFeatureFlagForTest('ENABLE_LIGHT_GET_ROOT_NODE_PATCH', false);
     });
-    it('should not dispatch events to window from elements slotted into synthetic shadow', () => {
-        const nodes = createTestElement('x-root', LightElement);
+    if (!process.env.NATIVE_SHADOW) {
+        it('should not dispatch events to window from elements slotted into synthetic shadow', () => {
+            const nodes = createTestElement('x-root', LightElement);
 
-        const log = dispatchEventWithLog(
-            nodes.button,
-            nodes,
-            new CustomEvent('test', { bubbles: true, composed: false })
-        );
+            const log = dispatchEventWithLog(
+                nodes.button,
+                nodes,
+                new CustomEvent('test', { bubbles: true, composed: false })
+            );
 
-        const composedPath = [nodes.button, nodes.slot, nodes['x-list'].shadowRoot];
-        expect(log).toEqual([
-            [nodes.button, nodes.button, composedPath],
-            [nodes.slot, nodes.button, composedPath],
-            [nodes['x-list'].shadowRoot, nodes.button, composedPath],
-        ]);
-    });
+            const composedPath = [nodes.button, nodes.slot, nodes['x-list'].shadowRoot];
+            expect(log).toEqual([
+                [nodes.button, nodes.button, composedPath],
+                [nodes.slot, nodes.button, composedPath],
+                [nodes['x-list'].shadowRoot, nodes.button, composedPath],
+            ]);
+        });
+    }
     it('should throw events properly with flag ENABLE_LIGHT_GET_ROOT_NODE_PATCH', () => {
         setFeatureFlagForTest('ENABLE_LIGHT_GET_ROOT_NODE_PATCH', true);
         const nodes = createTestElement('x-root', LightElement);
