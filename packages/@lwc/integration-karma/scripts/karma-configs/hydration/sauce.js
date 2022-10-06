bdc182575e7005438d91dc2f7e698cef018fcad5
James Tu <j.tu@salesforce.com>
2022-10-06 14:33:55 -0700

    Update packages/@lwc/integration-karma/scripts/karma-configs/hydration/sauce.js
    Co-authored-by: Nolan Lawson <nlawson@salesforce.com>

--- packages/@lwc/integration-karma/scripts/karma-configs/hydration/sauce.js
+++ packages/@lwc/integration-karma/scripts/karma-configs/hydration/sauce.js
@@ -37,7 +37,7 @@ const SAUCE_BROWSERS = [
         label: 'sl_safari_latest',
         browserName: 'safari',
         browserVersion: 'latest',
-        platformName: 'macOS 12',
+        platformName: 'macOS 12', // Note: this must be updated when macOS releases new updates
     },
 ];
 
