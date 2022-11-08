d8252899df107ae2d5a14dd76f8a70f0e6056eea
Ravi Jayaramappa <ravi.jayaramappa@salesforce.com>
2022-11-07 17:39:45 -0800

    test: more tests

--- packages/@lwc/template-compiler/src/__tests__/fixtures/attributes/valid-hyphen-attribute/expected.js
+++ packages/@lwc/template-compiler/src/__tests__/fixtures/attributes/valid-hyphen-attribute/expected.js
@@ -18,12 +18,40 @@ const stc2 = {
   },
   key: 2,
 };
+const stc3 = {
+  props: {
+    hyphen3pecial: "bar",
+  },
+  key: 3,
+};
+const stc4 = {
+  props: {
+    hyphenSSpecial: "bar",
+  },
+  key: 4,
+};
+const stc5 = {
+  props: {
+    hyphenÀpecial: "bar",
+  },
+  key: 5,
+};
+const stc6 = {
+  props: {
+    hyphenªpecial: "bar",
+  },
+  key: 6,
+};
 function tmpl($api, $cmp, $slotset, $ctx) {
   const { c: api_custom_element } = $api;
   return [
     api_custom_element("x-button", _xButton, stc0),
     api_custom_element("x-button", _xButton, stc1),
     api_custom_element("x-button", _xButton, stc2),
+    api_custom_element("x-button", _xButton, stc3),
+    api_custom_element("x-button", _xButton, stc4),
+    api_custom_element("x-button", _xButton, stc5),
+    api_custom_element("x-button", _xButton, stc6),
   ];
   /*LWC compiler vX.X.X*/
 }
