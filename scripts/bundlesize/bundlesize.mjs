6bd48833fea8f1f3ebc2648d5e7ae2d44d2f329b
Nolan Lawson <nlawson@salesforce.com>
2022-11-08 14:44:21 -0800

    fix: header

--- scripts/bundlesize/bundlesize.mjs
+++ scripts/bundlesize/bundlesize.mjs
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2019, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+
 // Simple script to check our bundlesize
 import fs from 'fs/promises';
 import { promisify } from 'util';
