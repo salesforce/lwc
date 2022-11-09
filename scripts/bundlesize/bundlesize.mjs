3b4abc89cb1df2df7712ffab720098a1a4b233de
Nolan Lawson <nlawson@salesforce.com>
2022-11-09 09:52:49 -0800

    fix: do not minify twice

--- scripts/bundlesize/bundlesize.mjs
+++ scripts/bundlesize/bundlesize.mjs
@@ -6,13 +6,12 @@
  */
 
 // Simple script to check our bundlesize
-import fs from 'fs/promises';
-import { promisify } from 'util';
-import { gzip as gzipCallback } from 'zlib';
-import { fileURLToPath } from 'url';
-import path from 'path';
+import fs from 'node:fs/promises';
+import { promisify } from 'node:util';
+import { gzip as gzipCallback } from 'node:zlib';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
 import bytes from 'bytes';
-import { minify } from 'terser';
 
 const gzip = promisify(gzipCallback);
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -23,9 +22,8 @@ let failed = false;
 
 for (const { path: filePath, maxSize } of files) {
     const absoluteFile = path.join(__dirname, '../..', filePath);
-    const source = await fs.readFile(absoluteFile, 'utf-8');
-    const { code } = await minify(source, { compress: true, mangle: true });
-    const bufferGzipped = await gzip(Buffer.from(code, 'utf-8'), { level: 9 });
+    const minifiedSource = await fs.readFile(absoluteFile, 'utf-8'); // file is assume to be pre-minified
+    const bufferGzipped = await gzip(Buffer.from(minifiedSource, 'utf-8'), { level: 9 });
 
     if (bufferGzipped.length > bytes.parse(maxSize)) {
         console.log(`FAIL: ${filePath}: ${bytes.format(bufferGzipped.length)} > ${maxSize}`);
