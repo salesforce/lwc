d5b7443b587c8366c2cd7134a3b8048f3a7be687
Nolan Lawson <nlawson@salesforce.com>
2022-11-15 10:59:32 -0800

    fix(synthetic-shadow): apply iframe-content-window polyfill only in IE11
    Fixes #1513

--- packages/@lwc/synthetic-shadow/src/polyfills/iframe-content-window/detect.ts
+++ packages/@lwc/synthetic-shadow/src/polyfills/iframe-content-window/detect.ts
@@ -4,7 +4,14 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
+import { isUndefined } from '@lwc/shared';
+
 export default function detect(): boolean {
     // Note: when using this in mobile apps, we might have a DOM that does not support iframes.
-    return typeof HTMLIFrameElement !== 'undefined';
+    const hasIframe = typeof HTMLIFrameElement !== 'undefined';
+
+    // Detect IE, via https://stackoverflow.com/a/9851769
+    const isIE11 = !isUndefined((document as any).documentMode);
+
+    return hasIframe && isIE11;
 }
