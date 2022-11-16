9fe8cd634fd1f1e7fd5234a6907aaca09174f474
Nolan Lawson <nlawson@salesforce.com>
2022-11-16 08:13:53 -0800

    fix: better compat check

--- packages/@lwc/synthetic-shadow/src/polyfills/iframe-content-window/detect.ts
+++ packages/@lwc/synthetic-shadow/src/polyfills/iframe-content-window/detect.ts
@@ -4,14 +4,14 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-import { isUndefined } from '@lwc/shared';
+import { isTrue } from '@lwc/shared';
 
 export default function detect(): boolean {
     // Note: when using this in mobile apps, we might have a DOM that does not support iframes.
     const hasIframe = typeof HTMLIFrameElement !== 'undefined';
 
-    // Detect IE, via https://stackoverflow.com/a/9851769
-    const isIE11 = !isUndefined((document as any).documentMode);
+    // This polyfill should only apply in compat mode; see https://github.com/salesforce/lwc/issues/1513
+    const isCompat = typeof Proxy !== 'undefined' && isTrue((Proxy as any).isCompat);
 
-    return hasIframe && isIE11;
+    return hasIframe && isCompat;
 }
