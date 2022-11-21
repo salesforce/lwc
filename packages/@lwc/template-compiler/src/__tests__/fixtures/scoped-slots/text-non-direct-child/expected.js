cf95a2a0807a8d8406ad907f85c98215092381ac
Ravi Jayaramappa <ravi.jayaramappa@salesforce.com>
2022-11-21 14:19:56 -0800

    feat: use vfragment node for scoped slot content @W-11984247 (#3187)
    * feat: use vfragment node for scoped slot content @W-11984247
    fixes https://github.com/salesforce/lwc/issues/3111
    add support to use key attribute on <slot> element
    
    * fix: remove todo
    
    * fix: use the key on the element data bag, no need for a default

--- packages/@lwc/template-compiler/src/__tests__/fixtures/scoped-slots/text-non-direct-child/expected.js
+++ packages/@lwc/template-compiler/src/__tests__/fixtures/scoped-slots/text-non-direct-child/expected.js
@@ -13,20 +13,25 @@ function tmpl($api, $cmp, $slotset, $ctx) {
     t: api_text,
     h: api_element,
     st: api_static_fragment,
+    fr: api_fragment,
     ssf: api_scoped_slot_factory,
     c: api_custom_element,
   } = $api;
   return [
     api_custom_element("x-list", _xList, stc0, [
-      api_scoped_slot_factory("", function (item) {
-        return [
-          api_element("span", stc1, [
-            api_text(
-              api_dynamic_text(item.id) + " - " + api_dynamic_text(item.name)
-            ),
-          ]),
-          api_static_fragment($fragment1(), 3),
-        ];
+      api_scoped_slot_factory("", function (item, key) {
+        return api_fragment(
+          key,
+          [
+            api_element("span", stc1, [
+              api_text(
+                api_dynamic_text(item.id) + " - " + api_dynamic_text(item.name)
+              ),
+            ]),
+            api_static_fragment($fragment1(), 3),
+          ],
+          0
+        );
       }),
     ]),
   ];
