1e11f39d4a245368baf5ac81e622ea308b59fc21
Mohammed Abdul Sattar <abdulsattarmohammed@salesforce.com>
2022-12-05 06:38:36 +0530

    fix: update snapshot

---
+++ packages/@lwc/template-compiler/src/__tests__/fixtures/slots/dynamic-name/expected.js
@@ -0,0 +1,21 @@
+import _nsCmp from "ns/cmp";
+import { registerTemplate } from "lwc";
+const stc0 = {
+  key: 0,
+};
+function tmpl($api, $cmp, $slotset, $ctx) {
+  const { h: api_element, c: api_custom_element } = $api;
+  return [
+    api_custom_element("ns-cmp", _nsCmp, stc0, [
+      api_element("p", {
+        attrs: {
+          slot: $cmp.mySlot,
+        },
+        key: 1,
+      }),
+    ]),
+  ];
+  /*LWC compiler vX.X.X*/
+}
+export default registerTemplate(tmpl);
+tmpl.stylesheets = [];
