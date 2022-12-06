83105021d17a3a60ed6ed982eeb3b70bd5e76ce8
James Tu <jmsjtu@gmail.com>
2022-12-06 13:28:18 -0800

    fix: update attribute parsing to account for '=' in attribute value (#3185) (#3209)

--- packages/@lwc/template-compiler/src/parser/attribute.ts
+++ packages/@lwc/template-compiler/src/parser/attribute.ts
@@ -38,14 +38,12 @@ import {
     TEMPLATE_DIRECTIVES,
 } from './constants';
 
-function isQuotedAttribute(rawAttribute: string) {
-    const [, value] = rawAttribute.split('=');
-    return value && value.startsWith('"') && value.endsWith('"');
+function isQuotedAttribute(attrVal: string) {
+    return attrVal && attrVal.startsWith('"') && attrVal.endsWith('"');
 }
 
-function isEscapedAttribute(rawAttribute: string) {
-    const [, value] = rawAttribute.split('=');
-    return !value || !(value.includes('{') && value.includes('}'));
+function isEscapedAttribute(attrVal: string) {
+    return !attrVal || !(attrVal.includes('{') && attrVal.includes('}'));
 }
 
 export function isIdReferencingAttribute(attrName: string): boolean {
@@ -107,8 +105,9 @@ export function normalizeAttributeValue(
         }
     }
 
-    const isQuoted = isQuotedAttribute(raw);
-    const isEscaped = isEscapedAttribute(raw);
+    const rawAttrVal = raw.slice(raw.indexOf('=') + 1);
+    const isQuoted = isQuotedAttribute(rawAttrVal);
+    const isEscaped = isEscapedAttribute(rawAttrVal);
     if (!isEscaped && isExpression(value)) {
         if (isQuoted) {
             // <input value="{myValue}" />
