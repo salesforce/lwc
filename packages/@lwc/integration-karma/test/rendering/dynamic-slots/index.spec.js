a7dd892fc825a3bc42d02f7d0f1752e7f75ff8e3
Mohammed Abdul Sattar <abdulsattarmohammed@salesforce.com>
2022-12-06 16:38:39 +0530

    fix: use assignedNodes instead of textContent

--- packages/@lwc/integration-karma/test/rendering/dynamic-slots/index.spec.js
+++ packages/@lwc/integration-karma/test/rendering/dynamic-slots/index.spec.js
@@ -10,10 +10,22 @@ describe('dynamic slotting', () => {
     it('should handle slot name being set to undefined', async function () {
         const elm = createElement('x-parent', { is: Parent });
         document.body.appendChild(elm);
+        const childElm = elm.shadowRoot.querySelector('x-child');
+        const defaultSlot = childElm.shadowRoot.querySelector('slot:not([name])');
+        const altSlot = childElm.shadowRoot.querySelector('slot[name="altdefault"]');
+
+        expect(defaultSlot.assignedNodes().length).toBe(1);
+        expect(altSlot.assignedNodes().length).toBe(0);
 
         elm.toggleUndefinedName();
         await Promise.resolve();
-        expect(elm.shadowRoot.textContent).toEqual('Named 1Overridden default contentDefault slot');
+        expect(defaultSlot.assignedNodes().length).toBe(0);
+        expect(altSlot.assignedNodes().length).toBe(1);
+
+        elm.toggleUndefinedName();
+        await Promise.resolve();
+        expect(defaultSlot.assignedNodes().length).toBe(1);
+        expect(altSlot.assignedNodes().length).toBe(0);
     });
     it('should rerender slots', async function () {
         const elm = createElement('x-parent', { is: Parent });
@@ -22,6 +34,6 @@ describe('dynamic slotting', () => {
 
         elm.increment();
         await Promise.resolve();
-        expect(elm.shadowRoot.textContent).toEqual('Default slotNamed 2Overridden default content');
+        expect(elm.shadowRoot.textContent).toEqual('Default slotNamed 2Overridden default content'); // notice the 2 in the text
     });
 });
