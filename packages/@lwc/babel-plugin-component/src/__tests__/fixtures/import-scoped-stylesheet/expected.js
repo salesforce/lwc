dd72f7c41095838d85fa8bb410dddf5a4eee1a3f
Rui Qiu <rui.qiu@salesforce.com>
2022-12-06 16:53:09 -0800

    fix: minor test fix

--- packages/@lwc/babel-plugin-component/src/__tests__/fixtures/import-scoped-stylesheet/expected.js
+++ packages/@lwc/babel-plugin-component/src/__tests__/fixtures/import-scoped-stylesheet/expected.js
@@ -5,7 +5,6 @@ import scoped from "./stylesheet.scoped.css?scoped=true";
 export default _registerComponent(class extends LightningElement {
   static stylesheets = [notScoped, scoped];
   /*LWC compiler vX.X.X*/
-
 }, {
   tmpl: _tmpl
 });
