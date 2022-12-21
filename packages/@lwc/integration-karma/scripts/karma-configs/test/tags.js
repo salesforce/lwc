d1d722df131c709e039b8d24dc275435a40df0b9
Nolan Lawson <nlawson@salesforce.com>
2022-12-21 10:34:22 -0800

    test: fix coverage tests

--- packages/@lwc/integration-karma/scripts/karma-configs/test/tags.js
+++ packages/@lwc/integration-karma/scripts/karma-configs/test/tags.js
@@ -11,12 +11,21 @@ const {
     COMPAT,
     SYNTHETIC_SHADOW_ENABLED,
     FORCE_NATIVE_SHADOW_MODE_FOR_TEST,
+    ENABLE_NATIVE_CUSTOM_ELEMENT_LIFECYCLE,
+    ENABLE_SCOPED_CUSTOM_ELEMENT_REGISTRY,
+    DISABLE_ARIA_REFLECTION_POLYFILL,
+    NODE_ENV_FOR_TEST,
 } = require('../../shared/options');
 
+// These are used to decide the directory that coverage is written to
 const TAGS = [
     `${SYNTHETIC_SHADOW_ENABLED ? 'synthetic' : 'native'}-shadow`,
     FORCE_NATIVE_SHADOW_MODE_FOR_TEST && 'force-native-shadow-mode',
     COMPAT && 'compat',
+    ENABLE_NATIVE_CUSTOM_ELEMENT_LIFECYCLE && 'native-lifecycle',
+    ENABLE_SCOPED_CUSTOM_ELEMENT_REGISTRY && 'scoped-registry',
+    DISABLE_ARIA_REFLECTION_POLYFILL && 'disable-aria-polyfill',
+    NODE_ENV_FOR_TEST,
 ].filter(Boolean);
 
 module.exports = TAGS;
