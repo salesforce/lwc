7268fabf2c26880d299de74b20e5531911a7d54b
Nolan Lawson <nlawson@salesforce.com>
2022-12-22 12:01:21 -0800

    fix(shared): simplify globalThis polyfill (#3257)

--- packages/@lwc/shared/src/global-this.ts
+++ packages/@lwc/shared/src/global-this.ts
@@ -5,42 +5,11 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
 
-// Inspired from: https://mathiasbynens.be/notes/globalthis
-const _globalThis = /*@__PURE__*/ (function (): any {
-    // On recent browsers, `globalThis` is already defined. In this case return it directly.
-    if (typeof globalThis === 'object') {
-        return globalThis;
-    }
+// See browser support for globalThis:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/globalThis#browser_compatibility
 
-    let _globalThis: any;
-    try {
-        // eslint-disable-next-line no-extend-native
-        Object.defineProperty(Object.prototype, '__magic__', {
-            get: function () {
-                return this;
-            },
-            configurable: true,
-        });
-
-        // __magic__ is undefined in Safari 10 and IE10 and older.
-        // @ts-ignore
-        // eslint-disable-next-line no-undef
-        _globalThis = __magic__;
-
-        // @ts-ignore
-        delete Object.prototype.__magic__;
-    } catch (ex) {
-        // In IE8, Object.defineProperty only works on DOM objects.
-    } finally {
-        // If the magic above fails for some reason we assume that we are in a legacy browser.
-        // Assume `window` exists in this case.
-        if (typeof _globalThis === 'undefined') {
-            // @ts-ignore
-            _globalThis = window;
-        }
-    }
-
-    return _globalThis;
-})();
+/* istanbul ignore next */
+// @ts-ignore
+const _globalThis = typeof globalThis === 'object' ? globalThis : window;
 
 export { _globalThis as globalThis };
