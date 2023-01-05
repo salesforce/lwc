bb0a3bf3d07333c64515789723088113d5df2074
James Tu <j.tu@salesforce.com>
2023-01-05 14:54:54 -0800

    test: update unit tests

--- packages/@lwc/template-compiler/src/__tests__/fixtures/attributes/valid-hyphen-underscore/expected.js
+++ packages/@lwc/template-compiler/src/__tests__/fixtures/attributes/valid-hyphen-underscore/expected.js
@@ -8,15 +8,22 @@ const stc0 = {
 };
 const stc1 = {
   props: {
-    _leading: "bar",
+    foo_Bar: "bar",
   },
   key: 1,
+};
+const stc2 = {
+  props: {
+    Foo_Bar: "bar",
+  },
+  key: 2,
 };
 function tmpl($api, $cmp, $slotset, $ctx) {
   const { c: api_custom_element } = $api;
   return [
     api_custom_element("x-button", _xButton, stc0),
     api_custom_element("x-button", _xButton, stc1),
+    api_custom_element("x-button", _xButton, stc2),
   ];
   /*LWC compiler vX.X.X*/
 }
