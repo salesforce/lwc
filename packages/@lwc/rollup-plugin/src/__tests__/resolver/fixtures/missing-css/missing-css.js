6a27ea9a1f0f293f599548d3c03f6e255f80db52
Rui Qiu <rui.qiu@salesforce.com>
2023-01-06 11:04:15 -0800

    fix: fix a typo in test

--- packages/@lwc/rollup-plugin/src/__tests__/resolver/fixtures/missing-css/missing-css.js
+++ packages/@lwc/rollup-plugin/src/__tests__/resolver/fixtures/missing-css/missing-css.js
@@ -1,6 +1,6 @@
 import { LightningElement } from 'lwc';
 import stylesheet from './stylesheet.css';
 
-export default class ImplicitCss extends LightningElement {
+export default class MissingCss extends LightningElement {
     static stylesheets = [stylesheet];
 }
