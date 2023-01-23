f0055c99fb5dde9590d72f44189ef35601b53cf4
Nolan Lawson <nlawson@salesforce.com>
2023-01-23 14:25:21 -0800

    fix(engine-dom): remove scoped custom element registry

--- packages/@lwc/integration-karma/test/custom-elements/x/observeFoo/observeFoo.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class extends LightningElement {
-    @api foo = 'foo';
-
-    attributeChangedCallback() {}
-}
--- packages/@lwc/integration-karma/test/upgradable-element/index.spec.js
+++ packages/@lwc/integration-karma/test/upgradable-element/index.spec.js
@@ -2,7 +2,6 @@ import { createElement } from 'lwc';
 
 import InteropParent from 'x/interopParent';
 import Interop from 'x/interop';
-import Interop2 from 'x/interop2';
 import Child from 'x/child';
 import DuplicateChild from 'x/dupChild';
 
@@ -54,57 +53,3 @@ it('should create elements with correct component behavior even when they share 
         'Duplicate Child Component'
     );
 });
-
-// In compat mode, the custom registry is bypassed and an equivalent implementation is used,
-// the native registry restrictions are not applicable
-const SUPPORTS_CUSTOM_ELEMENTS = !process.env.COMPAT && 'customElements' in window;
-if (SUPPORTS_CUSTOM_ELEMENTS && window.lwcRuntimeFlags.ENABLE_SCOPED_CUSTOM_ELEMENT_REGISTRY) {
-    describe('duplicate registration', () => {
-        function testDuplicateNativeRegistration(testCase, tag, wcClazz) {
-            it(`should not throw when registering a duplicate tag in custom element registry with ${testCase}`, () => {
-                // First time the x-interop tag will be registered
-                const interop = createElement(tag, { is: Interop });
-                document.body.appendChild(interop);
-                customElements.define(tag, wcClazz);
-                const native = document.createElement(tag);
-                document.body.appendChild(native);
-                expect(interop.tagName.toLowerCase()).toEqual(tag);
-                expect(native.tagName.toLowerCase()).toEqual(tag);
-            });
-        }
-        testDuplicateNativeRegistration(
-            'anonymous class',
-            'x-interop-wc',
-            class extends HTMLElement {}
-        );
-        testDuplicateNativeRegistration(
-            'CustomElementConstructor getter',
-            'x-interop-lwc-constructor',
-            Interop.CustomElementConstructor
-        );
-
-        function testDuplicateLwcRegistration(testCase, tag, WebComponentConstructor) {
-            it(`should register duplicate tag via lwc.createElement to override element registered with ${testCase}`, () => {
-                customElements.define(tag, WebComponentConstructor);
-                const customElement = document.createElement(tag);
-                document.body.appendChild(customElement);
-                const lwcElement = createElement(tag, { is: Interop });
-                document.body.appendChild(lwcElement);
-                // Both elements will be created with the same tag
-                expect(customElement.tagName.toLowerCase()).toEqual(tag);
-                expect(lwcElement.tagName.toLowerCase()).toEqual(tag);
-            });
-        }
-        testDuplicateLwcRegistration(
-            'anonymous class',
-            'x-interop-native-wc',
-            class extends HTMLElement {}
-        );
-        testDuplicateLwcRegistration(
-            'CustomElementConstructor getter',
-            'x-interop-lwc-wcclass',
-            // must use a different constructor or else this will fail because of the other test
-            Interop2.CustomElementConstructor
-        );
-    });
-}
