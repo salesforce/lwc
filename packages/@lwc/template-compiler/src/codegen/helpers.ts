efb9c78d70daf3e17c40a011b8eec58ac943c394
Nolan Lawson <nlawson@salesforce.com>
2023-01-23 16:30:24 -0800

    fix(engine): fix styling of static SVGs
    Fixes #3311

--- packages/@lwc/template-compiler/src/codegen/helpers.ts
+++ packages/@lwc/template-compiler/src/codegen/helpers.ts
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
+import { HTML_NAMESPACE } from '@lwc/shared';
 import * as t from '../shared/estree';
 import { toPropertyName } from '../shared/utils';
 import { BaseElement, ChildNode, LWCDirectiveRenderMode, Node, Root } from '../shared/types';
@@ -220,6 +221,10 @@ export function parseClassNames(classNames: string): string[] {
 function isStaticNode(node: BaseElement): boolean {
     let result = true;
     const { name: nodeName, namespace = '', attributes, directives, properties, listeners } = node;
+
+    if (namespace !== HTML_NAMESPACE) {
+        return false;
+    }
 
     result &&= isElement(node);
 
