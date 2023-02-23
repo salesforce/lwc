4d301124ce96fd0049630a5d08d236995a9876c2
Nolan Lawson <nlawson@salesforce.com>
2023-02-23 12:19:58 -0800

    fix: typo

--- packages/@lwc/integration-tests/scripts/wdio.local.conf.js
+++ packages/@lwc/integration-tests/scripts/wdio.local.conf.js
@@ -39,7 +39,7 @@ exports.config = merge(baseConfig.config, {
                     drivers,
                 },
             },
-            'devtools',
         ],
+        'devtools', // exposes browser.cdp, only works locally: https://webdriver.io/docs/devtools-service/
     ],
 });
