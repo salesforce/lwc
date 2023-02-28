185f71bf0150c5baeb4e705e566781a2c58154b5
James Tu <j.tu@salesforce.com>
2023-02-27 23:05:52 -0800

    refactor: update logic in rollup-plugin to account for invalid alias specifiers

---
+++ packages/@lwc/rollup-plugin/src/__tests__/resolver/fixtures/namespace/src/invalid.js
@@ -0,0 +1,7 @@
+import NoSlash from 'zoo';
+import TrailingSlash from 'baz/';
+import MultipleSlashes from 'foo/bar/baz';
+
+console.log(NoSlash);
+console.log(TrailingSlash);
+console.log(MultipleSlashes);
