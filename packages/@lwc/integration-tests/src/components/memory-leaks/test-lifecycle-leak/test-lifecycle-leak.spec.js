37cef678a8430fc1a3bb146f1dcad9cb810dc60d
Nolan Lawson <nlawson@salesforce.com>
2023-03-08 10:34:13 -0800

    chore: disable mem leak test for now (#3394)

--- packages/@lwc/integration-tests/src/components/memory-leaks/test-lifecycle-leak/test-lifecycle-leak.spec.js
+++ packages/@lwc/integration-tests/src/components/memory-leaks/test-lifecycle-leak/test-lifecycle-leak.spec.js
@@ -21,7 +21,8 @@ const assert = require('assert');
 // tunnelIdentifier exists, we know we're running in Sauce Labs and should bail out.
 // See: https://webdriver.io/docs/devtools-service/
 if (browser.capabilities.browserName === 'chrome' && !browser.capabilities.tunnelIdentifier) {
-    describe('Component does not leak', () => {
+    // TODO [#3393]: re-enable once Circle CI can handle CDP again
+    describe.skip('Component does not leak', () => {
         const URL = '/lifecycle-leak';
 
         // Count the number of objects using queryObjects(). Based on:
