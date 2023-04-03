192be5ee55d67dc52ae5f39c373f4e0b476efedd
Nolan Lawson <nlawson@salesforce.com>
2023-04-03 14:49:05 -0700

    fix(engine-server): do not escape `<style>` content
    Fixes #3439

--- packages/@lwc/engine-server/src/__tests__/fixtures.spec.ts
+++ packages/@lwc/engine-server/src/__tests__/fixtures.spec.ts
@@ -8,10 +8,9 @@
 import fs from 'fs';
 import path from 'path';
 
+import { format } from 'prettier';
 import { rollup, RollupWarning } from 'rollup';
-// @ts-ignore
 import lwcRollupPlugin from '@lwc/rollup-plugin';
-import { isVoidElement, HTML_NAMESPACE } from '@lwc/shared';
 import { testFixtureDir } from '@lwc/jest-utils-lwc-internals';
 import { setFeatureFlagForTest } from '../index';
 import type { FeatureFlagMap } from '@lwc/features';
@@ -59,69 +58,12 @@ async function compileFixture({ input, dirname }: { input: string; dirname: stri
     return outputFile;
 }
 
-/**
- * Naive HTML fragment formatter.
- *
- * This is a replacement for Prettier HTML formatting. Prettier formatting is too aggressive for
- * fixture testing. It not only indent the HTML code but also fixes HTML issues. For testing we want
- * to make sure that the fixture file is as close as possible to what the engine produces.
- *
- * @param src the original HTML fragment.
- * @returns the formatter HTML fragment.
- */
 function formatHTML(src: string): string {
-    let res = '';
-    let pos = 0;
-    let start = pos;
-
-    let depth = 0;
-
-    const getPadding = () => {
-        return '  '.repeat(depth);
-    };
-
-    while (pos < src.length) {
-        // Consume element tags and comments.
-        if (src.charAt(pos) === '<') {
-            const tagNameMatch = src.slice(pos).match(/(\w+)/);
-
-            const isVoid = isVoidElement(tagNameMatch![0], HTML_NAMESPACE);
-            const isClosing = src.charAt(pos + 1) === '/';
-            const isComment =
-                src.charAt(pos + 1) === '!' &&
-                src.charAt(pos + 2) === '-' &&
-                src.charAt(pos + 3) === '-';
-
-            start = pos;
-            while (src.charAt(pos++) !== '>') {
-                // Keep advancing until consuming the closing tag.
-            }
-
-            // Adjust current depth and print the element tag or comment.
-            if (isClosing) {
-                depth--;
-            }
-
-            res += getPadding() + src.slice(start, pos) + '\n';
-
-            const isSelfClosing = src.charAt(pos - 2) === '/';
-            if (!isClosing && !isSelfClosing && !isVoid && !isComment) {
-                depth++;
-            }
-        }
-
-        // Consume text content.
-        start = pos;
-        while (src.charAt(pos) !== '<' && pos < src.length) {
-            pos++;
-        }
-
-        if (start !== pos) {
-            res += getPadding() + src.slice(start, pos) + '\n';
-        }
-    }
-
-    return res.trim();
+    return format(src, {
+        parser: 'html',
+        htmlWhitespaceSensitivity: 'ignore',
+        bracketSameLine: true,
+    });
 }
 
 function testFixtures() {
