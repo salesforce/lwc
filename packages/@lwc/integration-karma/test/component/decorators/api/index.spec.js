bf44d9dc3a5c821bba842f43d6860bb7083fbe30
Nolan Lawson <nlawson@salesforce.com>
2023-04-04 09:43:58 -0700

    test: fix safari/firefox error messages

--- packages/@lwc/integration-karma/test/component/decorators/api/index.spec.js
+++ packages/@lwc/integration-karma/test/component/decorators/api/index.spec.js
@@ -142,7 +142,7 @@ describe('non-LightningElement `this` when calling accessor', () => {
     });
 
     const expectedErrorMessage =
-        /(undefined is not a VM|Cannot destructure property '(getHook|setHook)' of 'vm' as it is undefined|Cannot read properties of undefined)/;
+        /(undefined is not an object|Right side of assignment cannot be destructured|vm is undefined|undefined is not a VM|Cannot destructure property '(getHook|setHook)' of 'vm' as it is undefined|Cannot read properties of undefined)/;
 
     const scenarios = [
         { type: 'getterSetter', prop: 'getterSetterProp' },
