5897767045e9d9ae6e1ac8b23638b480ba417123
Eugene Kashida <ekashida@gmail.com>
2023-04-13 09:39:16 -0700

    chore: additional tests for removal and deduping

--- packages/@lwc/integration-karma/test/events/host-element-add-event-listener/index.spec.js
+++ packages/@lwc/integration-karma/test/events/host-element-add-event-listener/index.spec.js
@@ -1,16 +1,126 @@
 import { createElement } from 'lwc';
 import Test from 'x/test';
 
-it('should support handleEvent', () => {
-    const elm = createElement('x-test', { is: Test });
+describe('add handleEvent support', () => {
+    function test(elm) {
+        let invoked = false;
+        const listenerObject = {
+            handleEvent() {
+                invoked = true;
+            },
+        };
+        elm.addEventListener('foo', listenerObject);
+        elm.dispatchEvent(new CustomEvent('foo'));
+        expect(invoked).toBe(true);
+    }
 
-    let invoked = false;
-    elm.addEventListener('foo', {
-        handleEvent() {
-            invoked = true;
-        },
+    it('host element', () => {
+        test(createElement('x-test', { is: Test }));
     });
-    elm.dispatchEvent(new CustomEvent('foo'));
 
-    expect(invoked).toBe(true);
+    it('native element', () => {
+        test(document.createElement('div'));
+    });
+});
+
+describe('remove handleEvent support', () => {
+    describe('listener object identity', () => {
+        function test(elm) {
+            let invoked = false;
+            const listenerObject = {};
+
+            listenerObject.handleEvent = () => {
+                invoked = true;
+            };
+            elm.addEventListener('foo', listenerObject);
+
+            listenerObject.handleEvent = () => {
+                invoked = true;
+            };
+            elm.removeEventListener('foo', listenerObject);
+
+            elm.dispatchEvent(new CustomEvent('foo'));
+            expect(invoked).toBe(false);
+        }
+
+        it('host element', () => {
+            test(createElement('x-test', { is: Test }));
+        });
+
+        it('native element', () => {
+            test(document.createElement('div'));
+        });
+    });
+
+    describe('listener identity', () => {
+        function test(elm) {
+            let invoked = false;
+            const handleEvent = function () {
+                invoked = true;
+            };
+            elm.addEventListener('foo', { handleEvent });
+            elm.removeEventListener('foo', { handleEvent });
+            elm.dispatchEvent(new CustomEvent('foo'));
+            expect(invoked).toBe(true);
+        }
+
+        it('host element', () => {
+            test(createElement('x-test', { is: Test }));
+        });
+
+        it('native element', () => {
+            test(document.createElement('div'));
+        });
+    });
+});
+
+describe('dedupe behavior for add handleEvent', () => {
+    describe('listener object identity', () => {
+        function test(elm) {
+            let count = 0;
+            const listenerObject = {};
+
+            listenerObject.handleEvent = () => {
+                count += 1;
+            };
+            elm.addEventListener('foo', listenerObject);
+
+            listenerObject.handleEvent = () => {
+                count += 1;
+            };
+            elm.addEventListener('foo', listenerObject);
+
+            elm.dispatchEvent(new CustomEvent('foo'));
+            expect(count).toBe(1);
+        }
+
+        it('host element', () => {
+            test(createElement('x-test', { is: Test }));
+        });
+
+        it('native element', () => {
+            test(document.createElement('div'));
+        });
+    });
+
+    describe('listener identity', () => {
+        function test(elm) {
+            let count = 0;
+            const handleEvent = () => {
+                count += 1;
+            };
+            elm.addEventListener('foo', { handleEvent });
+            elm.addEventListener('foo', { handleEvent });
+            elm.dispatchEvent(new CustomEvent('foo'));
+            expect(count).toBe(2);
+        }
+
+        it('host element', () => {
+            test(createElement('x-test', { is: Test }));
+        });
+
+        it('native element', () => {
+            test(document.createElement('div'));
+        });
+    });
 });
