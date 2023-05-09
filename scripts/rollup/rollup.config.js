d3c2f9286fa0a049dfc07c3d09647b89a0fd606a
Nolan Lawson <nlawson@salesforce.com>
2023-05-09 13:49:21 -0700

    refactor: refactor rollup script

--- scripts/rollup/rollup.config.js
+++ scripts/rollup/rollup.config.js
@@ -23,8 +23,6 @@ const footer = `/** version: ${version} */`;
 const { ROLLUP_WATCH: watchMode } = process.env;
 const formats = ['es', 'cjs'];
 
-const RENDERER_REPLACEMENT_STRING = 'process.env.RENDERER';
-
 const onwarn = ({ code, message }) => {
     if (!process.env.ROLLUP_WATCH && code !== 'CIRCULAR_DEPENDENCY') {
         throw new Error(message);
@@ -59,17 +57,19 @@ function sharedPlugins() {
     ];
 }
 
+// Only used for @lwc/engine-dom
+// The renderer in the renderer factory needs to be inlined in the function, with all of its dependencies.
+// The reasons for this are due to how Locker does sandboxing.
+// So we run Rollup inside of a Rollup plugin to accomplish this. This resolves all dependencies and
+// builds them as an IIFE, which can then be injected into the source as process.env.RENDERER.
 function injectInlineRenderer() {
-    // Only used for @lwc/engine-dom
-    // The renderer in the renderer factory needs to be inlined in the function, with all of its dependencies.
-    // The reasons for this are due to how Locker does sandboxing.
-    // So we run Rollup inside of a Rollup plugin to accomplish this. This resolves all dependencies and
-    // builds them as an IIFE, which can then be injected into the source as process.env.RENDERER.
+    const rendererReplacementString = 'process.env.RENDERER';
+
     return {
         name: 'inject-inline-renderer',
 
         async transform(source) {
-            if (source.includes(RENDERER_REPLACEMENT_STRING)) {
+            if (source.includes(rendererReplacementString)) {
                 const bundle = await rollup({
                     input: path.resolve(
                         __dirname,
@@ -104,7 +104,7 @@ function injectInlineRenderer() {
 
                 // The IIFE will contain the string `var renderer = `, which we don't actually need. We just need the
                 // part after, which is the actual IIFE (e.g. `(function () { /* code */ })()`)
-                magic.replace(RENDERER_REPLACEMENT_STRING, code.replace('var renderer = ', ''));
+                magic.replace(rendererReplacementString, code.replace('var renderer = ', ''));
 
                 return {
                     code: magic.toString(),
