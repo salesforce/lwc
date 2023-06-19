fb8063ffdc229e2a9a1dac98713d11a1a3f85f58
Nolan Lawson <nlawson@salesforce.com>
2023-06-19 14:47:12 -0700

    fix: remove more compat

--- packages/@lwc/rollup-plugin/src/__tests__/integrations/integrations.spec.ts
+++ packages/@lwc/rollup-plugin/src/__tests__/integrations/integrations.spec.ts
@@ -9,8 +9,6 @@ import { rollup } from 'rollup';
 
 import lwc from '../../index';
 
-const compat = require('rollup-plugin-compat');
-
 describe('integration', () => {
     describe('typescript', () => {
         it(`resolves and transform .ts files`, async () => {
@@ -26,25 +24,6 @@ describe('integration', () => {
             const { code } = result.output[0];
             expect(code).toContain('class TypeScript');
             expect(code).toContain('registerComponent');
-        });
-    });
-
-    describe('compat', () => {
-        it('should be compatible with compat plugin', async () => {
-            const bundle = await rollup({
-                input: path.resolve(__dirname, 'fixtures/javascript/javascript.js'),
-                plugins: [lwc(), compat()],
-            });
-
-            const result = await bundle.generate({
-                format: 'esm',
-            });
-
-            const { code } = result.output[0];
-            expect(code).not.toContain('class JavaScript');
-            expect(code).toContain('var JavaScript');
-            expect(code).toContain('registerComponent');
-            expect(code).toContain('setKey');
         });
     });
 });
