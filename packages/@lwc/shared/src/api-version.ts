ba5d96a4daaccde7305574d4deabd770597d1c13
Nolan Lawson <nlawson@salesforce.com>
2023-06-26 07:28:41 -0700

    fix: update packages/@lwc/shared/src/api-version.ts
    Co-authored-by: Ravi Jayaramappa <ravi.jayaramappa@salesforce.com>

--- packages/@lwc/shared/src/api-version.ts
+++ packages/@lwc/shared/src/api-version.ts
@@ -56,7 +56,7 @@ export const enum APIFeature {
     TREAT_ALL_PARSE5_ERRORS_AS_ERRORS,
 }
 
-export function isAPIFeatureEnabled(apiVersionFeature: APIFeature, apiVersion: APIVersion) {
+export function isAPIFeatureEnabled(apiVersionFeature: APIFeature, apiVersion: APIVersion): boolean {
     switch (apiVersionFeature) {
         case APIFeature.LOWERCASE_SCOPE_TOKENS:
         case APIFeature.TREAT_ALL_PARSE5_ERRORS_AS_ERRORS:
