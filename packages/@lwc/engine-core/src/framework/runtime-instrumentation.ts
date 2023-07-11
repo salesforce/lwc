003d37e97ea84019b7f9d54a0c7d5255510d1d10
Dale Bustad <dbustad@salesforce.com>
2023-07-10 22:41:30 -0700

    chore: add injection points for devtools instrumentation (#3613)
    * chore: add injection points for devtools instrumentation
    
    * chore: use `noop` and `globalThis` from @lwc/shared
    
    * chore: add copyright header

---
+++ packages/@lwc/engine-core/src/framework/runtime-instrumentation.ts
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+import { noop } from '@lwc/shared';
+import { globalThis } from '@lwc/shared';
+
+export const instrumentDef = globalThis.__lwc_instrument_cmp_def ?? noop;
+export const instrumentInstance = globalThis.__lwc_instrument_cmp_instance ?? noop;
