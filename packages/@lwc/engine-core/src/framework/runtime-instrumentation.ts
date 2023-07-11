0eeef8820dcfb541051f34ee5fe1788259222fe5
Dale Bustad <dale@divmain.com>
2023-07-10 22:08:15 -0700

    chore: use `noop` and `globalThis` from @lwc/shared

--- packages/@lwc/engine-core/src/framework/runtime-instrumentation.ts
+++ packages/@lwc/engine-core/src/framework/runtime-instrumentation.ts
@@ -1,11 +1,5 @@
-type InstrumentedWindow = (Window & typeof globalThis) & {
-    __lwc_instrument_cmp_def?: (def: any) => void;
-    __lwc_instrument_cmp_instance?: (instance: any, vm: any) => void;
-};
+import { noop } from '@lwc/shared';
+import { globalThis } from '@lwc/shared';
 
-const win = typeof window !== 'undefined' ? (window as InstrumentedWindow) : null;
-
-function noop() {}
-
-export const instrumentDef = win?.__lwc_instrument_cmp_def ?? noop;
-export const instrumentInstance = win?.__lwc_instrument_cmp_instance ?? noop;
+export const instrumentDef = globalThis.__lwc_instrument_cmp_def ?? noop;
+export const instrumentInstance = globalThis.__lwc_instrument_cmp_instance ?? noop;
