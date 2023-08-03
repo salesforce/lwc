fe6f93b17f5f2683b46e2202fb7a4f4659a8b164
Nolan Lawson <nlawson@salesforce.com>
2023-08-03 12:17:30 -0700

    fix: fix ClipboardEvent in jest/jsdom

--- packages/@lwc/synthetic-shadow/src/polyfills/clipboard-event-composed/main.ts
+++ packages/@lwc/synthetic-shadow/src/polyfills/clipboard-event-composed/main.ts
@@ -6,20 +6,24 @@
  */
 import { assign, create, defineProperties } from '@lwc/shared';
 
-const isComposedType = assign(create(null), {
-    copy: 1,
-    cut: 1,
-    paste: 1,
-});
+// Note that ClipboardEvent is undefined in Jest/jsdom
+// See: https://github.com/jsdom/jsdom/issues/1568
+if (typeof ClipboardEvent !== 'undefined') {
+    const isComposedType = assign(create(null), {
+        copy: 1,
+        cut: 1,
+        paste: 1,
+    });
 
-// Patch the prototype to override the composed property on user-agent dispatched events
-defineProperties(ClipboardEvent.prototype, {
-    composed: {
-        get() {
-            const { type } = this;
-            return isComposedType[type] === 1;
+    // Patch the prototype to override the composed property on user-agent dispatched events
+    defineProperties(ClipboardEvent.prototype, {
+        composed: {
+            get() {
+                const { type } = this;
+                return isComposedType[type] === 1;
+            },
+            configurable: true,
+            enumerable: true,
         },
-        configurable: true,
-        enumerable: true,
-    },
-});
+    });
+}
