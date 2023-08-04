766bfab8e0f980f32891c69d136fca5d249d27fc
Nolan Lawson <nlawson@salesforce.com>
2023-08-04 10:46:44 -0700

    refactor: remove dead code

--- packages/@lwc/synthetic-shadow/src/env/shadow-root.ts
+++ packages/@lwc/synthetic-shadow/src/env/shadow-root.ts
@@ -4,10 +4,8 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-import { isNull } from '@lwc/shared';
 
+// Capture the global `ShadowRoot` since synthetic shadow will override it later
 const NativeShadowRoot = ShadowRoot;
 
-export const isInstanceOfNativeShadowRoot: (node: any) => boolean = isNull(NativeShadowRoot)
-    ? () => false
-    : (node) => node instanceof NativeShadowRoot;
+export const isInstanceOfNativeShadowRoot = (node: any) => node instanceof NativeShadowRoot;
