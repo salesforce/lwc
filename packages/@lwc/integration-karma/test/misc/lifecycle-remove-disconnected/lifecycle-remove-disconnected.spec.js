d58a67bd79e5b7cf3c7300edcdd00f62dfc0affc
Nolan Lawson <nlawson@salesforce.com>
2023-08-29 12:47:13 -0700

    test: reduce test

--- packages/@lwc/integration-karma/test/misc/lifecycle-remove-disconnected/lifecycle-remove-disconnected.spec.js
+++ packages/@lwc/integration-karma/test/misc/lifecycle-remove-disconnected/lifecycle-remove-disconnected.spec.js
@@ -1,22 +1,14 @@
 import { createElement } from 'lwc';
-import Grandparent from 'x/grandparent';
+import Parent from 'x/parent';
 
 describe('vdom removes component while it is already disconnected', () => {
     it('repro "must have been connected" error W-14037619', async () => {
-        const elm = createElement('x-grandparent', { is: Grandparent });
+        const elm = createElement('x-parent', { is: Parent });
         document.body.appendChild(elm);
-
-        await Promise.resolve();
         elm.showChild = true;
-        await Promise.resolve();
-        elm.showChild = false;
-
         document.body.removeChild(elm);
 
         await Promise.resolve();
-        elm.showChild = true;
-        await Promise.resolve();
-        elm.showChild = false;
-        await Promise.resolve();
+        elm.showChild = false; // trigger removal while parent is disconnected
     });
 });
