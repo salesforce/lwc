4c80dd559e0dc55218c24ea323e6f212945c6668
James Tu <j.tu@salesforce.com>
2023-09-14 18:03:36 -0700

    refactor: move element internals check to engine-dom

--- packages/@lwc/shared/src/index.ts
+++ packages/@lwc/shared/src/index.ts
@@ -14,7 +14,6 @@ export * from './keys';
 export * from './void-elements';
 export * from './html-attributes';
 export * from './html-escape';
-export * from './html-element';
 export * from './namespaces';
 export * from './meta';
 
