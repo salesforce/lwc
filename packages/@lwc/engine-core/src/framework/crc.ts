519b58668e17ba6a82657420ee8e24ee23ce40df
Dale Bustad <dale@divmain.com>
2023-09-15 21:18:28 -0700

    feat: update crc validation logic

--- packages/@lwc/engine-core/src/framework/crc.ts
+++ packages/@lwc/engine-core/src/framework/crc.ts
@@ -1,33 +1,45 @@
 import crc32 from 'crc/calculators/crc32';
-import {
-    VNode,
-    VNodes,
-    VNodeType,
-    VText,
-    // VComment,
-    VElement,
-    VCustomElement,
-    // VStatic,
-    // VFragment,
-    // VScopedSlotFragment,
-} from './vnodes';
+import { VElement, VNode, VNodes, VNodeType, VText } from './vnodes';
 import { VM } from './vm';
 
-type Check = (str?: string) => any;
+const EMPTY = new Uint8Array([0]);
+enum PositionCheck {
+    Text = 1,
+    Comment = 2,
+    Static = 3,
+    Fragment = 4,
+    ScopedSlotFragment = 5,
+    CustomElement = 6,
+}
+const PositionCheckEncodings = new Map(
+    Object.keys(PositionCheck)
+        .filter((key) => /\d+/.test(key))
+        .map((key) => {
+            const keyInt = Number.parseInt(key);
+            return [keyInt, new Uint8Array(keyInt)];
+        })
+);
+
+type Check = ((str?: string) => void) & {
+    position: (kind: PositionCheck) => void;
+};
 
 const te = new TextEncoder();
 const encode = te.encode.bind(te);
 
-function visitElement(vel: VElement | VCustomElement, check: Check): void {
+function visitElement(vel: VElement, check: Check): void {
     check(vel.sel);
+
     if (vel.data.attrs) {
         Object.entries(vel.data.attrs).forEach(([key, val]) => {
             check(key);
             check(val?.toString());
         });
     }
+
     check(vel.data.className);
     check(vel.data.style);
+
     for (const child of vel.children) {
         visitVNode(child, check);
     }
@@ -38,17 +50,32 @@ function visitVNode(vnode: VNode | null, check: Check): void {
         return;
     }
     switch (vnode.type) {
+        // The contents of these nodes are unimportant. However, we do care
+        // about the kind, number, and position of these nodes, so we account
+        // for that.
         case VNodeType.Text:
+            // Text content is patched during hydration, and does not cause
+            // hydration validation failures.
+            return check.position(PositionCheck.Text);
         case VNodeType.Comment:
+            return check.position(PositionCheck.Comment);
         case VNodeType.Static:
+            return check.position(PositionCheck.Static);
         case VNodeType.Fragment:
+            return check.position(PositionCheck.Fragment);
         case VNodeType.ScopedSlotFragment:
-            // The contents of these nodes are unimportant. However, we do care
-            // about the number of nodes & their position, so we account for that.
-            return check();
-
-        case VNodeType.Element:
+            return check.position(PositionCheck.ScopedSlotFragment);
         case VNodeType.CustomElement:
+            // We rely on hydration of each custom element to validate itself.
+            // If a parent component passes incorrect props to a custom child
+            // element, its own validation (or maybe its child) will fail.
+            // Because of that, we don't need to police correctness of
+            // non-primitive values passed as props here.
+            return check.position(PositionCheck.CustomElement);
+
+        // Attributes of <div>s and <span>s matter, and differences between
+        // CSR and SSR attribute values should cause validation to fail.
+        case VNodeType.Element:
             return visitElement(vnode, check);
     }
 }
@@ -63,10 +90,10 @@ function flatten(toFlatten: Uint8Array[], length: number): Uint8Array {
     return flattened;
 }
 
-const EMPTY = new Uint8Array([0]);
 export function generateCRC(vnodes: VNodes): string {
     const toVerify: Uint8Array[] = [];
     let length = 0;
+
     const check = (str?: string) => {
         if (!str) {
             toVerify.push(EMPTY);
@@ -77,9 +104,14 @@ export function generateCRC(vnodes: VNodes): string {
         length += uint8arr.length;
         toVerify.push(encode(str));
     };
+    check.position = (num: PositionCheck) => {
+        toVerify.push(PositionCheckEncodings.get(num)!);
+    };
+
     for (const vnode of vnodes) {
         visitVNode(vnode, check);
     }
+
     return crc32(flatten(toVerify, length)).toString(16);
 }
 
@@ -108,5 +140,6 @@ export function attachCRC(orig: VNodes, vm: VM): VNodes {
         owner: vm,
         key: 'crc',
     };
+
     return [...orig, scriptTag];
 }
