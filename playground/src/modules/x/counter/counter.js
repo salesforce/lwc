f3bffeb92c36b9a86493c12e75dbe5360f4642d0
Allan Oricil <allanoricilcos@outlook.com>
2023-09-18 23:16:34 -0300

    refactor: remove changes from the component used in the playground

--- playground/src/modules/x/counter/counter.js
+++ playground/src/modules/x/counter/counter.js
@@ -1,34 +1,7 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement } from 'lwc';
 
 export default class extends LightningElement {
     counter = 0;
-
-    _foo = 'foo';
-
-    get foo() {
-        return this._foo;
-    }
-
-    set foo(newValue) {
-        this._foo = newValue;
-    }
-
-    _bar = 'bar';
-
-    @api
-    get bar() {
-        return this._bar;
-    }
-
-    set bar(newValue) {
-        this._bar = newValue;
-    }
-
-    _doe = 'doe';
-
-    set doe(newValue) {
-        this._doe = newValue;
-    }
 
     increment() {
         this.counter++;
