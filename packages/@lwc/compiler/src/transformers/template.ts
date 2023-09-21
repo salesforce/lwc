7c769002eab49fc353c48c1dedc2401cbc2c3aa8
Nolan Lawson <nlawson@salesforce.com>
2023-09-21 12:25:51 -0700

    fix: use the api version as the salt

--- packages/@lwc/compiler/src/transformers/template.ts
+++ packages/@lwc/compiler/src/transformers/template.ts
@@ -127,9 +127,8 @@ function generateScopeToken(
     const uniqueToken = `${namespace}-${name}_${path.basename(filename, path.extname(filename))}`;
 
     if (isAPIFeatureEnabled(APIFeature.LOWERCASE_SCOPE_TOKENS, apiVersion)) {
-        // "Salt" the scope token using the LWC version, so that it changes with every release.
-        // This is to further discourage developers from relying on it.
-        const hashCode = generateHashCode(uniqueToken + (process.env.LWC_VERSION as string));
+        // "Salt" the scope token using the API version. This is to further discourage developers from relying on it.
+        const hashCode = generateHashCode(uniqueToken + apiVersion);
 
         // This scope token is all lowercase so that it works correctly in case-sensitive namespaces (e.g. SVG).
         // It is deliberately designed to discourage people from relying on it by appearing somewhat random.
