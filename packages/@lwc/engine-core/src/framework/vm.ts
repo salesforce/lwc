23d2d16a4904c9cdd8f63438a7d62a30b8986649
Gonzalo Cordero <gcordero@salesforce.com>
2023-10-03 13:46:21 -0700

    fix: signing commit

--- packages/@lwc/engine-core/src/framework/vm.ts
+++ packages/@lwc/engine-core/src/framework/vm.ts
@@ -470,17 +470,16 @@ function computeShadowMode(def: ComponentDef, owner: VM | null, renderer: Render
 
     let shadowMode;
     if (isSyntheticShadowDefined) {
-        if (
-            def.renderMode === RenderMode.Light
-        ) {
+        if (def.renderMode === RenderMode.Light) {
             // ShadowMode.Native implies "not synthetic shadow" which is consistent with how
             // everything defaults to native when the synthetic shadow polyfill is unavailable.
             shadowMode = ShadowMode.Native;
         } else if (
-                lwcRuntimeFlags.ENABLE_MIXED_SHADOW_MODE ||
-                shadowSupportMode === ShadowSupportMode.Native
-            ) {
-            if (def.shadowSupportMode === ShadowSupportMode.Any ||
+            lwcRuntimeFlags.ENABLE_MIXED_SHADOW_MODE ||
+            def.shadowSupportMode === ShadowSupportMode.Native
+        ) {
+            if (
+                def.shadowSupportMode === ShadowSupportMode.Any ||
                 def.shadowSupportMode === ShadowSupportMode.Native
             ) {
                 shadowMode = ShadowMode.Native;
