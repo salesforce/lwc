af7a0db747e16d27770184c03ce977dbc9574244
Nolan Lawson <nlawson@salesforce.com>
2023-10-16 15:41:55 -0700

    test: repro of metadata AST issue

---
+++ packages/@lwc/template-compiler/src/__tests__/fixtures/events/shallow/expected.js
@@ -0,0 +1,19 @@
+import { registerTemplate } from "lwc";
+function tmpl($api, $cmp, $slotset, $ctx) {
+  const { b: api_bind, h: api_element } = $api;
+  const { _m0 } = $ctx;
+  return [
+    api_element("button", {
+      attrs: {
+        "data-dynamic": $cmp.dynamic,
+      },
+      key: 0,
+      on: {
+        click: _m0 || ($ctx._m0 = api_bind($cmp.foo)),
+      },
+    }),
+  ];
+  /*LWC compiler vX.X.X*/
+}
+export default registerTemplate(tmpl);
+tmpl.stylesheets = [];
