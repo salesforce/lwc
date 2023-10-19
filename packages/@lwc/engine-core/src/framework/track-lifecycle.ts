019f554dd79a33b74802d00ae5ff2313c0a481d4
Nolan Lawson <nlawson@salesforce.com>
2023-10-19 10:04:32 -0700

    fix: header

--- packages/@lwc/engine-core/src/framework/track-lifecycle.ts
+++ packages/@lwc/engine-core/src/framework/track-lifecycle.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2023, Salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
 import { isUndefined, StringToLowerCase } from '@lwc/shared';
 import { logWarnOnce } from '../shared/logger';
 import { onReportingEnabled, report, ReportingEventId } from './reporting';
