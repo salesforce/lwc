231efb9b48adb443ec5b89b6d890c65250f55034
Nolan Lawson <nlawson@salesforce.com>
2023-10-19 10:25:53 -0700

    fix: refactor and fix diffing algo issues

--- packages/@lwc/engine-core/src/framework/track-lifecycle.ts
+++ packages/@lwc/engine-core/src/framework/track-lifecycle.ts
@@ -43,7 +43,41 @@ function getFriendlyErrorMessage(
     }
 }
 
-function reportLifecycleViolation(
+function getEventId(lifecycleType: LifecycleType, nativeCount: number, syntheticCount: number) {
+    if (lifecycleType === 'connected') {
+        if (nativeCount > syntheticCount) {
+            return ReportingEventId.NativeConnectedWithoutSynthetic;
+        } else {
+            return ReportingEventId.SyntheticConnectedWithoutNative;
+        }
+    } else {
+        // disconnected
+        if (nativeCount > syntheticCount) {
+            return ReportingEventId.NativeDisconnectedWithoutSynthetic;
+        } else {
+            return ReportingEventId.SyntheticDisconnectedWithoutNative;
+        }
+    }
+}
+
+function getCounts(lifecycleType: LifecycleType, native: boolean) {
+    if (lifecycleType === 'connected') {
+        if (native) {
+            return nativeConnectedCounts;
+        } else {
+            return syntheticConnectedCounts;
+        }
+    } else {
+        // disconnected
+        if (native) {
+            return nativeDisconnectedCounts;
+        } else {
+            return syntheticDisconnectedCounts;
+        }
+    }
+}
+
+function reportViolation(
     lifecycleType: LifecycleType,
     elm: any,
     nativeCount: number,
@@ -56,14 +90,7 @@ function reportLifecycleViolation(
         // vm should never be undefined here, but just to be safe, bail out and don't report
         return;
     }
-    const eventId =
-        lifecycleType === 'connected'
-            ? nativeCount > syntheticCount
-                ? ReportingEventId.NativeConnectedWithoutSynthetic
-                : ReportingEventId.SyntheticConnectedWithoutNative
-            : nativeCount > syntheticCount
-            ? ReportingEventId.NativeDisconnectedWithoutSynthetic
-            : ReportingEventId.SyntheticDisconnectedWithoutNative;
+    const eventId = getEventId(lifecycleType, nativeCount, syntheticCount);
     const tagName = StringToLowerCase.call(elm.tagName);
     report(eventId, {
         tagName: tagName,
@@ -80,10 +107,50 @@ function reportLifecycleViolation(
     }
 }
 
-/* For the purposes of reporting and logging, track every connected/disconnected lifecycle
+function checkForViolations(elm: any) {
+    const nativeConnected = nativeConnectedCounts.get(elm) ?? 0;
+    const nativeDisconnected = nativeDisconnectedCounts.get(elm) ?? 0;
+    const syntheticConnected = syntheticConnectedCounts.get(elm) ?? 0;
+    const syntheticDisconnected = syntheticDisconnectedCounts.get(elm) ?? 0;
+
+    try {
+        if (
+            nativeConnected === 1 &&
+            nativeDisconnected === 1 &&
+            syntheticConnected === 0 &&
+            syntheticDisconnected === 0
+        ) {
+            // Heuristic: ignore rapid disconnect/connect events that occur in the same tick of the event loop
+            // This is likely to be the LWC diffing algo, which does not report disconnect+reconnect
+            // when reordering elements in a list.
+            ignoredElements.add(elm);
+            return;
+        }
+
+        if (nativeConnected !== syntheticConnected) {
+            reportViolation('connected', elm, nativeConnected, syntheticConnected);
+        }
+
+        if (nativeDisconnected !== syntheticDisconnected) {
+            reportViolation('disconnected', elm, nativeDisconnected, syntheticDisconnected);
+        }
+    } finally {
+        // Clean up so that subsequent calls for this same element will ignore existing violations
+        nativeConnectedCounts.delete(elm);
+        nativeDisconnectedCounts.delete(elm);
+        syntheticConnectedCounts.delete(elm);
+        syntheticDisconnectedCounts.delete(elm);
+    }
+}
+
+/**
+ * For the purposes of reporting and logging, track every connected/disconnected lifecycle
  * callback and whether it is native or synthetic.
  *
  * This is designed to only run in the browser, and only when native lifecycle is disabled.
+ * @param elm - element to report for
+ * @param lifecycleType - connected or disconnected
+ * @param native - true if this is a native lifecycle event
  */
 export async function reportLifecycleCallback(
     elm: any,
@@ -98,46 +165,11 @@ export async function reportLifecycleCallback(
         return;
     }
 
-    const counts =
-        lifecycleType === 'connected'
-            ? native
-                ? nativeConnectedCounts
-                : syntheticConnectedCounts
-            : native
-            ? nativeDisconnectedCounts
-            : syntheticDisconnectedCounts;
+    const counts = getCounts(lifecycleType, native);
     counts.set(elm, (counts.get(elm) ?? 0) + 1);
 
     qM(() => {
-        const nativeConnected = nativeConnectedCounts.get(elm) ?? 0;
-        const nativeDisconnected = nativeDisconnectedCounts.get(elm) ?? 0;
-        const syntheticConnected = syntheticConnectedCounts.get(elm) ?? 0;
-        const syntheticDisconnected = syntheticDisconnectedCounts.get(elm) ?? 0;
-
-        if (nativeConnected !== syntheticConnected) {
-            reportLifecycleViolation('connected', elm, nativeConnected, syntheticConnected);
-        }
-
-        if (nativeDisconnected !== syntheticDisconnected) {
-            reportLifecycleViolation(
-                'disconnected',
-                elm,
-                nativeDisconnected,
-                syntheticDisconnected
-            );
-        }
-
-        nativeConnectedCounts.delete(elm);
-        nativeDisconnectedCounts.delete(elm);
-        syntheticConnectedCounts.delete(elm);
-        syntheticDisconnectedCounts.delete(elm);
-
-        if (lifecycleType === 'disconnected') {
-            // Ignore any further connected/disconnected events for this element.
-            // This is a heuristic for LWC's diffing algo, which generates extra connectedCallbacks in native mode,
-            // but the component author can't do anything about that, so it's useless to report/warn.
-            ignoredElements.add(elm);
-        }
+        checkForViolations(elm);
     });
 }
 
--- packages/@lwc/integration-karma/test/rendering/table-diffing/index.spec.js
+++ packages/@lwc/integration-karma/test/rendering/table-diffing/index.spec.js
@@ -28,106 +28,85 @@ describe('Table diffing', () => {
         });
     });
 
-    describe('re-ordering', () => {
-        let spy;
+    it('should reuse the elements when reversing the data', () => {
+        const data = [{ id: 0 }, { id: 1 }, { id: 2 }];
 
-        beforeEach(() => {
-            spy = spyOn(console, 'warn');
+        const elm = createElement('x-table', { is: Table });
+        elm.rows = [...data];
+        document.body.appendChild(elm);
+
+        const [e1, e2, e3] = elm.shadowRoot.querySelectorAll('x-row');
+        elm.rows = [...data].reverse();
+
+        return Promise.resolve().then(() => {
+            expect(elm.shadowRoot.querySelectorAll('x-row').length).toBe(3);
+            const [r1, r2, r3] = elm.shadowRoot.querySelectorAll('x-row');
+            expect(r1).toBe(e3);
+            expect(r2).toBe(e2);
+            expect(r3).toBe(e1);
         });
+    });
 
-        afterEach(async () => {
-            await Promise.resolve();
+    it('should reuse the elements when removing and adding data at the beginning and the end', () => {
+        const elm = createElement('x-table', { is: Table });
+        elm.rows = [{ id: 0 }, { id: 1 }, { id: 2 }];
+        document.body.appendChild(elm);
 
-            // apparently lists of components have this behavior during re-rendering
-            expect(spy).toHaveBeenCalledTimes(2);
-            expect(spy.calls.allArgs()[0][0]).toMatch(
-                /should have fired a connectedCallback, but did not/
+        const [, e2, e3] = elm.shadowRoot.querySelectorAll('x-row');
+        elm.rows = [{ id: 2 }, { id: 1 }, { id: 3 }];
+
+        return Promise.resolve().then(() => {
+            expect(elm.shadowRoot.querySelectorAll('x-row').length).toBe(3);
+            const [r1, r2] = elm.shadowRoot.querySelectorAll('x-row');
+            expect(r1).toBe(e3);
+            expect(r2).toBe(e2);
+        });
+    });
+
+    it('should reuse the elements when moving the end to the start and adding new to the end', () => {
+        const elm = createElement('x-table', { is: Table });
+        elm.rows = [{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }];
+        document.body.appendChild(elm);
+
+        const getRowContents = () => {
+            return [...elm.shadowRoot.querySelectorAll('x-row')].map((row) =>
+                parseInt(row.shadowRoot.querySelector('span').textContent, 10)
             );
-            expect(spy.calls.allArgs()[1][0]).toMatch(
-                /should have fired a disconnectedCallback, but did not/
+        };
+
+        const [, e2, e3, e4] = elm.shadowRoot.querySelectorAll('x-row');
+        expect(getRowContents()).toEqual([0, 1, 2, 3]);
+        elm.rows = [{ id: 3 }, { id: 1 }, { id: 2 }, { id: 4 }];
+
+        return Promise.resolve().then(() => {
+            expect(getRowContents()).toEqual([3, 1, 2, 4]);
+            expect(elm.shadowRoot.querySelectorAll('x-row').length).toBe(4);
+            const [r1, r2, r3] = elm.shadowRoot.querySelectorAll('x-row');
+            expect(r2).toBe(e2);
+            expect(r3).toBe(e3);
+            expect(r1).toBe(e4);
+        });
+    });
+
+    it('sorting a table', () => {
+        const elm = createElement('x-table', { is: Table });
+        const ids = [0, 9, 1, 5, 2, 3, 8, 4, 6, 7];
+        elm.rows = ids.map((id) => ({ id }));
+        document.body.appendChild(elm);
+
+        const getRowContents = () => {
+            return [...elm.shadowRoot.querySelectorAll('x-row')].map((row) =>
+                parseInt(row.shadowRoot.querySelector('span').textContent, 10)
             );
-        });
+        };
+        expect(getRowContents()).toEqual(ids);
 
-        it('should reuse the elements when reversing the data', () => {
-            const data = [{ id: 0 }, { id: 1 }, { id: 2 }];
+        const sortedIds = ids.sort((a, b) => a - b);
 
-            const elm = createElement('x-table', { is: Table });
-            elm.rows = [...data];
-            document.body.appendChild(elm);
+        elm.rows = sortedIds.map((id) => ({ id }));
 
-            const [e1, e2, e3] = elm.shadowRoot.querySelectorAll('x-row');
-            elm.rows = [...data].reverse();
-
-            return Promise.resolve().then(() => {
-                expect(elm.shadowRoot.querySelectorAll('x-row').length).toBe(3);
-                const [r1, r2, r3] = elm.shadowRoot.querySelectorAll('x-row');
-                expect(r1).toBe(e3);
-                expect(r2).toBe(e2);
-                expect(r3).toBe(e1);
-            });
-        });
-
-        it('should reuse the elements when removing and adding data at the beginning and the end', () => {
-            const elm = createElement('x-table', { is: Table });
-            elm.rows = [{ id: 0 }, { id: 1 }, { id: 2 }];
-            document.body.appendChild(elm);
-
-            const [, e2, e3] = elm.shadowRoot.querySelectorAll('x-row');
-            elm.rows = [{ id: 2 }, { id: 1 }, { id: 3 }];
-
-            return Promise.resolve().then(() => {
-                expect(elm.shadowRoot.querySelectorAll('x-row').length).toBe(3);
-                const [r1, r2] = elm.shadowRoot.querySelectorAll('x-row');
-                expect(r1).toBe(e3);
-                expect(r2).toBe(e2);
-            });
-        });
-
-        it('should reuse the elements when moving the end to the start and adding new to the end', () => {
-            const elm = createElement('x-table', { is: Table });
-            elm.rows = [{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }];
-            document.body.appendChild(elm);
-
-            const getRowContents = () => {
-                return [...elm.shadowRoot.querySelectorAll('x-row')].map((row) =>
-                    parseInt(row.shadowRoot.querySelector('span').textContent, 10)
-                );
-            };
-
-            const [, e2, e3, e4] = elm.shadowRoot.querySelectorAll('x-row');
-            expect(getRowContents()).toEqual([0, 1, 2, 3]);
-            elm.rows = [{ id: 3 }, { id: 1 }, { id: 2 }, { id: 4 }];
-
-            return Promise.resolve().then(() => {
-                expect(getRowContents()).toEqual([3, 1, 2, 4]);
-                expect(elm.shadowRoot.querySelectorAll('x-row').length).toBe(4);
-                const [r1, r2, r3] = elm.shadowRoot.querySelectorAll('x-row');
-                expect(r2).toBe(e2);
-                expect(r3).toBe(e3);
-                expect(r1).toBe(e4);
-            });
-        });
-
-        it('sorting a table', () => {
-            const elm = createElement('x-table', { is: Table });
-            const ids = [0, 9, 1, 5, 2, 3, 8, 4, 6, 7];
-            elm.rows = ids.map((id) => ({ id }));
-            document.body.appendChild(elm);
-
-            const getRowContents = () => {
-                return [...elm.shadowRoot.querySelectorAll('x-row')].map((row) =>
-                    parseInt(row.shadowRoot.querySelector('span').textContent, 10)
-                );
-            };
-            expect(getRowContents()).toEqual(ids);
-
-            const sortedIds = ids.sort((a, b) => a - b);
-
-            elm.rows = sortedIds.map((id) => ({ id }));
-
-            return Promise.resolve().then(() => {
-                expect(getRowContents()).toEqual(sortedIds);
-            });
+        return Promise.resolve().then(() => {
+            expect(getRowContents()).toEqual(sortedIds);
         });
     });
 });
