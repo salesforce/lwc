f71d965383d5dc349ffe00ccce23fe366d2c0d56
James Tu <j.tu@salesforce.com>
2023-10-26 15:07:04 -0700

    chore: revert playground changes

--- playground/src/main.js
+++ playground/src/main.js
@@ -1,8 +1,5 @@
-// import '@lwc/synthetic-shadow';
-import { createElement } from 'lwc';
-// import App from 'example/adapterApp';
-import App from 'slot/app';
+import { createElement } from "lwc";
+import App from "x/app";
 
-lwcRuntimeFlags.ENABLE_NATIVE_CUSTOM_ELEMENT_LIFECYCLE = true
-const elm = createElement('slot-app', { is: App });
+const elm = createElement("x-app", { is: App });
 document.body.appendChild(elm);
