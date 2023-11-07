5a1f783f34cbc97fb23d93f723558d4f52cadff0
Dale Bustad <dbustad@salesforce.com>
2023-11-07 15:23:59 -0800

    chore: use HIGHEST_API_VERSION from @lwc/shared in karma test options

--- packages/@lwc/integration-karma/scripts/shared/options.js
+++ packages/@lwc/integration-karma/scripts/shared/options.js
@@ -7,6 +7,8 @@
 
 'use strict';
 
+const { HIGHEST_API_VERSION } = require('@lwc/shared');
+
 // Helpful error. Remove after a few months.
 if (process.env.NATIVE_SHADOW) {
     throw new Error('NATIVE_SHADOW is deprecated. Use DISABLE_SYNTHETIC instead!');
@@ -28,10 +30,9 @@ const DISABLE_STATIC_CONTENT_OPTIMIZATION = Boolean(
     process.env.DISABLE_STATIC_CONTENT_OPTIMIZATION
 );
 const NODE_ENV_FOR_TEST = process.env.NODE_ENV_FOR_TEST;
-const LATEST_API_VERSION = 60;
 const API_VERSION = process.env.API_VERSION
     ? parseInt(process.env.API_VERSION, 10)
-    : LATEST_API_VERSION;
+    : HIGHEST_API_VERSION;
 
 module.exports = {
     // Test configuration
@@ -43,7 +44,6 @@ module.exports = {
     DISABLE_SYNTHETIC_SHADOW_SUPPORT_IN_COMPILER,
     DISABLE_STATIC_CONTENT_OPTIMIZATION,
     SYNTHETIC_SHADOW_ENABLED: !DISABLE_SYNTHETIC,
-    LATEST_API_VERSION,
     API_VERSION,
     GREP: process.env.GREP,
     COVERAGE: Boolean(process.env.COVERAGE),
