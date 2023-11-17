8fb1616c4234a712a3ba700d47c8c641a274c535
Nolan Lawson <nlawson@salesforce.com>
2023-11-16 16:44:43 -0800

    fix: license

--- packages/@lwc/integration-karma/scripts/karma-plugins/worker.js
+++ packages/@lwc/integration-karma/scripts/karma-plugins/worker.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
 const path = require('node:path');
 const { worker } = require('workerpool');
 const { rollup } = require('rollup');
