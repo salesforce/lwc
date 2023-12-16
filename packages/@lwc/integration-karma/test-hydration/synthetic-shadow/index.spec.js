859461177d3559a4abd354cd08c3238bc6bb43b0
Nolan Lawson <nlawson@salesforce.com>
2023-12-15 16:14:27 -0800

    test: ensure no errors/warnings

--- packages/@lwc/integration-karma/test-hydration/synthetic-shadow/index.spec.js
+++ packages/@lwc/integration-karma/test-hydration/synthetic-shadow/index.spec.js
@@ -8,7 +8,7 @@ export default {
             childP,
         };
     },
-    test(target, snapshots) {
+    test(target, snapshots, consoleCalls) {
         const parentP = target.shadowRoot.querySelector('p');
         const child = target.shadowRoot.querySelector('x-child');
         const childP = child.shadowRoot.querySelector('p');
@@ -16,14 +16,19 @@ export default {
         expect(childP).toBe(snapshots.childP);
         expect(parentP.textContent).toBe('Parent');
         expect(childP.textContent).toBe('Child');
+
         // both should be native no matter what
         expect(target.shadowRoot.synthetic).toBeUndefined();
         expect(child.shadowRoot.synthetic).toBeUndefined();
+
         // sanity check that the env var is working
         if (process.env.ENABLE_SYNTHETIC_SHADOW_IN_HYDRATION) {
             expect(document.body.attachShadow.toString()).not.toContain('[native code');
         } else {
             expect(document.body.attachShadow.toString()).toContain('[native code');
         }
+
+        expect(consoleCalls.warn).toHaveSize(0);
+        expect(consoleCalls.error).toHaveSize(0);
     },
 };
