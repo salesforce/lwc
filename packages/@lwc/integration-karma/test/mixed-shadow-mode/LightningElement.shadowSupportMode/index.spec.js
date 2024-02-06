4081913edf8ac07dad7549e526b3b585685e6e7d
Eugene Kashida <ekashida@gmail.com>
2024-02-05 17:38:59 -0800

    chore: fix test

--- packages/@lwc/integration-karma/test/mixed-shadow-mode/LightningElement.shadowSupportMode/index.spec.js
+++ packages/@lwc/integration-karma/test/mixed-shadow-mode/LightningElement.shadowSupportMode/index.spec.js
@@ -2,6 +2,7 @@ import { createElement } from 'lwc';
 import { isNativeShadowRootInstance, isSyntheticShadowRootInstance } from 'test-utils';
 
 import Any from 'x/any';
+import Any2 from 'x/any2';
 import Invalid from 'x/invalid';
 import Valid from 'x/valid';
 import NativeOnly from 'x/native';
@@ -21,14 +22,13 @@ describe('shadowSupportMode static property', () => {
 
     // TODO [#3971]: Completely remove shadowSupportMode "any"
     it('should warn for deprecated value "any"', () => {
-        let elm;
-
         // eslint-disable-next-line jest/valid-expect
         expect(() => {
-            elm = createElement('x-any', { is: Any });
+            createElement('x-any', { is: Any });
         }).toLogWarningDev(/Invalid value 'any' for static property shadowSupportMode/);
 
         if (process.env.SYNTHETIC_SHADOW_ENABLED && process.env.NATIVE_SHADOW_ROOT_DEFINED) {
+            const elm = createElement('x-any2', { is: Any2 });
             expect(isSyntheticShadowRootInstance(elm.shadowRoot)).toBe(true);
         }
     });
