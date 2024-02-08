3d5a3acc8e28786ed39afb53044398d6f5353538
Eugene Kashida <ekashida@gmail.com>
2024-02-08 11:50:21 -0800

    chore: review feedback

--- scripts/rollup/rollup.config.js
+++ scripts/rollup/rollup.config.js
@@ -58,7 +58,6 @@ function sharedPlugins() {
 
     return [
         typescript({
-            target: 'es2021',
             tsconfig: path.join(packageRoot, 'tsconfig.json'),
             noEmitOnError: !watchMode, // in watch mode, do not exit with an error if typechecking fails
             ...(watchMode && {
