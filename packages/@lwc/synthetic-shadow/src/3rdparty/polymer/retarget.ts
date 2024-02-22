3b61a9b8bfab8f2c6c820220548272948cea2faf
Will Harney <wharney@salesforce.com>
2024-02-22 11:28:54 -0500

    chore: revert jsdoc changes to 3rd party files

--- packages/@lwc/synthetic-shadow/src/3rdparty/polymer/retarget.ts
+++ packages/@lwc/synthetic-shadow/src/3rdparty/polymer/retarget.ts
@@ -9,8 +9,6 @@ import { pathComposer } from './path-composer';
 import { isSyntheticShadowRoot } from './../../faux-shadow/shadow-root';
 
 /**
- * @param refNode
- * @param path
 @license
 Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
 This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
@@ -18,7 +16,7 @@ The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
 The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
- */
+*/
 export function retarget(refNode: EventTarget | null, path: EventTarget[]): EventTarget | null {
     if (isNull(refNode)) {
         return null;
