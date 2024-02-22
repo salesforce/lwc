9431f2440991f59549e6c4f07bc8c81520fbc162
Will Harney <wharney@salesforce.com>
2024-02-22 15:50:41 -0500

    docs(module-resolver): add JSDoc for main export

--- packages/@lwc/module-resolver/src/shared.ts
+++ packages/@lwc/module-resolver/src/shared.ts
@@ -1,10 +1,16 @@
 /*
- * Copyright (c) 2018, salesforce.com, inc.
+ * Copyright (c) 2024, Salesforce, Inc.
  * All rights reserved.
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
 
-export function isObject(obj: any): obj is object {
+/**
+ * Determines whether the given value is an object or null.
+ * @param obj The value to check
+ * @returns true if the value is an object or null
+ * @example isObject(null) // true
+ */
+export function isObject(obj: any): obj is object | null {
     return typeof obj === 'object';
 }
