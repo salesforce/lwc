b030cf51dd59297bbd150d822647b6857b4e19aa
Nolan Lawson <nlawson@salesforce.com>
2024-02-23 15:53:26 -0800

    test: add tests for invocation order in light DOM

---
+++ packages/@lwc/integration-karma/test/component/lifecycle-callbacks/invocationorder/lightFoo/lightFoo.js
@@ -0,0 +1,22 @@
+import { LightningElement } from 'lwc';
+
+export default class Foo extends LightningElement {
+    static renderMode = 'light';
+    get externalClassName() {
+        return this.getAttribute('class');
+    }
+    get internalClassName() {
+        return `foo-internal-${this.externalClassName}`;
+    }
+    connectedCallback() {
+        window.timingBuffer.push(`foo-${this.externalClassName}:connectedCallback`);
+    }
+    renderedCallback() {
+        window.timingBuffer.push(`foo-${this.externalClassName}:renderedCallback`);
+    }
+    disconnectedCallback() {
+        if (window.timingBuffer) {
+            window.timingBuffer.push(`foo-${this.externalClassName}:disconnectedCallback`);
+        }
+    }
+}
