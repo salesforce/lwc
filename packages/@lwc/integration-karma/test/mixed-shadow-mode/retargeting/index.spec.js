5cf8fd8fcc1a1ab9c2f9abfc99368fdf5a9a76ec
Eugene Kashida <ekashida@gmail.com>
2024-02-25 22:08:59 -0800

    fix(synthetic-shadow): retargeting does not correctly account for native shadow roots

---
+++ packages/@lwc/integration-karma/test/mixed-shadow-mode/retargeting/index.spec.js
@@ -0,0 +1,29 @@
+import { createElement } from 'lwc';
+import SyntheticParent from 'x/syntheticParent';
+
+function test(name, selector) {
+    it(name, () => {
+        const elm = createElement('x-synthetic-parent', { is: SyntheticParent });
+        document.body.appendChild(elm);
+        return Promise.resolve().then(() => {
+            let target;
+            elm.addEventListener('click', (event) => {
+                target = event.target;
+            });
+            elm.shadowRoot.querySelector(selector).click();
+
+            expect(target).toBe(elm);
+        });
+    });
+}
+
+describe('retargeting should resolve to the outermost host element', () => {
+    describe('when event dispatched on an element slotted into a native shadow root', () => {
+        test('lwc component native shadow', '.lwc-slotted-button');
+        test('native web component', '.native-wc-slotted');
+    });
+    describe('when event dispatched on an element inside a native shadow root', () => {
+        test('lwc component native shadow', 'x-native-child-button');
+        test('native web component', 'mixed-shadow-mode-retargeting');
+    });
+});
