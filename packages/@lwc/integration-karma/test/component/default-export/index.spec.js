21a2f2360f7226d497386891ad2e7231210806a5
Nolan Lawson <nlawson@salesforce.com>
2024-02-27 14:47:39 -0800

    test: add test to confirm export behavior

--- packages/@lwc/integration-karma/test/component/default-export/index.spec.js
+++ packages/@lwc/integration-karma/test/component/default-export/index.spec.js
@@ -1,6 +1,13 @@
 import Component from 'x/component';
 import { createElement } from 'lwc';
 import { extractDataIds } from 'test-utils';
+import ExportAsDefault from 'x/exportAsDefault';
+import ExportAsDefaultWithOtherExports, {
+    exportee as exporteeAsDefault,
+} from 'x/exportAsDefaultWithOtherExports';
+import ExportDefaultClassWithOtherExports, {
+    exportee as exporteeDefaultClass,
+} from 'x/exportDefaultClassWithOtherExports';
 
 describe('default export', () => {
     it('should work when a module exports non-components as default', () => {
@@ -12,5 +19,34 @@ describe('default export', () => {
         expect(nodes.nil.textContent).toEqual('null');
         expect(nodes.zero.textContent).toEqual('0');
         expect(nodes.string.textContent).toEqual('"bar"');
+    });
+
+    it('should work with `export default class` syntax and other exports', async () => {
+        expect(exporteeDefaultClass).toBe('foo');
+        const elm = createElement('x-export-default-class-with-other-exports', {
+            is: ExportDefaultClassWithOtherExports,
+        });
+        document.body.appendChild(elm);
+        await Promise.resolve();
+        expect(elm.shadowRoot.querySelector('h1').textContent).toBe('hello world');
+    });
+
+    // TODO [#4020]: support additional syntax for default export
+    xit('should work with `export { ... as default }` syntax', async () => {
+        const elm = createElement('x-export-as-default', { is: ExportAsDefault });
+        document.body.appendChild(elm);
+        await Promise.resolve();
+        expect(elm.shadowRoot.querySelector('h1').textContent).toBe('hello world');
+    });
+
+    // TODO [#4020]: support additional syntax for default export
+    xit('should work with `export { ... as default }` syntax and other exports', async () => {
+        expect(exporteeAsDefault).toBe('foo');
+        const elm = createElement('x-export-as-default-with-other-exports', {
+            is: ExportAsDefaultWithOtherExports,
+        });
+        document.body.appendChild(elm);
+        await Promise.resolve();
+        expect(elm.shadowRoot.querySelector('h1').textContent).toBe('hello world');
     });
 });
