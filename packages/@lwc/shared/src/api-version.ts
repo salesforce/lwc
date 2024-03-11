58ddde6aa4be9ba63eeaa5ca929616d898a6da9a
Nolan Lawson <nlawson@salesforce.com>
2024-03-11 14:08:01 -0700

    chore: add Winter 25 API version, rebalance Karma tests

--- packages/@lwc/shared/src/api-version.ts
+++ packages/@lwc/shared/src/api-version.ts
@@ -12,6 +12,7 @@ export const enum APIVersion {
     V59_246_WINTER_24 = 59,
     V60_248_SPRING_24 = 60,
     V61_250_SUMMER_24 = 61,
+    V62_252_WINTER_25 = 62,
 }
 
 // These must be updated when the enum is updated.
@@ -19,12 +20,13 @@ export const enum APIVersion {
 // passing the `verify-treeshakeable.js` test.
 
 export const LOWEST_API_VERSION = APIVersion.V58_244_SUMMER_23;
-export const HIGHEST_API_VERSION = APIVersion.V61_250_SUMMER_24;
+export const HIGHEST_API_VERSION = APIVersion.V62_252_WINTER_25;
 const allVersions = [
     APIVersion.V58_244_SUMMER_23,
     APIVersion.V59_246_WINTER_24,
     APIVersion.V60_248_SPRING_24,
     APIVersion.V61_250_SUMMER_24,
+    APIVersion.V62_252_WINTER_25,
 ];
 const allVersionsSet = /*@__PURE__@*/ new Set(allVersions);
 
