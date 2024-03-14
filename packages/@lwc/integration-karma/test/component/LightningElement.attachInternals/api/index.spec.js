f41f4f48d9c7ce6b9182bdc2089f9163c0b4a552
Will Harney <wharney@salesforce.com>
2024-03-14 12:48:33 -0400

    chore: reduce indent

--- packages/@lwc/integration-karma/test/component/LightningElement.attachInternals/api/index.spec.js
+++ packages/@lwc/integration-karma/test/component/LightningElement.attachInternals/api/index.spec.js
@@ -79,24 +79,22 @@ if (ENABLE_ELEMENT_INTERNALS_AND_FACE) {
         describe('light DOM', () => {
             attachInternalsSanityTest('light-dom', LightDomCmp);
         });
-    } else {
+    } else if (!process.env.SYNTHETIC_SHADOW_ENABLED) {
         // ElementInternals API is not supported in the browser
         // Because of the order error messages are thrown, this error only appears when synthetic shadow
         // is disabled. Otherwise, 'attachInternals API is not supported in synthetic shadow.'
         // is thrown instead.
-        if (!process.env.SYNTHETIC_SHADOW_ENABLED) {
-            it('should throw an error when used with unsupported browser environments', () => {
-                createElementsThroughLwcAndCustomElementConstructor(
-                    'unsupported-env-component',
-                    ShadowDomCmp
-                ).forEach((elm) => {
-                    testConnectedCallbackError(
-                        elm,
-                        'attachInternals API is not supported in this browser environment.'
-                    );
-                });
+        it('should throw an error when used with unsupported browser environments', () => {
+            createElementsThroughLwcAndCustomElementConstructor(
+                'unsupported-env-component',
+                ShadowDomCmp
+            ).forEach((elm) => {
+                testConnectedCallbackError(
+                    elm,
+                    'attachInternals API is not supported in this browser environment.'
+                );
             });
-        }
+        });
     }
 } else {
     it(`should throw an error when api version < 61`, () => {
