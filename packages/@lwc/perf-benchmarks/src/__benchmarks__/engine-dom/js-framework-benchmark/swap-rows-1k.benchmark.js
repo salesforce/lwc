2c8e113cee68ee2b162c119b2cfc552a9d65cce5
Nolan Lawson <nlawson@salesforce.com>
2024-03-15 15:31:46 -0700

    perf: add throttling to tachometer benchmarks

--- packages/@lwc/perf-benchmarks/src/__benchmarks__/engine-dom/js-framework-benchmark/swap-rows-1k.benchmark.js
+++ packages/@lwc/perf-benchmarks/src/__benchmarks__/engine-dom/js-framework-benchmark/swap-rows-1k.benchmark.js
@@ -6,6 +6,9 @@
  */
 import { runJsFrameworkBenchmark, WARMUP_COUNT } from '../../../utils/runJsFrameworkBenchmark.js';
 
+// Throttling per https://krausest.github.io/js-framework-benchmark/2024/table_chrome_122.0.6261.69.html
+export const cpuThrottlingRate = 4;
+
 // Based on https://github.com/krausest/js-framework-benchmark/blob/6c9f43f/webdriver-ts/src/benchmarksPuppeteer.ts
 // See `benchSwapRows()`
 runJsFrameworkBenchmark(
