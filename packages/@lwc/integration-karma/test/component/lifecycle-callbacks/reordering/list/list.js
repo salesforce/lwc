206322f024546e7ccaedc9963c6e6ac39b871c38
James Tu <j.tu@salesforce.com>
2024-03-20 15:20:00 -0700

    chore: fix some more tests

--- packages/@lwc/integration-karma/test/component/lifecycle-callbacks/reordering/list/list.js
+++ packages/@lwc/integration-karma/test/component/lifecycle-callbacks/reordering/list/list.js
@@ -1,16 +1,5 @@
 import { LightningElement, api } from 'lwc';
 
 export default class extends LightningElement {
-<<<<<<< HEAD:packages/@lwc/integration-karma/test/component/lifecycle-callbacks/reordering/list/list.js
     @api uids = [];
-=======
-    mount = 'loaded on mount';
-    mountClassName = 'class_name_loaded_on_mount';
-
-    @api
-    update;
-
-    @api
-    updateClassName;
->>>>>>> b9d80ed23 (test: karma tests):packages/@lwc/integration-karma/test/static-content/x/attributeExpression/attributeExpression.js
 }
