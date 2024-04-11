e3190d1d2f563c336e0b17bf74cd5b56799ef232
Nolan Lawson <nlawson@salesforce.com>
2024-04-11 14:17:32 -0700

    chore: update estree-walker to v3 (#4128)
    Fixes #3451

--- scripts/shared/bundled-dependencies.js
+++ scripts/shared/bundled-dependencies.js
@@ -8,4 +8,10 @@
 // Dependencies that we bundle into our output JS files to avoid CJS/ESM compat issues.
 // These include packages owned by us (observable-membrane), as well as parse5
 // and its single dependency (entities), which are bundled because it makes it simpler to distribute.
-exports.BUNDLED_DEPENDENCIES = ['@parse5/tools', 'entities', 'observable-membrane', 'parse5'];
+exports.BUNDLED_DEPENDENCIES = [
+    '@parse5/tools',
+    'entities',
+    'estree-walker',
+    'observable-membrane',
+    'parse5',
+];
