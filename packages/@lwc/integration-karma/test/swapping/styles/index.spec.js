006635af0164516f6cc5f8c2d9c04a5f6ce72209
Nolan Lawson <nlawson@salesforce.com>
2024-04-19 13:16:57 -0700

    test: test `swapStyle()` on CSS library

--- packages/@lwc/integration-karma/test/swapping/styles/index.spec.js
+++ packages/@lwc/integration-karma/test/swapping/styles/index.spec.js
@@ -6,6 +6,10 @@ import LightSimple from 'light/simple';
 import LightStaleProp from 'light/staleProp';
 import LightGlobalSimple from 'light-global/simple';
 import LightGlobalStaleProp from 'light-global/staleProp';
+import LibraryUserA from 'x/libraryUserA';
+import LibraryUserB from 'x/libraryUserB';
+import libraryStyle from 'x/library';
+import libraryStyleV2 from 'x/libraryV2';
 
 function expectStyles(elm, styles) {
     const computed = getComputedStyle(elm);
@@ -150,6 +154,79 @@ if (process.env.NODE_ENV !== 'production') {
                             opacity: '0.5',
                         });
                     }
+                });
+            });
+        });
+
+        describe('CSS library', () => {
+            const { style: styleA, styleV2: styleAV2 } = LibraryUserA;
+            const { style: styleB, styleV2: styleBV2 } = LibraryUserB;
+
+            let elmA;
+            let elmB;
+
+            beforeEach(async () => {
+                elmA = createElement('x-library-user-a', { is: LibraryUserA });
+                elmB = createElement(`x-library-user-b`, { is: LibraryUserB });
+                document.body.appendChild(elmA);
+                document.body.appendChild(elmB);
+                await Promise.resolve();
+            });
+
+            function expectInitialState() {
+                expectStyles(extractDataIds(elmA).paragraph, {
+                    fontSize: '10px',
+                    fontWeight: '100',
+                });
+                expectStyles(extractDataIds(elmB).paragraph, {
+                    fontSize: '10px',
+                    fontWeight: '800',
+                });
+            }
+
+            it('swaps a library CSS file', async () => {
+                expectInitialState();
+
+                swapStyle(libraryStyle[0], libraryStyleV2[0]);
+
+                await Promise.resolve();
+                expectStyles(extractDataIds(elmA).paragraph, {
+                    fontSize: '20px',
+                    fontWeight: '100',
+                });
+                expectStyles(extractDataIds(elmB).paragraph, {
+                    fontSize: '20px',
+                    fontWeight: '800',
+                });
+            });
+
+            it('swaps a non-library CSS file while keeping library styles', async () => {
+                expectInitialState();
+
+                // The library (`@import`) is the first stylesheet, so grab the second instead
+                swapStyle(styleA[1], styleAV2[1]);
+
+                await Promise.resolve();
+                expectStyles(extractDataIds(elmA).paragraph, {
+                    fontSize: '10px',
+                    fontWeight: '200',
+                });
+                expectStyles(extractDataIds(elmB).paragraph, {
+                    fontSize: '10px',
+                    fontWeight: '800',
+                });
+
+                // The library (`@import`) is the first stylesheet, so grab the second instead
+                swapStyle(styleB[1], styleBV2[1]);
+
+                await Promise.resolve();
+                expectStyles(extractDataIds(elmA).paragraph, {
+                    fontSize: '10px',
+                    fontWeight: '200',
+                });
+                expectStyles(extractDataIds(elmB).paragraph, {
+                    fontSize: '10px',
+                    fontWeight: '900',
                 });
             });
         });
