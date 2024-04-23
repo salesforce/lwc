7b8b3892cbdb49fac9d659b9e814332a867280eb
Will Harney <wharney@salesforce.com>
2024-04-23 15:07:19 -0400

    feat(playground): add simple @api usage

--- playground/src/modules/x/counter/counter.js
+++ playground/src/modules/x/counter/counter.js
@@ -1,6 +1,8 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class extends LightningElement {
+    @api label;
+
     counter = 0;
 
     increment() {
