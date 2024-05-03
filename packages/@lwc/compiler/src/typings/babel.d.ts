ede0a497d08d0676ed5551dff827dded89a541ce
Will Harney <62956339+wjhsf@users.noreply.github.com>
2024-05-03 10:44:49 -0400

    chore(deps): migrate @babel/plugin-proposal-* to @babel/plugin-transform-* (#4195)
    * chore(deps): migrate @babel/plugin-proposal-* to @babel/plugin-transform-*
    
    * chore(oops): missed a file

---
+++ packages/@lwc/compiler/src/typings/babel.d.ts
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2024, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+declare module '@babel/plugin-transform-async-generator-functions' {
+    const props: any;
+    export = props;
+}
+declare module '@babel/plugin-transform-class-properties' {
+    const props: any;
+    export = props;
+}
+declare module '@babel/plugin-transform-object-rest-spread' {
+    const spread: any;
+    export = spread;
+}
+declare module '@babel/plugin-transform-async-to-generator' {
+    const props: any;
+    export = props;
+}
