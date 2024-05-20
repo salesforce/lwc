64199036a758e2be0430dbdbdcc37a290ecc6e36
Will Harney <wharney@salesforce.com>
2024-05-20 10:36:07 -0400

    chore(exports): export html helper from lwc only

--- packages/lwc/html.d.ts
+++ packages/lwc/html.d.ts
@@ -4,5 +4,15 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-// This file solely provides type definitions, so there is no corresponding .js file.
-import '@lwc/engine-dom/html';
+/**
+ * @file TypeScript type definition for imported HTML templates. Just a helper, not a re-export of
+ * an @lwc/ scoped package.
+ */
+/**
+ * Enables importing of HTML files in LWC components authored using TypeScript.
+ */
+declare module '*.html' {
+    import type { Template } from '@lwc/engine-core';
+    const value: Template;
+    export default value;
+}
