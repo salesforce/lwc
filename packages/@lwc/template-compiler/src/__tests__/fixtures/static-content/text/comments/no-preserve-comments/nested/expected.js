d01105368784b876311da02ffabb3dfe59c05fb5
James Tu <jmsjtu@gmail.com>
2024-05-24 13:31:05 -0700

    fix(template-compiler): avoid creating multiple static parts for text containing comments (#4221)

---
+++ packages/@lwc/template-compiler/src/__tests__/fixtures/static-content/text/comments/no-preserve-comments/nested/expected.js
@@ -0,0 +1,23 @@
+import { parseFragment, registerTemplate } from "lwc";
+const $fragment1 = parseFragment`<div${3}><span${3}>some  text</span><div${3}>${"t5"}<span${3}>${"t7"}</span><div${3}>${"t9"}</div></div></div>`;
+function tmpl($api, $cmp, $slotset, $ctx) {
+  const {
+    d: api_dynamic_text,
+    sp: api_static_part,
+    st: api_static_fragment,
+  } = $api;
+  return [
+    api_static_fragment($fragment1, 1, [
+      api_static_part(
+        5,
+        null,
+        api_dynamic_text($cmp.dynamic) + " " + " " + api_dynamic_text($cmp.text)
+      ),
+      api_static_part(7, null, " " + api_dynamic_text($cmp.dynamic) + " text"),
+      api_static_part(9, null, "text " + " " + api_dynamic_text($cmp.dynamic)),
+    ]),
+  ];
+  /*LWC compiler vX.X.X*/
+}
+export default registerTemplate(tmpl);
+tmpl.stylesheets = [];
