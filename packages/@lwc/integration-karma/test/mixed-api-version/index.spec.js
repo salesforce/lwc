a19ae4e2567f21339a718114b3d1293bf42b547f
Nolan Lawson <nlawson@salesforce.com>
2024-05-30 12:39:09 -0700

    test: repro api version 60 issue

---
+++ packages/@lwc/integration-karma/test/mixed-api-version/index.spec.js
@@ -0,0 +1,30 @@
+import { createElement } from 'lwc';
+import { extractDataIds } from 'test-utils';
+import Container from 'x/container';
+import UseApiVersion60 from 'x/useApiVersion60';
+
+describe('mixed API versions', () => {
+    it('should trigger connected callback for v60 component rendered late inside non-portal <div>', async () => {
+        const elm = createElement('x-container', { is: Container });
+        document.body.appendChild(elm);
+
+        await Promise.resolve();
+
+        const div = extractDataIds(elm).div;
+        expect(div.childNodes.length).toBe(0);
+
+        const apiVersion60Elm = createElement('x-use-api-version-60', { is: UseApiVersion60 });
+
+        expect(() => {
+            // insert manually without a portal
+            div.appendChild(apiVersion60Elm);
+        }).toLogWarningDev(
+            /The `appendChild` method is available only on elements that use the `lwc:dom="manual"` directive/
+        );
+
+        await Promise.resolve();
+
+        // It should render even though it's inserted manually
+        expect(extractDataIds(apiVersion60Elm).h1.textContent).toEqual('API version 60!');
+    });
+});
