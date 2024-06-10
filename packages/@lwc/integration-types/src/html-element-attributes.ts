41ba0f451059a145189573d38e88f24094752726
Will Harney <wharney@salesforce.com>
2024-06-10 17:07:48 -0400

    test(types): add basic checks for setAttribute

---
+++ packages/@lwc/integration-types/src/html-element-attributes.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2024, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+import { LightningElement } from 'lwc';
+
+export default class extends LightningElement {
+    handleClick() {
+        this.setAttribute('data-clicked', 'yes');
+    }
+}
