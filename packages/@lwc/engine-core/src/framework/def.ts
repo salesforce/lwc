28bb360a88451d3d817f42ba9ac6b9ba771b3be2
Will Harney <wharney@salesforce.com>
2024-06-10 17:09:59 -0400

    refactor(const-enum): update PropDefType

--- packages/@lwc/engine-core/src/framework/def.ts
+++ packages/@lwc/engine-core/src/framework/def.ts
@@ -350,9 +350,7 @@ const lightingElementDef: ComponentDef = {
     render: LightningElement.prototype.render,
 };
 
-const enum PropDefType {
-    any = 'any',
-}
+type PropDefType = 'any';
 
 interface PropDef {
     config: number;
@@ -384,7 +382,7 @@ export function getComponentDef(Ctor: any): PublicComponentDef {
         // avoid leaking the reference to the public props descriptors
         publicProps[key] = {
             config: propsConfig[key] || 0, // a property by default
-            type: PropDefType.any, // no type inference for public services
+            type: 'any', // no type inference for public services
             attr: htmlPropertyToAttribute(key),
         };
     }
