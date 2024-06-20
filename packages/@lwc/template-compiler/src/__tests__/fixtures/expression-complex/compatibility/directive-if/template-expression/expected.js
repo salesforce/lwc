6ee6bb8866b00ab6499dd9fbc8f15d93aae5d531
Eugene Kashida <ekashida@gmail.com>
2024-06-20 11:12:51 -0700

    chore: add existing test to demonstrate cte bugfix

---
+++ packages/@lwc/template-compiler/src/__tests__/fixtures/expression-complex/compatibility/directive-if/template-expression/expected.js
@@ -0,0 +1,17 @@
+import { registerTemplate } from "lwc";
+const stc0 = {
+  key: 0,
+};
+function tmpl($api, $cmp, $slotset, $ctx) {
+  const { d: api_dynamic_text, t: api_text, h: api_element } = $api;
+  return [
+    api_element("section", stc0, [
+      $cmp.state.isTrue
+        ? api_text(api_dynamic_text($cmp.foo) + api_dynamic_text($cmp.bar))
+        : null,
+    ]),
+  ];
+  /*LWC compiler vX.X.X*/
+}
+export default registerTemplate(tmpl);
+tmpl.stylesheets = [];
