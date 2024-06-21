306655241eaaa28cd523060e9e2ca9ee3633e063
Eugene Kashida <ekashida@gmail.com>
2024-06-20 17:00:12 -0700

    fix(template-compiler): parsed template content should be allocated to `content.childNodes` (#4311)

---
+++ packages/@lwc/template-compiler/src/__tests__/fixtures/expression-complex/compatibility/README.md
@@ -0,0 +1,1 @@
+The tests in this directory will end up being redundant as the code path for complex template expressions is iteratively merged.
---
+++ packages/@lwc/template-compiler/src/__tests__/fixtures/expression-complex/compatibility/directive-if/template-expression/expected.js
@@ -0,0 +1,28 @@
+import _implicitStylesheets from "./template-expression.css";
+import _implicitScopedStylesheets from "./template-expression.scoped.css?scoped=true";
+import { freezeTemplate, registerTemplate } from "lwc";
+const stc0 = {
+  key: 0,
+};
+function tmpl($api, $cmp, $slotset, $ctx) {
+  const { d: api_dynamic_text, t: api_text, h: api_element } = $api;
+  return [
+    api_element("section", stc0, [
+      $cmp.state.isTrue
+        ? api_text(api_dynamic_text($cmp.foo) + api_dynamic_text($cmp.bar))
+        : null,
+    ]),
+  ];
+  /*LWC compiler vX.X.X*/
+}
+export default registerTemplate(tmpl);
+tmpl.stylesheets = [];
+tmpl.stylesheetToken = "lwc-5uvenajcmlb";
+tmpl.legacyStylesheetToken = "x-template-expression_template-expression";
+if (_implicitStylesheets) {
+  tmpl.stylesheets.push.apply(tmpl.stylesheets, _implicitStylesheets);
+}
+if (_implicitScopedStylesheets) {
+  tmpl.stylesheets.push.apply(tmpl.stylesheets, _implicitScopedStylesheets);
+}
+freezeTemplate(tmpl);
