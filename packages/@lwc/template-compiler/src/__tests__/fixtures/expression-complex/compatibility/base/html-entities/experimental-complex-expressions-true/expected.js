565892d5ed4fe798e16b86baef8f8cd8327ec3b4
Eugene Kashida <ekashida@gmail.com>
2024-06-21 12:54:30 -0700

    chore: update snapshot

--- packages/@lwc/template-compiler/src/__tests__/fixtures/expression-complex/compatibility/base/html-entities/experimental-complex-expressions-true/expected.js
+++ packages/@lwc/template-compiler/src/__tests__/fixtures/expression-complex/compatibility/base/html-entities/experimental-complex-expressions-true/expected.js
@@ -1,5 +1,5 @@
-import _implicitStylesheets from "./html-entities.css";
-import _implicitScopedStylesheets from "./html-entities.scoped.css?scoped=true";
+import _implicitStylesheets from "./experimental-complex-expressions-true.css";
+import _implicitScopedStylesheets from "./experimental-complex-expressions-true.scoped.css?scoped=true";
 import { freezeTemplate, parseFragment, registerTemplate } from "lwc";
 const $fragment1 = parseFragment`<p${3}>foo&amp;bar</p>`;
 const $fragment2 = parseFragment`<p${3}>const { foo } = bar;</p>`;
@@ -13,8 +13,9 @@ function tmpl($api, $cmp, $slotset, $ctx) {
 }
 export default registerTemplate(tmpl);
 tmpl.stylesheets = [];
-tmpl.stylesheetToken = "lwc-582p6pifafh";
-tmpl.legacyStylesheetToken = "x-html-entities_html-entities";
+tmpl.stylesheetToken = "lwc-5t3hn3vkg17";
+tmpl.legacyStylesheetToken =
+  "x-experimental-complex-expressions-true_experimental-complex-expressions-true";
 if (_implicitStylesheets) {
   tmpl.stylesheets.push.apply(tmpl.stylesheets, _implicitStylesheets);
 }
