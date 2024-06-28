adc8d9e05908a4fb36ced16dd65992cbd52446c9
Brad Hill <brad-hill@hotmail.com.au>
2024-06-28 11:34:15 -0700

    fix: inline style parsing for important

---
+++ packages/@lwc/integration-karma/test/rendering/style-specificity-important/index.spec.js
@@ -0,0 +1,39 @@
+import { createElement } from 'lwc';
+import Component from 'x/component';
+
+describe('important styling and style override', () => {
+    it('should render !important styles correctly', async () => {
+        const elm = createElement('x-component', { is: Component });
+        document.body.appendChild(elm);
+
+        await Promise.resolve();
+
+        for (const div of elm.shadowRoot.querySelectorAll('.imporant')) {
+            expect(getComputedStyle(div).getPropertyValue('color')).toBe('rgb(255, 0, 0)');
+            expect(div.style.getPropertyPriority('color')).toBe('important');
+        }
+    });
+
+    it('should render inline styles correctly', async () => {
+        const elm = createElement('x-component', { is: Component });
+        document.body.appendChild(elm);
+
+        await Promise.resolve();
+
+        for (const div of elm.shadowRoot.querySelectorAll('.inline')) {
+            expect(getComputedStyle(div).getPropertyValue('color')).toBe('rgb(255, 0, 0)');
+            expect(div.style.getPropertyPriority('color')).not.toBe('important');
+        }
+    });
+
+    it('should render untouched styles correctly', async () => {
+        const elm = createElement('x-component', { is: Component });
+        document.body.appendChild(elm);
+
+        await Promise.resolve();
+
+        for (const div of elm.shadowRoot.querySelectorAll('.untouched')) {
+            expect(getComputedStyle(div).getPropertyValue('color')).toBe('rgb(0, 0, 255)');
+        }
+    });
+});
