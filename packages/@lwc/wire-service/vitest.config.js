bcf86b9358b70c829eed7ea579b7d9495dba966c
Matheus Cardoso <matheus@cardo.so>
2024-07-21 07:42:57 -0300

    chore: test @lwc/shared with vitest

---
+++ packages/@lwc/wire-service/vitest.config.js
@@ -0,0 +1,13 @@
+import { defineProject, mergeConfig } from 'vitest/config';
+import sharedConfig from '../../../vitest.shared';
+
+export default mergeConfig(
+    sharedConfig,
+    defineProject({
+        test: {
+            name: 'lwc-wire-service',
+            environment: 'jsdom',
+            root: 'src',
+        },
+    })
+);
---
+++ vitest.workspace.js
@@ -0,0 +1,3 @@
+import { defineWorkspace } from 'vitest/config';
+
+export default defineWorkspace(['packages/@lwc/shared', 'packages/@lwc/wire-service']);
