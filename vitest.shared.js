dddc404218fd5d4644e151ef7300cc6efd544f0a
Matheus Cardoso <matheus@cardo.so>
2024-07-21 23:33:46 -0300

    chore: add vitest coverage

--- vitest.shared.js
+++ vitest.shared.js
@@ -9,6 +9,21 @@ export default defineConfig({
             printBasicPrototype: true,
             callToJSON: true,
         },
+        coverage: {
+            exclude: [
+                '/node_modules/',
+                '/fixtures/',
+                '/dist/',
+                // Ignore helper files like test-utils.ts that might exist alongside spec files
+                '/__tests__/',
+            ],
+            thresholds: {
+                branches: 80,
+                functions: 90,
+                lines: 90,
+            },
+            reporter: ['clover', 'json', 'lcov', 'text', ['text', { file: 'coverage.txt' }]],
+        },
     },
     plugins,
 });
