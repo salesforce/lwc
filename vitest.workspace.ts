a5e2e6da56cf7e2ff97fba527d0867ee065002b2
Nolan Lawson <nlawson@salesforce.com>
2024-07-25 13:15:54 -0700

    fix: allow for testing @lwc/ssr-compiler with env var

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,19 +1,22 @@
 import { defineWorkspace } from 'vitest/config';
 
 export default defineWorkspace([
-    'packages/@lwc/shared',
-    'packages/@lwc/wire-service',
-    'packages/@lwc/style-compiler',
-    'packages/@lwc/template-compiler',
-    'packages/@lwc/errors',
-    'packages/@lwc/features',
-    'packages/@lwc/signals',
-    'packages/@lwc/rollup-plugin',
+    'packages/@lwc/babel-plugin-component',
+    'packages/@lwc/compiler',
     'packages/@lwc/engine-core',
     'packages/@lwc/engine-dom',
-    'packages/@lwc/compiler',
-    'packages/@lwc/babel-plugin-component',
-    'packages/@lwc/module-resolver',
     'packages/@lwc/engine-server',
+    'packages/@lwc/errors',
+    'packages/@lwc/features',
+    'packages/@lwc/module-resolver',
+    'packages/@lwc/rollup-plugin',
+    'packages/@lwc/shared',
+    'packages/@lwc/signals',
+    // We will enable this for realsies once all the tests are passing, but for now having the env var avoids
+    // running these tests in CI while still allowing for local testing.
+    ...(process.env.TEST_SSR_COMPILER ? ['packages/@lwc/ssr-compiler'] : []),
+    'packages/@lwc/style-compiler',
+    'packages/@lwc/template-compiler',
+    'packages/@lwc/wire-service',
     'packages/lwc',
 ]);
