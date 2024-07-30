c2b350d35e9f5af4d79e53b9d2efbc907b5eae9a
Will Harney <wharney@salesforce.com>
2024-07-30 16:25:56 -0400

    test(wire): expand type validations

--- packages/@lwc/integration-types/src/decorators/wire.ts
+++ packages/@lwc/integration-types/src/decorators/wire.ts
@@ -12,6 +12,8 @@ type WireConfig = { config: 'config' };
 type WireValue = { value: 'value' };
 type WireContext = { context: 'context' };
 
+const config: WireConfig = { config: 'config' };
+
 // `class C implements A` validates that the class instance matches type A
 // `const C = class C {} satisfies B` validates that the class constructor matches type B
 const FakeWireAdapter = class FakeWireAdapter implements WireAdapter<WireConfig, WireContext> {
@@ -30,26 +32,34 @@ export default class Decorators extends LightningElement {
 
     // Valid cases
     @wire(FakeWireAdapter, { config: 'config' }) baseConfigProp?: WireValue;
+    @wire(FakeWireAdapter, config) configVarProp?: WireValue;
     @wire(FakeWireAdapter, { config: '$plainProp' }) reactiveConfigProp?: WireValue;
     @wire(FakeWireAdapter, { config: 'config' } as const) baseConstConfigProp?: WireValue;
     @wire(FakeWireAdapter, { config: '$plainProp' } as const)
     reactiveConstConfigProp?: WireValue;
-    // Possibly shouldn't be valid?
-    @wire(FakeWireAdapter) noConfigProp?: WireValue;
     @wire(FakeWireAdapter, { config: 'config' }) baseConfigMethod(_?: WireValue) {}
+    @wire(FakeWireAdapter, config) configVarMethod(_?: WireValue) {}
     @wire(FakeWireAdapter, { config: '$plainProp' }) reactiveConfigMethod(_?: WireValue) {}
     @wire(FakeWireAdapter, { config: 'config' } as const) baseConstConfigMethod(_?: WireValue) {}
-    @wire(FakeWireAdapter, { config: '$plainProp' } as const) reactiveConstConfigMethod(
-        _?: WireValue
-    ) {}
-    // Possibly shouldn't be valid?
-    @wire(FakeWireAdapter) noConfigMethod(_value?: WireValue) {}
+    @wire(FakeWireAdapter, { config: '$plainProp' } as const)
+    reactiveConstConfigMethod(_?: WireValue) {}
 
     // Invalid cases
-    // @ts-expect-error because prop type is `string` but the adapter needs `WireValue`
-    @wire(FakeWireAdapter, { config: 'config' }) wireWrongPropType?: 'wrong type';
-    // @ts-expect-error because config type is `{wrong: string}` but the adapter needs `WireConfig`
-    @wire(FakeWireAdapter, { wrong: 'type' }) wireWrongConfig?: WireValue;
-    // @ts-expect-error because prop type is `string` but the adapter needs `WireValue`
-    @wire(FakeWireAdapter, { config: 'config' }) wireWrongMethodSignature(_value: 'wrong type') {}
+    // @ts-expect-error prop type is `string` but the adapter needs `WireValue`
+    @wire(FakeWireAdapter, { config: 'config' }) wrongPropType?: 'wrong type';
+    // @ts-expect-error config type is `{wrong: string}` but the adapter needs `WireConfig`
+    @wire(FakeWireAdapter, { wrong: 'type' }) wrongConfigProp?: WireValue;
+    // @ts-expect-error prop must be initialized or set as optional
+    @wire(FakeWireAdapter, { config: 'config' }) nonUndefinedProp: WireValue;
+    // @ts-expect-error prop type is `string` but the adapter needs `WireValue`
+    @wire(FakeWireAdapter, { config: 'config' })
+    wrongMethodSignature(_?: 'wrong type') {}
+    // @ts-expect-error config type is `{wrong: string}` but the adapter needs `WireConfig`
+    @wire(FakeWireAdapter, { wrong: 'type' }) wrongConfigMethod(_?: WireValue) {}
+
+    // Ambiguous cases -- possibly shouldn't be valid?
+    // Passing a config is optional because adapters don't strictly need to use it.
+    // Can we be smarter about the type and require a config if the adapter does?
+    @wire(FakeWireAdapter) noConfigProp?: WireValue;
+    @wire(FakeWireAdapter) noConfigMethod(_?: WireValue) {}
 }
