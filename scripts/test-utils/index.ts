9c9884f6381cfed4b3eceac2c1ffde1e987e2474
Matheus Cardoso <matheus@cardo.so>
2024-08-03 19:08:33 -0300

    test: remove vestiges of jest from test-utils

--- scripts/test-utils/index.ts
+++ scripts/test-utils/index.ts
@@ -4,6 +4,6 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-import './matchers'; // File has no exports, but registers jest matchers
+import './matchers'; // File has no exports, but registers vitest matchers
 export * from './test-fixture-dir';
 export * from './format-html';
