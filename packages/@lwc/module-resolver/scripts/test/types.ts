b96cf53cc5651e2a82c292274dea43c88a50f8b6
Matheus Cardoso <matheus@cardo.so>
2024-08-03 19:22:20 -0300

    test: remove vestiges of jest from module-resolver tets

--- packages/@lwc/module-resolver/scripts/test/types.ts
+++ packages/@lwc/module-resolver/scripts/test/types.ts
@@ -4,15 +4,16 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-export {}; // required to have a module with just `declare global` in it
+// export {}; // required to have a module with just `declare vitest` in it
 
-declare global {
-    // eslint-disable-next-line @typescript-eslint/no-namespace
-    namespace jest {
-        interface Matchers<R> {
-            __type: R; // unused, but makes TypeScript happy
-            toThrowErrorWithCode(received: any, ctor: any, message?: string): CustomMatcherResult;
-            toThrowErrorWithType(received: any, ctor: any, message?: string): CustomMatcherResult;
-        }
-    }
+import 'vitest';
+
+interface CustomMatchers<R = unknown> {
+    toThrowErrorWithCode: (received: any, ctor: any, message?: string) => R;
+    toThrowErrorWithType: (received: any, ctor: any, message?: string) => R;
+}
+
+declare module 'vitest' {
+    interface Assertion<T = any> extends CustomMatchers<T> {}
+    interface AsymmetricMatchersContaining extends CustomMatchers {}
 }
