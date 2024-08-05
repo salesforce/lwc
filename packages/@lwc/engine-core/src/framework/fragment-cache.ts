bd092fd79bacb8983a45eb53f734af0536c349bb
Nolan Lawson <nlawson@salesforce.com>
2024-08-05 15:10:42 -0700

    refactor: simplify further

--- packages/@lwc/engine-core/src/framework/fragment-cache.ts
+++ packages/@lwc/engine-core/src/framework/fragment-cache.ts
@@ -5,25 +5,23 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
 
-import { ArrayFill, ArrayMap } from '@lwc/shared';
-
 export const enum FragmentCacheKey {
     HAS_SCOPED_STYLE = 1,
     SHADOW_MODE_SYNTHETIC = 2,
 }
 
-// Maximum cache key we need to account for in the array size.
-// (HAS_SCOPED_STYLE | SHADOW_MODE_SYNTHETIC = 3)
-const MAX_CACHE_KEY = 3;
-
 // Mapping of cacheKeys to `string[]` (assumed to come from a tagged template literal) to an Element.
 // Note that every unique tagged template literal will have a unique `string[]`. So by using `string[]`
 // as the WeakMap key, we effectively associate each Element with a unique tagged template literal.
 // See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals#tagged_templates
-const fragmentCache = ArrayMap.call(
-    ArrayFill.call(new Array(MAX_CACHE_KEY + 1), undefined),
-    () => new WeakMap()
-) as WeakMap<string[], Element>[];
+// Also note that this array only needs to account for the maximum possible cache key, i.e. 3
+// (HAS_SCOPED_STYLE | SHADOW_MODE_SYNTHETIC = 3), so length is 4
+const fragmentCache: WeakMap<string[], Element>[] = [
+    new WeakMap(),
+    new WeakMap(),
+    new WeakMap(),
+    new WeakMap(),
+];
 
 // Only used in LWC's Karma tests
 if (process.env.NODE_ENV === 'test-karma-lwc') {
@@ -35,11 +33,9 @@ if (process.env.NODE_ENV === 'test-karma-lwc') {
 }
 
 export function getFromFragmentCache(cacheKey: number, strings: string[]) {
-    const stringsToElementsMap = fragmentCache[cacheKey];
-    return stringsToElementsMap.get(strings);
+    return fragmentCache[cacheKey].get(strings);
 }
 
 export function setInFragmentCache(cacheKey: number, strings: string[], element: Element) {
-    const stringsToElementsMap = fragmentCache[cacheKey];
-    return stringsToElementsMap.set(strings, element);
+    fragmentCache[cacheKey].set(strings, element);
 }
