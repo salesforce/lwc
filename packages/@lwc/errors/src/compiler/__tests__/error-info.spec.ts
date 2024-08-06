b26cf6180a4ea3cc93ec9f27f5af63f464b37f03
Matheus Cardoso <matheus@cardo.so>
2024-08-06 18:43:37 -0300

    Update error-info.spec.ts
    Co-authored-by: Nolan Lawson <nolan@nolanlawson.com>

--- packages/@lwc/errors/src/compiler/__tests__/error-info.spec.ts
+++ packages/@lwc/errors/src/compiler/__tests__/error-info.spec.ts
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-import { describe, it } from 'vitest';
 import * as errorInfo from '../error-info';
 // All exported objects are maps of label/error info, except for GENERIC_COMPILER_ERROR,
 // which is a top-level error info object
