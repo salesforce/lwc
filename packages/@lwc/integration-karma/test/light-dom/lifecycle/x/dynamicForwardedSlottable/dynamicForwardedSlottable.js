f07fbd02aae05b2f9984c78c3c3b63b450ea7090
Nolan Lawson <nlawson@salesforce.com>
2024-08-13 11:26:49 -0700

    fix: revert "prevent prematurely cloning light dom slot vnodes" (#4454)
    This reverts commit 11058232634a18c530dc7aeee884694a81df7a9e (#4258).

--- packages/@lwc/integration-karma/test/light-dom/lifecycle/x/dynamicForwardedSlottable/dynamicForwardedSlottable.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class extends LightningElement {
-    static renderMode = 'light';
-
-    @api top = 'bottom';
-    @api bottom = 'top';
-}
