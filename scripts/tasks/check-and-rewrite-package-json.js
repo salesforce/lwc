c09338f07ab3a00f8bdde8e42d7ecac241d7a9ca
Nolan Lawson <nlawson@salesforce.com>
2024-08-23 14:53:04 -0700

    fix: require Node v18+

--- scripts/tasks/check-and-rewrite-package-json.js
+++ scripts/tasks/check-and-rewrite-package-json.js
@@ -106,6 +106,9 @@ for (const dir of globSync('./packages/@lwc/*')) {
         bugs: { url: 'https://github.com/salesforce/lwc/issues' },
         license: 'MIT',
         publishConfig: { access: 'public' },
+        engines: {
+            node: '>=18',
+        },
         ...buildProps,
         dependencies,
         devDependencies,
