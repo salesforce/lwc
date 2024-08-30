daa20d25b6d7d2f78bea436a18b6d70ef1065473
Nolan Lawson <nlawson@salesforce.com>
2024-08-30 09:00:18 -0700

    fix(template-compiler): escape tag names (#4498)

---
+++ packages/@lwc/template-compiler/src/__tests__/fixtures/html-tags/backslash-in-tag-name/non-optimized/expected.js
@@ -0,0 +1,22 @@
+import _implicitStylesheets from "./non-optimized.css";
+import _implicitScopedStylesheets from "./non-optimized.scoped.css?scoped=true";
+import { freezeTemplate, registerTemplate } from "lwc";
+const stc0 = {
+  key: 0,
+};
+function tmpl($api, $cmp, $slotset, $ctx) {
+  const { h: api_element } = $api;
+  return [api_element("s\\ection", stc0)];
+  /*LWC compiler vX.X.X*/
+}
+export default registerTemplate(tmpl);
+tmpl.stylesheets = [];
+tmpl.stylesheetToken = "lwc-71tdq7g4m0k";
+tmpl.legacyStylesheetToken = "x-non-optimized_non-optimized";
+if (_implicitStylesheets) {
+  tmpl.stylesheets.push.apply(tmpl.stylesheets, _implicitStylesheets);
+}
+if (_implicitScopedStylesheets) {
+  tmpl.stylesheets.push.apply(tmpl.stylesheets, _implicitScopedStylesheets);
+}
+freezeTemplate(tmpl);
