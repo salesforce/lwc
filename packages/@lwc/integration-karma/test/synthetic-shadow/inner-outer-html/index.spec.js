b7be5f5beeb661b23cff83f88ad1745e73f41899
Nolan Lawson <nlawson@salesforce.com>
2024-09-23 12:33:15 -0700

    fix: fix full innerHTML

--- packages/@lwc/integration-karma/test/synthetic-shadow/inner-outer-html/index.spec.js
+++ packages/@lwc/integration-karma/test/synthetic-shadow/inner-outer-html/index.spec.js
@@ -11,23 +11,25 @@ beforeEach(() => {
     consoleSpy = spyOn(console, 'warn');
 });
 
-fit('does not render content from attributes', async () => {
+it('does not render content from attributes', async () => {
     const elm = createElement('x-inner', { is: Inner });
     document.body.appendChild(elm);
     await Promise.resolve();
 
-    const ids = extractDataIds(elm);
-    for (const [_, node] of Object.entries(ids)) {
+    const ids = Object.entries(extractDataIds(elm)).filter(([id]) => !id.endsWith('.shadowRoot'));
+    for (const [_id, node] of ids) {
         expect(node.childNodes.length).toBe(1);
         expect(node.firstChild.nodeType).toBe(Node.TEXT_NODE);
         expect(node.firstChild.nodeValue).toBe('original');
     }
 
-    const len = Object.values(ids).filter(elm => elm.dataset.expectWarning === '').length;
+    const len = ids.filter(([_id, elm]) => !elm.hasAttribute('data-expect-no-warning')).length;
     expect(consoleSpy).toHaveBeenCalledTimes(len);
 
     const calls = consoleSpy.calls;
     for (let i = 0; i < len; i += 1) {
-        expect(calls.argsFor(i)[0]).toBe('Cannot set property "innerHTML". Instead, use lwc:inner-html or lwc:dom-manual.');
+        expect(calls.argsFor(i)[0]).toBe(
+            'Cannot set property "innerHTML". Instead, use lwc:inner-html or lwc:dom-manual.'
+        );
     }
 });
