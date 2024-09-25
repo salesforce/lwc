01da0c6182c6da77bbdaad5884617de6bd96f2f4
Eugene Kashida <ekashida@gmail.com>
2024-09-25 09:27:46 -0700

    chore: update test

--- packages/@lwc/integration-karma/test-hydration/svg/with-empty-class/index.spec.js
+++ packages/@lwc/integration-karma/test-hydration/svg/with-empty-class/index.spec.js
@@ -1,7 +1,7 @@
 export default {
     test(elm, snapshots, consoleCalls) {
         // TODO [#3548]: inconsistent rendering of empty class/style
-        expect(elm.shadowRoot.querySelector('svg circle').getAttribute('class')).toEqual('');
+        expect(elm.shadowRoot.querySelector('svg circle').getAttribute('class')).toEqual(null);
         expect(consoleCalls.warn).toHaveSize(0);
         expect(consoleCalls.error).toHaveSize(0);
     },
