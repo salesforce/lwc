c348cd380afac033f0ce5cf06dc66b6da405d404
Eugene Kashida <ekashida@gmail.com>
2024-10-01 06:55:59 -0700

    fix(engine-server): setAttribute to handle non-string values (#4593)
    Co-authored-by: Nolan Lawson <nlawson@salesforce.com>
    Co-authored-by: Will Harney <62956339+wjhsf@users.noreply.github.com>

--- packages/@lwc/engine-server/src/renderer.ts
+++ packages/@lwc/engine-server/src/renderer.ts
@@ -257,7 +257,7 @@ function getAttribute(element: E, name: string, namespace: string | null = null)
     return attribute ? attribute.value : null;
 }
 
-function setAttribute(element: E, name: string, value: string, namespace: string | null = null) {
+function setAttribute(element: E, name: string, value: unknown, namespace: string | null = null) {
     reportMutation(element, name);
     const attribute = element[HostAttributesKey].find(
         (attr) => attr.name === name && attr[HostNamespaceKey] === namespace
@@ -274,7 +274,7 @@ function setAttribute(element: E, name: string, value: string, namespace: string
             value: String(value),
         });
     } else {
-        attribute.value = value;
+        attribute.value = String(value);
     }
 }
 
