6cf87e1e25e22498d6e705b57f2ff22371f8c566
Matheus Cardoso <matheus@cardo.so>
2024-10-02 11:33:50 -0300

    fix: update done callbacks to promises

--- packages/@lwc/integration-karma/test/shadow-dom/Event-properties/Event.defaultPrevented.spec.js
+++ packages/@lwc/integration-karma/test/shadow-dom/Event-properties/Event.defaultPrevented.spec.js
@@ -3,66 +3,69 @@ import { createElement } from 'lwc';
 import Container from 'x/container';
 
 describe('Event.defaultPrevented', () => {
-    it('should return true if cancelable and preventDefault() was invoked', (done) => {
-        const container = createElement('x-container', { is: Container });
-        document.body.appendChild(container);
+    it('should return true if cancelable and preventDefault() was invoked', () =>
+        new Promise((done) => {
+            const container = createElement('x-container', { is: Container });
+            document.body.appendChild(container);
 
-        container.shadowRoot.addEventListener('test', (event) => {
-            event.preventDefault();
-        });
-        container.addEventListener('test', (event) => {
-            expect(event.defaultPrevented).toBeTrue();
-            done();
-        });
+            container.shadowRoot.addEventListener('test', (event) => {
+                event.preventDefault();
+            });
+            container.addEventListener('test', (event) => {
+                expect(event.defaultPrevented).toBeTrue();
+                done();
+            });
 
-        const div = container.shadowRoot.querySelector('div');
-        div.dispatchEvent(
-            new CustomEvent('test', {
-                bubbles: true,
-                cancelable: true,
-                composed: true,
-            })
-        );
-    });
+            const div = container.shadowRoot.querySelector('div');
+            div.dispatchEvent(
+                new CustomEvent('test', {
+                    bubbles: true,
+                    cancelable: true,
+                    composed: true,
+                })
+            );
+        }));
 
-    it('should return false if cancelable and preventDefault() was not invoked', (done) => {
-        const container = createElement('x-container', { is: Container });
-        document.body.appendChild(container);
+    it('should return false if cancelable and preventDefault() was not invoked', () =>
+        new Promise((done) => {
+            const container = createElement('x-container', { is: Container });
+            document.body.appendChild(container);
 
-        container.addEventListener('test', (event) => {
-            expect(event.defaultPrevented).toBeFalse();
-            done();
-        });
+            container.addEventListener('test', (event) => {
+                expect(event.defaultPrevented).toBeFalse();
+                done();
+            });
 
-        const div = container.shadowRoot.querySelector('div');
-        div.dispatchEvent(
-            new CustomEvent('test', {
-                bubbles: true,
-                cancelable: true,
-                composed: true,
-            })
-        );
-    });
+            const div = container.shadowRoot.querySelector('div');
+            div.dispatchEvent(
+                new CustomEvent('test', {
+                    bubbles: true,
+                    cancelable: true,
+                    composed: true,
+                })
+            );
+        }));
 
-    it('should return false if not cancelable and preventDefault() was invoked', (done) => {
-        const container = createElement('x-container', { is: Container });
-        document.body.appendChild(container);
+    it('should return false if not cancelable and preventDefault() was invoked', () =>
+        new Promise((done) => {
+            const container = createElement('x-container', { is: Container });
+            document.body.appendChild(container);
 
-        container.shadowRoot.addEventListener('test', (event) => {
-            event.preventDefault();
-        });
-        container.addEventListener('test', (event) => {
-            expect(event.defaultPrevented).toBeFalse();
-            done();
-        });
+            container.shadowRoot.addEventListener('test', (event) => {
+                event.preventDefault();
+            });
+            container.addEventListener('test', (event) => {
+                expect(event.defaultPrevented).toBeFalse();
+                done();
+            });
 
-        const div = container.shadowRoot.querySelector('div');
-        div.dispatchEvent(
-            new CustomEvent('test', {
-                bubbles: true,
-                cancelable: false,
-                composed: true,
-            })
-        );
-    });
+            const div = container.shadowRoot.querySelector('div');
+            div.dispatchEvent(
+                new CustomEvent('test', {
+                    bubbles: true,
+                    cancelable: false,
+                    composed: true,
+                })
+            );
+        }));
 });
