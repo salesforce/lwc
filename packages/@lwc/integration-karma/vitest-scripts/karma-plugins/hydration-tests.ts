de390dd94b8354726ca50a5273a63135d712cedf
Matheus Cardoso <matheus@cardo.so>
2024-10-03 17:31:05 -0300

    chore: simpligy hydration plugin

--- packages/@lwc/integration-karma/vitest-scripts/karma-plugins/hydration-tests.ts
+++ packages/@lwc/integration-karma/vitest-scripts/karma-plugins/hydration-tests.ts
@@ -6,13 +6,13 @@
  */
 import path from 'node:path';
 import { Script, createContext } from 'node:vm';
-import { access } from 'node:fs/promises';
-import { format } from 'node:util';
+import fs from 'node:fs/promises';
+import util from 'node:util';
 import { InputOption, rollup, RollupCache } from 'rollup';
 import lwcRollupPlugin from '@lwc/rollup-plugin';
 import ssr, { renderComponent } from '@lwc/engine-server';
-import Watcher from './Watcher.js';
 import type { PathLike } from 'fs';
+import type { Plugin } from 'vitest/config';
 
 const context = {
     LWC: ssr,
@@ -23,7 +23,7 @@ let guid = 0;
 const COMPONENT_UNDER_TEST = 'main';
 
 export const TEMPLATE = `
-    (function (hydrateTest) {
+    function testSuite(hydrateTest) {
         const ssrRendered = %s;
         // Component code, set as Main
         %s;
@@ -35,13 +35,15 @@ export const TEMPLATE = `
                 return hydrateTest.runTest(ssrRendered, Main, config);
             })
         });
-    })(window.HydrateTest);
+    }
+    
+    testSuite(window.HydrateTest);
 `;
 
 // Like `fs.existsSync` but async
 export async function exists(path: PathLike) {
     try {
-        await access(path);
+        await fs.access(path);
         return true;
     } catch (_err) {
         return false;
@@ -135,89 +137,40 @@ export async function getTestModuleCode(input: InputOption) {
     return { code, watchFiles };
 }
 
-function createHCONFIG2JSPreprocessor(
-    config: { basePath: any },
-    logger: { create: (arg0: string) => any },
-    emitter: { _fileList: { changeFile: (arg0: string, arg1: boolean) => void } }
-) {
-    const { basePath } = config;
-    const log = logger.create('preprocessor-lwc');
-    const watcher = new Watcher(config, emitter, log);
-
-    return async (
-        _content: any,
-        file: { path: any },
-        done: (arg0: unknown, arg1: string | null) => void
-    ) => {
-        const filePath = file.path;
-        const suiteDir = path.dirname(filePath);
-        // Wrap all the tests into a describe block with the file stricture name
-        const describeTitle = path.relative(basePath, suiteDir).split(path.sep).join(' ');
-
-        try {
-            const { code: testCode, watchFiles: testWatchFiles } =
-                await getTestModuleCode(filePath);
-            const { code: componentDef, watchFiles: componentWatchFiles } =
-                await getCompiledModule(suiteDir);
-            // You can add an `.only` file alongside an `index.spec.js` file to make it `fdescribe()`
-            const onlyFileExists = await exists(path.join(suiteDir, '.only'));
-
-            const ssrOutput = getSsrCode(componentDef, testCode);
-
-            watcher.watchSuite(filePath, testWatchFiles.concat(componentWatchFiles));
-            const newContent = format(
-                TEMPLATE,
-                JSON.stringify(ssrOutput),
-                componentDef,
-                testCode,
-                onlyFileExists ? 'fdescribe' : 'describe',
-                JSON.stringify(describeTitle)
-            );
-            done(null, newContent);
-        } catch (error: any) {
-            const location = path.relative(basePath, filePath);
-            log.error('Error processing “%s”\n\n%s\n', location, error.stack || error.message);
-
-            done(error, null);
-        }
-    };
-}
-
-createHCONFIG2JSPreprocessor.$inject = ['config', 'logger', 'emitter'];
-
-import type { Plugin } from 'vitest/config';
-
 export default function BrowserCommands(): Plugin {
-    const basePath = __dirname;
     return {
         name: 'vitest-plugin-lwc-hydrate',
-        async transform(code, id, _options) {
+        async load(id, _options) {
             if (id.endsWith('index.spec.js')) {
-                const suiteDir = path.dirname(id);
-                const filePath = path.join(suiteDir, 'index.spec.js');
-                // Wrap all the tests into a describe block with the file stricture name
-                const describeTitle = path.relative(basePath, suiteDir).split(path.sep).join(' ');
-
-                const { code: testCode } = await getTestModuleCode(filePath);
-
-                const { code: componentDef } = await getCompiledModule(suiteDir);
-
-                const ssrOutput = getSsrCode(componentDef, testCode);
-
-                const onlyFileExists = await exists(path.join(suiteDir, '.only'));
-                // watcher.watchSuite(filePath, testWatchFiles.concat(componentWatchFiles));
-
-                const newContent = format(
-                    TEMPLATE,
-                    JSON.stringify(ssrOutput),
-                    componentDef,
-                    testCode,
-                    onlyFileExists ? 'fdescribe' : 'describe',
-                    JSON.stringify(describeTitle)
-                );
-
-                return newContent;
+                return await loadTest(id);
             }
         },
     };
+}
+
+async function loadTest(id: string) {
+    const suiteDir = path.dirname(id);
+    const filePath = path.join(suiteDir, 'index.spec.js');
+    // Wrap all the tests into a describe block with the file stricture name
+    const describeTitle = path.relative(__dirname, suiteDir).split(path.sep).join(' ');
+
+    const [{ code: testCode }, { code: componentDef }] = await Promise.all([
+        getTestModuleCode(filePath),
+        getCompiledModule(suiteDir),
+    ]);
+
+    const ssrOutput = getSsrCode(componentDef, testCode);
+
+    const onlyFileExists = await exists(path.join(suiteDir, '.only'));
+    // watcher.watchSuite(filePath, testWatchFiles.concat(componentWatchFiles));
+    const newContent = util.format(
+        TEMPLATE,
+        JSON.stringify(ssrOutput),
+        componentDef,
+        testCode,
+        onlyFileExists ? 'describe.only' : 'describe',
+        JSON.stringify(describeTitle)
+    );
+
+    return newContent;
 }
