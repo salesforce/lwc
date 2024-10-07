33081fa7c9205f1b1c495968189c8e9edb72ed9a
Will Harney <wharney@salesforce.com>
2024-10-07 13:52:07 -0400

    fix(ssr): update error in for-of directive for invalid value

--- packages/@lwc/ssr-runtime/src/to-iterator-directive.ts
+++ packages/@lwc/ssr-runtime/src/to-iterator-directive.ts
@@ -9,10 +9,20 @@
  * Converts an iterable into one that emits the object used by the [`iterator` directive](
  * https://lwc.dev/guide/html_templates#iterator).
  */
-export function* toIteratorDirective<T>(
-    $it: Iterable<T>
-): Generator<{ value: T; index: number; first: boolean; last: boolean }> {
-    const iterator = $it[Symbol.iterator]();
+export function* toIteratorDirective<T>(iterable: Iterable<T>): Generator<{
+    value: T;
+    index: number;
+    first: boolean;
+    last: boolean;
+}> {
+    if (iterable === undefined || iterable === null) return;
+    if (!iterable[Symbol.iterator]) {
+        throw new Error(
+            // Mimic error message from "[i]terable node" in engine-core's api.ts
+            `Invalid template iteration for value \`${iterable}\`. It must be an array-like object.`
+        );
+    }
+    const iterator = iterable[Symbol.iterator]();
     let next = iterator.next();
     let index = 0;
     let { value, done: last = false } = next;
