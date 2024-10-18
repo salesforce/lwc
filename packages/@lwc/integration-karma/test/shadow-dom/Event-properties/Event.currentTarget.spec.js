e8d12e08a811cf8a51e33c55bdd6923b48af5884
Matheus Cardoso <matheus@cardo.so>
2024-10-18 15:11:42 -0300

    Update packages/@lwc/integration-karma/test/shadow-dom/Event-properties/Event.currentTarget.spec.js
    Co-authored-by: Nolan Lawson <nolan@nolanlawson.com>

--- packages/@lwc/integration-karma/test/shadow-dom/Event-properties/Event.currentTarget.spec.js
+++ packages/@lwc/integration-karma/test/shadow-dom/Event-properties/Event.currentTarget.spec.js
@@ -36,7 +36,7 @@ describe('Event.currentTarget', () => {
             child.dispatchEvent(new CustomEvent('test', { bubbles: true, composed: true }));
         });
 
-        expect(currentTarget).toEqual(container);
+        expect(currentTarget).ToBe(container);
     });
 
     it('should reference the shadow root', async () => {
