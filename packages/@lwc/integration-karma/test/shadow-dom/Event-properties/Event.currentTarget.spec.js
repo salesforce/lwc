ed9e4816b9e25a5e676d031d77788b5eb51edaae
Matheus Cardoso <matheus@cardo.so>
2024-10-18 09:51:06 -0300

    chore: asyncify polyfills/event-composed

--- packages/@lwc/integration-karma/test/shadow-dom/Event-properties/Event.currentTarget.spec.js
+++ packages/@lwc/integration-karma/test/shadow-dom/Event-properties/Event.currentTarget.spec.js
@@ -3,44 +3,55 @@ import { createElement } from 'lwc';
 import Container from 'x/container';
 
 describe('Event.currentTarget', () => {
-    it('should be null when accessed asynchronously', function (done) {
+    it('should be null when accessed asynchronously', async () => {
         const container = createElement('x-container', { is: Container });
         document.body.appendChild(container);
 
-        container.addEventListener('test', (event) => {
-            expect(event.currentTarget).toEqual(container);
-            setTimeout(() => {
-                expect(event.currentTarget).toBeNull();
-                done();
+        const [first, second] = await new Promise((resolve) => {
+            container.addEventListener('test', (event) => {
+                const first = event.currentTarget;
+                setTimeout(() => {
+                    const second = event.currentTarget;
+                    resolve([first, second]);
+                });
             });
+            const div = container.shadowRoot.querySelector('div');
+            div.dispatchEvent(new CustomEvent('test', { bubbles: true, composed: true }));
         });
-        const div = container.shadowRoot.querySelector('div');
-        div.dispatchEvent(new CustomEvent('test', { bubbles: true, composed: true }));
+
+        expect(first).toEqual(container);
+        expect(second).toBeNull();
     });
 
-    it('should reference the host element', (done) => {
+    it('should reference the host element', async () => {
         const container = createElement('x-container', { is: Container });
         document.body.appendChild(container);
 
-        container.addEventListener('test', (event) => {
-            expect(event.currentTarget).toEqual(container);
-            done();
+        const currentTarget = await new Promise((resolve) => {
+            container.addEventListener('test', (event) => {
+                resolve(event.currentTarget);
+            });
+
+            const child = container.shadowRoot.querySelector('x-child');
+            child.dispatchEvent(new CustomEvent('test', { bubbles: true, composed: true }));
         });
 
-        const child = container.shadowRoot.querySelector('x-child');
-        child.dispatchEvent(new CustomEvent('test', { bubbles: true, composed: true }));
+        expect(currentTarget).toEqual(container);
     });
 
-    it('should reference the shadow root', (done) => {
+    it('should reference the shadow root', async () => {
         const container = createElement('x-container', { is: Container });
         document.body.appendChild(container);
 
-        container.shadowRoot.addEventListener('test', (event) => {
-            expect(event.currentTarget).toEqual(container.shadowRoot);
-            done();
+        const currentTarget = await new Promise((resolve) => {
+            container.shadowRoot.addEventListener('test', (event) => {
+                resolve(event.currentTarget);
+            });
+
+            const child = container.shadowRoot.querySelector('x-child');
+            child.dispatchEvent(new CustomEvent('test', { bubbles: true, composed: true }));
         });
 
-        const child = container.shadowRoot.querySelector('x-child');
-        child.dispatchEvent(new CustomEvent('test', { bubbles: true, composed: true }));
+        expect(currentTarget).toEqual(container.shadowRoot);
     });
 });
