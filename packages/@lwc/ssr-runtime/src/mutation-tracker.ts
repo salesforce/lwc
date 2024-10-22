90d9fc978f9ec0e170a8f6e4484a7af99a13bfd2
Eugene Kashida <ekashida@gmail.com>
2024-10-21 18:30:37 -0700

    feat: track mutations only when enabled

--- packages/@lwc/ssr-runtime/src/mutation-tracker.ts
+++ packages/@lwc/ssr-runtime/src/mutation-tracker.ts
@@ -8,15 +8,28 @@
 import { LightningElement } from './lightning-element';
 
 export class MutationTracker {
+    #enabledSet = new WeakSet<LightningElement>();
     mutationMap = new WeakMap<LightningElement, Set<string>>();
+
     add(instance: LightningElement, attrName: string): void {
-        let mutatedAttrs = this.mutationMap.get(instance);
-        if (!mutatedAttrs) {
-            mutatedAttrs = new Set();
-            this.mutationMap.set(instance, mutatedAttrs);
+        if (this.#enabledSet.has(instance)) {
+            let mutatedAttrs = this.mutationMap.get(instance);
+            if (!mutatedAttrs) {
+                mutatedAttrs = new Set();
+                this.mutationMap.set(instance, mutatedAttrs);
+            }
+            mutatedAttrs.add(attrName.toLowerCase());
         }
-        mutatedAttrs.add(attrName.toLowerCase());
     }
+
+    enable(instance: LightningElement) {
+        this.#enabledSet.add(instance);
+    }
+
+    disable(instance: LightningElement) {
+        this.#enabledSet.delete(instance);
+    }
+
     renderMutatedAttrs(instance: LightningElement): string {
         const mutatedAttrs = this.mutationMap.get(instance);
         if (mutatedAttrs) {
