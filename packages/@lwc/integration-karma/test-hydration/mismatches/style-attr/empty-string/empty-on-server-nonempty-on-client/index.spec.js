3fca2a9ecaed8e931f78ff5de5f637d6f0dbbd78
Nolan Lawson <nlawson@salesforce.com>
2024-10-24 16:00:39 -0700

    test(hydration): add tests for empty string mismatch

---
+++ packages/@lwc/integration-karma/test-hydration/mismatches/style-attr/empty-string/empty-on-server-nonempty-on-client/index.spec.js
@@ -0,0 +1,27 @@
+export default {
+    props: {
+        styles: '',
+    },
+    clientProps: {
+        styles: 'color: burlywood;',
+    },
+    snapshot(target) {
+        const p = target.shadowRoot.querySelector('p');
+        return {
+            p,
+            styles: p.className,
+        };
+    },
+    test(target, snapshots, consoleCalls) {
+        const p = target.shadowRoot.querySelector('p');
+        expect(p).not.toBe(snapshots.p);
+        expect(p.getAttribute('style')).toBe('color: burlywood;');
+
+        TestUtils.expectConsoleCallsDev(consoleCalls, {
+            error: [
+                'Mismatch hydrating element <p>: attribute "style" has different values, expected "color: burlywood;" but found ""',
+                'Hydration completed with errors.',
+            ],
+        });
+    },
+};
