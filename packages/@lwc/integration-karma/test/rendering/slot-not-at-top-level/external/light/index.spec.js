5efdfd1452439185d1d9ef619bf9aaaaf6790eb7
Nolan Lawson <nlawson@salesforce.com>
2024-10-24 11:18:28 -0700

    chore: fix old api versions

--- packages/@lwc/integration-karma/test/rendering/slot-not-at-top-level/external/light/index.spec.js
+++ packages/@lwc/integration-karma/test/rendering/slot-not-at-top-level/external/light/index.spec.js
@@ -1,5 +1,5 @@
 import { createElement } from 'lwc';
-import { expectEquivalentDOM } from 'test-utils';
+import { expectEquivalentDOM, USE_COMMENTS_FOR_FRAGMENT_BOOKENDS } from 'test-utils';
 import Outer from 'x/outer';
 
 beforeAll(() => {
@@ -7,14 +7,18 @@ beforeAll(() => {
 });
 
 // `expectEquivalentDOM` requires `Document.parseHTMLUnsafe`
-it.runIf(Document.parseHTMLUnsafe)('renders slots not at the top level', async () => {
-    const elm = createElement('x-outer', { is: Outer });
-    document.body.appendChild(elm);
+// USE_COMMENTS_FOR_FRAGMENT_BOOKENDS is needed because otherwise we end up with adjacent text nodes which parse as one
+it.runIf(Document.parseHTMLUnsafe && USE_COMMENTS_FOR_FRAGMENT_BOOKENDS)(
+    'renders slots not at the top level',
+    async () => {
+        const elm = createElement('x-outer', { is: Outer });
+        document.body.appendChild(elm);
 
-    await Promise.resolve();
+        await Promise.resolve();
 
-    expectEquivalentDOM(
-        elm,
-        `<x-outer><x-inner>a<!----><x-external-light><div slot="foo">I am the foo slot</div></x-external-light><x-external-light><div slot="foo">I am also the foo slot</div></x-external-light><!---->b<!---->fallback for foo<!---->c</x-inner></x-outer>`
-    );
-});
+        expectEquivalentDOM(
+            elm,
+            `<x-outer><x-inner>a<!----><x-external-light><div slot="foo">I am the foo slot</div></x-external-light><x-external-light><div slot="foo">I am also the foo slot</div></x-external-light><!---->b<!---->fallback for foo<!---->c</x-inner></x-outer>`
+        );
+    }
+);
--- packages/@lwc/integration-karma/test/rendering/slot-not-at-top-level/ifTrue/light/index.spec.js
+++ packages/@lwc/integration-karma/test/rendering/slot-not-at-top-level/ifTrue/light/index.spec.js
@@ -1,16 +1,20 @@
 import { createElement } from 'lwc';
-import { expectEquivalentDOM } from 'test-utils';
+import { expectEquivalentDOM, USE_COMMENTS_FOR_FRAGMENT_BOOKENDS } from 'test-utils';
 import Outer from 'x/outer';
 
 // `expectEquivalentDOM` requires `Document.parseHTMLUnsafe`
-it.runIf(Document.parseHTMLUnsafe)('renders slots not at the top level', async () => {
-    const elm = createElement('x-outer', { is: Outer });
-    document.body.appendChild(elm);
+// USE_COMMENTS_FOR_FRAGMENT_BOOKENDS is needed because otherwise we end up with adjacent text nodes which parse as one
+it.runIf(Document.parseHTMLUnsafe && USE_COMMENTS_FOR_FRAGMENT_BOOKENDS)(
+    'renders slots not at the top level',
+    async () => {
+        const elm = createElement('x-outer', { is: Outer });
+        document.body.appendChild(elm);
 
-    await Promise.resolve();
+        await Promise.resolve();
 
-    expectEquivalentDOM(
-        elm,
-        '<x-outer><x-inner>a<!---->fallback for default<!---->b<!----><div>I am the foo slot</div><div>I am also the foo slot</div><!---->c</x-inner></x-outer>'
-    );
-});
+        expectEquivalentDOM(
+            elm,
+            '<x-outer><x-inner>a<!---->fallback for default<!---->b<!----><div>I am the foo slot</div><div>I am also the foo slot</div><!---->c</x-inner></x-outer>'
+        );
+    }
+);
