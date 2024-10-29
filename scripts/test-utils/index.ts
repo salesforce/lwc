a8ea86915d3241dd7ddf205287d3ad49cfb6b89f
Matheus Cardoso <matheus@cardo.so>
2024-10-29 14:13:49 -0300

    chore(tests): use built-in snapshot matcher for fixtures (#4710)
    Co-authored-by: Nolan Lawson <nlawson@salesforce.com>

--- scripts/test-utils/index.ts
+++ scripts/test-utils/index.ts
@@ -4,6 +4,5 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-import './matchers'; // File has no exports, but registers vitest matchers
 export * from './test-fixture-dir';
 export * from './format-html';
