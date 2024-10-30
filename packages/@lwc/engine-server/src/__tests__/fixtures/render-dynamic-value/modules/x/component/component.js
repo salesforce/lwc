211897e84ebeaf428e7d393e86ef1016663ed70a
Nolan Lawson <nlawson@salesforce.com>
2024-10-30 09:34:56 -0700

    test(ssr): add tests for type coercion in rendering (#4730)
    Co-authored-by: Will Harney <62956339+wjhsf@users.noreply.github.com>

---
+++ packages/@lwc/engine-server/src/__tests__/fixtures/render-dynamic-value/modules/x/component/component.js
@@ -0,0 +1,22 @@
+import { LightningElement } from 'lwc';
+
+export default class extends LightningElement {
+  isUndefined = undefined;
+  isNull = null;
+  isFalse = false;
+  isTrue = true;
+  isZero = 0;
+  isNegZero = -0;
+  isNaN = NaN;
+  isInfinity = Infinity;
+  isNegInfinity = -Infinity;
+  isEmptyString = '';
+  isSymbol = Symbol('yolo')
+  isArray = ['foo', 'bar']
+  isObject = {foo: 'bar', baz: 'quux'}
+  isNullObject = Object.create(null)
+  isTrickyArray = [Object.create(null)]
+  isFunction = function whoWouldDoThis() { return 'somebody, probably!' }
+  isString = 'ooo eee, ooo ah ah, ting tang, walla walla, bing bang'
+  isNumber = 6.02214076e23
+}
