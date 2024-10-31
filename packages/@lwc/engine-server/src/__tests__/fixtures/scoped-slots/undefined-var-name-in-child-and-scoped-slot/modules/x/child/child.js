8bf3395e9c97ea54f4520ca280ced47289afd1c5
Nolan Lawson <nlawson@salesforce.com>
2024-10-31 11:13:03 -0700

    test: moar tests!!!

--- packages/@lwc/engine-server/src/__tests__/fixtures/scoped-slots/undefined-var-name-in-child-and-scoped-slot/modules/x/child/child.js
+++ packages/@lwc/engine-server/src/__tests__/fixtures/scoped-slots/undefined-var-name-in-child-and-scoped-slot/modules/x/child/child.js
@@ -2,6 +2,5 @@ import { LightningElement } from 'lwc';
 
 export default class Child extends LightningElement {
     static renderMode = 'light';
-    data = { id: 'from-child' }
     bound = { id: 'prop' }
 }
