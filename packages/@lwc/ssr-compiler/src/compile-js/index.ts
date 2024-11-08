9ce49491fdcfcd1c2496f29ac138ed9c12cba8c1
Matheus Cardoso <matheus@cardo.so>
2024-11-07 22:02:11 -0300

    fix(ssr-compiler): conflicting type definitions for estree

--- packages/@lwc/ssr-compiler/src/compile-js/index.ts
+++ packages/@lwc/ssr-compiler/src/compile-js/index.ts
@@ -57,7 +57,7 @@ const visitors: Visitors = {
         }
 
         // TODO [#4773]: Why do we get conflicting PropertyDefinition types when removing "vitest/globals"?
-        const decorators = (node as any).decorators;
+        const decorators = node.decorators;
         if (is.identifier(decorators[0]?.expression) && decorators[0].expression.name === 'api') {
             state.publicFields.push(node.key.name);
         } else {
