555bb8ee7f018d45160bd2db57003305b94ff978
Nolan Lawson <nlawson@salesforce.com>
2024-11-08 13:31:39 -0800

    fix: header

--- packages/@lwc/ssr-compiler/src/__tests__/utils/expected-failures.ts
+++ packages/@lwc/ssr-compiler/src/__tests__/utils/expected-failures.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2024, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+
 // We should slowly drive down these test failures or at least document where we expect the failures
 // TODO [#4815]: enable all SSR v2 tests
 export const expectedFailures = new Set([
