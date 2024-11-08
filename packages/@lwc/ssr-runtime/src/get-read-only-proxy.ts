d7bbdeee4d0a6d45552086bf5e4c0b0cb331935b
Nolan Lawson <nlawson@salesforce.com>
2024-11-08 12:15:49 -0800

    Update packages/@lwc/ssr-runtime/src/get-read-only-proxy.ts
    Co-authored-by: Will Harney <62956339+wjhsf@users.noreply.github.com>

--- packages/@lwc/ssr-runtime/src/get-read-only-proxy.ts
+++ packages/@lwc/ssr-runtime/src/get-read-only-proxy.ts
@@ -12,6 +12,6 @@ const reactiveMembrane = new ObservableMembrane();
 // Modeled after `getReadOnlyProxy` in `membrane.ts` in `engine-core`
 // Return a proxy over the given object so that access is immutable
 // https://github.com/salesforce/lwc/blob/e9db491/packages/%40lwc/engine-core/src/framework/membrane.ts#L29-L33
-export function getReadOnlyProxy(value: any) {
+export function getReadOnlyProxy<T>(value: T): Readonly<T> {
     return reactiveMembrane.getReadOnlyProxy(value);
 }
