a6be24f9a7667de96d0fbfde58c7414b4d0440bc
Dale Bustad <dbustad@salesforce.com>
2024-11-11 13:01:50 -0800

    feat: track contextful relationships
    feat: implement wire simple wire context for SSR
    
    fix: adjacent empty text node when prop is undefined
    
    feat: throw error when attempt to install context on non-LightningElement
    
    feat: get context working with slotted content
    
    feat: call wire.update with undefined value immediately upon instantiation
    
    refactor: move wire-related things into separate source module
    
    feat: support decorated methods & setters
    
    fix: ast types with/without decorators
    
    Update packages/@lwc/ssr-compiler/src/compile-js/generate-markup.ts
    
    feat: recompute wire config when updates occur
    
    refactor: use existing isValidIdentifier helper function
    
    refactor: add helpful comments in template

--- packages/@lwc/engine-server/src/__tests__/fixtures/context-simple-setter/modules/x/consumer/consumer.js
+++ packages/@lwc/engine-server/src/__tests__/fixtures/context-simple-setter/modules/x/consumer/consumer.js
@@ -2,5 +2,13 @@ import { LightningElement, wire } from 'lwc';
 import { WireAdapter } from '../../../wire-adapter';
 
 export default class ConsumerComponent extends LightningElement {
-  @wire(WireAdapter, { unused: '$definitelyUnused' }) foo;
+  _foo;
+
+  @wire(WireAdapter, { unused: '$definitelyUnused' })
+  set foo(newFoo) {
+    this._foo = newFoo;
+  }
+  get foo() {
+    return this._foo
+  };
 }
--- packages/@lwc/ssr-compiler/src/shared.ts
+++ packages/@lwc/ssr-compiler/src/shared.ts
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-
-import { Config as TemplateCompilerConfig } from '@lwc/template-compiler';
+import { reservedKeywords } from '@lwc/shared';
+import type { Config as TemplateCompilerConfig } from '@lwc/template-compiler';
 
 export type Expression = string;
 
@@ -62,3 +62,13 @@ export type TransformOptions = Pick<TemplateCompilerConfig, 'name' | 'namespace'
 
 /* SSR compilation mode. `async` refers to async functions, `sync` to sync functions, and `asyncYield` to async generator functions. */
 export type CompilationMode = 'asyncYield' | 'async' | 'sync';
+
+// This is a mostly-correct regular expression will only match if the entire string
+// provided is a valid ECMAScript identifier. Its imperfections lie in the fact that
+// it will match strings like "export" when "export" is actually a reserved keyword
+// and therefore not a valid identifier. When combined with a check against reserved
+// keywords, it is a reliable test for whether a provided string is a valid identifier.
+const imperfectIdentifierMatcher = /^[_$a-zA-Z\xA0-\uFFFF][_$a-zA-Z0-9\xA0-\uFFFF]*$/;
+
+export const isValidIdentifier = (str: string) =>
+    !reservedKeywords.has(str) && imperfectIdentifierMatcher.test(str);
