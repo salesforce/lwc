d47c56c7fbd6b4f891d3398109a8b975dab897b7
Nolan Lawson <nlawson@salesforce.com>
2024-11-12 09:50:14 -0800

    fix(ssr): infer the SSR mode in `renderComponent` (#4820)

--- packages/@lwc/perf-benchmarks/src/__benchmarks__/ssr/table/table-render-10k.benchmark.js
+++ packages/@lwc/perf-benchmarks/src/__benchmarks__/ssr/table/table-render-10k.benchmark.js
@@ -10,20 +10,13 @@ import { renderComponent } from '@lwc/ssr-runtime';
 import Table from '@lwc/perf-benchmarks-components/dist/ssr/benchmark/table/table.js';
 import Store from '@lwc/perf-benchmarks-components/dist/ssr/benchmark/store/store.js';
 
-const SSR_MODE = 'asyncYield';
-
 benchmark(`ssr/table-v2/render/10k`, () => {
     run(() => {
         const store = new Store();
         store.runLots();
 
-        return renderComponent(
-            'benchmark-table',
-            Table,
-            {
-                rows: store.data,
-            },
-            SSR_MODE
-        );
+        return renderComponent('benchmark-table', Table, {
+            rows: store.data,
+        });
     });
 });
