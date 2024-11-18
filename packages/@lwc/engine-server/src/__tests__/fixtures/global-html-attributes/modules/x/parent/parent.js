4840beaf8f5a9372bb390925b4f47e67faa843fe
Eugene Kashida <ekashida@gmail.com>
2024-11-18 09:45:42 -0800

    chore: supported global html attributes

--- packages/@lwc/engine-server/src/__tests__/fixtures/global-html-attributes/modules/x/parent/parent.js
+++ packages/@lwc/engine-server/src/__tests__/fixtures/global-html-attributes/modules/x/parent/parent.js
@@ -1,17 +1,40 @@
 import { LightningElement } from 'lwc';
 
 export default class extends LightningElement {
-  value = {
-    accesskey: "tata",
-    arialabel: "titi",
-    dir: "auto",
-    draggable: false,
-    hidden: false,
-    id: "tutu",
-    lang: "jp",
-    role: "scrollbar",
-    spellcheck: false,
-    tabindex: "0",
-    title: "tete",
+  supported = {
+    accesskey: "supported",
+    arialabel: "supported",
+    class: "supported",
+    dir: "supported",
+    draggable: "supported",
+    hidden: "supported",
+    id: "supported",
+    lang: "supported",
+    role: "supported",
+    spellcheck: "supported",
+    tabindex: "supported",
+    title: "supported",
+  };
+
+  unsupported = {
+    autocapitalize: "unsupported",
+    autofocus: "unsupported",
+    contenteditable: "unsupported",
+    enterkeyhint: "unsupported",
+    exportparts: "unsupported",
+    inert: "unsupported",
+    inputmode: "unsupported",
+    is: "unsupported",
+    itemid: "unsupported",
+    itemprop: "unsupported",
+    itemref: "unsupported",
+    itemscope: "unsupported",
+    itemtype: "unsupported",
+    nonce: "unsupported",
+    part: "unsupported",
+    popover: "unsupported",
+    slot: "unsupported",
+    translate: "unsupported",
+    writingsuggestions: "unsupported",
   };
 }
