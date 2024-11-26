1756d7995c7d4769563e451c7d27818f5e2a4048
Matheus Cardoso <matheus@cardo.so>
2024-11-26 19:18:58 -0300

    chore: tidy up

--- packages/@lwc/ssr-compiler/src/compile-template/transformers/text.ts
+++ packages/@lwc/ssr-compiler/src/compile-template/transformers/text.ts
@@ -15,6 +15,7 @@ import type {
     Expression as IrExpression,
     Literal as IrLiteral,
     Text as IrText,
+    Node as IrNode,
 } from '@lwc/template-compiler';
 import type { Transformer } from '../types';
 
@@ -44,13 +45,19 @@ export const Text: Transformer<IrText> = function Text(node, cxt): EsStatement[]
         return [bYield(b.literal(node.value.value))];
     }
 
+    const shouldIsolate = (node?: IrNode) => {
+        switch (node?.type) {
+            case 'Text':
+                return false;
+            case 'Comment':
+                return cxt.templateOptions.preserveComments;
+            default:
+                return true;
+        }
+    };
+
     const isIsolatedTextNode = b.literal(
-        (!cxt.prevSibling ||
-            (cxt.prevSibling.type !== 'Text' &&
-                (cxt.templateOptions.preserveComments || cxt.prevSibling.type !== 'Comment'))) &&
-            (!cxt.nextSibling ||
-                (cxt.nextSibling.type !== 'Text' &&
-                    (cxt.templateOptions.preserveComments || cxt.nextSibling.type !== 'Comment')))
+        shouldIsolate(cxt.prevSibling) && shouldIsolate(cxt.nextSibling)
     );
 
     const valueToYield = expressionIrToEs(node.value, cxt);
