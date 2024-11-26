fa8a4b06f0f4d77e9f53da7f31ab1e4e7fa4792d
Nolan Lawson <nlawson@salesforce.com>
2024-11-25 17:10:55 -0800

    chore(perf-benchmarks): avoid `yarn --immutable`

--- packages/@lwc/perf-benchmarks/scripts/build.js
+++ packages/@lwc/perf-benchmarks/scripts/build.js
@@ -114,7 +114,7 @@ function createTachometerJson(htmlFilename, benchmarkName, directoryHash, cpuThr
                                             // `@lwc/perf-benchmarks-components` itself.
                                             'rm -fr ./packages/@lwc/perf-benchmarks-components/{src,scripts}',
                                             `cp -R ${benchmarkComponentsDir}/{src,scripts} ./packages/@lwc/perf-benchmarks-components`,
-                                            'yarn --immutable',
+                                            'yarn --frozen-lockfile',
                                             // bust the Tachometer cache in case these files change locally
                                             `echo '${directoryHash}'`,
                                             'yarn build:performance:components',
