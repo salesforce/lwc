7fa4ad20ef70f118f5fbc6ef9b6ef17da212c80c
John Hefferman <jhefferman@salesforce.com>
2024-11-26 12:59:34 -0700

    chore: align export ssr-compiler isValidIdentifier with babel-plugin-component impl

--- packages/@lwc/ssr-compiler/src/shared.ts
+++ packages/@lwc/ssr-compiler/src/shared.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-import { reservedKeywords } from '@lwc/shared';
+import { isValidES3Identifier } from '@babel/types';
 import type { Config as TemplateCompilerConfig } from '@lwc/template-compiler';
 
 export type Expression = string;
@@ -60,12 +60,4 @@ export interface IHoistInstantiation {
 
 export type TransformOptions = Pick<TemplateCompilerConfig, 'name' | 'namespace'>;
 
-// This is a mostly-correct regular expression will only match if the entire string
-// provided is a valid ECMAScript identifier. Its imperfections lie in the fact that
-// it will match strings like "export" when "export" is actually a reserved keyword
-// and therefore not a valid identifier. When combined with a check against reserved
-// keywords, it is a reliable test for whether a provided string is a valid identifier.
-const imperfectIdentifierMatcher = /^[_$a-zA-Z\xA0-\uFFFF][_$a-zA-Z0-9\xA0-\uFFFF]*$/;
-
-export const isValidIdentifier = (str: string) =>
-    !reservedKeywords.has(str) && imperfectIdentifierMatcher.test(str);
+export const isValidIdentifier = (str: string) => isValidES3Identifier(str);
