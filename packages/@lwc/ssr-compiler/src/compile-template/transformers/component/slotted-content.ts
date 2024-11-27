15297aa55a4d5a37151491c744048544cb74898e
Nolan Lawson <nlawson@salesforce.com>
2024-11-27 09:56:18 -0800

    fix(ssr): non-top-level light slots with `if:true` (#4967)
    Co-authored-by: Will Harney <62956339+wjhsf@users.noreply.github.com>

--- packages/@lwc/ssr-compiler/src/compile-template/transformers/component/slotted-content.ts
+++ packages/@lwc/ssr-compiler/src/compile-template/transformers/component/slotted-content.ts
@@ -13,11 +13,22 @@ import { irChildrenToEs, irToEs } from '../../ir-to-es';
 import { isNullableOf } from '../../../estree/validators';
 import type { CallExpression as EsCallExpression, Expression as EsExpression } from 'estree';
 
-import type { Statement as EsStatement } from 'estree';
 import type {
+    Statement as EsStatement,
+    ExpressionStatement as EsExpressionStatement,
+} from 'estree';
+import type {
+    ChildNode as IrChildNode,
     Component as IrComponent,
+    Element as IrElement,
+    ElseBlock as IrElseBlock,
+    ElseifBlock as IrElseifBlock,
+    ExternalComponent as IrExternalComponent,
+    If as IrIf,
+    IfBlock as IrIfBlock,
     LwcComponent as IrLwcComponent,
-    ScopedSlotFragment,
+    ScopedSlotFragment as IrScopedSlotFragment,
+    Text as IrText,
 } from '@lwc/template-compiler';
 import type { TransformerContext } from '../../types';
 
@@ -62,6 +73,94 @@ const bAddLightContent = esTemplate`
     });
 `<EsCallExpression>;
 
+// Light DOM slots are a bit complex because of needing to handle slots _not_ at the top level
+// At the non-top level, it matters what the ancestors are. These are relevant to slots:
+// - If (`if:true`, `if:false`)
+// - IfBlock/ElseBlock/ElseifBlock (`lwc:if`, `lwc:elseif`, `lwc:else`)
+// Whereas anything else breaks the relationship between the slotted content and the containing
+// Component (e.g. another Component/ExternalComponent) or is disallowed (e.g. ForEach/ForOf).
+// Then there are the leaf nodes, which _may_ have a `slot` attribute on them:
+// - Element/Text/Component/ExternalComponent (e.g. `<div>`, `<x-foo>`)
+// Once you reach a leaf, you know what content should be rendered for a given slot name. But you
+// also need to consider all of its ancestors, which may cause the slot content to be conditionally
+// rendered (e.g. IfBlock/ElseBlock).
+// For example:
+//     <x-foo>
+//         <template lwc:if={darkTheme}>
+//             <div slot="footer"></div>
+//         </template>
+//         <template lwc:else>
+//             yolo
+//         </template>
+//     </x-foo>
+// In this example, we render the `<div>` into the `footer` slot, if `darkTheme` is true.
+// Otherwise, we will render the text node `yolo` into the default slot.
+// The goal here is to traverse through the tree and identify all unique `slot` attribute names
+// and group those into AST trees on a per-`slot` name basis, only for leafs/ancestors that are
+// relevant to slots (as mentioned above).
+function getLightSlottedContent(rootNodes: IrChildNode[], cxt: TransformerContext) {
+    type SlottableAncestorIrType = IrIf | IrIfBlock | IrElseifBlock | IrElseBlock;
+    type SlottableLeafIrType = IrElement | IrText | IrComponent | IrExternalComponent;
+
+    const results: EsExpressionStatement[] = [];
+
+    // For the given slot name, get the EsExpressions we should use to render it
+    // The ancestorIndices is an array of integers referring to the chain of ancestors
+    // and their positions in the child arrays of their own parents
+    const addLightDomSlotContent = (slotName: EsExpression, ancestorIndices: number[]) => {
+        const clone = produce(rootNodes[ancestorIndices[0]], (draft) => {
+            // Create a clone of the AST with only the ancestors and no other siblings
+            let current = draft;
+            for (let i = 1; i < ancestorIndices.length; i++) {
+                const nextIndex = ancestorIndices[i];
+
+                // If i >= 1 then the current must necessarily be a SlottableAncestorIrType
+                const next = (current as SlottableAncestorIrType).children[nextIndex];
+                (current as SlottableAncestorIrType).children = [next];
+                current = next;
+            }
+            // The leaf must necessarily be a SlottableLeafIrType
+            const leaf = current as SlottableLeafIrType;
+            // Light DOM slots do not actually render the `slot` attribute.
+            if (leaf.type !== 'Text') {
+                leaf.attributes = leaf.attributes.filter((attr) => attr.name !== 'slot');
+            }
+        });
+        const slotContent = irToEs(clone, cxt);
+        results.push(b.expressionStatement(bAddLightContent(slotName, null, slotContent)));
+    };
+
+    const traverse = (nodes: IrChildNode[], ancestorIndices: number[]) => {
+        for (let i = 0; i < nodes.length; i++) {
+            const node = nodes[i];
+            switch (node.type) {
+                // SlottableAncestorIrType
+                case 'If':
+                case 'IfBlock':
+                case 'ElseifBlock':
+                case 'ElseBlock': {
+                    traverse(node.children, [...ancestorIndices, i]);
+                    break;
+                }
+                // SlottableLeafIrType
+                case 'Element':
+                case 'Text':
+                case 'Component':
+                case 'ExternalComponent': {
+                    // '' is the default slot name. Text nodes are always slotted into the default slot
+                    const slotName =
+                        node.type === 'Text' ? b.literal('') : bAttributeValue(node, 'slot');
+                    addLightDomSlotContent(slotName, [...ancestorIndices, i]);
+                    break;
+                }
+            }
+        }
+    };
+
+    traverse(rootNodes, []);
+    return results;
+}
+
 export function getSlottedContent(
     node: IrLwcComponent | IrComponent,
     cxt: TransformerContext
@@ -70,23 +169,11 @@ export function getSlottedContent(
     const slottableChildren = node.children.filter((child) => child.type !== 'ScopedSlotFragment');
     const scopedSlottableChildren = node.children.filter(
         (child) => child.type === 'ScopedSlotFragment'
-    ) as ScopedSlotFragment[];
+    ) as IrScopedSlotFragment[];
 
     const shadowSlotContent = optimizeAdjacentYieldStmts(irChildrenToEs(slottableChildren, cxt));
 
-    const lightSlotContent = slottableChildren.map((child) => {
-        if ('attributes' in child) {
-            const slotName = bAttributeValue(child, 'slot');
-            // Light DOM slots do not actually render the `slot` attribute.
-            const clone = produce(child, (draft) => {
-                draft.attributes = draft.attributes.filter((attr) => attr.name !== 'slot');
-            });
-            const slotContent = irToEs(clone, cxt);
-            return b.expressionStatement(bAddLightContent(slotName, null, slotContent));
-        } else {
-            return b.expressionStatement(bAddLightContent(b.literal(''), null, irToEs(child, cxt)));
-        }
-    });
+    const lightSlotContent = getLightSlottedContent(slottableChildren, cxt);
 
     const scopedSlotContent = scopedSlottableChildren.map((child) => {
         const boundVariableName = child.slotData.value.name;
