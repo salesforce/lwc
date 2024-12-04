6ea8218cf8ba13627e7890613255506371e9c134
John Hefferman <jhefferman@salesforce.com>
2024-12-03 17:15:01 -0700

    fix: properly handle scoped class for void and foreign elements

--- packages/@lwc/engine-server/src/__tests__/fixtures/render-correct-closing-tags/modules/x/elements/elements.js
+++
@@ -1,6 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class extends LightningElement {
-    @api innerHtml =
-        '<pattern><image xlink:href="https://www.salesforce.com/"></image></pattern><image xlink:title="title"></image>';
-}
