87eb3c51382696b1b5a04e44fcef7a71bc28739c
John Hefferman <jhefferman@salesforce.com>
2024-12-10 17:57:25 -0700

    fix: barrel import test parity

--- packages/@lwc/engine-server/src/__tests__/fixtures/barrel-export-lwc-all/modules/x/cmp/cmp.js
+++ packages/@lwc/engine-server/src/__tests__/fixtures/barrel-export-lwc-all/modules/x/cmp/cmp.js
@@ -1,4 +1,31 @@
 // Everything else can be imported in a helper, so we must check in the helper
-import { LightningElement } from '../../../barrel.js';
+import {
+    LightningElement,
+    getComponentDef,
+    isComponentConstructor,
+    createContextProvider,
+    readonly,
+    setFeatureFlagForTest,
+    unwrap,
+    createElement,
+    renderComponent,
+} from '../../../barrel.js';
+
+// "Using" the imports so they don't get removed by the compiler
+console.log(
+    LightningElement,
+    // The LWC compiler doesn't let us use decorators like this, so we don't need to check them here
+    // api,
+    // track,
+    // wire,
+    getComponentDef,
+    isComponentConstructor,
+    createContextProvider,
+    readonly,
+    setFeatureFlagForTest,
+    unwrap,
+    createElement,
+    renderComponent
+);
 
 export default class extends LightningElement {}
