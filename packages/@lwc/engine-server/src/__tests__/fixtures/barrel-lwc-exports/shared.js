379453554ff20ba4bbf1b5d0234a19779103c599
John Hefferman <jhefferman@salesforce.com>
2024-12-11 17:13:23 -0700

    chore: explain function name massaging in test

--- packages/@lwc/engine-server/src/__tests__/fixtures/barrel-lwc-exports/shared.js
+++ packages/@lwc/engine-server/src/__tests__/fixtures/barrel-lwc-exports/shared.js
@@ -4,6 +4,7 @@ export function importsToString(imports) {
             i
                 .toString()
                 .split('(')[0]
+                // renderComponent is aliased here: https://github.com/salesforce/lwc/blob/5d01843a7733a03b9ccb59a70ad64af955f15b88/packages/%40lwc/ssr-runtime/src/index.ts#L31
                 .replace('async function serverSideRenderComponent', 'function renderComponent')
         )
         .join('\n      ');
