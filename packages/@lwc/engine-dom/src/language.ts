92cb5672ccee5258504366a49e2c07827010e28e
Will Harney <wharney@salesforce.com>
2024-12-20 12:01:56 -0500

    chore: 🛩️📦

--- packages/@lwc/engine-dom/src/language.ts
+++ packages/@lwc/engine-dom/src/language.ts
@@ -5,9 +5,11 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
 
+import { getOwnPropertyDescriptors } from '@lwc/shared';
+
 // Like @lwc/shared, but for DOM APIs
 
-export const ElementDescriptors = Object.getOwnPropertyDescriptors(Element.prototype);
+export const ElementDescriptors = getOwnPropertyDescriptors(Element.prototype);
 
 export const ElementAttachShadow = ElementDescriptors.attachShadow.value!;
 export const ElementShadowRootGetter = ElementDescriptors.shadowRoot.get!;
