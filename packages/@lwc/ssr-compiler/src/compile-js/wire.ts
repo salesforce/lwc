cf1a1b222c485f9dcc235c9ef52aae8bbc67f3b8
Matheus Cardoso <matheus@cardo.so>
2025-01-08 17:30:33 -0300

    feat(ssr): compiler error location

--- packages/@lwc/ssr-compiler/src/compile-js/wire.ts
+++ packages/@lwc/ssr-compiler/src/compile-js/wire.ts
@@ -44,7 +44,7 @@ function getWireParams(
     const { decorators } = node;
 
     if (decorators.length > 1) {
-        throw generateError(DecoratorErrors.ONE_WIRE_DECORATOR_ALLOWED);
+        throw generateError(node, DecoratorErrors.ONE_WIRE_DECORATOR_ALLOWED);
     }
 
     // validate the parameters
@@ -94,7 +94,10 @@ function validateWireId(
 
     // This is not the exact same validation done in @lwc/babel-plugin-component but it accomplishes the same thing
     if (path.scope?.getBinding(wireAdapterVar)?.kind !== 'module') {
-        throw generateError(DecoratorErrors.COMPUTED_PROPERTY_MUST_BE_CONSTANT_OR_LITERAL);
+        throw generateError(
+            path.node!,
+            DecoratorErrors.COMPUTED_PROPERTY_MUST_BE_CONSTANT_OR_LITERAL
+        );
     }
 }
 
@@ -129,9 +132,15 @@ function validateWireConfig(
                 continue;
             }
         } else if (is.templateLiteral(key)) {
-            throw generateError(DecoratorErrors.COMPUTED_PROPERTY_CANNOT_BE_TEMPLATE_LITERAL);
+            throw generateError(
+                path.node!,
+                DecoratorErrors.COMPUTED_PROPERTY_CANNOT_BE_TEMPLATE_LITERAL
+            );
         }
-        throw generateError(DecoratorErrors.COMPUTED_PROPERTY_MUST_BE_CONSTANT_OR_LITERAL);
+        throw generateError(
+            path.node!,
+            DecoratorErrors.COMPUTED_PROPERTY_MUST_BE_CONSTANT_OR_LITERAL
+        );
     }
 }
 
