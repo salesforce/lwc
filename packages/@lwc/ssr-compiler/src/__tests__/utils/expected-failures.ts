48828bc9f2ee6f5e125e98cd908e0489a4626fd3
Will Harney <wharney@salesforce.com>
2025-01-31 14:34:47 -0500

    chore: add new SSR failure
    will fix in next PR

--- packages/@lwc/ssr-compiler/src/__tests__/utils/expected-failures.ts
+++ packages/@lwc/ssr-compiler/src/__tests__/utils/expected-failures.ts
@@ -12,6 +12,6 @@ export const expectedFailures = new Set([
     'attribute-global-html/as-component-prop/without-@api/index.js',
     'exports/component-as-default/index.js',
     'known-boolean-attributes/default-def-html-attributes/static-on-component/index.js',
-    'wire/errors/throws-on-computed-key/index.js',
+    'wire/config/index.js',
     'wire/errors/throws-when-colliding-prop-then-method/index.js',
 ]);
