0aba234fa1ce276a5d4fbc28631f074a22a5f1b7
Will Harney <wharney@salesforce.com>
2025-01-31 13:51:37 -0500

    chore(wire): remove error test cases that are no longer errors

--- packages/@lwc/engine-server/src/__tests__/fixtures/wire/errors/throws-when-computed-prop-is-let-variable/modules/x/wire/wire.js
+++
@@ -1,7 +0,0 @@
-import { wire, LightningElement } from 'lwc';
-import { Adapter } from 'x/adapter';
-let key1 = 'key1';
-export default class Test extends LightningElement {
-    @wire(Adapter, { [key1]: '$prop1', key2: ['fixed', 'array'] })
-    wiredFoo;
-}
