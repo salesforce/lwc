b5678eda754ed2ba969142946b30b1413eb60340
Will Harney <wharney@salesforce.com>
2025-02-13 16:27:10 -0500

    test: invalid scope tokens

--- packages/@lwc/ssr-compiler/src/__tests__/utils/expected-failures.ts
+++ packages/@lwc/ssr-compiler/src/__tests__/utils/expected-failures.ts
@@ -11,5 +11,6 @@ export const expectedFailures = new Set([
     'attribute-global-html/as-component-prop/undeclared/index.js',
     'attribute-global-html/as-component-prop/without-@api/index.js',
     'known-boolean-attributes/default-def-html-attributes/static-on-component/index.js',
+    'scope-token/index.js',
     'wire/errors/throws-when-colliding-prop-then-method/index.js',
 ]);
