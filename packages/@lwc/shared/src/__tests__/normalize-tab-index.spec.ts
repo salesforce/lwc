09ef3120f9fae9284ebe7dc8443cbf7fe1ea8a33
John Hefferman <jhefferman@salesforce.com>
2025-02-28 16:34:00 -0700

    chore: revert hydration failure test

--- packages/@lwc/shared/src/__tests__/normalize-tab-index.spec.ts
+++ packages/@lwc/shared/src/__tests__/normalize-tab-index.spec.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, salesforce.com, inc.
+ * Copyright (c) 2025, salesforce.com, inc.
  * All rights reserved.
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
