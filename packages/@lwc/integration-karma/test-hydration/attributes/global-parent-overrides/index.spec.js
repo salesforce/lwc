7370ec3044bdb8d559ea14a9dd2954256c7c2bd3
John Hefferman <jhefferman@salesforce.com>
2025-02-28 18:43:52 -0700

    chore: test correction, production mode

--- packages/@lwc/integration-karma/test-hydration/attributes/global-parent-overrides/index.spec.js
+++ packages/@lwc/integration-karma/test-hydration/attributes/global-parent-overrides/index.spec.js
@@ -10,20 +10,24 @@ export default {
     test(target, snapshots, consoleCalls) {
         const divs = getAllDivs(target);
         expect(divs.length).toBe(snapshots.divs.length);
-        // For static and dynamic
-        for (let i = 0; i < 2; i++) {
-            expect(divs[0].textContent).toBe('id: parentProvided');
-            expect(divs[1].textContent).toBe('draggable: true');
-            expect(divs[2].textContent).toBe('hidden: true');
-            expect(divs[3].textContent).toBe('spellcheck: true');
-            expect(divs[4].textContent).toBe('tabindex: -1');
-        }
+        // dynamic
+        expect(divs[0].textContent).toBe('id: parentProvided');
+        expect(divs[1].textContent).toBe('draggable: true');
+        expect(divs[2].textContent).toBe('hidden: true');
+        expect(divs[3].textContent).toBe('spellcheck: true');
+        expect(divs[4].textContent).toBe('tabindex: -1');
+        // static
+        expect(divs[5].textContent).toBe('id: parentProvided');
+        expect(divs[6].textContent).toBe('draggable: true');
+        expect(divs[7].textContent).toBe('hidden: true');
+        expect(divs[8].textContent).toBe('spellcheck: true');
+        expect(divs[9].textContent).toBe('tabindex: -1');
 
         /**
          * Required because SSR V1 is wrong (parent does not override child) and this results in hydration errors
          */
-        if (process.env.ENGINE_SERVER) {
-            expect(consoleCalls.warn.length).toBeGreaterThan(1);
+        if (process.env.ENGINE_SERVER && process.env.NODE_ENV !== 'production') {
+            expect(consoleCalls.warn.toString()).toContain('Hydration text content mismatch');
         } else {
             expect(consoleCalls.warn).toHaveSize(0);
         }
