bc9ffd086a7301c51859cc4c0d4584743449da41
Will Harney <wharney@salesforce.com>
2025-03-03 16:35:11 -0500

    chore(release): update script to wait for approvals and auto-create GitHub release

--- scripts/release/publish.sh
+++ scripts/release/publish.sh
@@ -22,16 +22,38 @@ git pull
 # The last commit that changed the root package.json version
 # (in case new commits were merged onto the base branch since then that aren't ready for release)
 VERSION_SHA=$(git blame -- package.json | grep '"version"' | cut -d' ' -f1)
+VERSION=$(jq -r .version package.json)
 
 # Create a new branch with the changes to release
 git switch -c "$BRANCH" "$VERSION_SHA"
 git push origin HEAD
 
 if which gh 2>/dev/null 1>/dev/null; then
-  # Use GitHub CLI to create a PR and wait for it to be merged before exiting
+  # Use GitHub CLI to create a PR and wait for CI checks to pass
   gh pr create -t "chore: release $VERSION" -B "$RELEASE_BRANCH" -b ''
   gh pr checks --fail-fast --watch
+
+  # Usage: wait_until state CLOSED 10
+  function wait_until() {
+    while [ "$(gh pr view "$BRANCH" --json "$1" --jq ".$1")" != "$2" ]; do
+      sleep "$3"
+    done
+  }
+
+  # Also wait for approvals, if needed
+  if [ "$(gh pr view "$BRANCH" --json reviewDecision -q .reviewDecision)" != 'APPROVED' ]; then
+    echo 'Release cannot continue without approval.'
+    wait_until reviewDecision APPROVED 30
+  fi
+
+  # Do the release!
   gh pr comment -b "/nucleus release"
+  sleep 60 # Nucleus release always takes at least a minute
+  wait_until state CLOSED 10
+  LATEST=$([[ "$RELEASE_BRANCH" == release ]] && echo '--latest' || echo '')
+  gh release create "v$VERSION" --generate-notes "$LATEST"
+
+  # Cleanup
   git switch "$BASE_BRANCH"
   git branch -D "$BRANCH"
 else
