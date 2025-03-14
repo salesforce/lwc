a8eaf95fef515d814db85568f50f935c89c7d2fd
John Hefferman <jhefferman@salesforce.com>
2025-03-14 15:07:36 -0600

    chore: updated tests

--- packages/@lwc/engine-server/src/__tests__/fixtures/read-only-props/function/modules/x/component/component.js
+++ packages/@lwc/engine-server/src/__tests__/fixtures/read-only-props/function/modules/x/component/component.js
@@ -1,18 +1,20 @@
 import { LightningElement, readonly } from 'lwc';
 
 export default class extends LightningElement {
-    foo = { willSays: 'mobs 4eva' };
+    foo = {
+        mutated: false,
+        readonlyMutated: false,
+    };
     readonlyFoo = readonly(this.foo);
-    wasProxyMutated = false;
 
     connectedCallback() {
         this.foo.mutated = true;
 
-        // Mutating the proxy shouldn't work.
+        // Mutating the proxy should not work in V1 but will work in V2.
+        // The V2 implementation returns the original object as readonly invokation
+        // is deprecated and will be removed.
         try {
             this.readonlyFoo.readonlyMutated = true;
-        } catch (e) {
-            this.wasProxyMutated = !!this.readonlyFoo.readonlyMutated;
-        }
+        } catch (e) {}
     }
 }
