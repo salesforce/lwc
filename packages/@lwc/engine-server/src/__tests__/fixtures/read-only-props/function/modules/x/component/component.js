a72c3e2db11d9355a346b6317289c9d4bad70a32
John Hefferman <jhefferman@salesforce.com>
2025-03-14 09:09:13 -0600

    chore: more explicit proxy check

--- packages/@lwc/engine-server/src/__tests__/fixtures/read-only-props/function/modules/x/component/component.js
+++ packages/@lwc/engine-server/src/__tests__/fixtures/read-only-props/function/modules/x/component/component.js
@@ -3,7 +3,16 @@ import { LightningElement, readonly } from 'lwc';
 export default class extends LightningElement {
     foo = { willSays: 'mobs 4eva' };
     readonlyFoo = readonly(this.foo);
-    // Proxy is the same when stringified but it's a different object
-    areSameStringified = JSON.stringify(this.foo) === JSON.stringify(this.readonlyFoo);
-    areSame = this.foo == this.readonlyFoo;
+    wasReadonlyFooMutated = false;
+
+    connectedCallback() {
+        this.foo.willSays = 'cancel all mobs';
+        // Mutating the proxy should fail.
+        const newValue = 'mobs must never change';
+        try {
+            this.readonlyFoo.willSays = newValue;
+        } catch (e) {
+            this.wasReadonlyFooMutated = this.readonlyFoo.willSays === newValue;
+        }
+    }
 }
