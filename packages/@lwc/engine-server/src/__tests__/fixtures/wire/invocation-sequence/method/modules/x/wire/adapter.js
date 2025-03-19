451179c694bb11e9939a09abd97a24aff27008e3
John Hefferman <jhefferman@salesforce.com>
2025-03-18 19:17:30 -0600

    feat: api + wire test

--- packages/@lwc/engine-server/src/__tests__/fixtures/wire/invocation-sequence/method/modules/x/wire/adapter.js
+++ packages/@lwc/engine-server/src/__tests__/fixtures/wire/invocation-sequence/method/modules/x/wire/adapter.js
@@ -6,20 +6,12 @@ export class adapter {
     }
 
     connect() {
-        invocationSequence.push('adapter connect()');
+        invocationSequence.push('adapter: connect()');
     }
 
-    update(config) {
-        invocationSequence.push('adapter update()');
-        // JSON.stringify serializes differently for engine-server/ssr-compiler, so we do it manually
-        const output = Object.entries(config)
-            .sort(([a], [b]) => a.localeCompare(b))
-            .map(([key, value]) => `    ${key}: ${JSON.stringify(value)},`)
-            .join('\n')
-            // Quotes are encoded as &quot; in the output, which makes it harder to read...
-            .replace(/"/g, '');
-
-        this.dc(`{\n${output}\n}`);
+    update() {
+        invocationSequence.push('adapter: update()');
+        this.dc('wire provided value');
     }
 
     disconnect() {}
--- packages/@lwc/engine-server/src/__tests__/fixtures/wire/invocation-sequence/method/modules/x/wire/wire.js
+++ packages/@lwc/engine-server/src/__tests__/fixtures/wire/invocation-sequence/method/modules/x/wire/wire.js
@@ -1,14 +1,23 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
 
 import { adapter, invocationSequence } from './adapter';
 
 export default class Wire extends LightningElement {
-    prop = 'foo';
+    @api
+    set apiValue(value) {
+        invocationSequence.push(`component: setApiValue(${value})`);
+        this._apiValue = value;
+    }
 
-    @wire(adapter, { key1: '$prop' })
+    get apiValue() {
+        invocationSequence.push(`component: getApiValue(${this._apiValue})`);
+        return this._apiValue;
+    }
+
+    @wire(adapter, { apiValue: '$apiValue' })
     wiredMethod(value) {
-        invocationSequence.push('component wiredMethod()');
-        this.externalProp = value;
+        invocationSequence.push(`component: wiredMethod(${value})`);
+        this.apiValue = value;
     }
 
     get invocationSequence() {
