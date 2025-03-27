861705ad3683272cdec75c1bab12ba1d3a036f41
Will Harney <wharney@salesforce.com>
2025-03-27 12:37:18 -0400

    chore: convert comments to JSDoc for tooltips

--- packages/@lwc/ssr-compiler/src/compile-js/types.ts
+++ packages/@lwc/ssr-compiler/src/compile-js/types.ts
@@ -26,36 +26,36 @@ export interface WireAdapter {
 }
 
 export interface ComponentMetaState {
-    // indicates whether the LightningElement subclass is found in the JS being traversed
+    /** indicates whether the LightningElement subclass is found in the JS being traversed */
     isLWC: boolean;
-    // indicates whether the LightningElement subclass includes a constructor method
+    /** indicates whether the LightningElement subclass includes a constructor method */
     hasConstructor: boolean;
-    // indicates whether the subclass has a connectedCallback method
+    /** indicates whether the subclass has a connectedCallback method */
     hasConnectedCallback: boolean;
-    // indicates whether the subclass has a renderedCallback method
+    /** indicates whether the subclass has a renderedCallback method */
     hadRenderedCallback: boolean;
-    // indicates whether the subclass has a disconnectedCallback method
+    /** indicates whether the subclass has a disconnectedCallback method */
     hadDisconnectedCallback: boolean;
-    // indicates whether the subclass has a errorCallback method
+    /** indicates whether the subclass has a errorCallback method */
     hadErrorCallback: boolean;
-    // the local name corresponding to the `LightningElement` import
+    /** the local name corresponding to the `LightningElement` import */
     lightningElementIdentifier: string | null;
-    // the class name of the subclass
+    /** the class name of the subclass */
     lwcClassName: string | null;
-    // ties local variable names to explicitly-imported HTML templates
+    /** ties local variable names to explicitly-imported HTML templates */
     tmplExplicitImports: Map<string, string> | null;
-    // ties local variable names to explicitly-imported CSS files
+    /** ties local variable names to explicitly-imported CSS files */
     cssExplicitImports: Map<string, string> | null;
-    // the set of variable names associated with explicitly imported CSS files
+    /** the set of variable names associated with explicitly imported CSS files */
     staticStylesheetIds: Set<string> | null;
-    // the public (`@api`-annotated) properties of the component class
+    /** the public (`@api`-annotated) properties of the component class */
     publicProperties: Map<string, (MethodDefinition | PropertyDefinition) & { key: Identifier }>;
-    // the private properties of the component class
+    /** the private properties of the component class */
     privateProperties: Set<string>;
-    // indicates whether the LightningElement has any wired props
+    /** indicates whether the LightningElement has any wired props */
     wireAdapters: WireAdapter[];
-    // dynamic imports configuration
+    /** dynamic imports configuration */
     experimentalDynamicComponent: ComponentTransformOptions['experimentalDynamicComponent'];
-    // imports to add to the top of the program after parsing
+    /** imports to add to the top of the program after parsing */
     importManager: ImportManager;
 }
