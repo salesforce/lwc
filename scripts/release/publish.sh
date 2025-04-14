6b1809f83c78f33e739652b01e0698def9509339
Will Harney <62956339+wjhsf@users.noreply.github.com>
2025-04-14 12:37:31 -0400

    feat(release): add shell scripts to automate PR creation (#5330)
    * chore(release): allow using "major" or "minor" instead of exact version
    
    * fix(copy-fork): use correct package detection
    
    * chore: create release scripts that automate GitHub
    
    * fix(scripts): tidy up release scripts
    
    * fix(script): use path relative to file, not cwd
    
    * chore: minor fixes to streamline versioning script
    
    * chore: minor fixes to streamline publish script
    
    * chore(release): use commit SHA from local git
    
    * chore(release): update script to wait for approvals and auto-create GitHub release
    
    * chore(release): use $() instead of `` in shell script
    
    * chore(release): update publish script to work better with nucleus
    
    * chore(release): be more flexible in version bump input
    
    * chore(release): clean up locally before watching CI
    
    this way local dev can continue without worrying about git state
    
    * chore(scripts): ergonomic improvements to release scripts
    
    * chore(release): add release script that does all the steps
    
    * chore(release): remove outdated README
    
    the scripts all have explanatory comments in their files
    
    * chore(release): make version script watch for version PR merged

---
+++ scripts/release/publish.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# Find the commit on master that last changed the root package.json version,
+# then submit a PR to merge that commit onto the release branch and release via nucleus
+# Usage: yarn release:publish [branch=release]
+
+set -e
+
+BRANCH="release-publish-$(date -u +'%Y-%m-%d-%H_%M')"
+BASE_BRANCH='master'
+RELEASE_BRANCH="${1:-release}"
+
+# Avoid accidentally committing unrelated files
+if [[ -n $(git status --porcelain) ]]; then
+  echo -e '\033[1mPlease stash your work before continuing.\n\033[0m'
+  git status
+  exit 1
+fi
+
+git switch "$BASE_BRANCH"
+git pull
+# The last commit that changed the root package.json version
+# (in case new commits were merged onto the base branch since then that aren't ready for release)
+VERSION_SHA=$(git blame -- package.json | grep '"version":' | cut -d' ' -f1)
+VERSION=$(jq -r .version package.json)
+
+# Create a new branch with the changes to release
+git switch -c "$BRANCH" "$VERSION_SHA"
+git push origin HEAD
+
+if which gh >/dev/null; then
+  # Use GitHub CLI to create a PR and wait for CI checks to pass
+  gh pr create -t "chore: release $VERSION" -B "$RELEASE_BRANCH" -b ''
+  # Clean up locally
+  git switch "$BASE_BRANCH"
+  git branch -D "$BRANCH"
+
+  # Wait for CI to complete
+  ./wait-for-pr.sh "$BRANCH"
+  if ! gh pr checks --fail-fast --watch; then
+    echo 'CI failed. Cannot continue with release.'
+    gh pr view "$BRANCH" --web
+    exit 1
+  fi
+
+  sleep 10 # Give nucleus time to start the release job
+  RELEASE_JOB=$(gh pr checks "$BRANCH" --json name,link -q '.[]|select(.name=="continuous-integration/nucleus/release").link')
+  echo "Nucleus release started: $RELEASE_JOB"
+
+  # Wait for GitHub release to be created by Nucleus, then open it
+  echo 'The GitHub release notes must be added manually. You can exit the script now and open GitHub on your own, or wait until the release is created and the script will open the page for you.'
+  sleep 300 # Nucleus job usually takes ~5 minutes
+  while ! gh release view "v$VERSION" 1>/dev/null 2>/dev/null; do
+    sleep 15
+  done
+  gh release view "v$VERSION" --web
+else
+  # GitHub CLI not installed - clean up and prompt for manual branch creation
+  git switch "$BASE_BRANCH"
+  git branch -D "$BRANCH"
+  echo "Open a PR: https://github.com/salesforce/lwc/pull/new/$BRANCH"
+fi
