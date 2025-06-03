34c45b350b30ad050a8e4491bb6e60ab2a2029de
Will Harney <wharney@salesforce.com>
2025-06-03 15:35:45 -0400

    test(wtr): add env vars

--- packages/@lwc/integration-karma/web-test-runner.config.mjs
+++ packages/@lwc/integration-karma/web-test-runner.config.mjs
@@ -1,5 +1,25 @@
-import { LWC_VERSION } from '@lwc/shared';
+import { HIGHEST_API_VERSION, LWC_VERSION } from '@lwc/shared';
 import customRollup from './helpers/lwc.mjs';
+
+const pluck = (obj, keys) => Object.fromEntries(keys.map((k) => [k, Boolean(obj[k])]));
+
+const env = {
+    API_VERSION: Number(process.env.API_VERSION) || HIGHEST_API_VERSION,
+    LWC_VERSION,
+    NATIVE_SHADOW: Boolean(
+        process.env.DISABLE_SYNTHETIC || process.env.FORCE_NATIVE_SHADOW_MODE_FOR_TEST
+    ),
+    NODE_ENV: process.env.NODE_ENV_FOR_TEST || 'development',
+    ...pluck(process.env, [
+        'DISABLE_NATIVE_CUSTOM_ELEMENT_LIFECYCLE',
+        'DISABLE_STATIC_CONTENT_OPTIMIZATION',
+        'DISABLE_SYNTHETIC',
+        'ENABLE_ARIA_REFLECTION_GLOBAL_POLYFILL',
+        'ENABLE_SYNTHETIC_SHADOW_IN_HYDRATION',
+        'ENGINE_SERVER',
+        'FORCE_NATIVE_SHADOW_MODE_FOR_TEST',
+    ]),
+};
 
 /** @type {import("@web/test-runner").TestRunnerConfig} */
 export default {
@@ -27,12 +47,11 @@ export default {
         <html>
           <body>
             <script type="module">
-            globalThis.process = {
-                env: {
-                    NODE_ENV: 'test-karma-lwc',
-                    LWC_VERSION: '${LWC_VERSION}'
-                },
-            };
+            globalThis.process = ${JSON.stringify({ env })};
+            globalThis.lwcRuntimeFlags = ${JSON.stringify({
+                DISABLE_NATIVE_CUSTOM_ELEMENT_LIFECYCLE:
+                    env.DISABLE_NATIVE_CUSTOM_ELEMENT_LIFECYCLE,
+            })};
             </script>
             <script type="module" src="./helpers/setup.mjs"></script>
             <script type="module" src="./helpers/wtr-utils.mjs"></script>
