0f960b32a09758c2088dc2b63f47dce5c065e3ee
Will Harney <wharney@salesforce.com>
2025-06-24 14:12:49 -0400

    test(wtr): get hydration tests kinda running

---
+++ packages/@lwc/integration-not-karma/helpers/test-hydrate.js
@@ -0,0 +1,60 @@
+import * as LWC from 'lwc';
+
+window.TestUtils.setHooks({
+    sanitizeHtmlContent: (content) => content,
+});
+
+function parseStringToDom(html) {
+    return Document.parseHTMLUnsafe(html).body.firstChild;
+}
+
+function appendTestTarget(ssrText) {
+    const div = document.createElement('div');
+    const testTarget = parseStringToDom(ssrText);
+    div.appendChild(testTarget);
+    document.body.appendChild(div);
+    return div;
+}
+
+function setFeatureFlags(requiredFeatureFlags, value) {
+    requiredFeatureFlags?.forEach((featureFlag) => {
+        LWC.setFeatureFlagForTest(featureFlag, value);
+    });
+}
+
+async function runTest(ssrRendered, Component, testConfig) {
+    const container = appendTestTarget(ssrRendered);
+    const selector = container.firstChild.tagName.toLowerCase();
+    let target = container.querySelector(selector);
+
+    let testResult;
+    const consoleSpy = window.TestUtils.spyConsole();
+    setFeatureFlags(testConfig.requiredFeatureFlags, true);
+
+    if (testConfig.test) {
+        const snapshot = testConfig.snapshot ? testConfig.snapshot(target) : {};
+
+        const props = testConfig.props || {};
+        const clientProps = testConfig.clientProps || props;
+
+        LWC.hydrateComponent(target, Component, clientProps);
+
+        // let's select again the target, it should be the same elements as in the snapshot
+        target = container.querySelector(selector);
+        testResult = await testConfig.test(target, snapshot, consoleSpy.calls);
+    } else if (testConfig.advancedTest) {
+        testResult = await testConfig.advancedTest(target, {
+            Component,
+            hydrateComponent: LWC.hydrateComponent.bind(LWC),
+            consoleSpy,
+            container,
+            selector,
+        });
+    }
+
+    consoleSpy.reset();
+
+    return testResult;
+}
+
+export { runTest };
---
+++ packages/@lwc/integration-not-karma/web-test-runner.hydration.mjs
@@ -0,0 +1,69 @@
+import { LWC_VERSION } from '@lwc/shared';
+import * as options from './helpers/options.mjs';
+import wrapHydrationTest from './helpers/hydration-tests.mjs';
+
+const pluck = (obj, keys) => Object.fromEntries(keys.map((k) => [k, Boolean(obj[k])]));
+
+/** `process.env` to inject into test environment. */
+const env = {
+    ...pluck(options, [
+        'API_VERSION',
+        'DISABLE_NATIVE_CUSTOM_ELEMENT_LIFECYCLE',
+        'DISABLE_STATIC_CONTENT_OPTIMIZATION',
+        'DISABLE_SYNTHETIC',
+        'ENABLE_ARIA_REFLECTION_GLOBAL_POLYFILL',
+        'ENABLE_SYNTHETIC_SHADOW_IN_HYDRATION',
+        'ENGINE_SERVER',
+        'FORCE_NATIVE_SHADOW_MODE_FOR_TEST',
+        'NATIVE_SHADOW',
+    ]),
+    LWC_VERSION,
+    NODE_ENV: options.NODE_ENV_FOR_TEST,
+};
+/** @type {import("@web/test-runner").TestRunnerConfig} */
+export default {
+    files: [
+        // FIXME: These tests are just symlinks to integration-karma for now so the git diff smaller
+        'test-hydration/**/*.spec.js',
+    ],
+    nodeResolve: true,
+    rootDir: import.meta.dirname,
+    plugins: [
+        {
+            resolveImport({ source }) {
+                if (source === 'test-utils') {
+                    return '/helpers/wtr-utils.mjs';
+                } else if (source === 'wire-service') {
+                    return '@lwc/wire-service';
+                }
+            },
+            async serve(ctx) {
+                if (ctx.path.endsWith('.spec.js')) {
+                    return await wrapHydrationTest(ctx.path.slice(1)); // remove leading /
+                }
+            },
+            async transform(ctx) {
+                if (ctx.type === 'application/javascript') {
+                    // FIXME: copy/paste Nolan's spiel about why we do this ugly thing
+                    return ctx.body.replace(/process\.env\.NODE_ENV === 'test-karma-lwc'/g, 'true');
+                }
+            },
+        },
+    ],
+    testRunnerHtml: (testFramework) =>
+        `<!DOCTYPE html>
+        <html>
+          <body>
+            <script type="module">
+            globalThis.process = ${JSON.stringify({ env })};
+            globalThis.lwcRuntimeFlags = ${JSON.stringify(
+                pluck(options, ['DISABLE_NATIVE_CUSTOM_ELEMENT_LIFECYCLE'])
+            )};
+            </script>
+            <script type="module" src="./helpers/setup.mjs"></script>
+            <script type="module" src="./helpers/wtr-utils.mjs"></script>
+            <script type="module" src="./helpers/test-hydrate.js"></script>
+            <script type="module" src="${testFramework}"></script>
+          </body>
+        </html>`,
+};
