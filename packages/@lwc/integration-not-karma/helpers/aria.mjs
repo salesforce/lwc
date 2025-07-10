32557e68bee467609c8c32b92bebb172bdaba8b1
Will Harney <62956339+wjhsf@users.noreply.github.com>
2025-07-10 10:54:18 -0400

    test(web-test-runner): run hydration tests in CI @W-18780671 (#5398)
    * refactor(wtr): move NODE_ENV_FOR_TEST default into options file
    
    * chore(wtr): move hooks util to separate file
    
    * chore(wtr): move aria utils to separate file
    
    * chore(wtr): move constants to separate file
    
    * chore(wtr): clean up custom rollup plugin
    
    * test(wtr): get hydration tests kinda running
    
    * test(wtr): kinda start moving to ESM instead of IIFE
    
    * fix(shared): make sanitizeHtmlContent work
    
    * chore(ci): run hydration tests in ci
    
    * test(wtr): remove unused script

---
+++ packages/@lwc/integration-not-karma/helpers/aria.mjs
@@ -0,0 +1,80 @@
+// This mapping should be kept up-to-date with the mapping in @lwc/shared -> aria.ts
+export const ariaPropertiesMapping = {
+    ariaAutoComplete: 'aria-autocomplete',
+    ariaChecked: 'aria-checked',
+    ariaCurrent: 'aria-current',
+    ariaDisabled: 'aria-disabled',
+    ariaExpanded: 'aria-expanded',
+    ariaHasPopup: 'aria-haspopup',
+    ariaHidden: 'aria-hidden',
+    ariaInvalid: 'aria-invalid',
+    ariaLabel: 'aria-label',
+    ariaLevel: 'aria-level',
+    ariaMultiLine: 'aria-multiline',
+    ariaMultiSelectable: 'aria-multiselectable',
+    ariaOrientation: 'aria-orientation',
+    ariaPressed: 'aria-pressed',
+    ariaReadOnly: 'aria-readonly',
+    ariaRequired: 'aria-required',
+    ariaSelected: 'aria-selected',
+    ariaSort: 'aria-sort',
+    ariaValueMax: 'aria-valuemax',
+    ariaValueMin: 'aria-valuemin',
+    ariaValueNow: 'aria-valuenow',
+    ariaValueText: 'aria-valuetext',
+    ariaLive: 'aria-live',
+    ariaRelevant: 'aria-relevant',
+    ariaAtomic: 'aria-atomic',
+    ariaBusy: 'aria-busy',
+    ariaActiveDescendant: 'aria-activedescendant',
+    ariaControls: 'aria-controls',
+    ariaDescribedBy: 'aria-describedby',
+    ariaFlowTo: 'aria-flowto',
+    ariaLabelledBy: 'aria-labelledby',
+    ariaOwns: 'aria-owns',
+    ariaPosInSet: 'aria-posinset',
+    ariaSetSize: 'aria-setsize',
+    ariaColCount: 'aria-colcount',
+    ariaColSpan: 'aria-colspan',
+    ariaColIndex: 'aria-colindex',
+    ariaColIndexText: 'aria-colindextext',
+    ariaDescription: 'aria-description',
+    ariaDetails: 'aria-details',
+    ariaErrorMessage: 'aria-errormessage',
+    ariaKeyShortcuts: 'aria-keyshortcuts',
+    ariaModal: 'aria-modal',
+    ariaPlaceholder: 'aria-placeholder',
+    ariaRoleDescription: 'aria-roledescription',
+    ariaRowCount: 'aria-rowcount',
+    ariaRowIndex: 'aria-rowindex',
+    ariaRowIndexText: 'aria-rowindextext',
+    ariaRowSpan: 'aria-rowspan',
+    ariaBrailleLabel: 'aria-braillelabel',
+    ariaBrailleRoleDescription: 'aria-brailleroledescription',
+    role: 'role',
+};
+
+// See the README for @lwc/aria-reflection
+export const nonStandardAriaProperties = [
+    'ariaActiveDescendant',
+    'ariaControls',
+    'ariaDescribedBy',
+    'ariaDetails',
+    'ariaErrorMessage',
+    'ariaFlowTo',
+    'ariaLabelledBy',
+    'ariaOwns',
+];
+
+// These properties are not included in the global polyfill, but were added to LightningElement/BridgeElement
+// prototypes in https://github.com/salesforce/lwc/pull/3702
+export const nonPolyfilledAriaProperties = [
+    'ariaColIndexText',
+    'ariaBrailleLabel',
+    'ariaBrailleRoleDescription',
+    'ariaDescription',
+    'ariaRowIndexText',
+];
+
+export const ariaProperties = Object.keys(ariaPropertiesMapping);
+export const ariaAttributes = Object.values(ariaPropertiesMapping);
--- packages/@lwc/shared/src/overridable-hooks.ts
+++ packages/@lwc/shared/src/overridable-hooks.ts
@@ -16,19 +16,23 @@ interface OverridableHooks {
 
 let hooksAreSet = false;
 
+let sanitizeHtmlContentImpl: SanitizeHtmlContentHook = (): string => {
+    // locker-service patches this function during runtime to sanitize HTML content.
+    throw new Error('sanitizeHtmlContent hook must be implemented.');
+};
+
 /**
  * EXPERIMENTAL: This function acts like a hook for Lightning Locker Service and other similar
  * libraries to sanitize HTML content. This hook process the content passed via the template to
  * lwc:inner-html directive.
  * It is meant to be overridden via `setHooks`; it throws an error by default.
  */
-export let sanitizeHtmlContent: SanitizeHtmlContentHook = (): string => {
-    // locker-service patches this function during runtime to sanitize HTML content.
-    throw new Error('sanitizeHtmlContent hook must be implemented.');
+export const sanitizeHtmlContent: SanitizeHtmlContentHook = (value) => {
+    return sanitizeHtmlContentImpl(value);
 };
 
 export function setHooks(hooks: OverridableHooks) {
     assert.isFalse(hooksAreSet, 'Hooks are already overridden, only one definition is allowed.');
     hooksAreSet = true;
-    sanitizeHtmlContent = hooks.sanitizeHtmlContent;
+    sanitizeHtmlContentImpl = hooks.sanitizeHtmlContent;
 }
