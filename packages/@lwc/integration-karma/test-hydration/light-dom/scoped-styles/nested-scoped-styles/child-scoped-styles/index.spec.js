b7673c86f131c5d87f4216f322aef16383059644
John Hefferman <jhefferman@salesforce.com>
2025-07-31 12:53:14 -0600

    fix: prettier, dedicated dedupe test

--- packages/@lwc/integration-karma/test-hydration/light-dom/scoped-styles/nested-scoped-styles/child-scoped-styles/index.spec.js
+++ packages/@lwc/integration-karma/test-hydration/light-dom/scoped-styles/nested-scoped-styles/child-scoped-styles/index.spec.js
@@ -1,9 +1,18 @@
 export default {
-    test(target, snapshot, consoleCalls) {
-        // Expect no errors or warnings, hydration or otherwise
-        TestUtils.expectConsoleCalls(consoleCalls, {
-            error: [],
-            warn: [],
-        });
+    snapshot(target) {
+        return {
+            parent: target,
+            child: target.querySelector('x-child'),
+        };
+    },
+    advancedTest(target, { Component, hydrateComponent, consoleSpy }) {
+        const { parent, child } = this.snapshot(target);
+        hydrateComponent(target, Component, {});
+        const consoleCalls = consoleSpy.calls;
+        // Validate there is no class attribute mismatch
+        expect(consoleCalls.error).toHaveSize(0);
+        // Validate there is no hydration mismatch
+        expect(parent).toBe(target);
+        expect(child).toBe(target.querySelector('x-child'));
     },
 };
