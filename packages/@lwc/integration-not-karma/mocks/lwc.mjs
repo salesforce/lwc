4d11a882ec51a16fd0857a6c757deee04b14d2dd
Will Harney <wharney@salesforce.com>
2025-08-26 13:11:09 -0400

    chore(wtr): convert package to module

--- packages/@lwc/integration-not-karma/mocks/lwc.mjs
+++
@@ -1,6 +0,0 @@
-// IMPORTANT: we must use @lwc/engine-dom instead of lwc in order to avoid circular imports
-import { sanitizeAttribute as _sanitizeAttribute } from '@lwc/engine-dom';
-import { fn } from '@vitest/spy';
-
-export * from '@lwc/engine-dom';
-export const sanitizeAttribute = fn(_sanitizeAttribute);
