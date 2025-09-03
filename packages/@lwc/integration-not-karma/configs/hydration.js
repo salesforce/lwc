976c002d11128e7c3e297c4962014c3506af7243
Will Harney <wharney@salesforce.com>
2025-09-03 14:00:39 -0400

    test(wtr): enable all hydration tests
    I think the last one was a concurrency related timeout,
    which was previously addressed.

--- packages/@lwc/integration-not-karma/configs/hydration.js
+++ packages/@lwc/integration-not-karma/configs/hydration.js
@@ -6,11 +6,6 @@ import hydrationTestPlugin from './plugins/serve-hydration.js';
 /** @type {import("@web/test-runner").TestRunnerConfig} */
 export default {
     ...baseConfig,
-    files: [
-        // FIXME: These tests are just symlinks to integration-karma for now so the git diff smaller
-        'test-hydration/**/*.spec.js',
-        // FIXME: hits timeout?
-        '!test-hydration/light-dom/scoped-styles/replace-scoped-styles-with-dynamic-templates/index.spec.js',
-    ],
+    files: ['test-hydration/**/*.spec.js'],
     plugins: [...baseConfig.plugins, hydrationTestPlugin],
 };
