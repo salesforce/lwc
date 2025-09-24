8eece7ca307130e30987ecadecbf7872de3d39e0
Will Harney <wharney@salesforce.com>
2025-09-24 16:03:20 -0400

    test(wtr): move util into only file that uses it

--- packages/@lwc/integration-not-karma/configs/plugins/test-hydration.js
+++ packages/@lwc/integration-not-karma/configs/plugins/test-hydration.js
@@ -1,11 +1,33 @@
+import { spyOn } from '@vitest/spy';
 import * as LWC from 'lwc';
-import { spyConsole } from '../../helpers/console';
 import { setHooks } from '../../helpers/hooks';
 
 setHooks({ sanitizeHtmlContent: (content) => content });
 
 function parseStringToDom(html) {
     return Document.parseHTMLUnsafe(html).body.firstChild;
+}
+
+/**
+ * A much simplified version of the spies originally used for Karma.
+ * Should probably be eventually replaced with individual spies.
+ */
+export function spyConsole() {
+    const log = spyOn(console, 'log');
+    const warn = spyOn(console, 'warn');
+    const error = spyOn(console, 'error');
+    return {
+        calls: {
+            log: log.mock.calls,
+            warn: warn.mock.calls,
+            error: error.mock.calls,
+        },
+        reset() {
+            log.mockRestore();
+            warn.mockRestore();
+            error.mockRestore();
+        },
+    };
 }
 
 function appendTestTarget(ssrText) {
