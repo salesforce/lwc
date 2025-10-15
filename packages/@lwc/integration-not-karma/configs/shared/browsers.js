e83ec4e1bf00b9b368fb2f59d9d549786e6e9445
Will Harney <wharney@salesforce.com>
2025-10-15 17:07:03 -0400

    test(wtr): check correct CI env var

--- packages/@lwc/integration-not-karma/configs/shared/browsers.js
+++ packages/@lwc/integration-not-karma/configs/shared/browsers.js
@@ -3,7 +3,7 @@ import { createSauceLabsLauncher } from '@web/test-runner-saucelabs';
 
 /** @type {(options: typeof import('../../helpers/options.js')) => import("@web/test-runner").BrowserLauncher[]} */
 export function getBrowsers(options) {
-    if (options.IS_CI) {
+    if (options.CI) {
         if (!options.SAUCE_USERNAME || !options.SAUCE_ACCESS_KEY || !options.SAUCE_TUNNEL_ID) {
             throw new Error(
                 `SAUCE_USERNAME, SAUCE_ACCESS_KEY, and SAUCE_TUNNEL_ID must be configured in CI`
