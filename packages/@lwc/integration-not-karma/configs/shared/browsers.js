97f1ae6bce315c1a12cd1d071fe8e92d10370e7c
Will Harney <wharney@salesforce.com>
2025-10-22 14:41:04 -0400

    chore(ci): remove saucelabs

--- packages/@lwc/integration-not-karma/configs/shared/browsers.js
+++ packages/@lwc/integration-not-karma/configs/shared/browsers.js
@@ -1,42 +1,10 @@
 import { playwrightLauncher } from '@web/test-runner-playwright';
-import { createSauceLabsLauncher } from '@web/test-runner-saucelabs';
 
 /** @type {(options: typeof import('../../helpers/options.js')) => import("@web/test-runner").BrowserLauncher[]} */
-export function getBrowsers(options) {
-    if (options.USE_SAUCE) {
-        if (!options.SAUCE_USERNAME || !options.SAUCE_ACCESS_KEY || !options.SAUCE_TUNNEL_ID) {
-            throw new Error(
-                'SAUCE_USERNAME, SAUCE_ACCESS_KEY, and SAUCE_TUNNEL_ID must be configured to use SauceLabs.'
-            );
-        }
-        const sauceLabsLauncher = createSauceLabsLauncher(
-            {
-                user: options.SAUCE_USERNAME,
-                key: options.SAUCE_ACCESS_KEY,
-            },
-            {
-                tunnelName: options.SAUCE_TUNNEL_ID,
-            }
-        );
-        return [
-            sauceLabsLauncher({
-                browserName: 'chrome',
-                browserVersion: 'latest',
-            }),
-            sauceLabsLauncher({
-                browserName: 'firefox',
-                browserVersion: 'latest',
-            }),
-            sauceLabsLauncher({
-                browserName: 'safari',
-                browserVersion: 'latest',
-            }),
-        ];
-    } else {
-        return [
-            playwrightLauncher({ product: 'chromium' }),
-            playwrightLauncher({ product: 'firefox' }),
-            playwrightLauncher({ product: 'webkit' }),
-        ];
-    }
+export function getBrowsers(_options) {
+    return [
+        playwrightLauncher({ product: 'chromium' }),
+        playwrightLauncher({ product: 'firefox' }),
+        playwrightLauncher({ product: 'webkit' }),
+    ];
 }
