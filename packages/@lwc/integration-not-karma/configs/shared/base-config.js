fde5dd46274e5ed55d1306bdfc84973411885801
Will Harney <wharney@salesforce.com>
2025-10-31 15:38:52 -0400

    test(wtr): filter out [WDIO] logs

--- packages/@lwc/integration-not-karma/configs/shared/base-config.js
+++ packages/@lwc/integration-not-karma/configs/shared/base-config.js
@@ -49,6 +49,8 @@ export default (options) => {
             : {}),
         browsers,
         browserLogs: true,
+        filterBrowserLogs: (log) =>
+            log.args.some((a) => typeof a === 'string' && !a.startsWith('[WDIO]')),
         // FIXME: Parallelism breaks tests that rely on focus/requestAnimationFrame, because they often
         // time out before they receive focus. But it also makes the full suite take 3x longer to run...
         // Potential workaround: https://github.com/modernweb-dev/web/issues/2588
