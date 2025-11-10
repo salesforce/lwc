d2d455a9c80ce957d0cbebe9aa408fd64241bcd7
Will Harney <wharney@salesforce.com>
2025-11-10 17:14:08 -0500

    test(wtr): put assertion in test

--- packages/@lwc/integration-not-karma/test/misc/lifecycle-remove-disconnected/lifecycle-remove-disconnected.spec.js
+++ packages/@lwc/integration-not-karma/test/misc/lifecycle-remove-disconnected/lifecycle-remove-disconnected.spec.js
@@ -10,6 +10,18 @@ describe('vdom removes component while it is already disconnected', () => {
     });
 
     afterEach(() => {
+        spy.mockRestore();
+    });
+
+    it('repro "must have been connected" error W-14037619', async () => {
+        const elm = createElement('x-parent', { is: Parent });
+        document.body.appendChild(elm);
+        elm.showChild = true;
+        document.body.removeChild(elm);
+
+        await Promise.resolve();
+        elm.showChild = false; // trigger removal while parent is disconnected
+
         if (
             !lwcRuntimeFlags.DISABLE_NATIVE_CUSTOM_ELEMENT_LIFECYCLE ||
             process.env.NODE_ENV === 'production'
@@ -26,15 +38,5 @@ describe('vdom removes component while it is already disconnected', () => {
                 })
             );
         }
-    });
-
-    it('repro "must have been connected" error W-14037619', async () => {
-        const elm = createElement('x-parent', { is: Parent });
-        document.body.appendChild(elm);
-        elm.showChild = true;
-        document.body.removeChild(elm);
-
-        await Promise.resolve();
-        elm.showChild = false; // trigger removal while parent is disconnected
     });
 });
