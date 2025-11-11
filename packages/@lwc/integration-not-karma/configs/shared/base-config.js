39d2fd68a9872116cd20e09723c7b85d88ae01b8
Will Harney <62956339+wjhsf@users.noreply.github.com>
2025-11-11 21:07:40 +0000

    chore(wtr): fix FIXMEs (#5592)

--- packages/@lwc/integration-not-karma/configs/shared/base-config.js
+++ packages/@lwc/integration-not-karma/configs/shared/base-config.js
@@ -51,9 +51,7 @@ export default (options) => {
         coverage: options.COVERAGE,
         coverageConfig: {
             // This is a "magic" path that ultimately points to the lwc packages
-            include: ['__wds-outside-root__/**'],
-            // Coverage fails for this file, but it's just the barrel package so we can skip it
-            exclude: ['__wds-outside-root__/2/lwc/index.js'],
+            include: ['__wds-outside-root__/1/**'],
             reporters: ['html', 'text'],
         },
         nodeResolve: true,
@@ -68,10 +66,37 @@ export default (options) => {
                 },
                 async transform(ctx) {
                     if (ctx.type === 'application/javascript') {
-                        // FIXME: copy/paste Nolan's spiel about why we do this ugly thing
-                        return ctx.body.replace(
-                            /process\.env\.NODE_ENV === 'test-lwc-integration'/g,
-                            'true'
+                        /**
+                         * This transformation replaces `process.env.NODE_ENV === 'test-lwc-integration'` with `true`.
+                         *
+                         * You might wonder why we replace the whole thing rather than just `process.env.NODE_ENV`. Well, because we need a way
+                         * to test `process.env.NODE_ENV === "production"` (prod mode) vs `process.env.NODE_ENV !== "production"` (dev mode).
+                         * If we replaced `process.env.NODE_ENV`, then that would be impossible.
+                         *
+                         * Then you might wonder why we call it "test-lwc-integration" rather than something simple like "test". Well, because
+                         * "test" was already squatted by Jest, and we actually use it for Jest-specific (not WTR-specific) behavior:
+                         * - https://jestjs.io/docs/environment-variables#node_env
+                         * - https://github.com/search?q=repo%3Asalesforce%2Flwc%20node_env%20%3D%3D%3D%20%27test%27&type=code
+                         *
+                         * Then you might wonder why we don't invent our own thing like `process.env.IS_WEB_TEST_RUNNER`. Well, because we're
+                         * testing the artifacts we ship in the npm package, and we can't expect our consumers to replace the string
+                         * `process.env.IS_WEB_TEST_RUNNER`, although we do expect them to replace `process.env.NODE_ENV` (usually with "production").
+                         *
+                         * Then you might wonder why we don't just use a runtime check like `typeof __WTR_CONFIG__ !== 'undefined'`. And the reason
+                         * for that is that we want WTR-specific code to be tree-shaken in prod mode. (Assuming our consumers are replacing
+                         * `process.env.NODE_ENV` with "production".)
+                         *
+                         * So then you might wonder why we test against the same artifacts we ship, rather than testing against WTR-specific
+                         * artifacts. And that's totally reasonable, but then it introduces the risk that we're not testing our "real"
+                         * artifacts.
+                         *
+                         * So that's why this is so weird and complicated.
+                         */
+                        const replacee = `process.env.NODE_ENV === 'test-lwc-integration'`;
+                        return ctx.body.replaceAll(
+                            replacee,
+                            // pad to keep things pretty
+                            'true'.padEnd(replacee.length, ' ')
                         );
                     }
                 },
