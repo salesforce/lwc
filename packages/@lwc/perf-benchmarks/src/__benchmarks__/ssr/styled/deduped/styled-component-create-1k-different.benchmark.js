15bfa8018a6f1256c5e217138da86c0aad364557
Will Harney <wharney@salesforce.com>
2025-11-13 13:00:35 -0500

    test(perf): add perf tests for lwc-style

---
+++ packages/@lwc/perf-benchmarks/src/__benchmarks__/ssr/styled/deduped/styled-component-create-1k-different.benchmark.js
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+
+import components from '@lwc/perf-benchmarks-components/dist/ssr/benchmark/shadow/styledComponents.js';
+
+import { styledComponentSsrBenchmark } from '../../../utils/styledComponentSsrBenchmark';
+
+const NUM_COMPONENTS = 1000;
+
+// Create 1k components with different CSS in each component
+styledComponentSsrBenchmark(
+    `ssr/styled-component/shadow/create-different/1k`,
+    NUM_COMPONENTS,
+    components,
+    { after, before, benchmark, run },
+    true
+);
