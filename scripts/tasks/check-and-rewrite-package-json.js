03ec48f860e668052b585221d8193f8287f80ba6
Will Harney <wharney@salesforce.com>
2025-12-10 11:44:14 -0500

    chore(scripts): add engines.node to package.json validation

--- scripts/tasks/check-and-rewrite-package-json.js
+++ scripts/tasks/check-and-rewrite-package-json.js
@@ -103,6 +103,9 @@ for (const { package: pkg, path: dir } of SCOPED_PACKAGES) {
         bugs: { url: 'https://github.com/salesforce/lwc/issues' },
         license: 'MIT',
         publishConfig: { access: 'public' },
+        engines: {
+            node: '>= 20.19.0',
+        },
         // Use the same volta config in every subdirectory so that we always get the same node/yarn versions
         // See: https://docs.volta.sh/advanced/workspaces
         volta: {
