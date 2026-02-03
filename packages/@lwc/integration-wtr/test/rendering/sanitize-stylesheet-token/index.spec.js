6eab812d5fca84a01286d4e9dab43e7a403ecea9
Will Harney <62956339+wjhsf@users.noreply.github.com>
2026-02-03 16:48:05 +0000

    test(wtr): refine exception cases to match updated behavior (#5694)
    * test(wtr): refine exception cases to match updated behavior
    
    * chore: update comment

--- packages/@lwc/integration-wtr/test/rendering/sanitize-stylesheet-token/index.spec.js
+++ packages/@lwc/integration-wtr/test/rendering/sanitize-stylesheet-token/index.spec.js
@@ -82,12 +82,12 @@ props.forEach((prop) => {
                     if (
                         process.env.NATIVE_SHADOW &&
                         process.env.DISABLE_STATIC_CONTENT_OPTIMIZATION &&
-                        Ctor !== Scoping
+                        Ctor === Component &&
+                        prop === 'legacyStylesheetToken'
                     ) {
-                        // If we're rendering in native shadow and the static content optimization is disabled,
-                        // then there's no problem with invalid stylesheet tokens because they are only rendered
-                        // as class attribute values using either `classList` or `setAttribute` (and this only applies
-                        // when `*.scoped.css` is being used).
+                        // When using legacy stylesheet tokens with unscoped CSS in native shadow with static content
+                        // optimization disabled, there's no problem with invalid stylesheet tokens because they are
+                        // only rendered as class attribute values using either `classList` or `setAttribute`
                         expect(elm.shadowRoot.children.length).toBe(1);
                     } else {
                         expect(elm.shadowRoot.children.length).toBe(0); // does not render
