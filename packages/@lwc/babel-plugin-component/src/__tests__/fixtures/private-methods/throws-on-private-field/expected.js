6fda36af0cd4786988df9f97f1cfa8a64926bdc1
a-chabot <90475490+a-chabot@users.noreply.github.com>
2026-04-13 14:44:52 -0400

    Remove warning for private properties  (#5780)
    * feat: stop warn
    
    Signed-off-by: a.chabot <a.chabot@salesforce.com>
    
    * fix: code failure
    
    Signed-off-by: a.chabot <a.chabot@salesforce.com>
    
    ---------
    
    Signed-off-by: a.chabot <a.chabot@salesforce.com>

---
+++ packages/@lwc/babel-plugin-component/src/__tests__/fixtures/private-methods/throws-on-private-field/expected.js
@@ -0,0 +1,21 @@
+import _tmpl from "./test.html";
+var id = 0;
+function _classPrivateFieldLooseKey(e) { return "__private_" + id++ + "_" + e; }
+import { LightningElement, registerComponent as _registerComponent } from 'lwc';
+var _count = /*#__PURE__*/_classPrivateFieldLooseKey("count");
+class Test extends LightningElement {
+  constructor(...args) {
+    super(...args);
+    Object.defineProperty(this, _count, {
+      writable: true,
+      value: 0
+    });
+  }
+  /*LWC compiler vX.X.X*/
+}
+const __lwc_component_class_internal = _registerComponent(Test, {
+  tmpl: _tmpl,
+  sel: "lwc-test",
+  apiVersion: 9999999
+});
+export default __lwc_component_class_internal;
